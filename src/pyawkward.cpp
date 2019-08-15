aed39eb833e6ac208e3c09325c26e3fb5532cb92
Jim Pivarski <jpivarski@gmail.com>
2019-08-15 08:52:21 -0500

    CMake compiles cpu-kernels, libawkward.so, and the Python module through pybind11.

---
+++ src/pyawkward.cpp
@@ -0,0 +1,20 @@
+#include <pybind11/pybind11.h>
+
+#include "awkward/cpu-kernels/dummy.h"
+#include "awkward/dummy2.h"
+
+namespace py = pybind11;
+
+int dummy3(int x) {
+  return dummy2(x);
+}
+
+PYBIND11_MODULE(layout, m) {
+  m.def("dummy3", &dummy3);
+
+#ifdef VERSION_INFO
+  m.attr("__version__") = VERSION_INFO;
+#else
+  m.attr("__version__") = "dev";
+#endif
+}
