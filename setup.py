2b53961a75fc98d36193fbe148b0e58645af2ce9
Jim Pivarski <jpivarski@gmail.com>
2019-08-15 20:52:05 -0500

    Try setting CMAKE_LIBRARY_OUTPUT_DIRECTORY to Release/Release.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class CMakeBuild(setuptools.command.build_ext.build_ext):
         build_args = ["--config", cfg]
 
         if platform.system() == "Windows":
-            cmake_args += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + os.path.join(extdir, cfg)]
+            cmake_args += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + os.path.join(os.path.join(extdir, cfg), cfg)]
 
             if sys.maxsize > 2**32:
                 cmake_args += ["-A", "x64"]
