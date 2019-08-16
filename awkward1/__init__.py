b91e94dcc05973dae2eb176216cc762b2f68ab74
Jim Pivarski <jpivarski@gmail.com>
2019-08-15 22:39:10 -0500

    But that's the same thing. Instead, print out what we do have.

--- awkward1/__init__.py
+++ awkward1/__init__.py
@@ -10,11 +10,12 @@ print("RUNNING1", __file__)
 print("RUNNING2", os.listdir(os.path.dirname(os.path.abspath(__file__))))
 
 if platform.system() == "Windows":
-    lib1 = ctypes.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), "awkward-cpu-kernels.dll"))
+    lib1 = ctypes.cdll.LoadLibrary(os.path.join(os.path.dirname(os.path.abspath(__file__)), "awkward-cpu-kernels.dll"))
 else:
     lib1 = ctypes.cdll.LoadLibrary(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libawkward-cpu-kernels.so"))
 
 print("RUNNING3", lib1)
+print("RUNNING4", dir(lib1))
 
 dummy1 = lib1.dummy1
 dummy3 = layout.dummy3
