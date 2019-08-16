a3750fa771469482037b87680e49cb422c8a9dc7
Jim Pivarski <jpivarski@gmail.com>
2019-08-16 06:50:25 -0500

    MacOS has shared libraries with extension '.dylib'.

--- awkward1/__init__.py
+++ awkward1/__init__.py
@@ -6,11 +6,9 @@ import awkward1.layout
 
 __version__ = awkward1.layout.__version__
 
-print("platform.system()", platform.system())
-print("__file__", __file__)
-print("listdir", os.listdir(os.path.dirname(os.path.abspath(__file__))))
-
-if platform.system() == "Windows":
+if platform.system() == "Darwin":
+    lib1 = ctypes.cdll.LoadLibrary(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libawkward-cpu-kernels.dylib"))
+elif platform.system() == "Windows":
     lib1 = ctypes.cdll.LoadLibrary(os.path.join(os.path.dirname(os.path.abspath(__file__)), "awkward-cpu-kernels.dll"))
 else:
     lib1 = ctypes.cdll.LoadLibrary(os.path.join(os.path.dirname(os.path.abspath(__file__)), "libawkward-cpu-kernels.so"))
