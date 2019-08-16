8ea5ca1451c5145c142969af718b35dd2ffb46c7
Jim Pivarski <jpivarski@gmail.com>
2019-08-16 15:44:35 -0500

    Linux builds (specifically, manylinux1) must be C++11, not C++14.

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -2,6 +2,11 @@
 
 cd /io/
 
+if [[ $PLAT != "manylinux2010_x86_64" ]]; then
+    /opt/python/cp37-cp37m/bin/pip install cmake
+    export PATH=/opt/_internal/cpython-3.7.4/lib/python3.7/site-packages/cmake/data/bin:$PATH
+done
+
 for PYBIN in /opt/python/*/bin; do
     echo "========================================================="
     echo $PYBIN
