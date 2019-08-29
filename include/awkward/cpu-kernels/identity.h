3f0ea01edde2a518cd1582e3b2ced534a6c3d5ff
Jim Pivarski <jpivarski@gmail.com>
2019-08-29 11:21:02 -0500

    Maybe use Doxygen to generate XML to describe ctypes types.

--- include/awkward/cpu-kernels/identity.h
+++ include/awkward/cpu-kernels/identity.h
@@ -1,3 +1,6 @@
+//! @file
+//! @brief Low-level functions for manipulating Identity arrays.
+
 // BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
 
 #ifndef AWKWARDCPU_IDENTITY_H_
@@ -6,7 +9,12 @@
 #include "awkward/cpu-kernels/util.h"
 
 extern "C" {
+  //! Fill a new Identity array with increasing numbers.
+  //! @param length the number of elements in the Identity array.
+  //! @param to the Identity array.
+  //! @return nullptr if successful; a constant error string otherwise.
   Error awkward_identity_new(IndexType length, IndexType* to);
+
   Error awkward_identity_from_listfoffsets(IndexType length, IndexType width, IndexType* offsets, IndexType* from, IndexType tolength, IndexType* to);
 }
 
