80ce1f6dd3c49f3d478f3ffcd879cc3cfad3bcba
Jim Pivarski <jpivarski@gmail.com>
2019-08-30 08:16:25 -0400

    Iterators don't affect refcounts.

--- tests/test_PR6_deep_iteration.py
+++ tests/test_PR6_deep_iteration.py
@@ -16,3 +16,28 @@ def test_iterator():
     array = awkward1.layout.ListOffsetArray(offsets, content)
     assert list(content) == [1.1, 2.2, 3.3]
     assert [numpy.asarray(x).tolist() for x in array] == [[1.1, 2.2], [], [3.3]]
+
+def test_refcount():
+    content = awkward1.layout.NumpyArray(numpy.array([1.1, 2.2, 3.3]))
+    offsets = awkward1.layout.Index(numpy.array([0, 2, 2, 3], "i4"))
+    array = awkward1.layout.ListOffsetArray(offsets, content)
+
+    assert (sys.getrefcount(content), sys.getrefcount(array)) == (2, 2)
+
+    iter1 = iter(content)
+    assert (sys.getrefcount(content), sys.getrefcount(array)) == (2, 2)
+    x1 = next(iter1)
+    assert (sys.getrefcount(content), sys.getrefcount(array)) == (2, 2)
+
+    iter2 = iter(array)
+    assert (sys.getrefcount(content), sys.getrefcount(array)) == (2, 2)
+    x2 = next(iter2)
+    assert (sys.getrefcount(content), sys.getrefcount(array)) == (2, 2)
+
+    del iter1
+    del x1
+    assert (sys.getrefcount(content), sys.getrefcount(array)) == (2, 2)
+
+    del iter2
+    del x2
+    assert (sys.getrefcount(content), sys.getrefcount(array)) == (2, 2)
