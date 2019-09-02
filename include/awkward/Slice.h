943fe30816af58582ebb877396466b9746140d39
Jim Pivarski <jpivarski@gmail.com>
2019-09-02 14:56:07 -0500

    Start on PR7, deep __getitem__, for real this time.

---
+++ include/awkward/Slice.h
@@ -0,0 +1,13 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#ifndef AWKWARD_SLICE_H_
+#define AWKWARD_SLICE_H_
+
+#include "awkward/cpu-kernels/util.h"
+#include "awkward/util.h"
+
+namespace awkward {
+  
+}
+
+#endif // AWKWARD_SLICE_H_
