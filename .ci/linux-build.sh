0b1ecdadf13e4ed2f987b908a9be8262f498af07
Jim Pivarski <jpivarski@gmail.com>
2019-09-26 14:47:28 -0500

    Try to fix manylinux1 deployment again.

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -2,15 +2,12 @@
 
 cd /io/
 
-if [[ $PLAT != "manylinux2010_x86_64" ]]; then
-    /opt/python/cp37-cp37m/bin/pip install cmake
-    export PATH=/opt/_internal/cpython-3.7.4/lib/python3.7/site-packages/cmake/data/bin:$PATH
-fi
+/opt/python/cp37-cp37m/bin/pip install cmake==3.13.3
+export PATH=/opt/_internal/cpython-3.7.4/lib/python3.7/site-packages/cmake/data/bin:$PATH
 
 for PYBIN in /opt/python/*/bin; do
     echo "========================================================="
     echo $PYBIN
     echo "========================================================="
-    "${PYBIN}/pip" install scikit-build
     "${PYBIN}/python" setup.py bdist_wheel -p $PLAT
 done
