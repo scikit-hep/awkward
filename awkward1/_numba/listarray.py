5a8a89a975a6f914563b3eb4133d5d84a05b6189
Jim Pivarski <jpivarski@gmail.com>
2019-09-26 15:17:26 -0500

    [skip ci] add listarray.py

---
+++ awkward1/_numba/listarray.py
@@ -0,0 +1,10 @@
+# BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+import operator
+
+import numpy
+import numba
+import numba.typing.arraydecl
+
+import awkward1.layout
+from .._numba import cpu, util, content
