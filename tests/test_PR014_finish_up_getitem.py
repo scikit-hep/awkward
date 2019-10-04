8ea2413c83db2795b4a79ce11166072b2ad15596
Jim Pivarski <jpivarski@gmail.com>
2019-10-04 15:37:44 -0500

    listoffsetarray1 tests work.

--- tests/test_PR014_finish_up_getitem.py
+++ tests/test_PR014_finish_up_getitem.py
@@ -21,3 +21,31 @@ model2 = numpy.arange(2*3*5*7).reshape(2, 3, 5, 7)
 def test_basic():
     assert awkward1.tolist(model1) == awkward1.tolist(listoffsetarray1)
     assert awkward1.tolist(model2) == awkward1.tolist(listoffsetarray2)
+
+def test_listoffsetarray1():
+    for depth in 0, 1, 2, 3:
+        for cuts in itertools.permutations((0, 1, 4, -5), depth):
+            assert awkward1.tolist(model1[cuts]) == awkward1.tolist(listoffsetarray1[cuts])
+
+    for depth in 0, 1, 2, 3:
+        for cuts in itertools.permutations((slice(None), slice(1, None), slice(None, -1), slice(None, None, 2)), depth):
+            assert awkward1.tolist(model1[cuts]) == awkward1.tolist(listoffsetarray1[cuts])
+
+    for depth in 0, 1, 2, 3:
+        for cuts in itertools.permutations((slice(1, None), slice(None, -1), 2, -2), depth):
+            assert awkward1.tolist(model1[cuts]) == awkward1.tolist(listoffsetarray1[cuts])
+
+    for depth in 0, 1, 2, 3:
+        for cuts in itertools.permutations(([2, 0, 0, 1], [1, -2, 0, -1], 2, -2), depth):
+            assert awkward1.tolist(model1[cuts]) == awkward1.tolist(listoffsetarray1[cuts])
+
+    for depth in 0, 1, 2, 3:
+        for cuts in itertools.permutations(([2, 0, 0, 1], [1, -2, 0, -1], slice(1, None), slice(None, -1)), depth):
+            cuts = cuts
+            while len(cuts) > 0 and isinstance(cuts[0], slice):
+                cuts = cuts[1:]
+            while len(cuts) > 0 and isinstance(cuts[-1], slice):
+                cuts = cuts[:-1]
+            if any(isinstance(x, slice) for x in cuts):
+                continue
+            assert awkward1.tolist(model1[cuts]) == awkward1.tolist(listoffsetarray1[cuts])
