38fd7b734bf4c21396c8c63ebf1f839927158508
Jim Pivarski <jpivarski@gmail.com>
2019-10-10 12:50:36 -0500

    Start working on RawArray.

---
+++ tests/test_PR016_finish_getitem_for_rawarray.cpp
@@ -0,0 +1,25 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#include <cassert>
+#include <iostream>
+
+#include "awkward/Identity.h"
+// #include "awkward/RawArray.h"
+#include "awkward/Slice.h"
+
+using namespace awkward;
+
+void rawarray() {
+  // RawArrayOf<float> data(Identity::none(), 4);
+  // *data.borrow(0) = 0.0f;
+  // *data.borrow(1) = 1.1f;
+  // *data.borrow(2) = 2.2f;
+  // *data.borrow(3) = 3.3f;
+  // assert(*dynamic_cast<RawArrayOf<float>*>(data.get(1).get())->borrow() == 1.1f);
+  // assert(*dynamic_cast<RawArrayOf<float>*>(data.slice(1, 3).get())->borrow(0) == 1.1f);
+  // assert(*dynamic_cast<RawArrayOf<float>*>(data.slice(1, 3).get())->borrow(1) == 2.2f);
+}
+
+int main(int, char**) {
+  rawarray();
+}
