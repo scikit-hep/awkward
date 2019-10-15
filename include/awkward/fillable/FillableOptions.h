f478ad3b1f439a6e4f21507533239b5da95ef349
Jim Pivarski <jpivarski@gmail.com>
2019-10-15 07:33:07 -0500

    Include missing 'FillableOptions' files.

---
+++ include/awkward/fillable/FillableOptions.h
@@ -0,0 +1,25 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#ifndef AWKWARD_FILLABLEOPTIONS_H_
+#define AWKWARD_FILLABLEOPTIONS_H_
+
+#include <cmath>
+#include <cstring>
+
+#include "awkward/cpu-kernels/util.h"
+
+namespace awkward {
+  class FillableOptions {
+  public:
+    FillableOptions(int64_t initial, double resize): initial_(initial), resize_(resize) { }
+
+    int64_t initial() const { return initial_; }
+    double resize() const { return resize_; }
+
+  private:
+    int64_t initial_;
+    double resize_;
+  };
+}
+
+#endif // AWKWARD_FILLABLEOPTIONS_H_
---
+++ include/awkward/fillable/OptionFillable.h
@@ -0,0 +1,33 @@
+// // BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+//
+// #ifndef AWKWARD_OPTIONFILLABLE_H_
+// #define AWKWARD_OPTIONFILLABLE_H_
+//
+// #include <vector>
+//
+// #include "awkward/cpu-kernels/util.h"
+// #include "awkward/fillable/Fillable.h"
+//
+// namespace awkward {
+//   class OptionFillable: public Fillable {
+//   public:
+//     OptionFillable(Fillable* content): index_(), content_(content) { }
+//
+//
+//     OptionFillable(Fillable* content, int64_t nullcount): validindex_(), content_(content), length_(nullcount) { }
+//
+//     virtual int64_t length() const;
+//     virtual void clear();
+//     virtual const std::shared_ptr<Type> type() const;
+//     virtual const std::shared_ptr<Content> snapshot() const;
+//
+//     virtual Fillable* null();
+//     virtual Fillable* boolean(bool x);
+//
+//   private:
+//     GrowableBuffer<int64_t> index_;
+//     std::shared_ptr<Fillable> content_;
+//   };
+// }
+//
+// #endif // AWKWARD_OPTIONFILLABLE_H_
