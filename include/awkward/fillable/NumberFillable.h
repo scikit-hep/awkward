2bdd73aef8e8e9ee67b4fcd59a7144b567420e2f
Jim Pivarski <jpivarski@gmail.com>
2019-10-21 11:00:02 -0500

    Start on NumberFillable.

---
+++ include/awkward/fillable/NumberFillable.h
@@ -0,0 +1,30 @@
+// // BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+//
+// #ifndef AWKWARD_BOOLFILLABLE_H_
+// #define AWKWARD_BOOLFILLABLE_H_
+//
+// #include "awkward/cpu-kernels/util.h"
+// #include "awkward/fillable/FillableOptions.h"
+// #include "awkward/fillable/GrowableBuffer.h"
+// #include "awkward/fillable/Fillable.h"
+//
+// namespace awkward {
+//   class BoolFillable: public Fillable {
+//   public:
+//     BoolFillable(const FillableOptions& options): options_(options), buffer_(options) { }
+//
+//     virtual int64_t length() const;
+//     virtual void clear();
+//     virtual const std::shared_ptr<Type> type() const;
+//     virtual const std::shared_ptr<Content> snapshot() const;
+//
+//     virtual Fillable* null();
+//     virtual Fillable* boolean(bool x);
+//
+//   private:
+//     const FillableOptions options_;
+//     GrowableBuffer<uint8_t> buffer_;
+//   };
+// }
+//
+// #endif // AWKWARD_BOOLFILLABLE_H_
---
+++ src/libawkward/fillable/NumberFillable.cpp
@@ -0,0 +1,39 @@
+// // BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+//
+// #include "awkward/Identity.h"
+// #include "awkward/array/NumpyArray.h"
+// #include "awkward/type/PrimitiveType.h"
+// #include "awkward/fillable/OptionFillable.h"
+//
+// #include "awkward/fillable/BoolFillable.h"
+//
+// namespace awkward {
+//   int64_t BoolFillable::length() const {
+//     return buffer_.length();
+//   }
+//
+//   void BoolFillable::clear() {
+//     buffer_.clear();
+//   }
+//
+//   const std::shared_ptr<Type> BoolFillable::type() const {
+//     return std::shared_ptr<Type>(new PrimitiveType(PrimitiveType::boolean));
+//   }
+//
+//   const std::shared_ptr<Content> BoolFillable::snapshot() const {
+//     std::vector<ssize_t> shape = { (ssize_t)buffer_.length() };
+//     std::vector<ssize_t> strides = { (ssize_t)sizeof(bool) };
+//     return std::shared_ptr<Content>(new NumpyArray(Identity::none(), buffer_.ptr(), shape, strides, 0, sizeof(bool), "?"));
+//   }
+//
+//   Fillable* BoolFillable::null() {
+//     Fillable* out = OptionFillable::fromvalids(options_, this);
+//     out->null();
+//     return out;
+//   }
+//
+//   Fillable* BoolFillable::boolean(bool x) {
+//     buffer_.append(x);
+//     return this;
+//   }
+// }
