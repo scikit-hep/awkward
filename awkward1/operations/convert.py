9962237f0a9407ba157bd19d58642b70c4c4ca6c
Jim Pivarski <jpivarski@gmail.com>
2019-10-23 17:29:15 -0500

    Implemented but have not tested 'fromiter'.

--- awkward1/operations/convert.py
+++ awkward1/operations/convert.py
@@ -2,11 +2,53 @@
 
 import numbers
 import json
+try:
+    from collections.abc import Iterable
+except ImportError:
+    from collections import Iterable
 
 import numpy
 
 import awkward1.util
 import awkward1.layout
+
+def fromiter(iterable):
+    out = awkward1.layout.FillableArray()
+
+    def recurse(obj):
+        if obj is None:
+            out.null()
+        elif isinstance(obj, (bool, numpy.bool, numpy.bool_)):
+            out.boolean(obj)
+        elif isinstance(obj, (int, numbers.Integral, numpy.integer)):
+            out.integer(obj)
+        elif isinstance(obj, (float, numbers.Real, numpy.floating)):
+            out.real(obj)
+        elif isinstance(obj, bytes) and not isinstance(obj, str):
+            obj.decode("utf-8")
+            out.string(obj)
+        elif isinstance(obj, str):
+            out.string(obj)
+        elif isinstance(obj, dict):
+            out.beginrec()
+            for n, x in obj.items():
+                if isinstance(n, bytes) and not isinstance(n, str):
+                    n = n.decode("utf-8")
+                if not isinstance(n, str):
+                    raise ValueError("only dicts with string-valued keys can be converted with fromiter")
+                out.fieldname(n)
+                recurse(x)
+            out.endrec()
+        elif isinstance(obj, Iterable):
+            out.beginlist()
+            for x in obj:
+                recurse(x)
+            out.endlist()
+        else:
+            raise ValueError("cannot convert type: {} value: {}".format(type(obj), repr(obj)))
+
+    recurse(iterable)
+    return out.snapshot()
 
 def tolist(array):
     if array is None or isinstance(array, (bool, str, bytes, numbers.Number)):
@@ -27,6 +69,8 @@ def tolist(array):
     else:
         raise TypeError("unrecognized array type: {0}".format(repr(array)))
 
+fromjson = awkward1.layout.fromjson
+
 def tojson(array, *args, **kwargs):
     if array is None or isinstance(array, (bool, str, bytes, numbers.Number)):
         return json.dumps(array)
@@ -43,4 +87,4 @@ def tojson(array, *args, **kwargs):
     else:
         raise TypeError("unrecognized array type: {0}".format(repr(array)))
 
-__all__ = [x for x in list(globals()) if not x.startswith("_") and x not in ("numbers", "json", "numpy", "awkward1")]
+__all__ = [x for x in list(globals()) if not x.startswith("_") and x not in ("numbers", "json", "Iterable", "numpy", "awkward1")]
