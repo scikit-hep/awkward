af99fdcb93072210218e8047ad821730f346cfb4
Jim Pivarski <jpivarski@gmail.com>
2019-10-27 07:40:14 -0500

    Simplify CHEP 2019 example by moving array-building into 'fill' method.

--- tests/test_PR019_use_json_library.cpp
+++ tests/test_PR019_use_json_library.cpp
@@ -5,23 +5,12 @@
 
 namespace ak = awkward;
 
-void build(ak::FillableArray& builder, double x) {
-  builder.real(x);
-}
-
-template <typename T>
-void build(ak::FillableArray& builder, const std::vector<T>& vector) {
-  builder.beginlist();
-  for (auto x : vector) build(builder, x);
-  builder.endlist();
-}
-
 int main(int, char**) {
   std::vector<std::vector<std::vector<double>>> vector =
     {{{0.0, 1.1, 2.2}, {}, {3.3, 4.4}}, {{5.5}}, {}, {{6.6, 7.7, 8.8, 9.9}}};
 
   ak::FillableArray builder(ak::FillableOptions(1024, 2.0));
-  for (auto x : vector) build(builder, x);
+  for (auto x : vector) builder.fill(x);
 
   if (builder.snapshot().get()->tojson(false, 1) !=
          "[[[0.0,1.1,2.2],[],[3.3,4.4]],[[5.5]],[],[[6.6,7.7,8.8,9.9]]]")
