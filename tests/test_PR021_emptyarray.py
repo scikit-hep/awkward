681b4850843cc72287a0f3692955b24b4117abd6
Jim Pivarski <jpivarski@gmail.com>
2019-11-12 07:48:59 -0600

    Add tests file.

---
+++ tests/test_PR021_emptyarray.py
@@ -0,0 +1,14 @@
+# BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+import sys
+import os
+import json
+
+import pytest
+import numpy
+numba = pytest.importorskip("numba")
+
+import awkward1
+
+def test_nothing():
+    pass
