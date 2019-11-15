f95fc9ddbed98eaf48636769d6c1343e67e032a7
Jim Pivarski <jpivarski@gmail.com>
2019-11-15 10:27:34 -0600

    Infrastructure for RegularArray.

---
+++ include/awkward/array/RegularArray.h
@@ -0,0 +1,21 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#ifndef AWKWARD_REGULARARRAY_H_
+#define AWKWARD_REGULARARRAY_H_
+
+#include <cassert>
+#include <string>
+#include <memory>
+#include <vector>
+
+#include "awkward/cpu-kernels/util.h"
+#include "awkward/Slice.h"
+#include "awkward/Content.h"
+
+namespace awkward {
+  class RegularArray {
+
+  };
+}
+
+#endif // AWKWARD_REGULARARRAY_H_
---
+++ src/libawkward/array/RegularArray.cpp
@@ -0,0 +1,15 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#include <iomanip>
+#include <sstream>
+#include <stdexcept>
+
+#include "awkward/cpu-kernels/identity.h"
+#include "awkward/cpu-kernels/getitem.h"
+#include "awkward/type/RegularType.h"
+
+#include "awkward/array/RegularArray.h"
+
+namespace awkward {
+  
+}
