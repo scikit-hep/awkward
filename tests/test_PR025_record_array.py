4cb431b967472d07486fd62fe2137a55b12b5da2
Jim Pivarski <jpivarski@gmail.com>
2019-11-21 13:13:43 -0600

    Tests pass.

--- tests/test_PR025_record_array.py
+++ tests/test_PR025_record_array.py
@@ -9,4 +9,10 @@ import numpy
 import awkward1
 
 def test_nothing():
-    array = awkward1.layout.RecordArray()
+    recordarray = awkward1.layout.RecordArray()
+    recordarray.append(awkward1.layout.NumpyArray(numpy.array([1, 2, 3, 4, 5])), "one")
+    recordarray.append(awkward1.layout.NumpyArray(numpy.array([1.1, 2.2, 3.3, 4.4, 5.5])), "two")
+    recordarray.alias(0, "wonky")
+    assert awkward1.tolist(recordarray.content(0)) == [1, 2, 3, 4, 5]
+    assert awkward1.tolist(recordarray.content("two")) == [1.1, 2.2, 3.3, 4.4, 5.5]
+    assert awkward1.tolist(recordarray.content("wonky")) == [1, 2, 3, 4, 5]
