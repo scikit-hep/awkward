979398c3b35b70bcfdc599387e19ff998a3a4c5c
Jim Pivarski <jpivarski@gmail.com>
2019-11-26 09:43:46 -0600

    Stubbed out Fillable implementations.

--- studies/fillable.py
+++ studies/fillable.py
@@ -1,3 +1,5 @@
+################################################################ Contents
+
 class Content:
     def __iter__(self):
         def convert(x):
@@ -44,12 +46,12 @@ class ListArray(Content):
             return ListArray(self.offsets[start:stop], self.content)
 
 class UnionArray(Content):
-    def __init__(self, tags, index, contents):
+    def __init__(self, tags, offsets, contents):
         assert isinstance(tags, list)
-        assert isinstance(index, list)
+        assert isinstance(offsets, list)
         assert all(isinstance(x, Content) for x in contents)
         self.tags = tags
-        self.index = index
+        self.offsets = offsets
         self.contents = contents
 
     def __len__(self):
@@ -57,28 +59,28 @@ class UnionArray(Content):
 
     def __getitem__(self, where):
         if isinstance(where, int):
-            return self.contents[self.tags[where]][self.index[where]]
+            return self.contents[self.tags[where]][self.offsets[where]]
         else:
-            return UnionArray(self.tags[where], self.index[where], self.contents)
+            return UnionArray(self.tags[where], self.offsets[where], self.contents)
 
 class OptionArray(Content):
-    def __init__(self, index, content):
-        assert isinstance(index, list)
+    def __init__(self, offsets, content):
+        assert isinstance(offsets, list)
         assert isinstance(content, Content)
-        self.index = index
+        self.offsets = offsets
         self.content = content
 
     def __len__(self):
-        return len(self.index)
+        return len(self.offsets)
 
     def __getitem__(self, where):
         if isinstance(where, int):
-            if self.index[where] == -1:
+            if self.offsets[where] == -1:
                 return None
             else:
-                return self.content[self.index[where]]
+                return self.content[self.offsets[where]]
         else:
-            return OptionArray(self.index[where], self.content)
+            return OptionArray(self.offsets[where], self.content)
 
 class EmptyArray(Content):
     def __init__(self):
@@ -112,7 +114,229 @@ class TupleArray(Content):
         else:
             return TupleArray([x[where] for x in self.contents])
 
+################################################################ Content tests
+
 assert list(OptionArray([0, -1, 1, -1, 2, -1, 3, -1, 4, -1, 5, -1, 6], UnionArray([1, 0, 1, 0, 1, 0, 1], [0, 0, 1, 1, 2, 2, 3], [FloatArray([100, 200, 300]), ListArray([0, 1, 4, 4, 5], ListArray([0, 3, 3, 5, 6, 9], FloatArray([1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9])))]))) == [[[1.1, 2.2, 3.3]], None, 100, None, [[], [4.4, 5.5], [6.6]], None, 200, None, [], None, 300, None, [[7.7, 8.8, 9.9]]]
 
 assert list(ListArray([0, 2, 2, 2, 3], TupleArray([FloatArray([1, 2, 3]), ListArray([0, 3, 3, 5], FloatArray([1.1, 2.2, 3.3, 4.4, 5.5]))]))) == [[(1, [1.1, 2.2, 3.3]), (2, [])], [], [], [(3, [4.4, 5.5])]]
 
+################################################################ Fillables
+
+class Fillable:
+    pass
+
+class UnknownFillable(Fillable):
+    def __init__(self, nullcount):
+        assert isinstance(nullcount, int)
+
+    @classmethod
+    def fromempty(cls):
+        return UnknownFillable(0)
+
+    def __len__(self):
+        return self.nullcount
+
+    def null(self):
+        raise NotImplementedError
+
+    def real(self, x):
+        raise NotImplementedError
+
+    def beginlist(self):
+        raise NotImplementedError
+
+    def endlist(self):
+        raise NotImplementedError
+
+    def begintuple(self, numfields):
+        raise NotImplementedError
+
+    def index(self, i):
+        raise NotImplementedError
+
+    def endtuple(self):
+        raise NotImplementedError
+
+class OptionFillable(Fillable):
+    def __init__(self, offsets, content):
+        assert isinstance(offsets, list)
+        assert isinstance(content, Fillable)
+        self.offsets = offsets
+        self.content = content
+
+    @classmethod
+    def fromnulls(cls, nullcount, content):
+        return cls([-1] * nullcount, content)
+
+    @classmethod
+    def fromvalids(cls, content):
+        return cls(list(range(len(content))), content)
+
+    def __len__(self):
+        return len(self.offsets)
+
+    def null(self):
+        raise NotImplementedError
+
+    def real(self, x):
+        raise NotImplementedError
+
+    def beginlist(self):
+        raise NotImplementedError
+
+    def endlist(self):
+        raise NotImplementedError
+
+    def begintuple(self, numfields):
+        raise NotImplementedError
+
+    def index(self, i):
+        raise NotImplementedError
+
+    def endtuple(self):
+        raise NotImplementedError
+
+class UnionFillable(Fillable):
+    def __init__(self, tags, offsets, contents):
+        assert isinstance(tags, list)
+        assert isinstance(offsets, list)
+        assert all(isinstance(x, Fillable) for x in contents)
+        self.tags = tags
+        self.offsets = offsets
+        self.contents = contents
+
+    @classmethod
+    def fromsingle(cls, firstcontent):
+        return UnionFillable([0] * len(firstcontent),
+                             list(range(len(firstcontent))),
+                             [firstcontent])
+
+    def __len__(self):
+        return len(self.tags)
+
+    def null(self):
+        raise NotImplementedError
+
+    def real(self, x):
+        raise NotImplementedError
+
+    def beginlist(self):
+        raise NotImplementedError
+
+    def endlist(self):
+        raise NotImplementedError
+
+    def begintuple(self, numfields):
+        raise NotImplementedError
+
+    def index(self, i):
+        raise NotImplementedError
+
+    def endtuple(self):
+        raise NotImplementedError
+
+class ListFillable(Fillable):
+    def __init__(self, offsets, content):
+        assert isinstance(offsets, list)
+        assert isinstance(content, Fillable)
+        self.offsets = offsets
+        self.content = content
+
+    @classmethod
+    def fromempty(cls):
+        return ListFillable([0], UnknownFillable.fromempty())
+
+    def __len__(self):
+        return len(self.offsets) - 1
+
+    def null(self):
+        raise NotImplementedError
+
+    def real(self, x):
+        raise NotImplementedError
+
+    def beginlist(self):
+        raise NotImplementedError
+
+    def endlist(self):
+        raise NotImplementedError
+
+    def begintuple(self, numfields):
+        raise NotImplementedError
+
+    def index(self, i):
+        raise NotImplementedError
+
+    def endtuple(self):
+        raise NotImplementedError
+
+class TupleFillable(Fillable):
+    def __init__(self, contents):
+        assert all(isinstance(x, Fillable) for x in contents)
+        self.contents = contents
+
+    @classmethod
+    def fromempty(cls):
+        return TupleFillable([])
+
+    def __len__(self):
+        if len(self.contents) == 0:
+            return 0
+        else:
+            return len(self.contents[0])
+
+    def null(self):
+        raise NotImplementedError
+
+    def real(self, x):
+        raise NotImplementedError
+
+    def beginlist(self):
+        raise NotImplementedError
+
+    def endlist(self):
+        raise NotImplementedError
+
+    def begintuple(self, numfields):
+        raise NotImplementedError
+
+    def index(self, i):
+        raise NotImplementedError
+
+    def endtuple(self):
+        raise NotImplementedError
+
+class FloatFillable(Fillable):
+    def __init__(self, data):
+        assert isinstance(data, list)
+        self.data = data
+
+    @classmethod
+    def fromempty(cls):
+        return FloatFillable([])
+
+    def __len__(self):
+        return len(self.data)
+
+    def null(self):
+        raise NotImplementedError
+
+    def real(self, x):
+        raise NotImplementedError
+
+    def beginlist(self):
+        raise NotImplementedError
+
+    def endlist(self):
+        raise NotImplementedError
+
+    def begintuple(self, numfields):
+        raise NotImplementedError
+
+    def index(self, i):
+        raise NotImplementedError
+
+    def endtuple(self):
+        raise NotImplementedError
+
+################################################################ Fillable tests
