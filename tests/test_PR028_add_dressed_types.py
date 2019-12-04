aa95e23521da78bf8877ba1f09be44f0f792e6c5
Jim Pivarski <jpivarski@gmail.com>
2019-12-04 10:09:43 -0600

    Suppose all static libraries have hidden symbols. Is MacOS happy?

--- tests/test_PR028_add_dressed_types.py
+++ tests/test_PR028_add_dressed_types.py
@@ -19,13 +19,13 @@ def test_highlevel():
     assert repr(a) == "<Array [[1.1 2.2 3.3] [] ... [7.7 8.8 9.9]] type='5 * var * float64'>"
     assert str(a) == "[[1.1 2.2 3.3] [] [4.4 5.5] [6.6] [7.7 8.8 9.9]]"
 
-    b = awkward1.Array(numpy.arange(100, dtype=numpy.int64))
-    assert repr(b) == "<Array [0 1 2 3 4 5 6 ... 93 94 95 96 97 98 99] type='100 * int64'>"
+    b = awkward1.Array(numpy.arange(100, dtype=numpy.int32))
+    assert repr(b) == "<Array [0 1 2 3 4 5 6 ... 93 94 95 96 97 98 99] type='100 * int32'>"
     assert str(b) == "[0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 ... 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99]"
 
-    c = awkward1.Array('[{"one": 1, "two": [1.1, 2.2]}, {"one": 999, "two": [-3.1415926]}]')
-    assert repr(c) == "<Array [{one: 1, two: [1.1 ... two: [-3.14]}] type='2 * {\"one\": int64, \"two\": va...'>"
-    assert str(c) == "[{one: 1, two: [1.1 2.2]} {one: 999, two: [-3.14]}]"
+    c = awkward1.Array('[{"one": 3.14, "two": [1.1, 2.2]}, {"one": 99.9, "two": [-3.1415926]}]')
+    assert repr(c) == "<Array [{one: 3.14, two: [1.1 ... -3.14]}] type='2 * {\"one\": float64, \"two\": var...'>"
+    assert str(c) == "[{one: 3.14, two: [1.1 2.2]} {one: 99.9, two: [-3.14]}]"
 
 def test_string():
     a = awkward1.Array(numpy.array([ord(x) for x in "hey there"], dtype=numpy.uint8))
