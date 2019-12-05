7f6fd41f73e3c289f7bc440456878e57871cdda9
Jim Pivarski <jpivarski@gmail.com>
2019-12-05 09:36:14 -0600

    Now fix the non-Python 2.7.

--- tests/test_PR028_add_dressed_types.py
+++ tests/test_PR028_add_dressed_types.py
@@ -80,8 +80,11 @@ def test_string2():
 
     if py27:
         assert repr(a) == "<Array [u'hey', u'', u'there'] type='3 * string'>"
+        assert repr(a[0]) == "u'hey'"
+        assert repr(a[1]) == "u''"
+        assert repr(a[2]) == "u'there'"
     else:
         assert repr(a) == "<Array ['hey', '', 'there'] type='3 * string'>"
-    assert repr(a[0]) == "u'hey'"
-    assert repr(a[1]) == "u''"
-    assert repr(a[2]) == "u'there'"
+        assert repr(a[0]) == "'hey'"
+        assert repr(a[1]) == "''"
+        assert repr(a[2]) == "'there'"
