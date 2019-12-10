2d22475edad62dbc51c178f20a89eac7ba87d857
Jim Pivarski <jpivarski@gmail.com>
2019-12-10 17:38:14 -0600

    All Types in Numba work, for all array types.

--- awkward1/_numba/array/recordarray.py
+++ awkward1/_numba/array/recordarray.py
@@ -11,11 +11,7 @@ from ..._numba import cpu, util, content
 
 @numba.extending.typeof_impl.register(awkward1.layout.RecordArray)
 def typeof(val, c):
-    import awkward1._numba.types
-    # type = val.type
-    # if isinstance(type, awkward1.layout.ArrayType):
-    #     type = type.type
-    return RecordArrayType([numba.typeof(x) for x in val.fields()], val.lookup, val.reverselookup, numba.typeof(val.id), awkward1._numba.types.typeof(val.type))   # numba.typeof(type))
+    return RecordArrayType([numba.typeof(x) for x in val.fields()], val.lookup, val.reverselookup, numba.typeof(val.id), numba.typeof(val.type))
 
 @numba.extending.typeof_impl.register(awkward1.layout.Record)
 def typeof(val, c):
@@ -158,6 +154,8 @@ class RecordArrayModel(numba.datamodel.models.StructModel):
             members.append((field(i), x))
         if fe_type.idtpe != numba.none:
             members.append(("id", fe_type.idtpe))
+        if fe_type.typetpe != numba.none:
+            members.append(("type", fe_type.typetpe))
         super(RecordArrayModel, self).__init__(dmm, fe_type, members)
 
 @numba.datamodel.registry.register_default(RecordType)
@@ -187,6 +185,12 @@ def unbox(tpe, obj, c):
         id_obj = c.pyapi.object_getattr_string(obj, "id")
         proxyout.id = c.pyapi.to_native_value(tpe.idtpe, id_obj).value
         c.pyapi.decref(id_obj)
+    if tpe.typetpe != numba.none:
+        type1_obj = c.pyapi.object_getattr_string(obj, "type")
+        type2_obj = c.pyapi.object_getattr_string(type1_obj, "type")
+        proxyout.type = c.pyapi.to_native_value(tpe.typetpe, type2_obj).value
+        c.pyapi.decref(type1_obj)
+        c.pyapi.decref(type2_obj)
     is_error = numba.cgutils.is_not_null(c.builder, c.pyapi.err_occurred())
     return numba.extending.NativeValue(proxyout._getvalue(), is_error)
 
@@ -214,7 +218,7 @@ def box(tpe, val, c):
     else:
         args.append(c.pyapi.make_none())
     if tpe.typetpe != numba.none:
-        args.append(c.pyapi.unserialize(c.pyapi.serialize_object(tpe.typetpe.literal_type)))
+        args.append(c.pyapi.from_native_value(tpe.typetpe, proxyin.type, c.env_manager))
     else:
         args.append(c.pyapi.make_none())
 
