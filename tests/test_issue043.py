c53bc2ca335ac35c718de60b6792e688b9af4960
Jim Pivarski <jpivarski@users.noreply.github.com>
2020-01-06 09:31:56 -0600

    Reproduce and fix issue #43. (#44)
    * [WIP] Reproduce and fix issue #43.
    
    * Fixed #43.

---
+++ tests/test_issue043.py
@@ -0,0 +1,15 @@
+# BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+import sys
+
+import pytest
+import numpy
+
+import awkward1
+
+def test():
+    array = awkward1.Array([{"x": "one"}, {"x": "two"}, {"x": "three"}])
+    assert awkward1.tolist(array) == [{"x": "one"}, {"x": "two"}, {"x": "three"}]
+    assert awkward1.tolist(awkward1.fromiter(awkward1.tolist(array))) == [{"x": "one"}, {"x": "two"}, {"x": "three"}]
+    assert awkward1.tolist(array.layout) == [{"x": "one"}, {"x": "two"}, {"x": "three"}]
+    assert awkward1.tolist(awkward1.fromiter(awkward1.tolist(array.layout))) == [{"x": "one"}, {"x": "two"}, {"x": "three"}]
