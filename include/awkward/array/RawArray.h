6aaf021551adcdbe2cdd27018715732c6915e4c6
Ianna Osborne <ianna.osborne@cern.ch>
2020-01-07 16:33:46 +0100

    [skip ci] correct RawArray

--- include/awkward/array/RawArray.h
+++ include/awkward/array/RawArray.h
@@ -377,7 +377,7 @@ namespace awkward {
 
     // operations
     const std::shared_ptr<Content> flatten(int64_t axis) const override {
-      throw std::runtime_error("FIXME: not implemented");
+      throw std::invalid_argument("RawArray is strictly one-dimentional");
     }
 
   protected:
