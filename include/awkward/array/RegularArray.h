d7fd599b7360c4e27ff919d4ea8f6ebe11bc42e1
Ianna Osborne <ianna.osborne@cern.ch>
2020-01-29 14:16:16 +0100

    revert change

--- include/awkward/array/RegularArray.h
+++ include/awkward/array/RegularArray.h
@@ -47,7 +47,7 @@ namespace awkward {
     const std::string key(int64_t fieldindex) const override;
     bool haskey(const std::string& key) const override;
     const std::vector<std::string> keys() const override;
-    
+
     // operations
     const Index64 count64() const override;
     const std::shared_ptr<Content> count(int64_t axis) const override;
