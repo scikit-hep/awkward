9e9217165db6e101bf832b60ad0a1e6c5007400e
Jim Pivarski <jpivarski@gmail.com>
2020-01-30 18:34:22 -0500

    Reviewing those classes...

--- studies/flatten.py
+++ studies/flatten.py
@@ -1,1 +1,200 @@
-HERE
+################################################################ Contents
+
+class Content:
+    def __iter__(self):
+        def convert(x):
+            if isinstance(x, Content):
+                return list(x)
+            elif isinstance(x, tuple):
+                return tuple(convert(y) for y in x)
+            else:
+                return x
+
+        for i in range(len(self)):
+            yield convert(self[i])
+
+    def __repr__(self):
+        return self.tostring_part("", "", "").rstrip()
+
+class NumpyArray(Content):
+    def __init__(self, data):
+        assert isinstance(data, list)
+        for x in data:
+            assert isinstance(x, (bool, int, float))
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, where):
+        if isinstance(where, int):
+            assert 0 <= where < len(self)
+            return self.data[where]
+        elif isinstance(where, slice):
+            return NumpyArray(self.data[where])
+        else:
+            raise AssertionError(where)
+
+    def tostring_part(self, indent, pre, post):
+        out = indent + pre + "<NumpyArray>\n"
+        out += indent + "    " + " ".join(repr(x) for x in self.data) + "\n"
+        out += indent + "</NumpyArray>" + post
+        return out
+
+class ListOffsetArray(Content):
+    def __init__(self, offsets, content):
+        assert isinstance(offsets, list)
+        assert isinstance(content, Content)
+        assert len(offsets) != 0
+        for i in range(len(offsets) - 1):
+            start = offsets[i]
+            stop = offsets[i + 1]
+            assert isinstance(start, int)
+            assert isinstance(stop, int)
+            if start != stop:
+                assert start < stop   # i.e. start <= stop
+                assert start >= 0
+                assert stop <= len(content)
+        self.offsets = offsets
+        self.content = content
+        
+    def __len__(self):
+        return len(self.offsets) - 1
+
+    def __getitem__(self, where):
+        if isinstance(where, int):
+            assert 0 <= where < len(self)
+            return self.content[self.offsets[where]:self.offsets[where + 1]]
+        elif isinstance(where, slice):
+            start = where.start
+            stop = where.stop + 1
+            return ListOffsetArray(self.offsets[start:stop], self.content)
+        else:
+            raise AssertionError(where)
+
+    def tostring_part(self, indent, pre, post):
+        out = indent + pre + "<ListOffsetArray>\n"
+        out += indent + "    <offsets>" + " ".join(repr(x) for x in self.offsets) + "</offsets>\n"
+        out += self.content.tostring_part(indent + "    ", "<content>", "</content>\n")
+        out += indent + "</ListOffsetArray>" + post
+        return out
+
+class ListArray(Content):
+    def __init__(self, starts, stops, content):
+        assert isinstance(starts, list)
+        assert isinstance(stops, list)
+        assert isinstance(content, Content)
+        assert len(stops) >= len(starts)
+        for i in range(len(starts)):
+            start = starts[i]
+            stop = stops[i]
+            assert isinstance(start, int)
+            assert isinstance(stop, int)
+            if start != stop:
+                assert start < stop   # i.e. start <= stop
+                assert start >= 0
+                assert stop <= len(content)
+        self.starts = starts
+        self.stops = stops
+        self.content = content
+        
+    def __len__(self):
+        return len(self.starts)
+
+    def __getitem__(self, where):
+        if isinstance(where, int):
+            assert 0 <= where < len(self)
+            return self.content[self.offsets[where]:self.offsets[where + 1]]
+        elif isinstance(where, slice):
+            start = where.start
+            stop = where.stop
+            return ListArray(self.starts[start:stop], self.stops[start:stop], self.content)
+        else:
+            raise AssertionError(where)
+
+    def tostring_part(self, indent, pre, post):
+        out = indent + pre + "<ListArray>\n"
+        out += indent + "    <offsets>" + " ".join(repr(x) for x in self.offsets) + "</offsets>\n"
+        out += self.content.tostring_part(indent + "    ", "<content>", "</content>\n")
+        out += indent + "</ListArray>" + post
+        return out
+
+
+# HERE HERE HERE HERE HERE HERE HERE HERE HERE HERE HERE
+
+
+class UnionArray(Content):
+    def __init__(self, tags, offsets, contents):
+        assert isinstance(tags, list)
+        assert isinstance(offsets, list)
+        assert all(isinstance(x, Content) for x in contents)
+        self.tags = tags
+        self.offsets = offsets
+        self.contents = contents
+
+    def __len__(self):
+        return len(self.tags)
+
+    def __getitem__(self, where):
+        if isinstance(where, int):
+            return self.contents[self.tags[where]][self.offsets[where]]
+        else:
+            return UnionArray(self.tags[where], self.offsets[where], self.contents)
+
+    def tostring_part(self, indent, pre, post):
+        out = indent + pre + "<UnionArray>\n"
+        out += indent + "    <tags>" + " ".join(repr(x) for x in self.tags) + "</tags>\n"
+        out += indent + "    <offsets>" + " ".join(repr(x) for x in self.offsets) + "</offsets>\n"
+        for i, content in enumerate(self.contents):
+            out += content.tostring_part(indent + "    ", "<content i=\"{}\">".format(i), "</content>\n")
+        out += indent + "</UnionArray>" + post
+        return out
+
+class OptionArray(Content):
+    def __init__(self, offsets, content):
+        assert isinstance(offsets, list)
+        assert isinstance(content, Content)
+        self.offsets = offsets
+        self.content = content
+
+    def __len__(self):
+        return len(self.offsets)
+
+    def __getitem__(self, where):
+        if isinstance(where, int):
+            if self.offsets[where] == -1:
+                return None
+            else:
+                return self.content[self.offsets[where]]
+        else:
+            return OptionArray(self.offsets[where], self.content)
+
+    def tostring_part(self, indent, pre, post):
+        out = indent + pre + "<OptionArray>\n"
+        out += indent + "    <offsets>" + " ".join(repr(x) for x in self.offsets) + "</offsets>\n"
+        out += self.content.tostring_part(indent + "    ", "<content>", "</content>\n")
+        out += indent + "</OptionArray>" + post
+        return out
+
+class TupleArray(Content):
+    def __init__(self, contents):
+        assert len(contents) != 0
+        assert all(isinstance(x, Content) for x in contents)
+        assert all(isinstance(x, EmptyArray) or len(contents[0]) == len(x) for x in contents)
+        self.contents = contents
+
+    def __len__(self):
+        return len(self.contents[0])
+
+    def __getitem__(self, where):
+        if isinstance(where, int):
+            return tuple(x[where] for x in self.contents)
+        else:
+            return TupleArray([x[where] for x in self.contents])
+
+    def tostring_part(self, indent, pre, post):
+        out = indent + pre + "<TupleArray>\n"
+        for i, content in enumerate(self.contents):
+            out += content.tostring_part(indent + "    ", "<content i=\"{}\">".format(i), "</content>\n")
+        out += indent + "</TupleArray>" + post
+        return out
