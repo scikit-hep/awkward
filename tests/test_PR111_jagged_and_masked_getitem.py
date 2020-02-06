e739dbdfc344bbb7f73cf9df958218a41805d5e5
Jim Pivarski <jpivarski@gmail.com>
2020-02-06 17:48:50 -0600

    Without a 'raise Exception'.

--- tests/test_PR111_jagged_and_masked_getitem.py
+++ tests/test_PR111_jagged_and_masked_getitem.py
@@ -70,5 +70,5 @@ def test_new_slices():
 def test_missing():
     array = awkward1.Array([0.0, 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9])
 
-    print(array[awkward1.Array([3, 6, None, None, 8, 6])])
-    raise Exception
+    # print(array[awkward1.Array([3, 6, None, None, 8, 6])])
+    # raise Exception
