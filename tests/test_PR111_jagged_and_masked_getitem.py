f531fdd63414a04bbb8b8ffff4ee5d38dfe03c03
Jim Pivarski <jpivarski@gmail.com>
2020-02-07 12:51:04 -0600

    Bisect on Azure: that wasn't it; do gc.collect()

--- tests/test_PR111_jagged_and_masked_getitem.py
+++ tests/test_PR111_jagged_and_masked_getitem.py
@@ -56,13 +56,26 @@ def test_new_slices():
     # # assert repr(awkward1.layout.Slice(listoffsetarray)) == "[jagged([0, 3, 3, 5], array([0, 9, 3, 2, 2]))]"
     # awkward1.layout.Slice(listoffsetarray)
 
+    import gc
+
     for i in range(1000):
+        print("i", i)
+
         offsets = awkward1.layout.Index64(numpy.array([1, 4, 4, 6], dtype=numpy.int64))
         listoffsetarray = awkward1.layout.ListOffsetArray64(offsets, content)
         assert awkward1.tolist(listoffsetarray) == [[0, 9, 3], [], [2, 2]]
 
         # assert repr(awkward1.layout.Slice(listoffsetarray)) == "[jagged([0, 3, 3, 5], array([0, 9, 3, 2, 2]))]"
-        awkward1.layout.Slice(listoffsetarray)
+        tmp = awkward1.layout.Slice(listoffsetarray)
+
+        del tmp
+        gc.collect()
+
+        del listoffsetarray
+        gc.collect()
+
+        del offsets
+        gc.collect()
 
     # starts = awkward1.layout.Index64(numpy.array([1, 99, 5], dtype=numpy.int64))
     # stops = awkward1.layout.Index64(numpy.array([4, 99, 7], dtype=numpy.int64))
