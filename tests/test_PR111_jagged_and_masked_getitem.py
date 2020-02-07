b2c33bcf045d82e6837662607d691094cb9a2b15
Jim Pivarski <jpivarski@gmail.com>
2020-02-07 12:13:41 -0600

    Bisect on Azure: indexedarray, 1000 times redefining one listoffsetarray

--- tests/test_PR111_jagged_and_masked_getitem.py
+++ tests/test_PR111_jagged_and_masked_getitem.py
@@ -57,12 +57,12 @@ def test_new_slices():
         # assert repr(awkward1.layout.Slice(listoffsetarray)) == "[jagged([0, 3, 3, 5], array([0, 9, 3, 2, 2]))]"
         awkward1.layout.Slice(listoffsetarray)
 
-        offsets = awkward1.layout.Index64(numpy.array([1, 4, 4, 6], dtype=numpy.int64))
-        listoffsetarray = awkward1.layout.ListOffsetArray64(offsets, content)
-        assert awkward1.tolist(listoffsetarray) == [[0, 9, 3], [], [2, 2]]
+    # offsets = awkward1.layout.Index64(numpy.array([1, 4, 4, 6], dtype=numpy.int64))
+    # listoffsetarray = awkward1.layout.ListOffsetArray64(offsets, content)
+    # assert awkward1.tolist(listoffsetarray) == [[0, 9, 3], [], [2, 2]]
 
-        # assert repr(awkward1.layout.Slice(listoffsetarray)) == "[jagged([0, 3, 3, 5], array([0, 9, 3, 2, 2]))]"
-        awkward1.layout.Slice(listoffsetarray)
+    # # assert repr(awkward1.layout.Slice(listoffsetarray)) == "[jagged([0, 3, 3, 5], array([0, 9, 3, 2, 2]))]"
+    # awkward1.layout.Slice(listoffsetarray)
 
     # starts = awkward1.layout.Index64(numpy.array([1, 99, 5], dtype=numpy.int64))
     # stops = awkward1.layout.Index64(numpy.array([4, 99, 7], dtype=numpy.int64))
