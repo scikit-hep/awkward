790786c2bf71fa4631b23630884f1ed8632d22ca
Jim Pivarski <jpivarski@gmail.com>
2020-02-10 08:16:00 -0600

    Move pyawkward.so into its own directory.

---
+++ include/awkward/python/pyawkward.h
@@ -0,0 +1,8 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#ifndef AWKWARDPY_PYAWKWARD_H_
+#define AWKWARDPY_PYAWKWARD_H_
+
+// nothing here yet
+
+#endif // AWKWARDPY_PYAWKWARD_H_
