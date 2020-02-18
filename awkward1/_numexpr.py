e8066bc9c5d4a738314bdc5f7696958c3ce4e30a
Jim Pivarski <jpivarski@gmail.com>
2020-02-18 06:58:16 -0600

    Implemented support for NumExpr.

---
+++ awkward1/_numexpr.py
@@ -0,0 +1,90 @@
+# BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+from __future__ import absolute_import
+
+import sys
+
+import awkward1.layout
+import awkward1.operations.convert
+import awkward1._util
+
+def getArguments(names, local_dict=None, global_dict=None):
+    """Replaces numexpr.necompiler.getArguments to avoid calling numpy.asarray on Awkward Arrays."""
+    call_frame = sys._getframe(2)
+
+    clear_local_dict = False
+    if local_dict is None:
+        local_dict = call_frame.f_locals
+        clear_local_dict = True
+    try:
+        frame_globals = call_frame.f_globals
+        if global_dict is None:
+            global_dict = frame_globals
+
+        # If `call_frame` is the top frame of the interpreter we can't clear its 
+        # `local_dict`, because it is actually the `global_dict`.
+        clear_local_dict = clear_local_dict and not frame_globals is local_dict
+
+        arguments = []
+        for name in names:
+            try:
+                a = local_dict[name]
+            except KeyError:
+                a = global_dict[name]
+            arguments.append(a)    # <--- This is different from NumExpr: don't call numpy.asarray(a)
+    finally:
+        # If we generated local_dict via an explicit reference to f_locals,
+        # clear the dict to prevent creating extra ref counts in the caller's scope
+        # See https://github.com/pydata/numexpr/issues/310
+        if clear_local_dict:
+            local_dict.clear()
+
+    return arguments
+
+def evaluate(expression, local_dict=None, global_dict=None, order="K", casting="safe", **kwargs):
+    import numexpr
+    import awkward1.highlevel
+
+    context = numexpr.necompiler.getContext(kwargs, frame_depth=1)
+    expr_key = (expression, tuple(sorted(context.items())))
+    if expr_key not in numexpr.necompiler._names_cache:
+        numexpr.necompiler._names_cache[expr_key] = numexpr.necompiler.getExprNames(expression, context)
+    names, ex_uses_vml = numexpr.necompiler._names_cache[expr_key]
+    arguments = getArguments(names, local_dict, global_dict)
+
+    arrays = [awkward1.operations.convert.tolayout(x, allowrecord=True, allowother=True) for x in arguments]
+
+    def getfunction(inputs):
+        if all(isinstance(x, awkward1.layout.NumpyArray) or not isinstance(x, awkward1.layout.Content) for x in inputs):
+
+            return lambda depth: awkward1.layout.NumpyArray(numexpr.evaluate(expression, dict(zip(names, inputs)), {}, order=order, casting=casting, **kwargs))
+
+        return None
+
+    return awkward1._util.wrap(awkward1._util.broadcast_and_apply(arrays, getfunction), awkward1._util.behaviorof(arrays))
+
+evaluate.evaluate = evaluate
+
+def re_evaluate(local_dict=None):
+    import numexpr
+    import awkward1.highlevel
+
+    try:
+        compiled_ex = numexpr.necompiler._numexpr_last["ex"]
+    except KeyError:
+        raise RuntimeError("not a previous evaluate() execution found")
+    names = numexpr.necompiler._numexpr_last["argnames"]
+    arguments = getArguments(names, local_dict)
+
+    arrays = [awkward1.operations.convert.tolayout(x, allowrecord=True, allowother=True) for x in arguments]
+
+    def getfunction(inputs):
+        if all(isinstance(x, awkward1.layout.NumpyArray) or not isinstance(x, awkward1.layout.Content) for x in inputs):
+
+            return lambda depth: awkward1.layout.NumpyArray(numexpr.re_evaluate(dict(zip(names, inputs))))
+
+        return None
+
+    return awkward1._util.wrap(awkward1._util.broadcast_and_apply(arrays, getfunction), awkward1._util.behaviorof(arrays))
+
+evaluate.re_evaluate = re_evaluate
---
+++ tests/test_PR119_numexpr_and_broadcast_arrays.py
@@ -0,0 +1,17 @@
+# BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+from __future__ import absolute_import
+
+import sys
+
+import pytest
+import numpy
+
+import awkward1
+
+def test_numexpr():
+    a = awkward1.Array([[1.1, 2.2, 3.3], [], [4.4, 5.5]])
+    b = awkward1.Array([100, 200, 300])
+    assert awkward1.tolist(awkward1.numexpr.evaluate("a + b")) == [[101.1, 102.2, 103.3], [], [304.4, 305.5]]
+    a = [1, 2, 3]
+    assert awkward1.tolist(awkward1.numexpr.re_evaluate()) == [101, 202, 303]
