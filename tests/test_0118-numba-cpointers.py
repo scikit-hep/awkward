d30fa017048166e9d54ae16a3c090f272adc795f
Jim Pivarski <jpivarski@gmail.com>
2020-02-20 07:11:00 -0600

    Not a complete test yet, but soon.

--- tests/test_0118-numba-cpointers.py
+++ tests/test_0118-numba-cpointers.py
@@ -39,7 +39,7 @@ class AwkwardLookupModel(numba.datamodel.models.StructModel):
 
 @numba.extending.unbox(AwkwardLookupType)
 def unbox_AwkwardLookupType(altype, alobj, c):
-    proxyout = numba.cgutils.create_struct_proxy(altype)(c.context, c.builder)
+    proxyout = c.context.make_helper(c.builder, altype)
 
     lookup_obj = c.pyapi.object_getattr_string(alobj, "lookup")
     proxyout.lookup = c.pyapi.to_native_value(altype.lookuptype, lookup_obj).value
@@ -55,12 +55,55 @@ def unbox_AwkwardLookupType(altype, alobj, c):
 
     is_error = numba.cgutils.is_not_null(c.builder, c.pyapi.err_occurred())
     return numba.extending.NativeValue(proxyout._getvalue(), is_error)
-    
+
+@numba.typing.templates.infer_getattr
+class AwkwardLookupType_methods(numba.typing.templates.AttributeTemplate):
+    key = AwkwardLookupType
+
+    def generic_resolve(self, altype, attr):
+        if attr == "getview":
+            return AwkwardViewType(altype, 0)
+
+@numba.extending.lower_getattr(AwkwardLookupType, "getview")
+def AwkwardLookupType_getview(context, builder, altype, alval):
+    alproxy = context.make_helper(builder, altype, alval)
+    arrayproxy = context.make_helper(builder, altype.arraytypes[0], getattr(alproxy, "array" + str(0)))
+    proxyout = context.make_helper(builder, AwkwardViewType(altype, 0))
+    proxyout.ptr = arrayproxy.data
+    return proxyout._getvalue()
+
+class AwkwardViewType(numba.types.Type):
+    def __init__(self, altype, which):
+        super(AwkwardViewType, self).__init__(name="AwkwardViewType({0}, {1})".format(altype.name, which))
+        self.altype = altype
+        self.which = which
+
+@numba.extending.register_model(AwkwardViewType)
+class AwkwardViewModel(numba.datamodel.models.StructModel):
+    def __init__(self, dmm, fe_type):
+        members = [("ptr", numba.types.CPointer(fe_type.altype.arraytypes[fe_type.which].dtype))]
+        super(AwkwardViewModel, self).__init__(dmm, fe_type, members)
+
+@numba.typing.templates.infer_getattr
+class AwkwardViewType_methods(numba.typing.templates.AttributeTemplate):
+    key = AwkwardViewType
+
+    def generic_resolve(self, avtype, attr):
+        if attr == "getval":
+            return avtype.altype.arraytypes[avtype.which].dtype
+
+@numba.extending.lower_getattr(AwkwardViewType, "getval")
+def AwkwardViewType_getval(context, builder, avtype, avval):
+    avproxy = context.make_helper(builder, avtype, avval)
+
+
 def test_tests():
     awkwardlookup = AwkwardLookup([numpy.array([1, 2, 3, 4, 5]), numpy.array([1.1, 2.2, 3.3])])
 
     @numba.njit
     def f1(x):
+        y = x.getview
+
         return 3.14
 
     f1(awkwardlookup)
