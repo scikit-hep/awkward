084582f4c82a68fe6088e73229c28e109503b05d
Jim Pivarski <jpivarski@gmail.com>
2020-02-29 20:01:58 -0500

    Now Awkward-Numba works again.

--- awkward1/_libawkward.py
+++ awkward1/_libawkward.py
@@ -17,110 +17,110 @@ libpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), libname)
 
 lib = ctypes.cdll.LoadLibrary(libpath)
 
-# bool awkward_FillableArray_length(void* fillablearray, int64_t* result);
-FillableArray_length = lib.awkward_FillableArray_length
-FillableArray_length.name = "FillableArray.length"
-FillableArray_length.argtypes  = [ctypes.c_voidp, ctypes.POINTER(ctypes.c_int64)]
-FillableArray_length.restype   = ctypes.c_uint8
+# bool awkward_ArrayBuilder_length(void* fillablearray, int64_t* result);
+ArrayBuilder_length = lib.awkward_ArrayBuilder_length
+ArrayBuilder_length.name = "ArrayBuilder.length"
+ArrayBuilder_length.argtypes  = [ctypes.c_voidp, ctypes.POINTER(ctypes.c_int64)]
+ArrayBuilder_length.restype   = ctypes.c_uint8
 
-# bool awkward_FillableArray_clear(void* fillablearray);
-FillableArray_clear = lib.awkward_FillableArray_clear
-FillableArray_clear.name = "FillableArray.clear"
-FillableArray_clear.argtypes  = [ctypes.c_voidp]
-FillableArray_clear.restype   = ctypes.c_uint8
+# bool awkward_ArrayBuilder_clear(void* fillablearray);
+ArrayBuilder_clear = lib.awkward_ArrayBuilder_clear
+ArrayBuilder_clear.name = "ArrayBuilder.clear"
+ArrayBuilder_clear.argtypes  = [ctypes.c_voidp]
+ArrayBuilder_clear.restype   = ctypes.c_uint8
 
-# bool awkward_FillableArray_null(void* fillablearray);
-FillableArray_null = lib.awkward_FillableArray_null
-FillableArray_null.name = "FillableArray.null"
-FillableArray_null.argtypes  = [ctypes.c_voidp]
-FillableArray_null.restype   = ctypes.c_uint8
+# bool awkward_ArrayBuilder_null(void* fillablearray);
+ArrayBuilder_null = lib.awkward_ArrayBuilder_null
+ArrayBuilder_null.name = "ArrayBuilder.null"
+ArrayBuilder_null.argtypes  = [ctypes.c_voidp]
+ArrayBuilder_null.restype   = ctypes.c_uint8
 
-# bool awkward_FillableArray_boolean(void* fillablearray, bool x);
-FillableArray_boolean = lib.awkward_FillableArray_boolean
-FillableArray_boolean.name = "FillableArray.boolean"
-FillableArray_boolean.argtypes  = [ctypes.c_voidp, ctypes.c_uint8]
-FillableArray_boolean.restype   = ctypes.c_uint8
+# bool awkward_ArrayBuilder_boolean(void* fillablearray, bool x);
+ArrayBuilder_boolean = lib.awkward_ArrayBuilder_boolean
+ArrayBuilder_boolean.name = "ArrayBuilder.boolean"
+ArrayBuilder_boolean.argtypes  = [ctypes.c_voidp, ctypes.c_uint8]
+ArrayBuilder_boolean.restype   = ctypes.c_uint8
 
-# bool awkward_FillableArray_integer(void* fillablearray, int64_t x);
-FillableArray_integer = lib.awkward_FillableArray_integer
-FillableArray_integer.name = "FillableArray.integer"
-FillableArray_integer.argtypes  = [ctypes.c_voidp, ctypes.c_int64]
-FillableArray_integer.restype   = ctypes.c_uint8
+# bool awkward_ArrayBuilder_integer(void* fillablearray, int64_t x);
+ArrayBuilder_integer = lib.awkward_ArrayBuilder_integer
+ArrayBuilder_integer.name = "ArrayBuilder.integer"
+ArrayBuilder_integer.argtypes  = [ctypes.c_voidp, ctypes.c_int64]
+ArrayBuilder_integer.restype   = ctypes.c_uint8
 
-# bool awkward_FillableArray_real(void* fillablearray, double x);
-FillableArray_real = lib.awkward_FillableArray_real
-FillableArray_real.name = "FillableArray.real"
-FillableArray_real.argtypes  = [ctypes.c_voidp, ctypes.c_double]
-FillableArray_real.restype   = ctypes.c_uint8
+# bool awkward_ArrayBuilder_real(void* fillablearray, double x);
+ArrayBuilder_real = lib.awkward_ArrayBuilder_real
+ArrayBuilder_real.name = "ArrayBuilder.real"
+ArrayBuilder_real.argtypes  = [ctypes.c_voidp, ctypes.c_double]
+ArrayBuilder_real.restype   = ctypes.c_uint8
 
-# bool awkward_FillableArray_beginlist(void* fillablearray);
-FillableArray_beginlist = lib.awkward_FillableArray_beginlist
-FillableArray_beginlist.name = "FillableArray.beginlist"
-FillableArray_beginlist.argtypes  = [ctypes.c_voidp]
-FillableArray_beginlist.restype   = ctypes.c_uint8
+# bool awkward_ArrayBuilder_beginlist(void* fillablearray);
+ArrayBuilder_beginlist = lib.awkward_ArrayBuilder_beginlist
+ArrayBuilder_beginlist.name = "ArrayBuilder.beginlist"
+ArrayBuilder_beginlist.argtypes  = [ctypes.c_voidp]
+ArrayBuilder_beginlist.restype   = ctypes.c_uint8
 
-# bool awkward_FillableArray_endlist(void* fillablearray);
-FillableArray_endlist = lib.awkward_FillableArray_endlist
-FillableArray_endlist.name = "FillableArray.endlist"
-FillableArray_endlist.argtypes  = [ctypes.c_voidp]
-FillableArray_endlist.restype   = ctypes.c_uint8
+# bool awkward_ArrayBuilder_endlist(void* fillablearray);
+ArrayBuilder_endlist = lib.awkward_ArrayBuilder_endlist
+ArrayBuilder_endlist.name = "ArrayBuilder.endlist"
+ArrayBuilder_endlist.argtypes  = [ctypes.c_voidp]
+ArrayBuilder_endlist.restype   = ctypes.c_uint8
 
-# uint8_t awkward_FillableArray_begintuple(void* fillablearray, int64_t numfields);
-FillableArray_begintuple = lib.awkward_FillableArray_begintuple
-FillableArray_begintuple.name = "FillableArray.begintuple"
-FillableArray_begintuple.argtypes  = [ctypes.c_voidp, ctypes.c_int64]
-FillableArray_begintuple.restype   = ctypes.c_uint8
+# uint8_t awkward_ArrayBuilder_begintuple(void* fillablearray, int64_t numfields);
+ArrayBuilder_begintuple = lib.awkward_ArrayBuilder_begintuple
+ArrayBuilder_begintuple.name = "ArrayBuilder.begintuple"
+ArrayBuilder_begintuple.argtypes  = [ctypes.c_voidp, ctypes.c_int64]
+ArrayBuilder_begintuple.restype   = ctypes.c_uint8
 
-# uint8_t awkward_FillableArray_index(void* fillablearray, int64_t index);
-FillableArray_index = lib.awkward_FillableArray_index
-FillableArray_index.name = "FillableArray.index"
-FillableArray_index.argtypes  = [ctypes.c_voidp, ctypes.c_int64]
-FillableArray_index.restype   = ctypes.c_uint8
+# uint8_t awkward_ArrayBuilder_index(void* fillablearray, int64_t index);
+ArrayBuilder_index = lib.awkward_ArrayBuilder_index
+ArrayBuilder_index.name = "ArrayBuilder.index"
+ArrayBuilder_index.argtypes  = [ctypes.c_voidp, ctypes.c_int64]
+ArrayBuilder_index.restype   = ctypes.c_uint8
 
-# uint8_t awkward_FillableArray_endtuple(void* fillablearray);
-FillableArray_endtuple = lib.awkward_FillableArray_endtuple
-FillableArray_endtuple.name = "FillableArray.endtuple"
-FillableArray_endtuple.argtypes  = [ctypes.c_voidp]
-FillableArray_endtuple.restype   = ctypes.c_uint8
+# uint8_t awkward_ArrayBuilder_endtuple(void* fillablearray);
+ArrayBuilder_endtuple = lib.awkward_ArrayBuilder_endtuple
+ArrayBuilder_endtuple.name = "ArrayBuilder.endtuple"
+ArrayBuilder_endtuple.argtypes  = [ctypes.c_voidp]
+ArrayBuilder_endtuple.restype   = ctypes.c_uint8
 
-# uint8_t awkward_FillableArray_beginrecord(void* fillablearray);
-FillableArray_beginrecord = lib.awkward_FillableArray_beginrecord
-FillableArray_beginrecord.name = "FillableArray.beginrecord"
-FillableArray_beginrecord.argtypes  = [ctypes.c_voidp]
-FillableArray_beginrecord.restype   = ctypes.c_uint8
+# uint8_t awkward_ArrayBuilder_beginrecord(void* fillablearray);
+ArrayBuilder_beginrecord = lib.awkward_ArrayBuilder_beginrecord
+ArrayBuilder_beginrecord.name = "ArrayBuilder.beginrecord"
+ArrayBuilder_beginrecord.argtypes  = [ctypes.c_voidp]
+ArrayBuilder_beginrecord.restype   = ctypes.c_uint8
 
-# uint8_t awkward_FillableArray_beginrecord_fast(void* fillablearray, const char* name);
-FillableArray_beginrecord_fast = lib.awkward_FillableArray_beginrecord_fast
-FillableArray_beginrecord_fast.name = "FillableArray.beginrecord_fast"
-FillableArray_beginrecord_fast.argtypes  = [ctypes.c_voidp, ctypes.c_voidp]
-FillableArray_beginrecord_fast.restype   = ctypes.c_uint8
+# uint8_t awkward_ArrayBuilder_beginrecord_fast(void* fillablearray, const char* name);
+ArrayBuilder_beginrecord_fast = lib.awkward_ArrayBuilder_beginrecord_fast
+ArrayBuilder_beginrecord_fast.name = "ArrayBuilder.beginrecord_fast"
+ArrayBuilder_beginrecord_fast.argtypes  = [ctypes.c_voidp, ctypes.c_voidp]
+ArrayBuilder_beginrecord_fast.restype   = ctypes.c_uint8
 
-# uint8_t awkward_FillableArray_beginrecord_check(void* fillablearray, const char* name);
-FillableArray_beginrecord_check = lib.awkward_FillableArray_beginrecord_check
-FillableArray_beginrecord_check.name = "FillableArray.beginrecord_check"
-FillableArray_beginrecord_check.argtypes  = [ctypes.c_voidp, ctypes.c_voidp]
-FillableArray_beginrecord_check.restype   = ctypes.c_uint8
+# uint8_t awkward_ArrayBuilder_beginrecord_check(void* fillablearray, const char* name);
+ArrayBuilder_beginrecord_check = lib.awkward_ArrayBuilder_beginrecord_check
+ArrayBuilder_beginrecord_check.name = "ArrayBuilder.beginrecord_check"
+ArrayBuilder_beginrecord_check.argtypes  = [ctypes.c_voidp, ctypes.c_voidp]
+ArrayBuilder_beginrecord_check.restype   = ctypes.c_uint8
 
-# uint8_t awkward_FillableArray_field_fast(void* fillablearray, const char* key);
-FillableArray_field_fast = lib.awkward_FillableArray_field_fast
-FillableArray_field_fast.name = "FillableArray.field_fast"
-FillableArray_field_fast.argtypes  = [ctypes.c_voidp, ctypes.c_voidp]
-FillableArray_field_fast.restype   = ctypes.c_uint8
+# uint8_t awkward_ArrayBuilder_field_fast(void* fillablearray, const char* key);
+ArrayBuilder_field_fast = lib.awkward_ArrayBuilder_field_fast
+ArrayBuilder_field_fast.name = "ArrayBuilder.field_fast"
+ArrayBuilder_field_fast.argtypes  = [ctypes.c_voidp, ctypes.c_voidp]
+ArrayBuilder_field_fast.restype   = ctypes.c_uint8
 
-# uint8_t awkward_FillableArray_field_check(void* fillablearray, const char* key);
-FillableArray_field_check = lib.awkward_FillableArray_field_check
-FillableArray_field_check.name = "FillableArray.field_check"
-FillableArray_field_check.argtypes  = [ctypes.c_voidp, ctypes.c_voidp]
-FillableArray_field_check.restype   = ctypes.c_uint8
+# uint8_t awkward_ArrayBuilder_field_check(void* fillablearray, const char* key);
+ArrayBuilder_field_check = lib.awkward_ArrayBuilder_field_check
+ArrayBuilder_field_check.name = "ArrayBuilder.field_check"
+ArrayBuilder_field_check.argtypes  = [ctypes.c_voidp, ctypes.c_voidp]
+ArrayBuilder_field_check.restype   = ctypes.c_uint8
 
-# uint8_t awkward_FillableArray_endrecord(void* fillablearray);
-FillableArray_endrecord = lib.awkward_FillableArray_endrecord
-FillableArray_endrecord.name = "FillableArray.endrecord"
-FillableArray_endrecord.argtypes  = [ctypes.c_voidp]
-FillableArray_endrecord.restype   = ctypes.c_uint8
+# uint8_t awkward_ArrayBuilder_endrecord(void* fillablearray);
+ArrayBuilder_endrecord = lib.awkward_ArrayBuilder_endrecord
+ArrayBuilder_endrecord.name = "ArrayBuilder.endrecord"
+ArrayBuilder_endrecord.argtypes  = [ctypes.c_voidp]
+ArrayBuilder_endrecord.restype   = ctypes.c_uint8
 
-# uint8_t awkward_FillableArray_append_nowrap(void* fillablearray, const void* shared_ptr_ptr, int64_t at);
-FillableArray_append_nowrap = lib.awkward_FillableArray_append_nowrap
-FillableArray_append_nowrap.name = "FillableArray.append_nowrap"
-FillableArray_append_nowrap.argtypes  = [ctypes.c_voidp, ctypes.c_voidp, ctypes.c_int64]
-FillableArray_append_nowrap.restype   = ctypes.c_uint8
+# uint8_t awkward_ArrayBuilder_append_nowrap(void* fillablearray, const void* shared_ptr_ptr, int64_t at);
+ArrayBuilder_append_nowrap = lib.awkward_ArrayBuilder_append_nowrap
+ArrayBuilder_append_nowrap.name = "ArrayBuilder.append_nowrap"
+ArrayBuilder_append_nowrap.argtypes  = [ctypes.c_voidp, ctypes.c_voidp, ctypes.c_int64]
+ArrayBuilder_append_nowrap.restype   = ctypes.c_uint8
