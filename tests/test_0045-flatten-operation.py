c06475cd5ef1fedb14847172e35dde13a2def232
Jim Pivarski <jpivarski@gmail.com>
2020-03-06 07:32:02 -0500

    Removing tests that currently raise RuntimeError: these will have to be re-thought, not patched-up.

--- tests/test_0045-flatten-operation.py
+++ tests/test_0045-flatten-operation.py
@@ -39,8 +39,6 @@ def test_flatten_list_array():
 
     assert awkward1.tolist(array) == [[0.0, 1.1, 2.2], [], [4.4, 5.5], [5.5, 6.6, 7.7], [8.8]]
     assert awkward1.tolist(array.flatten()) == [0.0, 1.1, 2.2, 4.4, 5.5, 5.5, 6.6, 7.7, 8.8]
-    with pytest.raises(RuntimeError) as err:
-        awkward1.tolist(array.flatten(-1)) == [0.0, 1.1, 2.2, 4.4, 5.5, 5.5, 6.6, 7.7, 8.8]
 
     array2 = array[2:-1]
     assert awkward1.tolist(array2) == [[4.4, 5.5], [5.5, 6.6, 7.7]]
@@ -69,8 +67,6 @@ def test_flatten_list_offset_array():
 
     assert awkward1.tolist(array) == [[0.0, 1.1, 2.2], [], [3.3, 4.4], [5.5], [6.6, 7.7, 8.8, 9.9]]
     assert awkward1.tolist(array.flatten()) == [0.0, 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9]
-    with pytest.raises(RuntimeError) as err:
-        awkward1.tolist(array.flatten(-1)) == [0.0, 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9]
 
     # ValueError: ListOffsetArrayOf<T> cannot be flattened in axis -2 because its depth is 2
     # assert awkward1.tolist(array.flatten(-2)) == [0.0, 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9]
@@ -86,10 +82,6 @@ def test_flatten_numpy_array():
     assert awkward1.tolist(array.flatten()) == [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14], [15, 16, 17, 18, 19], [20, 21, 22, 23, 24], [25, 26, 27, 28, 29]]
     assert flatten(awkward1.tolist(array), 1) == [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]]
     assert awkward1.tolist(array.flatten(1)) == [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]]
-    with pytest.raises(RuntimeError) as err:
-        awkward1.tolist(array.flatten(-1)) == [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]]
-    with pytest.raises(RuntimeError) as err:
-        awkward1.tolist(array.flatten(-2)) == [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14], [15, 16, 17, 18, 19], [20, 21, 22, 23, 24], [25, 26, 27, 28, 29]]
 
     array2 = awkward1.layout.NumpyArray(numpy.arange(1*6*5, dtype=numpy.int64).reshape(1, 6, 5))
     assert awkward1.tolist(array2.flatten()) == [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14], [15, 16, 17, 18, 19], [20, 21, 22, 23, 24], [25, 26, 27, 28, 29]]
@@ -163,13 +155,6 @@ def test_flatten_regular_array():
     assert awkward1.tolist(regulararray.flatten()) == [[0.0, 1.1, 2.2], [], [3.3, 4.4], [5.5], [6.6, 7.7, 8.8, 9.9], []]
     assert flatten(awkward1.tolist(regulararray), 1) == [[0.0, 1.1, 2.2], [3.3, 4.4, 5.5], [6.6, 7.7, 8.8, 9.9]]
     assert awkward1.tolist(regulararray.flatten(1)) == [[0.0, 1.1, 2.2], [3.3, 4.4, 5.5], [6.6, 7.7, 8.8, 9.9]]
-    with pytest.raises(RuntimeError) as err:
-        awkward1.tolist(regulararray.flatten(-1)) == [[0.0, 1.1, 2.2], [3.3, 4.4, 5.5], [6.6, 7.7, 8.8, 9.9]]
-    assert str(err.value) == "FIXME: negative axis not implemented yet"
-
-    with pytest.raises(RuntimeError) as err:
-        awkward1.tolist(regulararray.flatten(-2)) == [[0.0, 1.1, 2.2], [], [3.3, 4.4], [5.5], [6.6, 7.7, 8.8, 9.9], []]
-    assert str(err.value) == "FIXME: negative axis not implemented yet"
 
     # ValueError: RegularArray cannot be flattened in axis -3 because its depth is 3
     # assert awkward1.tolist(regulararray.flatten(-3)) == [[0.0, 1.1, 2.2], [], [3.3, 4.4], [5.5], [6.6, 7.7, 8.8, 9.9], []]
@@ -198,19 +183,6 @@ def test_flatten_regular_array():
                                                       [[3.3, 4.4, 5.5], [6.6, 7.7, 8.8, 9.9]]]
     assert awkward1.tolist(listarray.flatten(2)) == [[[0.0, 1.1, 2.2], [3.3, 4.4, 5.5]],
                                                       [[3.3, 4.4, 5.5], [6.6, 7.7, 8.8, 9.9]]]
-    with pytest.raises(RuntimeError) as err:
-        awkward1.tolist(listarray.flatten(-1)) == [[[0.0, 1.1, 2.2], [3.3, 4.4, 5.5]],
-                                                   [[3.3, 4.4, 5.5], [6.6, 7.7, 8.8, 9.9]]]
-    assert str(err.value) == "FIXME: negative axis not implemented yet"
-
-    with pytest.raises(RuntimeError) as err:
-        awkward1.tolist(listarray.flatten(-2)) == [[[0.0, 1.1, 2.2], [],    [3.3, 4.4],           [5.5]],
-                                                   [[3.3, 4.4],      [5.5], [6.6, 7.7, 8.8, 9.9], []]]
-    assert str(err.value) == "FIXME: negative axis not implemented yet"
-
-    with pytest.raises(RuntimeError) as err:
-        awkward1.tolist(listarray.flatten(-3)) == [[[0.0, 1.1, 2.2], []], [[3.3, 4.4], [5.5]], [[3.3, 4.4], [5.5]], [[6.6, 7.7, 8.8, 9.9], []]]
-    assert str(err.value) == "FIXME: negative axis not implemented yet"
 
     # ValueError: ListArrayOf<T> cannot be flattened in axis -4 because its depth is 4
     # assert awkward1.tolist(listarray.flatten(-4)) == []
@@ -239,6 +211,3 @@ def test_flatten_regular_array():
     assert awkward1.tolist(regulararray_m3.flatten()) == [[0.0, 1.1, 2.2], [], [4.4, 5.5], [5.5, 6.6, 7.7], [8.8]]
     assert flatten(awkward1.tolist(regulararray_m3), 1) == [[0.0, 1.1, 2.2], [], [4.4, 5.5], [5.5, 6.6, 7.7], [8.8]]
     assert awkward1.tolist(regulararray_m3.flatten(1)) == [[0.0, 1.1, 2.2], [], [4.4, 5.5], [5.5, 6.6, 7.7], [8.8]]
-    with pytest.raises(RuntimeError) as err:
-        awkward1.tolist(regulararray_m3.flatten(-1)) == [[0.0, 1.1, 2.2], [], [4.4, 5.5], [5.5, 6.6, 7.7], [8.8]]
-    assert str(err.value) == "FIXME: negative axis not implemented yet"
