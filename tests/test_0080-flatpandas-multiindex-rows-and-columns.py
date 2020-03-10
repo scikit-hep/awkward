6ee57c928469d5c02750340036b64c6853558504
Jim Pivarski <jpivarski@gmail.com>
2020-03-10 00:16:27 -0500

    Stubs for the function and tests.

---
+++ tests/test_0080-flatpandas-multiindex-rows-and-columns.py
@@ -0,0 +1,15 @@
+# BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+from __future__ import absolute_import
+
+import sys
+
+import pytest
+import numpy
+
+import awkward1
+
+pandas = pytest.importorskip("pandas")
+
+def test():
+    pass
