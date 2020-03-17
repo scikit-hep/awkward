956482dab16e98982923c77593e68d9ecb0df2b5
Jim Pivarski <jpivarski@gmail.com>
2020-03-17 16:49:10 -0500

    [WIP] Issues #166, #167, #170 in one PR.

---
+++ tests/test_0166-0167-0170-random-issues.py
@@ -0,0 +1,19 @@
+# BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+from __future__ import absolute_import
+
+import sys
+
+import pytest
+import numpy
+
+import awkward1
+
+def test_0166():
+    pass
+
+def test_0167():
+    pass
+
+def test_0170():
+    pass
