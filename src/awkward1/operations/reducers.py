43bdc047829b566b1ad13f860e6a5c5a46b50f3f
Jim Pivarski <jpivarski@gmail.com>
2020-03-29 09:58:56 -0500

    [skip ci] Writing about reducers.

--- src/awkward1/operations/reducers.py
+++ src/awkward1/operations/reducers.py
@@ -704,7 +704,7 @@ def moment(x, n, weight=None, axis=None, keepdims=False, maskidentity=True):
             calculation is followed through with the reducers' identities,
             usually resulting in floating-point `nan`.
 
-    Computes the `n`th moment in each group of elements from `array` (many
+    Computes the `n`th moment in each group of elements from `x` (many
     types supported, including all Awkward Arrays and Records). The grouping
     is performed the same way as for reducers, though this operation is not a
     reducer and has no identity.
@@ -758,7 +758,7 @@ def mean(x, weight=None, axis=None, keepdims=False, maskidentity=True):
             calculation is followed through with the reducers' identities,
             usually resulting in floating-point `nan`.
 
-    Computes the mean in each group of elements from `array` (many
+    Computes the mean in each group of elements from `x` (many
     types supported, including all Awkward Arrays and Records). The grouping
     is performed the same way as for reducers, though this operation is not a
     reducer and has no identity. It is the same as NumPy's
@@ -836,7 +836,7 @@ def var(x, weight=None, ddof=0, axis=None, keepdims=False, maskidentity=True):
             calculation is followed through with the reducers' identities,
             usually resulting in floating-point `nan`.
 
-    Computes the variance in each group of elements from `array` (many
+    Computes the variance in each group of elements from `x` (many
     types supported, including all Awkward Arrays and Records). The grouping
     is performed the same way as for reducers, though this operation is not a
     reducer and has no identity. It is the same as NumPy's
@@ -880,6 +880,47 @@ def var(x, weight=None, ddof=0, axis=None, keepdims=False, maskidentity=True):
 
 @awkward1._connect._numpy.implements(numpy.std)
 def std(x, weight=None, ddof=0, axis=None, keepdims=False, maskidentity=True):
+    """
+    Args:
+        x: the data on which to compute the standard deviation.
+        weight: data that can be broadcasted to `x` to give each value a
+            weight. Weighting values equally is the same as no weights;
+            weighting some values higher increases the significance of those
+            values. Weights can be zero or negative.
+        ddof (int): "delta degrees of freedom": the divisor used in the
+            calculation is `sum(weights) - ddof`. Use this for "reduced
+            standard deviation."
+        axis (None or int): If None, combine all values from the array into
+            a single scalar result; if an int, group by that axis: `0` is the
+            outermost, `1` is the first level of nested lists, etc., and
+            negative `axis` counts from the innermost: `-1` is the innermost,
+            `-2` is the next level up, etc.
+        keepdims (bool): If False, this function descreases the number of
+            dimensions by 1; if True, the output values are wrapped in a new
+            length-1 dimension so that the result of this operation may be
+            broadcasted with the original array.
+        maskidentity (bool): If True, the application of this function on
+            empty lists results in None (an option type); otherwise, the
+            calculation is followed through with the reducers' identities,
+            usually resulting in floating-point `nan`.
+
+    Computes the standard deviation in each group of elements from `x`
+    (many types supported, including all Awkward Arrays and Records). The
+    grouping is performed the same way as for reducers, though this operation
+    is not a reducer and has no identity. It is the same as NumPy's
+    [std](https://docs.scipy.org/doc/numpy/reference/generated/numpy.std.html)
+    if all lists at a given dimension have the same length and no None values,
+    but it generalizes to cases where they do not.
+
+    Passing all arguments to the reducers, the standard deviation is
+    calculated as
+
+        np.sqrt(ak.var(x, weight))
+
+    See #ak.sum for a complete description of handling nested lists and
+    missing values (None) in reducers, and #ak.mean for an example with another
+    non-reducer.
+    """
     with numpy.errstate(invalid="ignore"):
         return numpy.sqrt(var(x,
                               weight=weight,
@@ -889,6 +930,43 @@ def std(x, weight=None, ddof=0, axis=None, keepdims=False, maskidentity=True):
                               maskidentity=maskidentity))
 
 def covar(x, y, weight=None, axis=None, keepdims=False, maskidentity=True):
+    """
+    Args:
+        x: one coordinate to use in the covariance calculation.
+        y: the other coordinate to use in the covariance calculation.
+        weight: data that can be broadcasted to `x` to give each value a
+            weight. Weighting values equally is the same as no weights;
+            weighting some values higher increases the significance of those
+            values. Weights can be zero or negative.
+        axis (None or int): If None, combine all values from the array into
+            a single scalar result; if an int, group by that axis: `0` is the
+            outermost, `1` is the first level of nested lists, etc., and
+            negative `axis` counts from the innermost: `-1` is the innermost,
+            `-2` is the next level up, etc.
+        keepdims (bool): If False, this function descreases the number of
+            dimensions by 1; if True, the output values are wrapped in a new
+            length-1 dimension so that the result of this operation may be
+            broadcasted with the original array.
+        maskidentity (bool): If True, the application of this function on
+            empty lists results in None (an option type); otherwise, the
+            calculation is followed through with the reducers' identities,
+            usually resulting in floating-point `nan`.
+
+    Computes the covariance of `x` and `y` (many types supported, including
+    all Awkward Arrays and Records, must be broadcastable to each other).
+    The grouping is performed the same way as for reducers, though this
+    operation is not a reducer and has no identity.
+
+    This function has no NumPy equivalent.
+
+    Passing all arguments to the reducers, the covariance is calculated as
+
+        ak.sum((x - ak.mean(x))*(y - ak.mean(y))*weight) / ak.sum(weight)
+
+    See #ak.sum for a complete description of handling nested lists and
+    missing values (None) in reducers, and #ak.mean for an example with another
+    non-reducer.
+    """
     with numpy.errstate(invalid="ignore"):
         xmean = mean(x, weight=weight,
                   axis=axis, keepdims=keepdims, maskidentity=maskidentity)
