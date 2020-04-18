da7a4947cb14f72e09cea7934996d7585af5c7e9
Jim Pivarski <jpivarski@gmail.com>
2020-04-18 11:56:48 -0500

    Turn those reference counts into checked assertions.

--- tests/test_0056b-partitioned-array-numba.py
+++ tests/test_0056b-partitioned-array-numba.py
@@ -43,56 +43,87 @@ def test_view():
             asview.stop = stop
             assert awkward1.to_list(asview.toarray()) == aslist[start:stop]
 
-def test_boxing():
+def test_boxing1():
     asnumpy = numpy.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    print(sys.getrefcount(asnumpy))
+    assert sys.getrefcount(asnumpy) == 2
 
     aslayout = awkward1.layout.NumpyArray(asnumpy)
     aspart = awkward1.repartition(aslayout, 3, highlevel=False)
     asarray = awkward1.Array(aspart)
 
-    print(sys.getrefcount(asnumpy), sys.getrefcount(aslayout), sys.getrefcount(aspart))
+    assert (sys.getrefcount(asnumpy), sys.getrefcount(aslayout), sys.getrefcount(aspart)) == (3, 2, 3)
 
     @numba.njit
     def f1(x):
         return 3.14
 
-    print("f1")
     for i in range(10):
         f1(asarray)
-        print(sys.getrefcount(asnumpy), sys.getrefcount(aslayout), sys.getrefcount(aspart))
+        assert (sys.getrefcount(asnumpy), sys.getrefcount(aslayout), sys.getrefcount(aspart)) == (3, 2, 3)
+
+    del asarray
+    del aspart
+    del aslayout
+    import gc
+    gc.collect()
+    assert sys.getrefcount(asnumpy) == 2
+
+def test_boxing2():
+    asnumpy = numpy.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    assert sys.getrefcount(asnumpy) == 2
+
+    aslayout = awkward1.layout.NumpyArray(asnumpy)
+    aspart = awkward1.repartition(aslayout, 3, highlevel=False)
+    asarray = awkward1.Array(aspart)
+
+    assert (sys.getrefcount(asnumpy), sys.getrefcount(aslayout), sys.getrefcount(aspart)) == (3, 2, 3)
 
     @numba.njit
     def f2(x):
         return x
 
-    print("f2")
     for i in range(10):
         out = f2(asarray)
 
         assert isinstance(out.layout, awkward1.partition.PartitionedArray)
         assert awkward1.to_list(out) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        print(sys.getrefcount(asnumpy), sys.getrefcount(aslayout), sys.getrefcount(aspart))
+        assert (sys.getrefcount(asnumpy), sys.getrefcount(aslayout), sys.getrefcount(aspart)) == (3, 2, 3)
 
-    @numba.njit
-    def f3(x):
-        return x, x
-
-    print("f3")
-    for i in range(10):
-        out1, out2 = f3(asarray)
-        assert isinstance(out.layout, awkward1.partition.PartitionedArray)
-        assert awkward1.to_list(out) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        print(sys.getrefcount(asnumpy), sys.getrefcount(aslayout), sys.getrefcount(aspart))
-
-    del out1
-    del out2
     del out
     del asarray
     del aspart
     del aslayout
     import gc
     gc.collect()
-    print(sys.getrefcount(asnumpy))
+    assert sys.getrefcount(asnumpy) == 2
 
-    # raise Exception
+def test_boxing3():
+    asnumpy = numpy.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    assert sys.getrefcount(asnumpy) == 2
+
+    aslayout = awkward1.layout.NumpyArray(asnumpy)
+    aspart = awkward1.repartition(aslayout, 3, highlevel=False)
+    asarray = awkward1.Array(aspart)
+
+    assert (sys.getrefcount(asnumpy), sys.getrefcount(aslayout), sys.getrefcount(aspart)) == (3, 2, 3)
+
+    @numba.njit
+    def f3(x):
+        return x, x
+
+    for i in range(10):
+        out1, out2 = f3(asarray)
+        assert isinstance(out1.layout, awkward1.partition.PartitionedArray)
+        assert isinstance(out2.layout, awkward1.partition.PartitionedArray)
+        assert awkward1.to_list(out1) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        assert awkward1.to_list(out2) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        assert (sys.getrefcount(asnumpy), sys.getrefcount(aslayout), sys.getrefcount(aspart)) == (3, 2, 3)
+
+    del out1
+    del out2
+    del asarray
+    del aspart
+    del aslayout
+    import gc
+    gc.collect()
+    assert sys.getrefcount(asnumpy) == 2
