f0ad3f9782ace20bf371883832ad4bb39a9e7e41
Jim Pivarski <jpivarski@gmail.com>
2020-04-23 17:52:05 -0500

    numba test

---
+++ tests/test_0057b-virtual-array-numba.py
@@ -0,0 +1,16 @@
+# BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+from __future__ import absolute_import
+
+import sys
+import json
+
+import pytest
+import numpy
+
+import awkward1
+
+numba = pytest.importorskip("numba")
+
+def test():
+    pass
