85f2fd9b99395d9327804faeb977dc227a543224
Aditya Puranik <adityapuranik97@googlemail.com>
2020-04-25 02:44:37 +0530

    fix docs

--- src/awkward1/operations/structure.py
+++ src/awkward1/operations/structure.py
@@ -1150,6 +1150,10 @@ def is_none(array, axis = 1, highlevel=True):
     """ TODO
     Args:
         array: Data to check for missing values (None).
+        axis (int): The dimension at which this operation is applied. The
+            outermost dimension is `0`, followed by `1`, etc., and negative
+            values count backward from the innermost: `-1` is the innermost
+            dimension, `-2` is the next level up, etc.
         highlevel (bool): If True, return an #ak.Array; otherwise, return
             a low-level #ak.layout.Content subclass.
 
@@ -1163,42 +1167,6 @@ def is_none(array, axis = 1, highlevel=True):
         return  awkward1._util.wrap(
                  out, behavior=awkward1._util.behaviorof(array)) 
     return out
-    
-    # def apply(layout):
-    #     if isinstance(layout, awkward1._util.unknowntypes):
-    #         return apply(awkward1.layout.NumpyArray(numpy.array([])))
-
-    #     elif isinstance(layout, awkward1._util.indexedtypes):
-    #         return apply(layout.project())
-
-    #     elif isinstance(layout, awkward1._util.uniontypes):
-    #         contents = [apply(layout.project(i))
-    #                       for i in range(layout.numcontents)]
-    #         out = numpy.empty(len(layout), dtype=numpy.bool_)
-    #         tags = numpy.asarray(layout.tags)
-    #         for tag, content in enumerate(contents):
-    #             out[tags == tag] = content
-    #         return out
-
-    #     elif isinstance(layout, awkward1._util.optiontypes):
-    #         return numpy.asarray(layout.bytemask()).view(numpy.bool_)
-
-    #     else:
-    #         return numpy.zeros(len(layout), dtype=numpy.bool_)
-
-    # layout = awkward1.operations.convert.to_layout(array, allow_record=False)
-
-    # if isinstance(layout, awkward1.partition.PartitionedArray):
-    #     out = awkward1.partition.apply(
-    #         lambda x: awkward1.layout.NumpyArray(apply(x)), layout)
-    # else:
-    #     out = awkward1.layout.NumpyArray(apply(layout))
-
-    # if highlevel:
-    #     return awkward1._util.wrap(out,
-    #                                behavior=awkward1._util.behaviorof(array))
-    # else:
-    #     return out
 
 def singletons(array, highlevel=True):
     """
