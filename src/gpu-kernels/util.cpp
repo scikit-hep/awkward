44ae60f53b4312e6404f7343701581ca42f4f6fa
trickarcher <anishbiswas271@gmail.com>
2020-05-19 09:19:42 +0530

    Fix macOS build

---
+++ src/gpu-kernels/util.cpp
@@ -0,0 +1,23 @@
+// BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+#include <cstring>
+
+#include "awkward/gpu-kernels/util.h"
+
+struct Error success() {
+  struct Error out;
+  out.str = nullptr;
+  out.identity = kSliceNone;
+  out.attempt = kSliceNone;
+  out.extra = 0;
+  return out;
+}
+
+struct Error failure(const char* str, int64_t identity, int64_t attempt) {
+  struct Error out;
+  out.str = str;
+  out.identity = identity;
+  out.attempt = attempt;
+  out.extra = 0;
+  return out;
+}
