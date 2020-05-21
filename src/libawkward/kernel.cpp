63b5a06dc13eae26b56e98598cea5f61af2a3e00
trickarcher <anishbiswas271@gmail.com>
2020-05-21 11:29:26 +0530

    Updated localbuild.py to allow for GPU builds

--- src/libawkward/kernel.cpp
+++ src/libawkward/kernel.cpp
@@ -6,7 +6,7 @@
 #include "awkward/cpu-kernels/identities.h"
 #include "awkward/cpu-kernels/reducers.h"
 
-#ifdef ENABLE_CUDA
+#ifdef ENABLE_GPU_KERNELS
   #include "awkward/gpu-kernels/identities.h"
 #endif
 
@@ -17,7 +17,7 @@ ERROR new_identities<int32_t>(int64_t memory_loc,
                               int64_t length) {
   if (memory_loc < 0)
     return awkward_new_identities32(toptr, length);
-  #ifdef ENABLE_CUDA
+  #ifdef ENABLE_GPU_KERNELS
   else {
     return awkward_gpu_new_identities32(memory_loc, toptr, length);
   }
@@ -30,7 +30,7 @@ ERROR new_identities<int64_t>(int64_t memory_loc,
                               int64_t length) {
   if (memory_loc < 0)
     return awkward_new_identities64(toptr, length);
-  #ifdef ENABLE_CUDA
+  #ifdef ENABLE_GPU_KERNELS
   else {
     return awkward_gpu_new_identities64(memory_loc, toptr, length);
   }
