f0419daceb4dbc833831d09d7e85b49baa16efc5
trickarcher <anishbiswas271@gmail.com>
2020-05-21 11:09:41 +0530

    Added conditional flags to both CMake and kernels.cpp

--- src/libawkward/kernel.cpp
+++ src/libawkward/kernel.cpp
@@ -5,7 +5,10 @@
 #include "awkward/cpu-kernels/getitem.h"
 #include "awkward/cpu-kernels/identities.h"
 #include "awkward/cpu-kernels/reducers.h"
-#include "awkward/gpu-kernels/identities.h"
+
+#ifdef ENABLE_CUDA
+  #include "awkward/gpu-kernels/identities.h"
+#endif
 
 namespace kernel {
 template<>
@@ -14,9 +17,11 @@ ERROR new_identities<int32_t>(int64_t memory_loc,
                               int64_t length) {
   if (memory_loc < 0)
     return awkward_new_identities32(toptr, length);
+  #ifdef ENABLE_CUDA
   else {
     return awkward_gpu_new_identities32(memory_loc, toptr, length);
   }
+  #endif
 }
 
 template<>
@@ -25,9 +30,11 @@ ERROR new_identities<int64_t>(int64_t memory_loc,
                               int64_t length) {
   if (memory_loc < 0)
     return awkward_new_identities64(toptr, length);
+  #ifdef ENABLE_CUDA
   else {
     return awkward_gpu_new_identities64(memory_loc, toptr, length);
   }
+  #endif
 }
 }
 
