da13d7424749f5e134e880dfa3d27696ddc8d1ac
Jim Pivarski <jpivarski@gmail.com>
2020-06-05 20:27:55 -0500

    Test more cases.

--- tests/test_0290-bug-fixes-for-hats.py
+++ tests/test_0290-bug-fixes-for-hats.py
@@ -27,16 +27,54 @@ def test_unmasked():
     array = awkward1.Array(unmasked)
     assert awkward1.to_list(find_it(array)) == {"x": 3}
 
-# def test_indexedoption():
-#     @numba.njit
-#     def find_it(array):
-#         for i in range(3):
-#             item = array[i]
-#             if item is None:
-#                 pass
-#             elif item.x == 3:
-#                 return item
-#         return None
+def test_indexedoption():
+    @numba.njit
+    def find_it(array):
+        for i in range(4):
+            item = array[i]
+            if item is None:
+                pass
+            elif item.x == 3:
+                return item
+        return None
 
-#     array = awkward1.Array([{"x": 1}, {"x": 2}, None, {"x": 3}])
-#     assert awkward1.to_list(find_it(array)) == {"x": 3}
+    array = awkward1.Array([{"x": 1}, {"x": 2}, None, {"x": 3}])
+    assert awkward1.to_list(find_it(array)) == {"x": 3}
+
+def test_indexed_1():
+    @numba.njit
+    def f1(array, check):
+        for i in range(3):
+            item = array[i]
+            if item.x == check:
+                return i
+        return 999
+
+    content = awkward1.Array([{"x": 100}, {"x": 101}, {"x": 102}]).layout
+    index = awkward1.layout.Index64(numpy.array([2, 0, 1], dtype=numpy.int64))
+    indexedarray = awkward1.layout.IndexedArray64(index, content)
+    array = awkward1.Array(indexedarray)
+
+    assert f1(array, 100) == 1
+    assert f1(array, 101) == 2
+    assert f1(array, 102) == 0
+    assert f1(array, 12345) == 999
+
+def test_indexed_2():
+    @numba.njit
+    def f1(array, check):
+        for i in range(3):
+            item = array[i]
+            if item.x == check:
+                return item
+        return None
+
+    content = awkward1.Array([{"x": 100}, {"x": 101}, {"x": 102}]).layout
+    index = awkward1.layout.Index64(numpy.array([2, 0, 1], dtype=numpy.int64))
+    indexedarray = awkward1.layout.IndexedArray64(index, content)
+    array = awkward1.Array(indexedarray)
+
+    assert f1(array, 100) == {"x": 100}
+    assert f1(array, 101) == {"x": 101}
+    assert f1(array, 102) == {"x": 102}
+    assert f1(array, 12345) == None
