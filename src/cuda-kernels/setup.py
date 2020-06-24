e2c1616cdca964e99d32a5813584195094668714
Jim Pivarski <jpivarski@gmail.com>
2020-06-24 10:26:29 -0500

    Remove {long_,}description from setup.py and rename.

--- src/cuda-kernels/setup.py
+++ src/cuda-kernels/setup.py
@@ -8,13 +8,11 @@ import sys
 import distutils.util
 
 import setuptools.command.build_ext
-import setuptools.command.install
 
 from setuptools import setup, Extension
 
 if platform.system() == "Windows":
-    print("awkward1_cuda is not supported on Windows")
-    exit(1)
+    raise OSError("awkward1-cuda is not supported on Windows")
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=""):
@@ -46,30 +44,22 @@ class CMakeBuild(setuptools.command.build_ext.build_ext):
             os.makedirs(self.build_temp)
         build_dir = self.build_temp
 
-        # for scikit-build:
-        # build_dir = os.path.join(DIR, "_pybuild")
-
         subprocess.check_call(["cmake", "-S", ext.sourcedir, "-B", build_dir] + cmake_args)
         subprocess.check_call(["cmake", "--build", build_dir])
         subprocess.check_call(["cmake", "--build", build_dir, "--target", "install"])
 
-if platform.system() == "Windows":
-    print("awkward1_cuda is not supported on Windows")
-    exit(1)
+
+# Libraries do not exist yet, so they cannot be determined with a glob pattern.
+libdir = os.path.join(os.path.join("build", "lib.%s-%d.%d" % (distutils.util.get_platform(), sys.version_info[0], sys.version_info[1])), "awkward1")
+static = ".a"
+if platform.system() == "Darwin":
+    shared = ".dylib"
 else:
-    # Libraries do not exist yet, so they cannot be determined with a glob pattern.
-    libdir = os.path.join(os.path.join("build", "lib.%s-%d.%d" % (distutils.util.get_platform(), sys.version_info[0], sys.version_info[1])), "awkward1")
-    static = ".a"
-    if platform.system() == "Darwin":
-        shared = ".dylib"
-    else:
-        shared = ".so"
-    libraries = [("lib", [os.path.join(libdir, "libawkward-cuda-kernels-static" + static),
-                          os.path.join(libdir, "libawkward-cuda-kernels" + shared)])]
+    shared = ".so"
+libraries = [("lib", [os.path.join(libdir, "libawkward-cuda-kernels-static" + static),
+                      os.path.join(libdir, "libawkward-cuda-kernels" + shared)])]
 
-    Install = setuptools.command.install.install
-
-setup(name = "awkward1_cuda",
+setup(name = "awkward1-cuda-kernels",
       packages = setuptools.find_packages(where="src"),
       package_dir = {"": "src"},
       include_package_data = True,
@@ -82,65 +72,16 @@ setup(name = "awkward1_cuda",
       author_email = "pivarski@princeton.edu",
       maintainer = "Jim Pivarski",
       maintainer_email = "pivarski@princeton.edu",
-      description = "Development of awkward 1.0, to replace scikit-hep/awkward-array in 2020.",
-      long_description = """<a href="https://github.com/scikit-hep/awkward-1.0#readme"><img src="https://github.com/scikit-hep/awkward-1.0/raw/master/docs-img/logo/logo-300px.png"></a>
-Awkward Array is a library for **nested, variable-sized data**, including arbitrary-length lists, records, mixed types, and missing data, using **NumPy-like idioms**.
-
-Arrays are **dynamically typed**, but operations on them are **compiled and fast**. Their behavior coincides with NumPy when array dimensions are regular and generalizes when they're not.
-
-<table>
-  <tr>
-    <td width="33%" valign="top">
-      <a href="https://awkward-array.org/how-to.html">
-        <img src="https://github.com/scikit-hep/awkward-1.0/raw/master/docs-img/panel-data-analysts.png" width="268">
-      </a>
-      <p align="center"><b>
-        <a href="https://awkward-array.org/how-to.html">
-        How-to documentation<br>for data analysts
-        </a>
-      </b></p>
-    </td>
-    <td width="33%" valign="top">
-      <a href="https://awkward-array.org/how-it-works.html">
-        <img src="https://github.com/scikit-hep/awkward-1.0/raw/master/docs-img/panel-developers.png" width="268">
-      </a>
-      <p align="center"><b>
-        <a href="https://awkward-array.org/how-it-works.html">
-        How-it-works tutorials<br>for developers
-        </a>
-      </b></p>
-    </td>
-    <td width="33%" valign="top">
-      <a href="https://awkward-array.readthedocs.io/en/latest/index.html">
-        <img src="https://github.com/scikit-hep/awkward-1.0/raw/master/docs-img/panel-sphinx.png" width="268">
-      </a>
-      <p align="center"><b>
-        <a href="https://awkward-array.readthedocs.io/en/latest/index.html">
-        Python<br>API reference
-        </a>
-      </b></p>
-      <a href="https://awkward-array.readthedocs.io/en/latest/_static/index.html">
-        <img src="https://github.com/scikit-hep/awkward-1.0/raw/master/docs-img/panel-doxygen.png" width="268">
-      </a>
-      <p align="center"><b>
-        <a href="https://awkward-array.readthedocs.io/en/latest/_static/index.html">
-        C++<br>API reference
-        </a>
-      </b></p>
-    </td>
-  </tr>
-</table>
-""",
+      description = "",
+      long_description = "",
       long_description_content_type = "text/markdown",
       url = "https://github.com/scikit-hep/awkward-1.0",
       download_url = "https://github.com/scikit-hep/awkward-1.0/releases",
       license = "BSD 3-clause",
       python_requires = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
 
-
-      ext_modules = [CMakeExtension("awkward_cuda_kernels")], # NOT scikit-build
-      cmdclass = {"build_ext": CMakeBuild,       # NOT scikit-build
-                  "install": Install},
+      ext_modules = [CMakeExtension("awkward-cuda-kernels")],
+      cmdclass = {"build_ext": CMakeBuild},
 
       classifiers = [
           #         "Development Status :: 1 - Planning",
