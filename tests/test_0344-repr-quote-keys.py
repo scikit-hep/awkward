c57e560695e7cc0a525d5a9dc7cedd3ef7633b7d
Jim Pivarski <jpivarski@gmail.com>
2020-07-17 09:05:44 -0500

    dict order for Python 3.5 and below.

--- tests/test_0344-repr-quote-keys.py
+++ tests/test_0344-repr-quote-keys.py
@@ -11,5 +11,15 @@ import awkward1
 
 
 def test():
-    assert str(awkward1.Array([{"a": 1, "a b": 2}, {"a": 1, "a b": 2}, {"a": 1, "a b": 2}, {"a": 1, "a b": 2}])) == "[{a: 1, 'a b': 2}, {a: 1, 'a b': 2}, {a: 1, 'a b': 2}, {a: 1, 'a b': 2}]"
-    assert repr(awkward1.Array([{"a": 1, "a b": 2}, {"a": 1, "a b": 2}, {"a": 1, "a b": 2}])) == "<Array [{a: 1, 'a b': 2}, ... {a: 1, 'a b': 2}] type='3 * {\"a\": int64, \"a b\": in...'>"
+    assert str(awkward1.Array([
+        {"a": 1, "a b": 2}, {"a": 1, "a b": 2}, {"a": 1, "a b": 2}, {"a": 1, "a b": 2}
+    ])) in (
+        "[{a: 1, 'a b': 2}, {a: 1, 'a b': 2}, {a: 1, 'a b': 2}, {a: 1, 'a b': 2}]",
+        "[{'a b': 2, a: 1}, {'a b': 2, a: 1}, {'a b': 2, a: 1}, {'a b': 2, a: 1}]"
+    )
+    assert repr(awkward1.Array([
+        {"a": 1, "a b": 2}, {"a": 1, "a b": 2}, {"a": 1, "a b": 2}
+    ])) in (
+        "<Array [{a: 1, 'a b': 2}, ... {a: 1, 'a b': 2}] type='3 * {\"a\": int64, \"a b\": in...'>"
+        "<Array [{'a b': 2, a: 1}, ... {'a b': 2, a: 1}] type='3 * {\"a b\": int64, \"a\": in...'>"
+    )
