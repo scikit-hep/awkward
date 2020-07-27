1a4fdc9555b4318ab484a85f585201fcc32de17e
trickarcher <anishbiswas271@gmail.com>
2020-07-27 10:49:37 +0530

    Introduced folder level logging

--- .ci/launch-cuda-docker-ci.sh
+++ .ci/launch-cuda-docker-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build -f ../dev/docker/docker_cuda102/Dockerfile.build -t awkward1-cuda-tests:1.0-cuda10.2 ../.
-docker run -it --gpus all awkward1-cuda-tests:1.0-cuda10.2 sh .ci/run-cuda-tests.sh master
+docker run -it --gpus all -v .ci/logs:/awkward1/.ci/logs awkward1-cuda-tests:1.0-cuda10.2 sh .ci/run-cuda-tests.sh master
 
 docker build -f ../dev/docker/docker_cuda100/Dockerfile.build -t awkward1-cuda-tests:1.0-cuda10.0 ../.
 docker run -it --gpus all awkward1-cuda-tests:1.0-cuda10.0 sh .ci/run-cuda-tests.sh master
