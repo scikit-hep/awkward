05ec87f7b70149120f127086462e166c15fdb1da
Ubuntu <ubuntu@ip-172-31-71-10.ec2.internal>
2020-07-27 06:04:59 +0000

    Fixed the script to accept whitespace

--- .ci/run-cuda-tests.sh
+++ .ci/run-cuda-tests.sh
@@ -5,7 +5,7 @@ set -e
 LOG_FILE_AWKWARD1BUILD="awkward1-cuda-tests "$1" awkward1-build-test"
 LOG_FILE_AWKWARD1CUDABUILD="awkward1-cuda-tests "$1" awkward1-cuda-build"
 LOG_FILE_AWKWARD1CUDATEST="awkward1-cuda-tests "$1" awkward1-cuda-test"
-LOG_FOLDER=".ci/logs/""$( date )"
+LOG_FOLDER=".ci/logs/"$( date )
 mkdir LOG_FOLDER
 
 conda --version
@@ -19,12 +19,12 @@ git submodule update --init
 cp -a include src VERSION_INFO cuda-kernels
 
 cd cuda-kernels
-pip install . | tee ${LOG_FOLDER}/${LOG_FILE_AWKWARD1CUDABUILD}
+pip install . | tee "${LOG_FOLDER}"/"${LOG_FILE_AWKWARD1CUDABUILD}"
 cd ..
 
 python -c 'import awkward1_cuda_kernels; print(awkward1_cuda_kernels.shared_library_path)'
 
-python localbuild.py --pytest tests | tee ${LOG_FOLDER}/${LOG_FILE_AWKWARD1BUILD}
+python localbuild.py --pytest tests | tee "${LOG_FOLDER}"/"${LOG_FILE_AWKWARD1BUILD}"
 
 python -m pytest -vvrs tests-cuda | tee
-${LOG_FOLDER}/${LOG_FILE_AWKWARD1CUDATEST}
+"${LOG_FOLDER}"/"${LOG_FILE_AWKWARD1CUDATEST}"
