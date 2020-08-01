bfa5a0808f9564dd8dd8617836acab17bd131f0e
Pratyush Das <reikdas@gmail.com>
2020-08-01 23:35:07 +0530

    Fix finding cpu-kernels object file

--- tests-kernels/__init__.py
+++ tests-kernels/__init__.py
@@ -1,16 +1,28 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/master/LICENSE
 
-import os
 import ctypes
+import os
+import platform
 
-CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-TOP_DIR = os.path.join(CURRENT_DIR, "..")
+import pkg_resources
 
-for root, _, files in os.walk(TOP_DIR):
-    for filename in files:
-        if filename.endswith("libawkward-cpu-kernels.so"):
-            CPU_KERNEL_SO = os.path.join(root, filename)
-            break
+if platform.system() == "Windows":
+    name = "awkward-cpu-kernels.dll"
+elif platform.system() == "Darwin":
+    name = "libawkward-cpu-kernels.dylib"
+else:
+    name = "libawkward-cpu-kernels.so"
+
+try:
+    CPU_KERNEL_SO = pkg_resources.resource_filename("awkward1", name)
+except ModuleNotFoundError:
+    CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+    TOP_DIR = os.path.join(CURRENT_DIR, "..")
+    for root, _, files in os.walk(TOP_DIR):
+        for filename in files:
+            if filename == name:
+                CPU_KERNEL_SO = os.path.join(root, filename)
+                break
 
 lib = ctypes.CDLL(CPU_KERNEL_SO)
 
