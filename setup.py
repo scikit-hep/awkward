2c226906494ad8fc7ac20d7db24fcdf5ac49d18a
Jim Pivarski <jpivarski@gmail.com>
2020-09-07 14:04:20 -0500

    Libraries and includes should only go into the Python directory, not multiple places.

--- setup.py
+++ setup.py
@@ -16,17 +16,14 @@ import setuptools.command.install
 from setuptools import setup, Extension
 
 
-install_requires = open("requirements.txt").read().strip().split()
-
-extras = {"cuda": ["awkward1-cuda-kernels"],
-          "test": open("requirements-test.txt").read().strip().split(),
-          "dev":  ['numba>=0.50.0;python_version>="3.6"',
-                   'pandas>=0.24.0;python_version>="3.6"',
-                   'numexpr;python_version>="3.6"',
-                   'autograd;python_version>="3.6"',
-                   'pyarrow>=1.0;python_version>="3.6" and sys_platform != "win32"']}
+extras = {
+    "cuda": ["awkward1-cuda-kernels"],
+    "test": open("requirements-test.txt").read().strip().split("\n"),
+    "dev":  open("requirements-dev.txt").read().strip().split("\n"),
+}
 extras["all"] = sum(extras.values(), [])
 
+install_requires = open("requirements.txt").read().strip().split("\n")
 tests_require = extras["test"]
 
 
@@ -34,6 +31,7 @@ class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=""):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
+
 
 class CMakeBuild(setuptools.command.build_ext.build_ext):
     def build_extensions(self):
@@ -46,14 +44,15 @@ class CMakeBuild(setuptools.command.build_ext.build_ext):
             self.build_extension(x)
 
     def build_extension(self, ext):
-
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ["-DCMAKE_INSTALL_PREFIX={0}".format(extdir),
-                      "-DPYTHON_EXECUTABLE={0}".format(sys.executable),
-                      "-DPYBUILD=ON",
-                      "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9",
-                      "-DPYBUILD=ON",
-                      "-DBUILD_TESTING=OFF"]
+        cmake_args = [
+            "-DCMAKE_INSTALL_PREFIX={0}".format(extdir),
+            "-DPYTHON_EXECUTABLE={0}".format(sys.executable),
+            "-DPYBUILD=ON",
+            "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9",
+            "-DPYBUILD=ON",
+            "-DBUILD_TESTING=OFF",
+        ]
         try:
            compiler_path = self.compiler.compiler_cxx[0]
            cmake_args.append("-DCMAKE_CXX_COMPILER={0}".format(compiler_path))
@@ -64,8 +63,10 @@ class CMakeBuild(setuptools.command.build_ext.build_ext):
         build_args = ["--config", cfg]
 
         if platform.system() == "Windows":
-            cmake_args += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{0}={1}".format(cfg.upper(), extdir),
-                           "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE"]
+            cmake_args += [
+                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{0}={1}".format(cfg.upper(), extdir),
+                "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE",
+            ]
             if sys.maxsize > 2**32:
                 cmake_args += ["-A", "x64"]
             build_args += ["--", "/m"]
@@ -88,16 +89,15 @@ class CMakeBuild(setuptools.command.build_ext.build_ext):
         subprocess.check_call(["cmake", "--build", build_dir] + build_args)
         subprocess.check_call(["cmake", "--build", build_dir, "--target", "install"])
 
+
+def tree(x):
+    print(x + (" (dir)" if os.path.isdir(x) else ""))
+    if os.path.isdir(x):
+        for y in os.listdir(x):
+            tree(os.path.join(x, y))
+
+
 if platform.system() == "Windows":
-    # Libraries are not installed system-wide on Windows, but they do have to be in the awkward1 directory.
-    libraries = []
-
-    def tree(x):
-        print(x + (" (dir)" if os.path.isdir(x) else ""))
-        if os.path.isdir(x):
-            for y in os.listdir(x):
-                tree(os.path.join(x, y))
-
     class Install(setuptools.command.install.install):
         def run(self):
             print("--- build directory -------------------------------------------")
@@ -112,8 +112,8 @@ if platform.system() == "Windows":
                 if x.startswith("awkward"):
                     print("copying", os.path.join(dlldir, x), "-->", os.path.join(self.build_lib, "awkward1", x))
                     shutil.copyfile(os.path.join(dlldir, x), os.path.join(self.build_lib, "awkward1", x))
-            print("--- deleting --------------------------------------------------")
 
+            print("--- deleting --------------------------------------------------")
             outerdir = os.path.join(os.path.join("build", "lib.%s-%d.%d" % (distutils.util.get_platform(), sys.version_info[0], sys.version_info[1])))
             for x in os.listdir(outerdir):
                 if x.endswith(".pyd"):
@@ -124,146 +124,31 @@ if platform.system() == "Windows":
             setuptools.command.install.install.run(self)
 
 else:
-    # Libraries do not exist yet, so they cannot be determined with a glob pattern.
-    libdir = os.path.join(os.path.join("build", "lib.%s-%d.%d" % (distutils.util.get_platform(), sys.version_info[0], sys.version_info[1])), "awkward1")
-    static = ".a"
-    if platform.system() == "Darwin":
-        shared = ".dylib"
-    else:
-        shared = ".so"
-    libraries = [("lib", [os.path.join(libdir, "libawkward-cpu-kernels-static" + static),
-                          os.path.join(libdir, "libawkward-cpu-kernels" + shared),
-                          os.path.join(libdir, "libawkward-static" + static),
-                          os.path.join(libdir, "libawkward" + shared)])]
+    class Install(setuptools.command.install.install):
+        def run(self):
+            print("--- build directory -------------------------------------------")
+            tree("build")
 
-    Install = setuptools.command.install.install
+            print("--- copying ---------------------------------------------------")
+            shutil.copytree(os.path.join("include"), os.path.join(outerdir, "awkward1", "include"))
+
+            print("--- outerdir after copy ---------------------------------------")
+            tree(outerdir)
+
+            print("--- begin normal install --------------------------------------")
+            setuptools.command.install.install.run(self)
+
 
 setup(name = "awkward1",
-      packages = setuptools.find_packages(where="src"),
-      package_dir = {"": "src"},
-      include_package_data = True,
-      package_data = {"": ["*.dll"]},
-      data_files = libraries + [
-          ("include/awkward",              glob.glob("include/awkward/*.h")),
-          ("include/awkward/cpu-kernels",  glob.glob("include/awkward/cpu-kernels/*.h")),
-          ("include/awkward/python",       glob.glob("include/awkward/python/*.h")),
-          ("include/awkward/array",        glob.glob("include/awkward/array/*.h")),
-          ("include/awkward/builder",      glob.glob("include/awkward/builder/*.h")),
-          ("include/awkward/partition",    glob.glob("include/awkward/partition/*.h")),
-          ("include/awkward/io",           glob.glob("include/awkward/io/*.h")),
-          ("include/awkward/type",         glob.glob("include/awkward/type/*.h"))],
+      packages = [x for x in setuptools.find_packages(where="src") if x != "awkward1_cuda_kernels"],
+      package_dir = {"awkward1": "src/awkward1"},
       version = open("VERSION_INFO").read().strip(),
       author = "Jim Pivarski",
       author_email = "pivarski@princeton.edu",
       maintainer = "Jim Pivarski",
       maintainer_email = "pivarski@princeton.edu",
       description = "Manipulate JSON-like data with NumPy-like idioms.",
-      long_description = """<a href="https://github.com/scikit-hep/awkward-1.0#readme"><img src="https://github.com/scikit-hep/awkward-1.0/raw/master/docs-img/logo/logo-300px.png"></a>
-
-Awkward Array is a library for **nested, variable-sized data**, including arbitrary-length lists, records, mixed types, and missing data, using **NumPy-like idioms**.
-
-Arrays are **dynamically typed**, but operations on them are **compiled and fast**. Their behavior coincides with NumPy when array dimensions are regular and generalizes when they're not.
-
-<table>
-  <tr>
-    <td width="66%" valign="top">
-      <a href="https://awkward-array.org">
-        <img src="https://github.com/scikit-hep/awkward-1.0/raw/master/docs-img/panel-tutorials.png" width="570">
-      </a>
-      <p align="center"><b>
-        <a href="https://awkward-array.org">
-        How-to tutorials
-        </a>
-      </b></p>
-    </td>
-    <td width="33%" valign="top">
-      <a href="https://awkward-array.readthedocs.io/en/latest/index.html">
-        <img src="https://github.com/scikit-hep/awkward-1.0/raw/master/docs-img/panel-sphinx.png" width="268">
-      </a>
-      <p align="center"><b>
-        <a href="https://awkward-array.readthedocs.io/en/latest/index.html">
-        Python API reference
-        </a>
-      </b></p>
-      <a href="https://awkward-array.readthedocs.io/en/latest/_static/index.html">
-        <img src="https://github.com/scikit-hep/awkward-1.0/raw/master/docs-img/panel-doxygen.png" width="268">
-      </a>
-      <p align="center"><b>
-        <a href="https://awkward-array.readthedocs.io/en/latest/_static/index.html">
-        C++ API reference
-        </a>
-      </b></p>
-    </td>
-  </tr>
-</table>
-
-# Motivating example
-
-Given an array of objects with `x`, `y` fields and variable-length nested lists like
-
-```python
-array = ak.Array([
-    [{"x": 1.1, "y": [1]}, {"x": 2.2, "y": [1, 2]}, {"x": 3.3, "y": [1, 2, 3]}],
-    [],
-    [{"x": 4.4, "y": {1, 2, 3, 4]}, {"x": 5.5, "y": [1, 2, 3, 4, 5]}]
-])
-```
-
-the following slices out the `y` values, drops the first element from each inner list, and runs NumPy's `np.square` function on everything that is left:
-
-```python
-output = np.square(array["y", ..., 1:])
-```
-
-The result is
-
-```python
-[
-    [[], [4], [4, 9]],
-    [],
-    [[4, 9, 16], [4, 9, 16, 25]]
-]
-```
-
-The equivalent using only Python is
-
-```python
-output = []
-for sublist in array:
-    tmp1 = []
-    for record in sublist:
-        tmp2 = []
-        for number in record["y"][1:]:
-            tmp2.append(np.square(number))
-        tmp1.append(tmp2)
-    output.append(tmp1)
-```
-
-Not only is the expression using Awkward Arrays more concise, using idioms familiar from NumPy, but it's much faster and uses less memory.
-
-For a similar problem 10 million times larger than the one above (on a single-threaded 2.2 GHz processor),
-
-   * the Awkward Array one-liner takes **4.6 seconds** to run and uses **2.1 GB** of memory,
-   * the equivalent using Python lists and dicts takes **138 seconds** to run and uses **22 GB** of memory.
-
-Speed and memory factors in the double digits are common because we're replacing Python's dynamically typed, pointer-chasing virtual machine with type-specialized, precompiled routines on contiguous data. (In other words, for the same reasons as NumPy.) Even higher speedups are possible when Awkward Array is paired with [Numba](https://numba.pydata.org/).
-
-Our [presentation at SciPy 2020](https://youtu.be/WlnUF3LRBj4) provides a good introduction, showing how to use these arrays in a real analysis.
-
-# Installation
-
-Awkward Array can be installed [from PyPI](https://pypi.org/project/awkward1/) using pip:
-
-```bash
-pip install awkward1
-```
-
-Most users will get a precompiled binary (wheel) for your operating system and Python version. If not, the above attempts to compile from source.
-
-   * Report bugs, request features, and ask for additional documentation on [GitHub Issues](https://github.com/scikit-hep/awkward-1.0/issues). If you have a general "How do I…?" question, we'll answer it as a new [example in the tutorial](https://awkward-array.org/how-to.html).
-   * If you have a problem that's too specific to be new documentation or it isn't exclusively related to Awkward Array, it might be more appropriate to ask on [StackOverflow with the [awkward-array] tag](https://stackoverflow.com/questions/tagged/awkward-array). Be sure to include tags for any other libraries that you use, such as Pandas or PyTorch.
-   * The [Gitter Scikit-HEP/community](https://gitter.im/Scikit-HEP/community) is a way to get in touch with all Scikit-HEP developers and users.
-""",
+      long_description = open("README-pypi.md").read(),
       long_description_content_type = "text/markdown",
       url = "https://github.com/scikit-hep/awkward-1.0",
       download_url = "https://github.com/scikit-hep/awkward-1.0/releases",
@@ -278,9 +163,13 @@ Most users will get a precompiled binary (wheel) for your operating system and P
       extras_require = extras,
 
       # cmake_args=['-DBUILD_TESTING=OFF'],      # for scikit-build
-      ext_modules = [CMakeExtension("awkward")], # NOT scikit-build
-      cmdclass = {"build_ext": CMakeBuild,       # NOT scikit-build
-                  "install": Install},
+      ext_modules = [
+          CMakeExtension("awkward"),             # NOT scikit-build
+      ],
+      cmdclass = {
+          "build_ext": CMakeBuild,               # NOT scikit-build
+          "install": Install,
+      },
 
       classifiers = [
 #         "Development Status :: 1 - Planning",
