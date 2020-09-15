7de768351b392be4319a58a376a16b7845d37353
trickarcher <anishbiswas271@gmail.com>
2020-09-15 16:46:41 +0530

    Able to run automated test generators on hand written kernels

---
+++ studies/cuda-kernels/loop-dependent-variable-kernels/manual_awkward_ByteMaskedArray_getitem_nextcarry.cu
@@ -0,0 +1,64 @@
+#define FILENAME(line) FILENAME_FOR_EXCEPTIONS_CUDA("src/cuda-kernels/manual_awkward_ByteMaskedArray_getitem_nextcarry.cu", line)
+
+#include "standard_parallel_algorithms.h"
+#include "awkward/kernels/getitem.h"
+
+__global__ void
+awkward_ByteMaskedArray_getitem_nextcarry_filter_mask(int8_t* mask,
+                                                      bool validwhen,
+                                                      int64_t length) {
+  int64_t block_id =
+      blockIdx.x + blockIdx.y * gridDim.x + gridDim.x * gridDim.y * blockIdx.z;
+  int64_t thread_id = block_id * blockDim.x + threadIdx.x;
+
+  if(thread_id < length) {
+    if ((mask[thread_id] != 0) == validwhen) {
+      mask[thread_id] = 1;
+    }
+  }
+}
+
+__global__ void
+awkward_ByteMaskedArray_getitem_nextcarry_kernel(int64_t* prefixed_mask,
+                                                 int64_t* to_carry,
+                                                 int8_t* mask,
+                                                 int64_t length) {
+  int64_t block_id =
+      blockIdx.x + blockIdx.y * gridDim.x + gridDim.x * gridDim.y * blockIdx.z;
+  int64_t thread_id = block_id * blockDim.x + threadIdx.x;
+
+  if(thread_id < length) {
+    if (mask[thread_id] != 0) {
+      to_carry[prefixed_mask[thread_id] - 1] = thread_id;
+    }
+  }
+}
+
+ERROR
+awkward_ByteMaskedArray_getitem_nextcarry_64(int64_t* tocarry,
+                                             const int8_t* mask,
+                                             int64_t length,
+                                             bool validwhen) {
+  int64_t* res_temp;
+  int8_t* filtered_mask;
+
+  dim3 blocks_per_grid = blocks(length);
+  dim3 threads_per_block = threads(length);
+
+  HANDLE_ERROR(cudaMalloc((void**)&res_temp, sizeof(int64_t) * length));
+  HANDLE_ERROR(cudaMalloc((void**)&filtered_mask, sizeof(int8_t) * length));
+  HANDLE_ERROR(cudaMemcpy(
+      filtered_mask, mask, sizeof(int8_t) * length, cudaMemcpyDeviceToDevice));
+
+  awkward_ByteMaskedArray_getitem_nextcarry_filter_mask<<<blocks_per_grid, threads_per_block>>>(
+      filtered_mask, validwhen, length);
+
+  exclusive_scan<int64_t, int8_t>(res_temp, filtered_mask, length);
+
+  awkward_ByteMaskedArray_getitem_nextcarry_kernel<<<blocks_per_grid, threads_per_block>>>(
+      res_temp, tocarry, filtered_mask, length);
+
+  cudaDeviceSynchronize();
+
+  return success();
+}
---
+++ studies/cuda-kernels/test-cuda-kernels.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Copying files"
+cp standard_parallel_algorithms.h loop-dependent-variable-kernels/*.cu ../../src/cuda-kernels/
+cd ../..
+
+echo "Running setup"
+sudo pip uninstall awkward1_cuda_kernels && sudo ./cuda-build.sh --install
+python dev/generate-kernelspec.py && python dev/generate-tests.py
+
+echo "Testing"
+pytest -vvrs tests-cuda-kernels
+
+echo "Cleaning up"
+mv src/cuda-kernels/manual_awkward_ListArray_num.cu src/
+
+rm src/cuda-kernels/manual* src/cuda-kernels/standard_parallel_algorithms.h
+
+mv src/manual_awkward_ListArray_num.cu src/cuda-kernels/
