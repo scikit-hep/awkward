b7dec20ccdc04a5dafa47f990013022e93226baf
Jim Pivarski <jpivarski@gmail.com>
2020-11-05 16:32:51 -0600

    Provide ak.local_index.

--- src/awkward1/operations/structure.py
+++ src/awkward1/operations/structure.py
@@ -26,6 +26,47 @@ np = awkward1.nplike.NumpyMetadata.instance()
 @awkward1._connect._numpy.implements("copy")
 def copy(array):
     """
+    Returns a deep copy of the array (no memory shared with original).
+
+    This is identical to `np.copy` and `copy.deepcopy`.
+
+    It's only useful to explicitly copy an array if you're going to change it
+    in-place. This doesn't come up often because Awkward Arrays are immutable.
+    That is to say, the Awkward Array library doesn't have any operations that
+    change an array in-place, but the data in the array might be owned by another
+    library that can change it in-place.
+
+    For example, if the array comes from NumPy:
+
+        >>> underlying_array = np.array([1.1, 2.2, 3.3, 4.4, 5.5])
+        >>> wrapper = ak.Array(underlying_array)
+        >>> duplicate = ak.copy(wrapper)
+        >>> underlying_array[2] = 123
+        >>> underlying_array
+        array([  1.1,   2.2, 123. ,   4.4,   5.5])
+        >>> wrapper
+        <Array [1.1, 2.2, 123, 4.4, 5.5] type='5 * float64'>
+        >>> duplicate
+        <Array [1.1, 2.2, 3.3, 4.4, 5.5] type='5 * float64'>
+
+    There is an exception to this rule: you can add fields to records in an
+    #ak.Array in-place. However, this changes the #ak.Array wrapper without
+    affecting the underlying layout data (it *replaces* its layout), so a
+    shallow copy will do:
+
+        >>> original = ak.Array([{"x": 1}, {"x": 2}, {"x": 3}])
+        >>> shallow_copy = copy.copy(original)
+        >>> shallow_copy["y"] = original.x**2
+        >>> shallow_copy
+        <Array [{x: 1, y: 1}, ... y: 4}, {x: 3, y: 9}] type='3 * {"x": int64, "y": int64}'>
+        >>> original
+        <Array [{x: 1}, {x: 2}, {x: 3}] type='3 * {"x": int64}'>
+
+    This is key to Awkward Array's efficiency (memory and speed): operations that
+    only change part of a structure re-use pieces from the original ("structural
+    sharing"). Changing data in-place would result in many surprising long-distance
+    changes, so we don't support it. However, an #ak.Array's data might come from
+    a mutable third-party library, so this function allows you to make a true copy.
     """
     layout = awkward1.operations.convert.to_layout(
         array,
@@ -1051,6 +1092,80 @@ def flatten(array, axis=1, highlevel=True):
     else:
         out = layout.flatten(axis)
 
+    if highlevel:
+        return awkward1._util.wrap(out, awkward1._util.behaviorof(array))
+    else:
+        return out
+
+
+def local_index(array, axis=-1, highlevel=True):
+    """
+    Args:
+        array: Array to index.
+        axis (int): The dimension at which this operation is applied. The
+            outermost dimension is `0`, followed by `1`, etc., and negative
+            values count backward from the innermost: `-1` is the innermost
+            dimension, `-2` is the next level up, etc.
+        highlevel (bool): If True, return an #ak.Array; otherwise, return
+            a low-level #ak.layout.Content subclass.
+
+    For example,
+
+        >>> array = ak.Array([
+        ...     [[0.0, 1.1, 2.2], []],
+        ...     [[3.3, 4.4]],
+        ...     [],
+        ...     [[5.5], [], [6.6, 7.7, 8.8, 9.9]]])
+        >>> ak.local_index(array, axis=0)
+        <Array [0, 1, 2, 3] type='4 * int64'>
+        >>> ak.local_index(array, axis=1)
+        <Array [[0, 1], [0], [], [0, 1, 2]] type='4 * var * int64'>
+        >>> ak.local_index(array, axis=2)
+        <Array [[[0, 1, 2], []], ... [], [0, 1, 2, 3]]] type='4 * var * var * int64'>
+
+    Note that you can make a Pandas-style MultiIndex by calling this function on
+    every axis.
+
+        >>> multiindex = ak.zip([ak.local_index(array, i) for i in range(array.ndim)])
+        >>> multiindex
+        <Array [[[(0, 0, 0), (0, 0, ... ), (3, 2, 3)]]] type='4 * var * var * (int64, in...'>
+        >>> ak.to_list(multiindex)
+        [[[(0, 0, 0), (0, 0, 1), (0, 0, 2)], []],
+         [[(1, 0, 0), (1, 0, 1)]],
+         [],
+         [[(3, 0, 0)], [], [(3, 2, 0), (3, 2, 1), (3, 2, 2), (3, 2, 3)]]]
+        >>> ak.to_list(ak.flatten(ak.flatten(multiindex)))
+        [(0, 0, 0),
+         (0, 0, 1),
+         (0, 0, 2),
+         (1, 0, 0),
+         (1, 0, 1),
+         (3, 0, 0),
+         (3, 2, 0),
+         (3, 2, 1),
+         (3, 2, 2),
+         (3, 2, 3)]
+
+    But if you're interested in Pandas, you may want to use #ak.to_pandas directly.
+
+        >>> ak.to_pandas(array)
+                                    values
+        entry subentry subsubentry
+        0     0        0               0.0
+                       1               1.1
+                       2               2.2
+        1     0        0               3.3
+                       1               4.4
+        3     0        0               5.5
+              2        0               6.6
+                       1               7.7
+                       2               8.8
+                       3               9.9
+    """
+    layout = awkward1.operations.convert.to_layout(
+        array, allow_record=True, allow_other=False
+    )
+    out = layout.localindex(axis)
     if highlevel:
         return awkward1._util.wrap(out, awkward1._util.behaviorof(array))
     else:
