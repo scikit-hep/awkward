3a542f60f28c845c44dd65852406f0d7f92c41f8
Pratyush Das <reikdas@gmail.com>
2020-12-01 13:16:28 +0530

    Tool to check if kernel is implemented in all places
    and reword flag from --check-sorted to --check-spec-sorted

--- dev/kernel-diagnostics.py
+++ dev/kernel-diagnostics.py
@@ -65,7 +65,7 @@ def sort_specializations(keystring):
 
 
 def check_specorder(kerneldict):
-    print("Checking kernel specification order")
+    print("Checking kernel specification order...")
     kernelnames = []
     with open(os.path.join(CURRENT_DIR, "..", "kernel-specification.yml")) as specfile:
         indspec = yaml.safe_load(specfile)["kernels"]
@@ -112,14 +112,94 @@ def check_specorder(kerneldict):
     print("Kernel specification file is properly sorted")
 
 
+def check_spec_implementation():
+    count = 0
+    with open(os.path.join(CURRENT_DIR, "..", "kernel-specification.yml")) as specfile:
+        indspec = yaml.safe_load(specfile)["kernels"]
+        for spec in indspec:
+            if "def awkward" not in spec["definition"]:
+                if count == 0:
+                    print("\nKernels not implemented in specification file - ")
+                print(spec["name"])
+                count += 1
+
+
+def check_cpu_implementation(kerneldict):
+    count = 0
+    for kernelname, specializations in kerneldict.items():
+        if os.path.isfile(
+            os.path.join(CURRENT_DIR, "..", "src", "cpu-kernels", kernelname + ".cpp")
+        ):
+            with open(
+                os.path.join(
+                    CURRENT_DIR, "..", "src", "cpu-kernels", kernelname + ".cpp"
+                )
+            ) as kernelfile:
+                contents = kernelfile.read()
+                for childname in specializations:
+                    if childname not in contents:
+                        if count == 0:
+                            print("\nKernels not implemented as a CPU kernel - ")
+                        print(kernelname + ": " + childname)
+                        count += 1
+        else:
+            if count == 0:
+                print("\nKernels not implemented as a CPU kernel - ")
+            print(kernelname)
+            count += 1
+
+
+def check_cuda_implementation(kerneldict):
+    count = 0
+    for kernelname in kerneldict.keys():
+        if not (
+            os.path.isfile(
+                os.path.join(
+                    CURRENT_DIR, "..", "src", "cuda-kernels", kernelname + ".cu"
+                )
+            )
+            or os.path.isfile(
+                os.path.join(
+                    CURRENT_DIR,
+                    "..",
+                    "src",
+                    "cuda-kernels",
+                    "manual_" + kernelname + ".cu",
+                )
+            )
+        ):
+            if count == 0:
+                print("\nKernels not implemented as a CUDA kernel - ")
+            print(kernelname)
+            count += 1
+
+
+def check_implementations(kerneldict):
+    print("Checking if kernels are implemented everywhere...")
+    check_spec_implementation()
+    check_cpu_implementation(kerneldict)
+    check_cuda_implementation(kerneldict)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Awkward Kernel Diagnostics Tool")
     parser.add_argument(
-        "--check-sorted",
+        "--check-spec-sorted",
         action="store_true",
         help="Check if kernel specification file is sorted",
     )
+    parser.add_argument(
+        "--check-implemented",
+        action="store_true",
+        help="Check if kernel is present in specification file, as a CPU kernel and as a CUDA kernel",
+    )
     args = parser.parse_args()
     kernels = parse_spec()
-    if args.check_sorted:
+    count = 0
+    if args.check_spec_sorted:
         check_specorder(kernels)
+        count += 1
+    if args.check_implemented:
+        if count != 0:
+            print("\n")
+        check_implementations(kernels)
