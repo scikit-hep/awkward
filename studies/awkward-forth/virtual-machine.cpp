36d078e1513e0bde7cc9fd94e279f072449626a0
Jim Pivarski <jpivarski@gmail.com>
2021-01-04 18:34:22 -0600

    Prototype Forth virtual machine in C++ (close to what will be added to Awkward Array).

---
+++ studies/awkward-forth/virtual-machine.cpp
@@ -0,0 +1,7 @@
+// c++ virtual-machine.cpp -o virtual-machine-test  &&  ./virtual-machine-test
+
+#include "iostream"
+
+int main() {
+  std::cout << "At least *start* something today." << std::endl;
+}
