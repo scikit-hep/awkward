ea935ecb5487b201b8978f818ec6759efb84dab1
Jim Pivarski <jpivarski@gmail.com>
2021-01-20 18:34:49 -0600

    AwkwardForth is 8x faster than the fastavro library.

--- studies/awkward-forth-performance/forth-read-avro.py
+++ studies/awkward-forth-performance/forth-read-avro.py
@@ -1,6 +1,9 @@
 import zlib
 import struct
+import time
 
+import fastavro
+import numpy as np
 import awkward as ak
 import awkward.forth
 
@@ -19,145 +22,156 @@ def decode_varint(pos, data):
 def decode_zigzag(n):
     return (n >> 1) ^ (-(n & 1))
 
-jagged0 = awkward.forth.ForthMachine32("""
-input stream
-output content float32
+# jagged0 = awkward.forth.ForthMachine32("""
+# input stream
+# output content float32
 
-stream #f-> content
-""")
+# stream #f-> content
+# """)
 
-with open("/home/jpivarski/storage/data/chep-2021-jagged-jagged-jagged/zlib1-jagged0.avro", "rb") as file:
-    data = file.read()
-    pos = data.index(0) + 1
+# with open("/home/jpivarski/storage/data/chep-2021-jagged-jagged-jagged/zlib1-jagged0.avro", "rb") as file:
+#     data = file.read()
+#     pos = data.index(0) + 1
 
-    while pos + 16 < len(data):
-        pos += 16
+#     while pos + 16 < len(data):
+#         pos += 16
 
-        pos, varint = decode_varint(pos, data)
-        num_items = decode_zigzag(varint)
+#         pos, varint = decode_varint(pos, data)
+#         num_items = decode_zigzag(varint)
 
-        pos, varint = decode_varint(pos, data)
-        num_bytes = decode_zigzag(varint)
+#         pos, varint = decode_varint(pos, data)
+#         num_bytes = decode_zigzag(varint)
 
-        decompressor = zlib.decompressobj(-zlib.MAX_WBITS)
-        uncompressed = decompressor.decompress(data[pos : pos + num_bytes])
-        uncompressed += decompressor.flush()
-        pos += num_bytes
+#         decompressor = zlib.decompressobj(-zlib.MAX_WBITS)
+#         uncompressed = decompressor.decompress(data[pos : pos + num_bytes])
+#         uncompressed += decompressor.flush()
+#         pos += num_bytes
 
-        jagged0.begin({"stream": uncompressed})
-        jagged0.stack_push(num_items)
-        jagged0.resume()
-        assert jagged0.stack == []
+#         jagged0.begin({"stream": uncompressed})
+#         jagged0.stack_push(num_items)
+#         jagged0.resume()
+#         assert jagged0.stack == []
 
-        array = ak.Array(
-            jagged0.output_NumpyArray("content"), check_valid=True
-        )
-        assert len(array) == num_items
-        print(array)
+#         array = ak.Array(
+#             jagged0.output_NumpyArray("content"), check_valid=True
+#         )
+#         assert len(array) == num_items
+#         print(array)
 
-jagged1 = awkward.forth.ForthMachine32("""
-input stream
-output offset0 int32
-output content float32
+# jagged1 = awkward.forth.ForthMachine32("""
+# input stream
+# output offset0 int32
+# output content float32
 
-0 offset0 <- stack
+# 0 offset0 <- stack
 
-0 do
-  stream zigzag-> stack
-  dup offset0 +<- stack
-  stream #f-> content
-  stream b-> stack drop
-loop
-""")
+# 0 do
+#   stream zigzag-> stack
+#   dup offset0 +<- stack
+#   stream #f-> content
+#   stream b-> stack drop
+# loop
+# """)
 
-with open("/home/jpivarski/storage/data/chep-2021-jagged-jagged-jagged/zlib1-jagged1.avro", "rb") as file:
-    data = file.read()
-    pos = data.index(0) + 1
+# with open("/home/jpivarski/storage/data/chep-2021-jagged-jagged-jagged/zlib1-jagged1.avro", "rb") as file:
+#     data = file.read()
+#     pos = data.index(0) + 1
 
-    while pos + 16 < len(data):
-        pos += 16
+#     while pos + 16 < len(data):
+#         pos += 16
 
-        pos, varint = decode_varint(pos, data)
-        num_items = decode_zigzag(varint)
+#         pos, varint = decode_varint(pos, data)
+#         num_items = decode_zigzag(varint)
 
-        pos, varint = decode_varint(pos, data)
-        num_bytes = decode_zigzag(varint)
+#         pos, varint = decode_varint(pos, data)
+#         num_bytes = decode_zigzag(varint)
 
-        decompressor = zlib.decompressobj(-zlib.MAX_WBITS)
-        uncompressed = decompressor.decompress(data[pos : pos + num_bytes])
-        uncompressed += decompressor.flush()
-        pos += num_bytes
+#         decompressor = zlib.decompressobj(-zlib.MAX_WBITS)
+#         uncompressed = decompressor.decompress(data[pos : pos + num_bytes])
+#         uncompressed += decompressor.flush()
+#         pos += num_bytes
 
-        jagged1.begin({"stream": uncompressed})
-        jagged1.stack_push(num_items)
-        jagged1.resume()
-        assert jagged1.stack == []
+#         jagged1.begin({"stream": uncompressed})
+#         jagged1.stack_push(num_items)
+#         jagged1.resume()
+#         assert jagged1.stack == []
 
-        array = ak.Array(
-            ak.layout.ListOffsetArray32(
-                jagged1.output_Index32("offset0"),
-                jagged1.output_NumpyArray("content")
-            ), check_valid=True
-        )
-        assert len(array) == num_items
-        print(array)
+#         array = ak.Array(
+#             ak.layout.ListOffsetArray32(
+#                 jagged1.output_Index32("offset0"),
+#                 jagged1.output_NumpyArray("content")
+#             ), check_valid=True
+#         )
+#         assert len(array) == num_items
+#         print(array)
 
-jagged2 = awkward.forth.ForthMachine32("""
-input stream
-output offset0 int32
-output offset1 int32
-output content float32
+# jagged2 = awkward.forth.ForthMachine32("""
+# input stream
+# output offset0 int32
+# output offset1 int32
+# output content float32
 
-0 offset0 <- stack
-0 offset1 <- stack
+# 0 offset0 <- stack
+# 0 offset1 <- stack
 
-0 do
-  stream zigzag-> stack
-  dup offset0 +<- stack
-  0 do
-    stream zigzag-> stack
-    dup offset1 +<- stack
-    stream #f-> content
-    stream b-> stack drop
-  loop
-  stream b-> stack drop
-loop
-""")
+# 0 do
+#   stream zigzag-> stack
+#   dup offset0 +<- stack
+#   0 do
+#     stream zigzag-> stack
+#     dup offset1 +<- stack
+#     stream #f-> content
+#     stream b-> stack drop
+#   loop
+#   stream b-> stack drop
+# loop
+# """)
 
-with open("/home/jpivarski/storage/data/chep-2021-jagged-jagged-jagged/zlib1-jagged2.avro", "rb") as file:
-    data = file.read()
-    pos = data.index(0) + 1
+# with open("/home/jpivarski/storage/data/chep-2021-jagged-jagged-jagged/zlib1-jagged2.avro", "rb") as file:
+#     data = file.read()
+#     pos = data.index(0) + 1
 
-    while pos + 16 < len(data):
-        pos += 16
+#     while pos + 16 < len(data):
+#         pos += 16
 
-        pos, varint = decode_varint(pos, data)
-        num_items = decode_zigzag(varint)
+#         pos, varint = decode_varint(pos, data)
+#         num_items = decode_zigzag(varint)
 
-        pos, varint = decode_varint(pos, data)
-        num_bytes = decode_zigzag(varint)
+#         pos, varint = decode_varint(pos, data)
+#         num_bytes = decode_zigzag(varint)
 
-        decompressor = zlib.decompressobj(-zlib.MAX_WBITS)
-        uncompressed = decompressor.decompress(data[pos : pos + num_bytes])
-        uncompressed += decompressor.flush()
-        pos += num_bytes
+#         decompressor = zlib.decompressobj(-zlib.MAX_WBITS)
+#         uncompressed = decompressor.decompress(data[pos : pos + num_bytes])
+#         uncompressed += decompressor.flush()
+#         pos += num_bytes
 
-        jagged2.begin({"stream": uncompressed})
-        jagged2.stack_push(num_items)
-        jagged2.resume()
-        assert jagged2.stack == []
+#         jagged2.begin({"stream": uncompressed})
+#         jagged2.stack_push(num_items)
+#         jagged2.resume()
+#         assert jagged2.stack == []
 
-        array = ak.Array(
-            ak.layout.ListOffsetArray32(
-                jagged2.output_Index32("offset0"),
-                ak.layout.ListOffsetArray32(
-                    jagged2.output_Index32("offset1"),
-                    jagged2.output_NumpyArray("content")
-                )
-            ), check_valid=True
-        )
-        assert len(array) == num_items
-        print(array)
+#         array = ak.Array(
+#             ak.layout.ListOffsetArray32(
+#                 jagged2.output_Index32("offset0"),
+#                 ak.layout.ListOffsetArray32(
+#                     jagged2.output_Index32("offset1"),
+#                     jagged2.output_NumpyArray("content")
+#                 )
+#             ), check_valid=True
+#         )
+#         assert len(array) == num_items
+#         print(array)
+
+cumulative_entries = 0
+data = []
+with open("/home/jpivarski/storage/data/chep-2021-jagged-jagged-jagged/zlib1-jagged3.avro", "rb") as file:
+    begintime = time.time()
+    for item in fastavro.reader(file):
+        data.append(item)
+        if len(data) == 285080:
+            break
+    endtime = time.time()
+    print("their time", endtime - begintime)
 
 jagged3 = awkward.forth.ForthMachine32("""
 input stream
@@ -188,40 +202,51 @@ output content float32
 loop
 """)
 
-with open("/home/jpivarski/storage/data/chep-2021-jagged-jagged-jagged/zlib1-jagged3.avro", "rb") as file:
-    data = file.read()
-    pos = data.index(0) + 1
+cumulative_entries = 0
+data = np.memmap("/home/jpivarski/storage/data/chep-2021-jagged-jagged-jagged/zlib1-jagged3.avro", np.uint8)
 
-    while pos + 16 < len(data):
-        pos += 16
+begintime = time.time()
+pos = 0
+while data[pos] != 0:
+    pos += 1
+pos += 1
 
-        pos, varint = decode_varint(pos, data)
-        num_items = decode_zigzag(varint)
+while pos + 16 < len(data):
+    pos += 16
 
-        pos, varint = decode_varint(pos, data)
-        num_bytes = decode_zigzag(varint)
+    pos, varint = decode_varint(pos, data)
+    num_items = decode_zigzag(varint)
 
-        decompressor = zlib.decompressobj(-zlib.MAX_WBITS)
-        uncompressed = decompressor.decompress(data[pos : pos + num_bytes])
-        uncompressed += decompressor.flush()
-        pos += num_bytes
+    pos, varint = decode_varint(pos, data)
+    num_bytes = decode_zigzag(varint)
 
-        jagged3.begin({"stream": uncompressed})
-        jagged3.stack_push(num_items)
-        jagged3.resume()
-        assert jagged3.stack == []
+    decompressor = zlib.decompressobj(-zlib.MAX_WBITS)
+    uncompressed = decompressor.decompress(data[pos : pos + num_bytes])
+    uncompressed += decompressor.flush()
+    pos += num_bytes
 
-        array = ak.Array(
+    jagged3.begin({"stream": uncompressed})
+    jagged3.stack_push(num_items)
+    jagged3.resume()
+    assert jagged3.stack == []
+
+    array = ak.Array(
+        ak.layout.ListOffsetArray32(
+            jagged3.output_Index32("offset0"),
             ak.layout.ListOffsetArray32(
-                jagged3.output_Index32("offset0"),
+                jagged3.output_Index32("offset1"),
                 ak.layout.ListOffsetArray32(
-                    jagged3.output_Index32("offset1"),
-                    ak.layout.ListOffsetArray32(
-                        jagged3.output_Index32("offset2"),
-                        jagged3.output_NumpyArray("content")
-                    )
+                    jagged3.output_Index32("offset2"),
+                    jagged3.output_NumpyArray("content")
                 )
-            ), check_valid=True
-        )
-        assert len(array) == num_items
-        print(array)
+            )
+        ), check_valid=True
+    )
+    assert len(array) == num_items
+    # print(array)
+    cumulative_entries += len(array)
+    if cumulative_entries == 285080:
+        break
+
+endtime = time.time()
+print("my time", endtime - begintime)
