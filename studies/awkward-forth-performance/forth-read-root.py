ef41efa60eac73687c582508245832c79402cb11
Jim Pivarski <jpivarski@gmail.com>
2021-01-20 19:22:31 -0600

    AwkwardForth is 150x faster than Uproot's library='np' and 300x faster than library='ak' (because of its two passes).

---
+++ studies/awkward-forth-performance/forth-read-root.py
@@ -0,0 +1,71 @@
+import time
+
+import uproot
+import awkward.forth
+import awkward as ak
+import numpy as np
+
+jagged3 = awkward.forth.ForthMachine32("""
+input data
+input byte_offsets
+output offsets0 int32
+output offsets1 int32
+output offsets2 int32
+output content float32
+
+0 offsets0 <- stack
+0 offsets1 <- stack
+0 offsets2 <- stack
+
+begin
+  byte_offsets i-> stack
+  6 + data seek
+  data !i-> stack
+  dup offsets0 +<- stack
+  0 do
+    data !i-> stack
+    dup offsets1 +<- stack
+    0 do
+      data !i-> stack
+      dup offsets2 +<- stack
+      data #!f-> content
+    loop
+  loop
+again
+""")
+
+branch = uproot.open("/home/jpivarski/storage/data/chep-2021-jagged-jagged-jagged/zlib1-jagged3.root:tree/branch")
+
+for basketid in range(branch.num_baskets):
+    print(basketid)
+    basket = branch.basket(basketid)
+    start, stop = branch.basket_entry_start_stop(basketid)
+
+    beforetime = time.time()
+    jagged3.run(
+        {"data": basket.data, "byte_offsets": basket.byte_offsets},
+        raise_read_beyond=False,
+        raise_seek_beyond=False
+    )
+    array2 = ak.Array(
+        ak.layout.ListOffsetArray32(
+            jagged3.output_Index32("offsets0"),
+            ak.layout.ListOffsetArray32(
+                jagged3.output_Index32("offsets1"),
+                ak.layout.ListOffsetArray32(
+                    jagged3.output_Index32("offsets2"),
+                    jagged3.output_NumpyArray("content")
+                )
+            )
+        )
+    )
+    endtime = time.time()
+    print("AwkwardForth", endtime - beforetime)
+
+    beforetime = time.time()
+    array1 = branch.array(entry_start=start, entry_stop=stop, library="np")
+    endtime = time.time()
+    print("Python", endtime - beforetime)
+
+    if basketid == 2:
+        break
