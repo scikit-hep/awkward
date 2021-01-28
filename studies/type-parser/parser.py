b1f7d43288abe8d3b6345a1baac6b9ed262978be
Pratyush Das <reikdas@gmail.com>
2021-01-28 18:41:49 +0530

    Fix parser to prepare for RecordType

--- studies/type-parser/parser.py
+++ studies/type-parser/parser.py
@@ -11,9 +11,12 @@ class TypeParser(object):
 def rettype(infocls):
     if infocls.arraytype == "Primitive":
         assert hasattr(infocls, "typename")
-        assert hasattr(infocls, "json")
-        return ak.types.PrimitiveType(infocls.typename, infocls.json)
+        if hasattr(infocls, "json"):
+            return ak.types.PrimitiveType(infocls.typename, infocls.json)
+        return ak.types.PrimitiveType(infocls.typename)
     elif infocls.arraytype == "Unknown":
+        if hasattr(infocls, "json"):
+            return ak.types.UnknownType(parameters=infocls.json)
         return ak.types.UnknownType()
 
 
@@ -33,36 +36,56 @@ class TreeToJson(Transformer):
 
 def toast(ptnode, infocls):
     if ptnode.__class__.__name__ == "Token":
-        if ptnode.value == "unknown":
-            infocls.arraytype = "Unknown"
-        else:
-            setattr(infocls, "typename", ptnode.value)
-            infocls.arraytype = "Primitive"
+        setattr(infocls, "typename", ptnode.value)
     elif ptnode.data == "start":
         toast(ptnode.children[0], infocls)
     elif ptnode.data == "input":
         for node in ptnode.children:
             toast(node, infocls)
-    elif ptnode.data == "primitiveoption":
+    elif ptnode.data == "primitive":
+        infocls.arraytype = "Primitive"
+        for node in ptnode.children:
+            toast(node, infocls)
+    elif ptnode.data == "unknown":
+        infocls.arraytype = "Unknown"
+        for node in ptnode.children:
+            toast(node, infocls)
+    elif ptnode.data == "def_option":
         assert len(ptnode.children) == 1
         setattr(infocls, "json", ptnode.children[0])
     elif ptnode.data == "options":
         assert len(ptnode.children) == 1
         toast(ptnode.children[0], infocls)
+    else:
+        raise Exception("Unhandled node")
 
 
 if __name__ == "__main__":
     test = Lark_StandAlone(transformer=TreeToJson())
-    text = 'int64[parameters={"wonky": ["parameter", 3.14]}]'
-    print(test.parse(text).pretty())
+    text = "int64"
     tp = TypeParser()
     toast(test.parse(text), tp)
     parsedtype = rettype(tp)
-    print(parsedtype)
-    print(type(parsedtype))
+    assert isinstance(parsedtype, ak._ext.PrimitiveType)
+    assert parsedtype.dtype == "int64"
+    text = 'int64[parameters={"wonky": ["parameter", 3.14]}]'
+    # print(test.parse(text).pretty())
+    tp = TypeParser()
+    toast(test.parse(text), tp)
+    parsedtype = rettype(tp)
+    # print(parsedtype)
+    # print(type(parsedtype))
+    assert isinstance(parsedtype, ak._ext.PrimitiveType)
+    assert parsedtype.dtype == "int64"
+    assert parsedtype.parameters == {'"wonky"': ['"parameter"', 3.14]}
     text = "unknown"
     tp = TypeParser()
     toast(test.parse(text), tp)
     parsedtype = rettype(tp)
-    print(parsedtype)
-    print(type(parsedtype))
+    assert isinstance(parsedtype, ak._ext.UnknownType)
+    text = 'unknown[parameters={"wonky": ["parameter", 3.14]}]'
+    tp = TypeParser()
+    toast(test.parse(text), tp)
+    parsedtype = rettype(tp)
+    assert isinstance(parsedtype, ak._ext.UnknownType)
+    assert parsedtype.parameters == {'"wonky"': ['"parameter"', 3.14]}
