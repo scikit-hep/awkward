3905458d11cce58500c0580e90701e4c32f294b9
Pratyush Das <reikdas@gmail.com>
2021-01-28 21:01:55 +0530

    We don't need intermediate object

--- studies/type-parser/parser.py
+++ studies/type-parser/parser.py
@@ -3,23 +3,6 @@ import awkward as ak
 from generated_parser import Lark_StandAlone, Transformer
 
 
-class TypeParser(object):
-    def __init__(self):
-        self.arraytype = "Unknown"
-
-
-def rettype(infocls):
-    if infocls.arraytype == "Primitive":
-        assert hasattr(infocls, "typename")
-        if hasattr(infocls, "json"):
-            return ak.types.PrimitiveType(infocls.typename, infocls.json)
-        return ak.types.PrimitiveType(infocls.typename)
-    elif infocls.arraytype == "Unknown":
-        if hasattr(infocls, "json"):
-            return ak.types.UnknownType(parameters=infocls.json)
-        return ak.types.UnknownType()
-
-
 class TreeToJson(Transformer):
     def number(self, n):
         (n,) = n
@@ -34,28 +17,40 @@ class TreeToJson(Transformer):
     false = lambda self, _: False
 
 
-def toast(ptnode, infocls):
+def toast(ptnode):
     if ptnode.__class__.__name__ == "Token":
-        setattr(infocls, "typename", ptnode.value)
+        return ptnode.value
     elif ptnode.data == "start":
-        toast(ptnode.children[0], infocls)
+        return toast(ptnode.children[0])
     elif ptnode.data == "input":
-        for node in ptnode.children:
-            toast(node, infocls)
+        assert len(ptnode.children) == 1
+        return toast(ptnode.children[0])
     elif ptnode.data == "primitive":
-        infocls.arraytype = "Primitive"
-        for node in ptnode.children:
-            toast(node, infocls)
+        if len(ptnode.children) == 1:
+            return ak.types.PrimitiveType(toast(ptnode.children[0]))
+        elif len(ptnode.children) == 2:
+            return ak.types.PrimitiveType(
+                toast(ptnode.children[0]), toast(ptnode.children[1])
+            )
+        else:
+            raise Exception("Unhandled PrimitiveType node")
     elif ptnode.data == "unknown":
-        infocls.arraytype = "Unknown"
+        if len(ptnode.children) == 0:
+            return ak.types.UnknownType()
+        elif len(ptnode.children) == 1:
+            return ak.types.UnknownType(parameters=toast(ptnode.children[0]))
+        else:
+            raise Exception("Unhandled UnknownType node")
+    elif ptnode.data == "record":
+        infocls.arraytype = "Record"
         for node in ptnode.children:
             toast(node, infocls)
     elif ptnode.data == "def_option":
         assert len(ptnode.children) == 1
-        setattr(infocls, "json", ptnode.children[0])
+        return ptnode.children[0]
     elif ptnode.data == "options":
         assert len(ptnode.children) == 1
-        toast(ptnode.children[0], infocls)
+        return toast(ptnode.children[0])
     else:
         raise Exception("Unhandled node")
 
@@ -63,29 +58,27 @@ def toast(ptnode, infocls):
 if __name__ == "__main__":
     test = Lark_StandAlone(transformer=TreeToJson())
     text = "int64"
-    tp = TypeParser()
-    toast(test.parse(text), tp)
-    parsedtype = rettype(tp)
-    assert isinstance(parsedtype, ak._ext.PrimitiveType)
+    parsedtype = toast(test.parse(text))
+    assert isinstance(parsedtype, ak.types.PrimitiveType)
     assert parsedtype.dtype == "int64"
     text = 'int64[parameters={"wonky": ["parameter", 3.14]}]'
     # print(test.parse(text).pretty())
-    tp = TypeParser()
-    toast(test.parse(text), tp)
-    parsedtype = rettype(tp)
+    parsedtype = toast(test.parse(text))
     # print(parsedtype)
     # print(type(parsedtype))
-    assert isinstance(parsedtype, ak._ext.PrimitiveType)
+    assert isinstance(parsedtype, ak.types.PrimitiveType)
     assert parsedtype.dtype == "int64"
     assert parsedtype.parameters == {'"wonky"': ['"parameter"', 3.14]}
     text = "unknown"
-    tp = TypeParser()
-    toast(test.parse(text), tp)
-    parsedtype = rettype(tp)
-    assert isinstance(parsedtype, ak._ext.UnknownType)
+    parsedtype = toast(test.parse(text))
+    assert isinstance(parsedtype, ak.types.UnknownType)
     text = 'unknown[parameters={"wonky": ["parameter", 3.14]}]'
-    tp = TypeParser()
-    toast(test.parse(text), tp)
-    parsedtype = rettype(tp)
-    assert isinstance(parsedtype, ak._ext.UnknownType)
+    parsedtype = toast(test.parse(text))
+    assert isinstance(parsedtype, ak.types.UnknownType)
     assert parsedtype.parameters == {'"wonky"': ['"parameter"', 3.14]}
+    """
+    text = "(int64)"
+    parsedtype = toast(test.parse(text), tp)
+    assert isinstance(parsedtype, ak.types.RecordType)
+    assert (parsedtype.types == (ak.types.PrimitiveType("int64"), ))
+    """
