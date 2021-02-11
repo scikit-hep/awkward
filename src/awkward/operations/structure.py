8e00a7cde63736edd2830b2f8fbc6e242a61c662
Jim Pivarski <jpivarski@gmail.com>
2021-02-11 17:06:09 -0600

    Add an 'axis' parameter to 'ak.unflatten'.

--- src/awkward/operations/structure.py
+++ src/awkward/operations/structure.py
@@ -1672,86 +1672,152 @@ def flatten(array, axis=1, highlevel=True, behavior=None):
         return out
 
 
-def unflatten(array, counts, highlevel=True, behavior=None):
+def unflatten(array, counts, axis=0, highlevel=True, behavior=None):
     """
-    Args:
-        array: Data to create an array with an additional level from.
-        counts (int or array): Number of elements the new level should have.
-            If an integer, the new level will be regularly sized; otherwise,
-            it will consist of variable-length lists with the given lengths.
-        highlevel (bool): If True, return an #ak.Array; otherwise, return
-            a low-level #ak.layout.Content subclass.
-        behavior (None or dict): Custom #ak.behavior for the output array, if
-            high-level.
+        Args:
+            array: Data to create an array with an additional level from.
+            counts (int or array): Number of elements the new level should have.
+                If an integer, the new level will be regularly sized; otherwise,
+                it will consist of variable-length lists with the given lengths.
+            axis (int): The dimension at which this operation is applied. The
+                outermost dimension is `0`, followed by `1`, etc., and negative
+                values count backward from the innermost: `-1` is the innermost
+                dimension, `-2` is the next level up, etc.
+            highlevel (bool): If True, return an #ak.Array; otherwise, return
+                a low-level #ak.layout.Content subclass.
+            behavior (None or dict): Custom #ak.behavior for the output array, if
+                high-level.
 
-    Returns an array with an additional level of nesting. This is roughly the
-    inverse of #ak.flatten, where `counts` were obtained by #ak.num (both with
-    `axis=1`).
+        Returns an array with an additional level of nesting. This is roughly the
+        inverse of #ak.flatten, where `counts` were obtained by #ak.num (both with
+        `axis=1`).
 
-    For example,
+        For example,
 
-        >>> original = ak.Array([[0, 1, 2], [], [3, 4], [5], [6, 7, 8, 9]])
-        >>> counts = ak.num(original)
-        >>> array = ak.flatten(original)
-        >>> counts
-        <Array [3, 0, 2, 1, 4] type='5 * int64'>
-        >>> array
-        <Array [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] type='10 * int64'>
-        >>> ak.unflatten(array, counts)
-        <Array [[0, 1, 2], [], ... [5], [6, 7, 8, 9]] type='5 * var * int64'>
+            >>> original = ak.Array([[0, 1, 2], [], [3, 4], [5], [6, 7, 8, 9]])
+            >>> counts = ak.num(original)
+            >>> array = ak.flatten(original)
+            >>> counts
+            <Array [3, 0, 2, 1, 4] type='5 * int64'>
+            >>> array
+            <Array [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] type='10 * int64'>
+            >>> ak.unflatten(array, counts)
+            <Array [[0, 1, 2], [], ... [5], [6, 7, 8, 9]] type='5 * var * int64'>
+
+        An inner dimension can be unflattened by setting the `axis` parameter, but
+        operations like this constrain the `counts` more tightly.
+
+        For example, we can subdivide an already divided list:
+
+            >>> original = ak.Array([[1, 2, 3, 4], [], [5, 6, 7], [8, 9]])
+            >>> print(ak.unflatten(original, [2, 2, 1, 2, 1, 1], axis=1))
+            [[[1, 2], [3, 4]], [], [[5], [6, 7]], [[8], [9]]]
+
+        But the counts have to add up to the lengths of those lists. We can't mix
+        values from the first `[1, 2, 3, 4]` with values from the next `[5, 6, 7]`.
+
+            >>> print(ak.unflatten(original, [2, 1, 2, 2, 1, 1], axis=1))
+            Traceback (most recent call last):
+            ...
+            ValueError: structure imposed by 'counts' does not fit in the array at axis=1
     """
+    nplike = ak.nplike.of(array)
+
+    def doit(layout, counts):
+        if isinstance(counts, (numbers.Integral, np.integer)):
+            if counts < 0 or counts > len(layout):
+                raise ValueError(
+                    "too large counts for array or negative counts"
+                    + ak._util.exception_suffix(__file__)
+                )
+            out = ak.layout.RegularArray(layout, counts)
+
+        else:
+            counts = ak.operations.convert.to_layout(
+                counts, allow_record=False, allow_other=False
+            )
+            ptr_lib = ak.operations.convert.kernels(array)
+            counts = ak.operations.convert.to_kernels(counts, ptr_lib, highlevel=False)
+            if ptr_lib == "cpu":
+                counts = ak.operations.convert.to_numpy(counts, allow_missing=True)
+                mask = ak.nplike.numpy.ma.getmask(counts)
+                counts = ak.nplike.numpy.ma.filled(counts, 0)
+            elif ptr_lib == "cuda":
+                counts = ak.operations.convert.to_cupy(counts)
+                mask = False
+            else:
+                raise AssertionError(
+                    "unrecognized kernels lib" + ak._util.exception_suffix(__file__)
+                )
+            if counts.ndim != 1:
+                raise ValueError(
+                    "counts must be one-dimensional"
+                    + ak._util.exception_suffix(__file__)
+                )
+
+            if not issubclass(counts.dtype.type, np.integer):
+                raise ValueError(
+                    "counts must be integers" + ak._util.exception_suffix(__file__)
+                )
+            offsets = nplike.empty(len(counts) + 1, np.int64)
+            offsets[0] = 0
+            nplike.cumsum(counts, out=offsets[1:])
+            if offsets[-1] > len(layout):
+                raise ValueError(
+                    "sum of counts exceeds length of array"
+                    + ak._util.exception_suffix(__file__)
+                )
+            offsets = ak.layout.Index64(offsets)
+            out = ak.layout.ListOffsetArray64(offsets, layout)
+            if not isinstance(mask, (bool, np.bool_)):
+                index = ak.layout.Index8(nplike.asarray(mask).astype(np.int8))
+                out = ak.layout.ByteMaskedArray(index, out, valid_when=False)
+
+        return out
+
     layout = ak.operations.convert.to_layout(
         array, allow_record=False, allow_other=False
     )
 
-    if isinstance(counts, (numbers.Integral, np.integer)):
-        if counts < 0 or counts > len(layout):
-            raise ValueError(
-                "too large counts for array or negative counts"
-                + ak._util.exception_suffix(__file__)
-            )
-        out = ak.layout.RegularArray(layout, counts)
+    if axis == 0 or layout.axis_wrap_if_negative(axis) == 0:
+        out = doit(layout, counts)
 
     else:
-        counts = ak.operations.convert.to_layout(
-            counts, allow_record=False, allow_other=False
-        )
-        ptr_lib = ak.operations.convert.kernels(array)
-        counts = ak.operations.convert.to_kernels(counts, ptr_lib, highlevel=False)
-        if ptr_lib == "cpu":
-            counts = ak.operations.convert.to_numpy(counts, allow_missing=True)
-            mask = ak.nplike.numpy.ma.getmask(counts)
-            counts = ak.nplike.numpy.ma.filled(counts, 0)
-        elif ptr_lib == "cuda":
-            counts = ak.operations.convert.to_cupy(counts)
-            mask = False
-        else:
-            raise AssertionError(
-                "unrecognized kernels lib" + ak._util.exception_suffix(__file__)
-            )
-        if counts.ndim != 1:
-            raise ValueError(
-                "counts must be one-dimensional" + ak._util.exception_suffix(__file__)
-            )
 
-        nplike = ak.nplike.of(array)
-        if not issubclass(counts.dtype.type, np.integer):
-            raise ValueError(
-                "counts must be integers" + ak._util.exception_suffix(__file__)
-            )
-        offsets = nplike.empty(len(counts) + 1, np.int64)
-        offsets[0] = 0
-        nplike.cumsum(counts, out=offsets[1:])
-        if offsets[-1] > len(layout):
-            raise ValueError(
-                "sum of counts exceeds length of array"
-                + ak._util.exception_suffix(__file__)
-            )
-        offsets = ak.layout.Index64(offsets)
-        out = ak.layout.ListOffsetArray64(offsets, layout)
-        if not isinstance(mask, (bool, np.bool_)):
-            index = ak.layout.Index8(nplike.asarray(mask).astype(np.int8))
-            out = ak.layout.ByteMaskedArray(index, out, valid_when=False)
+        def getfunction(layout, depth, posaxis):
+            posaxis = layout.axis_wrap_if_negative(posaxis)
+            if posaxis == depth and isinstance(layout, ak._util.listtypes):
+                # We are one *above* the level where we want to apply this.
+                listoffsetarray = layout.toListOffsetArray64(True)
+                outeroffsets = nplike.asarray(listoffsetarray.offsets)
+
+                content = doit(layout.content, counts)
+                if isinstance(content, ak.layout.ByteMaskedArray):
+                    inneroffsets = nplike.asarray(content.content.offsets)
+                else:
+                    inneroffsets = nplike.asarray(content.offsets)
+
+                positions = nplike.searchsorted(inneroffsets, outeroffsets, side="left")
+                if not nplike.array_equal(inneroffsets[positions], outeroffsets):
+                    raise ValueError(
+                        "structure imposed by 'counts' does not fit in the array "
+                        "at axis={0}".format(axis) + ak._util.exception_suffix(__file__)
+                    )
+
+                return lambda: ak.layout.ListOffsetArray64(
+                    ak.layout.Index64(positions), content
+                )
+
+            else:
+                return posaxis
+
+        out = ak._util.recursively_apply(
+            layout,
+            getfunction,
+            pass_depth=True,
+            pass_user=True,
+            user=axis,
+        )
 
     if highlevel:
         return ak._util.wrap(out, ak._util.behaviorof(array, behavior=behavior))
