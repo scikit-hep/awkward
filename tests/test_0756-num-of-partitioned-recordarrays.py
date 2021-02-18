fdfd182090285466d2fe82aa04fe7dfb17381e49
Jim Pivarski <jpivarski@gmail.com>
2021-02-18 14:18:49 -0600

    Fixes #756; ak.num on PartitionedArrays.

---
+++ tests/test_0756-num-of-partitioned-recordarrays.py
@@ -0,0 +1,12 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+from __future__ import absolute_import
+
+import pytest  # noqa: F401
+import numpy as np  # noqa: F401
+import awkward as ak  # noqa: F401
+
+
+def test():
+    array = ak.repartition([{"x": x, "y": x*10} for x in range(10)], 2)
+    assert ak.to_list(ak.num(array.layout, axis=0)) == {"x": 10, "y": 10}
