772144124f7a7c09b9ae5f6cbe6d38bf1f96c4af
Jim Pivarski <jpivarski@gmail.com>
2021-02-23 16:09:20 -0600

    This version implements the state entirely in Forth, and I found that end_record is superfluous, too.

--- studies/TypedArrayBuilder-forth-test.py
+++ studies/TypedArrayBuilder-forth-test.py
@@ -12,7 +12,7 @@ form = ak.forms.Form.fromjson("""
             "x": {
                 "class": "NumpyArray",
                 "primitive": "float64",
-                "form_key": "node1"
+                "form_key": "node2"
             },
             "y": {
                 "class": "ListOffsetArray64",
@@ -20,11 +20,12 @@ form = ak.forms.Form.fromjson("""
                 "content": {
                     "class": "NumpyArray",
                     "primitive": "int64",
-                    "form_key": "node3"
+                    "form_key": "node4"
                 },
-                "form_key": "node2"
+                "form_key": "node3"
             }
-        }
+        },
+        "form_key": "node1"
     },
     "form_key": "node0"
 }
@@ -39,104 +40,111 @@ class TypedArrayBuilder:
         self.vm = awkward.forth.ForthMachine32("""
             input data
             output part0-node0-offsets int64
-            output part0-node1-data float64
-            output part0-node2-offsets int64
-            output part0-node3-data int64
+            output part0-node2-data float64
+            output part0-node3-offsets int64
+            output part0-node4-data int64
+
+            : node4-int64
+                ." node4-int64" cr
+                ( no pause because this is first after an implicit begin_list )
+                0 = if ( int64 )
+                    0 data seek
+                    data q-> part0-node4-data
+                else
+                    halt
+                then
+            ;
+
+            : node3-list
+                ." node3-list" cr
+                0   ( initialize the node3 list counter )
+                begin
+                    pause
+                    dup 2 = if ( end_list )
+                        drop
+                        part0-node3-offsets +<- stack ( absorbs node3 counter )
+                        exit
+                    else
+                        node4-int64
+                        1+   ( increment the node3 list counter )
+                    then
+                again
+            ;
+
+            : node2-float64
+                ." node2-float64" cr
+                ( no pause because this is first after an implicit begin_list )
+                1 = if ( float64 )
+                    0 data seek
+                    data d-> part0-node2-data
+                else
+                    halt
+                then
+            ;
+
+            : node1-record
+                ." node1-record" cr
+                node2-float64
+                node3-list
+
+                ( hmm... end_record is superfluous, too )
+                pause
+                3 <> if ( end_record )
+                    halt
+                then
+            ;
+
+            : node0-list
+                ." node0-list" cr
+                0   ( initialize the node0 list counter )
+                begin
+                    pause
+                    dup 2 = if ( end_list )
+                        drop
+                        part0-node0-offsets +<- stack ( absorbs node0 counter )
+                        exit
+                    else
+                        node1-record
+                        1+   ( increment the node0 list counter )
+                    then
+                again
+            ;
 
             0 part0-node0-offsets <- stack
-            0 part0-node2-offsets <- stack
+            0 part0-node3-offsets <- stack
 
-            0   ( initialize the total counter )
-            0   ( initialize the node0 list counter )
-
-            : fill-node1-goto-1
-                0 data seek
-                data d-> part0-node1-data
-                0   ( initialize the node2 list counter )
-                1   ( every routine ends with new FSM state )
-            ;
-
-            : fill-node0-inc-total-goto-0
-                part0-node0-offsets +<- stack
-                1+  ( increment the total counter )
-                0   ( initialize the node0 list counter )
-                0   ( every routine ends with new FSM state )
-            ;
-
-            : fill-node3-inc-node2-goto-1
-                0 data seek
-                data q-> part0-node3-data
-                1+  ( increment the node2 list counter )
-                1   ( every routine ends with new FSM state )
-            ;
-
-            : fill-node2-goto-2
-                part0-node2-offsets +<- stack
-                2   ( every routine ends with new FSM state )
-            ;
-
-            : inc-node0-goto-0
-                1+  ( increment the node0 list counter )
-                0   ( every routine ends with new FSM state )
-            ;
+            0    ( initialize the total counter )
+            begin
+                node0-list
+                1+   ( increment the total counter )
+            again
         """)
 
-        self.fsm = [
-            # 0
-            {
-                "float64": "fill-node1-goto-1",
-                "end_list": "fill-node0-inc-total-goto-0",
-            },
-            # 1
-            {
-                "int64": "fill-node3-inc-node2-goto-1",
-                "end_list": "fill-node2-goto-2",
-            },
-            # 2
-            {
-                "end_record": "inc-node0-goto-0",
-            },
-        ]
-
-        self.state = 0
         self.data = np.empty(8, np.uint8)
         self.vm.run({"data": self.data})
 
     def int64(self, x):
         self.data.view(np.int64)[0] = x
-        word = self.fsm[self.state].get("int64")
-        if word is None:
-            raise ValueError("can't call 'int64' at this point in the process")
-        self.vm.call(word)
-        self.state = self.vm.stack_pop()
+        self.vm.stack_push(0)
+        self.vm.resume()
 
     def float64(self, x):
         self.data.view(np.float64)[0] = x
-        word = self.fsm[self.state].get("float64")
-        if word is None:
-            raise ValueError("can't call 'float64' at this point in the process")
-        self.vm.call(word)
-        self.state = self.vm.stack_pop()
+        self.vm.stack_push(1)
+        self.vm.resume()
 
     def end_list(self):
-        word = self.fsm[self.state].get("end_list")
-        if word is None:
-            raise ValueError("can't call 'end_list' at this point in the process")
-        self.vm.call(word)
-        self.state = self.vm.stack_pop()
+        self.vm.stack_push(2)
+        self.vm.resume()
 
     def end_record(self):
-        word = self.fsm[self.state].get("end_record")
-        if word is None:
-            raise ValueError("can't call 'end_record' at this point in the process")
-        self.vm.call(word)
-        self.state = self.vm.stack_pop()
+        self.vm.stack_push(3)
+        self.vm.resume()
 
     def snapshot(self):
         return ak.from_buffers(self.form, self.vm.stack[0], self.vm.outputs)
 
     def debug_step(self):
-        print("FSM state:", self.state)
         print("stack: ", builder.vm.stack)
         for k, v in builder.vm.outputs.items():
             print(k + ":", np.asarray(v))
