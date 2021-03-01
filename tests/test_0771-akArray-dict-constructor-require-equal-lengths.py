3687f808439a6dc792ed734aa6e73529ab1a659e
Jim Pivarski <jpivarski@users.noreply.github.com>
2021-03-01 11:28:21 -0600

    Fixes #771; constructing Array with different length columns should raise error. (#772)

---
+++ tests/test_0771-akArray-dict-constructor-require-equal-lengths.py
@@ -0,0 +1,12 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+from __future__ import absolute_import
+
+import pytest  # noqa: F401
+import numpy as np  # noqa: F401
+import awkward as ak  # noqa: F401
+
+
+def test():
+    with pytest.raises(ValueError):
+        ak.Array({"a": [1, 2], "b": [1, 2, 3]})
