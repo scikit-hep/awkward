d16a17d9e023b20e0e38353a8998b56794df7bf3
Jim Pivarski <jpivarski@gmail.com>
2021-03-29 14:32:53 -0500

    Explicit dtypes for Windows.

--- tests/test_0794-ak-cartesian-on-empty-array.py
+++ tests/test_0794-ak-cartesian-on-empty-array.py
@@ -8,9 +8,11 @@ import awkward as ak  # noqa: F401
 
 
 def test():
-    nparray = ak.layout.NumpyArray([1, 2, 3])
+    nparray = ak.layout.NumpyArray(np.array([1, 2, 3], dtype=np.int64))
     listarray = ak.layout.ListArray64(
-        ak.layout.Index64([0]), ak.layout.Index64([3]), nparray
+        ak.layout.Index64(np.array([0], dtype=np.int64)),
+        ak.layout.Index64(np.array([3], dtype=np.int64)),
+        nparray,
     )
     indexedarray = ak.layout.IndexedArray64(ak.layout.Index64([]), listarray)
 
