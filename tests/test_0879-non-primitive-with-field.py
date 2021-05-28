619fc3787c32c845044977730fe5c767b94e7a20
Angus Hollands <goosey15@gmail.com>
2021-05-28 10:47:44 +0100

    Test: add test for #879
    This test simply triggers the ValueError that is produced
    in the high-level API.

---
+++ tests/test_0879-non-primitive-with-field.py
@@ -0,0 +1,17 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+from __future__ import absolute_import
+
+import pytest  # noqa: F401
+import numpy as np  # noqa: F401
+import awkward as ak  # noqa: F401
+
+
+def test():
+    x = ak.Array({"x": np.arange(10)})
+
+    xy = ak.with_field(base=x, what=object(), where="y")
+
+    # Try to access the type of a single element
+    # This raises a ValueError in #879
+    xy_type = xy.type  # noqa: F841
