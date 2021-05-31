3a431f3608372fc9ce84d3cdaccf102c6b730039
Jim Pivarski <jpivarski@gmail.com>
2021-05-31 09:07:58 -0500

    more tweaks

--- src/awkward/_v2/identifier.py
+++ src/awkward/_v2/identifier.py
@@ -6,26 +6,33 @@ import awkward as ak
 
 np = ak.nplike.NumpyMetadata.instance()
 
+
 class Identifier(object):
+    _numrefs = 0
 
-    numrefs = 0
-
+    @staticmethod
     def newref():
-        numrefs += 1
-        return numrefs
+        out = Identifier._numrefs
+        Identifier._numrefs += 1
+        return out
 
-    def __init__(self, ref, filedloc, data):
+    def __init__(self, ref, fieldloc, data):
         self._ref = ref
-        self._fieldloc = filedloc
-        self._data = data
+        self._fieldloc = fieldloc
+
+        self._nplike = ak.nplike.of(data)
+
+        self._data = self._nplike.asarray(data, order="C")
         if len(self._data.shape) != 2:
-            #FIXME
             raise TypeError("Identifer data must be 2-dimenional")
-        self.nplike = ak.nplike.of(data)
 
     @classmethod
     def zeros(cls, ref, fieldloc, length, width, nplike, dtype):
-        return nplike.zeros((length,width), dtype=dtype)???
+        return Identifier(ref, fieldloc, nplike.zeros((length, width), dtype=dtype))
+
+    @classmethod
+    def empty(cls, ref, fieldloc, length, width, nplike, dtype):
+        return Identifier(ref, fieldloc, nplike.empty((length, width), dtype=dtype))
 
     @property
     def ref(self):
@@ -41,30 +48,36 @@ class Identifier(object):
 
     @property
     def nplike(self):
-        return  self._nplike
+        return self._nplike
 
-    def  __len__(self):
+    def __len__(self):
         return len(self._data)
 
-    def  width(self):
+    def width(self):
         return self._data.shape[1]
 
     def to64(self):
-        return Identifier(self._data.astype(np.int64))
+        return Identifier(self._ref, self._fieldloc, self._data.astype(np.int64))
 
-    #is this retrieving just one dimension?
     def __getitem__(self, where):
         return self._data[where]
 
     def __copy__(self):
-        return Identifier(self._data.copy())
+        return Identifier(self._ref, self._fieldloc, self._data.copy())
 
     def __repr__(self):
         # FIXME
         return self._nplike.array_str(self._data)
 
     def convert_to(self, nplike):
-        return Identifier(nplike.asarray(self._data))
+        return Identifier(self._ref, self._fieldloc, nplike.asarray(self._data))
 
-    def referentially_equal (self):
-        return self._ref is self._filedloc is self._data
+    def referentially_equal(self, other):
+        return (
+            self._ref == other._ref
+            and self._fieldloc == other._fieldloc
+            and self._data.ctypes.data == other._data.ctypes.data
+            and self._data.shape == other._data.shape
+            and self._data.strides == other._data.strides
+            and self._data.dtype == other._data.dtype
+        )
