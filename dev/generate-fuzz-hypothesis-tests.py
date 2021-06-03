00ac1e261aaa55455bb1ff1f08434d048df67226
SantamRC <santamdev404@gmail.com>
2021-06-04 02:08:13 +0530

    [skip ci]added some changes

--- dev/generate-fuzz-hypothesis-tests.py
+++ dev/generate-fuzz-hypothesis-tests.py
@@ -4,42 +4,64 @@ import yaml
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-@given(st.lists(st.integers().filter(lambda x: x > 1),min_size=24,max_size=24, unique=True))
+
+@given(
+    st.lists(
+        st.integers().filter(lambda x: x > 1), min_size=24, max_size=24, unique=True
+    )
+)
 @settings(max_examples=1)
-def genInput(x):
-    input=[{'input':x}]
+def gen_input(x):
+    doc = None
     with open(
-        os.path.join(CURRENT_DIR, "..","test_fuzz.yml"), "w"
-    ) as fuzz:
-        doc=yaml.dump(input,fuzz)
-    genTest(str(x))
-
-values=[
-    "toindex = (ctypes.c_int64*len(toindex))(*toindex)",
-    "frombitmask = [1, 1, 1, 1, 1]",
-    "frombitmask = (ctypes.c_uint8*len(frombitmask))(*frombitmask)",
-    "bitmasklength = 3",
-    "validwhen = True",
-    "lsb_order = True",
-    "funcC = getattr(lib, 'awkward_BitMaskedArray_to_IndexedOptionArray64')",
-    "funcC.restype = Error",
-    "funcC.argtypes = (ctypes.POINTER(ctypes.c_int64), ctypes.POINTER(ctypes.c_uint8), ctypes.c_int64, ctypes.c_bool, ctypes.c_bool)",
-    "pytest_toindex = [0, -1, -1, -1, -1, -1, -1, -1, 8, -1, -1, -1, -1, -1, -1, -1, 16, -1, -1, -1, -1, -1, -1, -1]",
-    "assert toindex[:len(pytest_toindex)] == pytest.approx(pytest_toindex)",
-]
-
-def genTest(val):
-    num=0
-    with open(
-        os.path.join(CURRENT_DIR, "..","tests-spec","test_fuzz_pyawkward_BitMaskedArray_to_IndexedOptionArray64.py"), "w"
+        os.path.join(
+            CURRENT_DIR,
+            "..",
+            "fuzz_test.yml",
+        ),
+        "r",
     ) as f:
-        f.write("import ctypes\nimport pytest\nfrom __init__ import lib, Error\n\n")
-        f.write("def test_cpuawkward_BitMaskedArray_to_IndexedOptionArray64():\n")
-        f.write("\t"+"toindex="+val+"\n")
-        for i in range(11):
-            f.write("\t"+values[num]+"\n")
-            num +=1
+        doc = yaml.load(f, Loader=yaml.FullLoader)
+        doc[0]["inputs"][0]["toindex"] = x
+    with open(
+        os.path.join(
+            CURRENT_DIR,
+            "..",
+            "fuzz_test.yml",
+        ),
+        "w",
+    ) as y:
+        yaml.dump(doc, y, default_flow_style=True)
+        gen_test(doc)
+
+
+def gen_test(doc):
+    index = 0
+    file_name = "test_fuzz_py" + doc[index]["name"] + ".py"
+    with open(os.path.join(CURRENT_DIR, "..", "tests-spec", file_name), "w") as f:
+        for name in doc:
+            num = 1
+            f.write("import ctypes\nimport pytest\nfrom __init__ import lib, Error\n\n")
+            f.write("def " + str(name["name"]) + "_" + str(num) + "():\n")
+            for arg in name["args"]:
+                f.write(
+                    "\t"
+                    + str(arg["name"])
+                    + " = "
+                    + str(name["inputs"][num - 1][str(arg["name"])])
+                    + "\n"
+                )
+                num += 1
+            f.write("\t" + "funcPy = getattr(kernels," + str(name["name"]) + ")\n")
+            f.write("\tfuncPy(")
+            num = 0
+            for arg in name["args"]:
+                if num != 0:
+                    f.write(",")
+                f.write(str(arg["name"]) + "=" + str(arg["name"]))
+                num += 1
+            f.write(")\n")
+
 
 if __name__ == "__main__":
-    genInput()
-
+    gen_input()
