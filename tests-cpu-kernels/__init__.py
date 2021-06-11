4e5eaecda665b641fb9cab554436d912feeb0f63
Ioana Ifrim <ioana.ifrim@gmail.com>
2021-06-10 20:02:56 -0400

    Recommit of previous commited Python 2.7 fixes

--- tests-cpu-kernels/__init__.py
+++ tests-cpu-kernels/__init__.py
@@ -3,12 +3,33 @@
 from __future__ import absolute_import
 
 import ctypes
+import os
+import platform
 
-import awkward as ak
+import pkg_resources
 
+ if platform.system() == "Windows":
+     name = "awkward-cpu-kernels.dll"
+ elif platform.system() == "Darwin":
+     name = "libawkward-cpu-kernels.dylib"
+ else:
+     name = "libawkward-cpu-kernels.so"
 
-lib = ak._cpu_kernels.lib
+ CPU_KERNEL_SO = None
+ try:
+     CPU_KERNEL_SO = pkg_resources.resource_filename("awkward", name)
+ except ImportError:
+     CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+     TOP_DIR = os.path.join(CURRENT_DIR, "..")
+     for root, _, files in os.walk(TOP_DIR):
+         for filename in files:
+             if filename == name:
+                 CPU_KERNEL_SO = os.path.join(root, filename)
+                 break
 
+ if CPU_KERNEL_SO is None:
+     raise Exception("Unable to find {0}.".format(name))
+ lib = ctypes.CDLL(CPU_KERNEL_SO)
 
 class Error(ctypes.Structure):
     _fields_ = [
