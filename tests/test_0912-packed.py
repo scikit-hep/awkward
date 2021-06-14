dd636d13bed2408581560da6b9d346238bcfc0f4
Angus Hollands <goosey15@gmail.com>
2021-06-14 06:56:21 +0100

    Test: expand IndexedOptionArray test
    Rename IndexedArray test

--- tests/test_0912-packed.py
+++ tests/test_0912-packed.py
@@ -29,9 +29,12 @@ def test_indexed_option_array():
 
     packed = ak.packed(layout, highlevel=False)
     assert ak.to_list(layout) == ak.to_list(packed)
+    assert isinstance(packed, ak.layout.IndexedOptionArray64)
+    assert packed.index.tolist() == [0, -1, 1, -1, 2]
+    assert len(packed.content) == 3
 
 
-def test_indexed_numpy_array():
+def test_indexed_array():
     index = ak.layout.Index64(np.array([0, 1, 2, 3, 6, 7, 8]))
     content = ak.layout.NumpyArray(np.arange(10))
     layout = ak.layout.IndexedArray64(index, content)
