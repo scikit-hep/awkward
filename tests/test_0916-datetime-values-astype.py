7330b440e6355d5088c90285c28fdca7541058c2
Ianna Osborne <ianna.osborne@cern.ch>
2021-06-15 11:04:33 +0200

    add a modulo units example

--- tests/test_0916-datetime-values-astype.py
+++ tests/test_0916-datetime-values-astype.py
@@ -20,3 +20,13 @@ def test_values_astype_datetime():
         ak.Array([1567416600000000, None]), np.datetime64  # default unit is 'us'
     )
     assert array3.to_list() == [np.datetime64("2019-09-02T09:30:00"), None]
+
+
+def test_modulo_units():
+    array1 = ak.values_astype(ak.Array([1]), np.dtype("datetime64[100as/1]"))
+    assert array1.to_list() == [
+        np.datetime64("1970-01-01T00:00:00.000000000000000100", "100as")
+    ]
+
+    array2 = ak.values_astype(ak.Array([1]), np.dtype("datetime64[10s/2]"))
+    assert array2.to_list() == [np.datetime64("1970-01-01T00:00:05.000", "5000ms")]
