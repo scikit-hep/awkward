3242bc67b772956d9da15ea9044ca4047c75fb16
Jim Pivarski <jpivarski@gmail.com>
2021-06-28 14:31:58 -0500

    Now generating _cpu_kernels_signatures.py.

--- dev/generate-kernel-signatures.py
+++ dev/generate-kernel-signatures.py
@@ -9,16 +9,16 @@ CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def type_to_ctype(typename):
-    flag = False
+    is_const = False
     if "Const[" in typename:
-        flag = True
+        is_const = True
         typename = typename[len("Const[") : -1]
-    arraycount = 0
+    count = 0
     while "List[" in typename:
-        arraycount += 1
+        count += 1
         typename = typename[len("List[") : -1]
-    typename = typename + "*" * arraycount
-    if flag:
+    typename = typename + "*" * count
+    if is_const:
         typename = "const " + typename
     return typename
 
@@ -73,7 +73,119 @@ extern "C" {{
     print("Done with  include/awkward/kernels.h.")
 
 
+type_to_dtype = {
+    "bool": "bool_",
+    "int8": "int8",
+    "uint8": "uint8",
+    "int16": "int16",
+    "uint16": "uint16",
+    "int32": "int32",
+    "uint32": "uint32",
+    "int64": "int64",
+    "uint64": "uint64",
+    "float": "float32",
+    "double": "float64",
+}
+
+
+def type_to_pytype(typename, special):
+    if "Const[" in typename:
+        typename = typename[len("Const[") : -1]
+    count = 0
+    while "List[" in typename:
+        count += 1
+        typename = typename[len("List[") : -1]
+    if typename.endswith("_t"):
+        typename = typename[:-2]
+    if count != 0:
+        special.append(type_to_dtype[typename])
+    return ("POINTER(" * count) + ("c_" + typename) + (")" * count)
+
+
+def cpu_kernels_signatures_py(specification):
+    print("Generating src/awkward/_cpu_kernels_signatures.py...")
+
+    with open(
+        os.path.join(CURRENT_DIR, "..", "src", "awkward", "_cpu_kernels_signatures.py"), "w"
+    ) as file:
+        file.write("""# AUTO GENERATED ON {0}
+# DO NOT EDIT BY HAND!
+#
+# To regenerate file, run
+#
+#     python dev/generate-kernel-signatures.py
+#
+# (It is usually run as part of pip install . or localbuild.py.)
+
+# fmt: off
+
+from ctypes import (
+    POINTER,
+    Structure,
+    c_bool,
+    c_int8,
+    c_uint8,
+    c_int16,
+    c_uint16,
+    c_int32,
+    c_uint32,
+    c_int64,
+    c_uint64,
+    c_float,
+    c_double,
+    c_char_p,
+)
+
+import numpy as np
+
+from numpy import (
+    bool_,
+    int8,
+    uint8,
+    int16,
+    uint16,
+    int32,
+    uint32,
+    int64,
+    uint64,
+    float32,
+    float64,
+)
+
+class ERROR(Structure):
+    _fields_ = [
+        ("str", c_char_p),
+        ("fliename", c_char_p),
+        ("id", c_int64),
+        ("attempt", c_int64),
+        ("pass_through", c_bool),
+    ]
+
+
+def by_signature(lib):
+    out = {{}}
+""".format(datetime.datetime.now().isoformat().replace("T", " AT ")[:22]))
+
+        for spec in specification["kernels"]:
+            for childfunc in spec["specializations"]:
+                special = [repr(spec["name"])]
+                arglist = [type_to_pytype(x["type"], special) for x in childfunc["args"]]
+                file.write("""
+    f = lib.{0}
+    f.argtypes = [{1}]
+    f.restype = ERROR
+    out[{2}] = f
+""".format(childfunc["name"], ", ".join(arglist), ", ".join(special)))
+
+        file.write("""
+    return out
+""")
+
+    print("Done with  src/awkward/_cpu_kernels_signatures.py...")
+
+
 if __name__ == "__main__":
     with open(os.path.join(CURRENT_DIR, "..", "kernel-specification.yml")) as specfile:
         specification = yaml.safe_load(specfile)
         include_kernels_h(specification)
+        cpu_kernels_signatures_py(specification)
