86f1831dad5e6d0445e5cf3fd57b548f7df35a25
Ianna Osborne <ianna.osborne@cern.ch>
2021-07-09 12:09:06 +0200

    move c++ calls to a separate file that can be safely deleted when the c++ tests are deleted and the json parser is refactored Please enter the commit message for your changes. Lines starting

---
+++ tests/test_0977-array-builder.py
@@ -0,0 +1,29 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+from __future__ import absolute_import
+
+import pytest  # noqa: F401
+import numpy as np  # noqa: F401
+import awkward as ak  # noqa: F401
+
+import awkward.forth
+
+
+def test_array_builder():
+    builder = ak.ArrayBuilder()
+
+    builder.real(1.1)
+    builder.real(2.2)
+    builder.real(3.3)
+
+    assert ak.to_list(builder.snapshot()) == [1.1, 2.2, 3.3]
+
+
+def test_array_bool_builder():
+    builder = ak.ArrayBuilder()
+
+    builder.boolean(True)
+    builder.boolean(False)
+    builder.boolean(True)
+
+    assert ak.to_list(builder.snapshot()) == [True, False, True]
