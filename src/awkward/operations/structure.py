eb61e07f52e06708d6c6d4238014218fd5288eb7
Angus Hollands <goosey15@gmail.com>
2021-07-20 16:34:27 +0100

    Docs: make link to layout (#1023)

--- src/awkward/operations/structure.py
+++ src/awkward/operations/structure.py
@@ -2355,7 +2355,7 @@ def packed(array, highlevel=True, behavior=None):
     - #ak.layout.BitMaskedArray becomes an #ak.layout.IndexedOptionArray if it contains records, stays a #ak.layout.BitMaskedArray otherwise
     - #ak.layout.UnionArray gets projected contents
     - #ak.layout.VirtualArray gets materialized
-    - #ak.layout.Record becomes a record over a single-item RecordArray
+    - #ak.layout.Record becomes a record over a single-item #ak.layout.RecordArray
 
     Example:
 
