9a7338133ace947d821878b884ebcb8c2d345517
SantamRC <santamdev404@gmail.com>
2021-07-29 14:32:33 +0530

    [skip ci] minor change in Identities.cpp

--- src/libawkward/Identities.cpp
+++ src/libawkward/Identities.cpp
@@ -17,6 +17,7 @@
 #include "awkward/Identities.h"
 
 namespace awkward {
+  void printMe();
   std::atomic<Identities::Ref> numrefs{0};
 
   Identities::Ref
@@ -265,6 +266,7 @@ namespace awkward {
         carry.length(),
         width_,
         length_);
+      std::cout<<"the result: "<<reinterpret_cast<int32_t*>(rawout->printMe())<<std::endl;
       util::handle_error(err, classname(), nullptr);
     }
     else if (std::is_same<T, int64_t>::value) {
@@ -284,6 +286,13 @@ namespace awkward {
     }
 
     return out;
+  }
+  template <typename T>
+  void
+  IdentitiesOf<T>::printMe() {
+      for (int64_t i = 0; i < length(); i++) {
+        std::cout << data()[i] << ", ";
+      }
   }
 
   const std::string
