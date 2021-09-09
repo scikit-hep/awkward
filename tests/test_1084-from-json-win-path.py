9dda72d2f302d58b54788eec4312db79ee272452
Angus Hollands <goosey15@gmail.com>
2021-09-09 19:39:12 +0100

    Test: add test for #1084

---
+++ tests/test_1084-from-json-win-path.py
@@ -0,0 +1,19 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+from __future__ import absolute_import
+import pytest  # noqa: F401
+import numpy as np  # noqa: F401
+import awkward as ak  # noqa: F401
+import urllib.request
+import urllib.error
+
+
+@pytest.mark.skipif(not ak._util.win, reason="requires Windows")
+def test():
+    url = "https://raw.githubusercontent.com/Chicago/osd-bike-routes/master/data/Bikeroutes.geojson"
+    try:
+        bikeroutes_json = urllib.request.urlopen(url).read()
+    except urllib.error.URLError:
+        pytest.skip(reason="couldn't download sample dataset")
+
+    # This shouldn't fail (see #1084)
+    ak.from_json(bikeroutes_json)
