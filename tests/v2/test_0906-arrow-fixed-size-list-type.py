26f5ef531226d66bfd541d6b765f3819565ba740
Jim Pivarski <jpivarski@gmail.com>
2021-11-02 16:47:11 -0500

    Uncommented test_0906, and that's the end of this PR.

--- tests/v2/test_0906-arrow-fixed-size-list-type.py
+++ tests/v2/test_0906-arrow-fixed-size-list-type.py
@@ -9,29 +9,31 @@ import awkward as ak  # noqa: F401
 pyarrow = pytest.importorskip("pyarrow")
 
 
-# def test():
-#     struct = pyarrow.struct(
-#         [
-#             pyarrow.field("x", pyarrow.list_(pyarrow.float64(), 2)),
-#             pyarrow.field("y", pyarrow.list_(pyarrow.float64(), 2)),
-#         ]
-#     )
-#     array = pyarrow.array(
-#         [
-#             {"x": [1.1, 2.1], "y": [3.1, 4.1]},
-#             {"x": [1.2, 2.2], "y": [3.2, 4.2]},
-#             {"x": [1.3, 2.3], "y": [3.3, 4.3]},
-#         ],
-#         type=struct,
-#     )
-#     as_awkward = ak.from_arrow(array)
+def test():
+    import awkward._v2._connect.pyarrow
 
-#     assert ak.to_list(as_awkward) == [
-#         {"x": [1.1, 2.1], "y": [3.1, 4.1]},
-#         {"x": [1.2, 2.2], "y": [3.2, 4.2]},
-#         {"x": [1.3, 2.3], "y": [3.3, 4.3]},
-#     ]
-#     assert (
-#         str(as_awkward.type)
-#         == '3 * {"x": option[2 * ?float64], "y": option[2 * ?float64]}'
-#     )
+    struct = pyarrow.struct(
+        [
+            pyarrow.field("x", pyarrow.list_(pyarrow.float64(), 2)),
+            pyarrow.field("y", pyarrow.list_(pyarrow.float64(), 2)),
+        ]
+    )
+    array = pyarrow.array(
+        [
+            {"x": [1.1, 2.1], "y": [3.1, 4.1]},
+            {"x": [1.2, 2.2], "y": [3.2, 4.2]},
+            {"x": [1.3, 2.3], "y": [3.3, 4.3]},
+        ],
+        type=struct,
+    )
+    as_awkward = awkward._v2._connect.pyarrow.handle_arrow(array)
+
+    assert ak.to_list(as_awkward) == [
+        {"x": [1.1, 2.1], "y": [3.1, 4.1]},
+        {"x": [1.2, 2.2], "y": [3.2, 4.2]},
+        {"x": [1.3, 2.3], "y": [3.3, 4.3]},
+    ]
+    assert (
+        str(as_awkward.form.type)
+        == "{x: option[2 * ?float64], y: option[2 * ?float64]}"
+    )
