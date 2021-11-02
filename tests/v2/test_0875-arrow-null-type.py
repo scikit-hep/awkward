f8a601e825d698f6f7ebc95b848f4344052dea69
Jim Pivarski <jpivarski@gmail.com>
2021-11-02 16:40:47 -0500

    Uncommented test_0875.

--- tests/v2/test_0875-arrow-null-type.py
+++ tests/v2/test_0875-arrow-null-type.py
@@ -2,29 +2,39 @@
 
 from __future__ import absolute_import
 
-# import os
+import os
 
 import pytest  # noqa: F401
 import numpy as np  # noqa: F401
 import awkward as ak  # noqa: F401
 
+from awkward._v2.tmp_for_testing import v1_to_v2
 
 pyarrow = pytest.importorskip("pyarrow")
 
 
-# def test_issue1():
-#     null_array = ak.Array({"null_col": [None]})
-#     tpe = ak.to_arrow_table(null_array).to_batches()[0].field("null_col").type
-#     assert tpe == pyarrow.null()
+def test_issue1():
+    null_array = v1_to_v2(ak.Array({"null_col": [None]}).layout)
+    tpe = null_array.to_arrow().storage.field("null_col").type
+    assert tpe.storage_type == pyarrow.null()
 
 
-# pyarrow_parquet = pytest.importorskip("pyarrow.parquet")
+pyarrow_parquet = pytest.importorskip("pyarrow.parquet")
 
 
-# def test_issue2(tmp_path):
-#     filename = os.path.join(tmp_path, "whatever.parquet")
+def test_issue2(tmp_path):
+    import awkward._v2._connect.pyarrow
 
-#     null_table = pyarrow.Table.from_pydict({"null_col": pyarrow.array([None])})
-#     pyarrow_parquet.write_table(null_table, filename)
+    filename = os.path.join(tmp_path, "whatever.parquet")
 
-#     assert ak.from_parquet(filename).type == ak.from_parquet(filename, lazy=True).type
+    null_table = pyarrow.Table.from_pydict({"null_col": pyarrow.array([None])})
+    pyarrow_parquet.write_table(null_table, filename)
+
+    assert (
+        str(
+            awkward._v2._connect.pyarrow.handle_arrow(
+                pyarrow_parquet.read_table(filename)
+            ).form.type
+        )
+        == "{null_col: ?unknown}"
+    )
