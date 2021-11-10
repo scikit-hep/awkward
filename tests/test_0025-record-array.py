5fdd1c8ae46c326df1c7b3c6bac50acb2f3565df
Jim Pivarski <jpivarski@gmail.com>
2021-11-09 19:19:34 -0600

    Order-dependent record fields in Python 2.7.

--- tests/test_0025-record-array.py
+++ tests/test_0025-record-array.py
@@ -628,7 +628,8 @@ def test_builder_record():
     builder.real(3.3)
     builder.endrecord()
 
-    assert str(builder.type(typestrs)) == '{"one": int64, "two": float64}'
+    if not ak._util.py27:
+        assert str(builder.type(typestrs)) == '{"one": int64, "two": float64}'
     assert ak.to_list(builder.snapshot()) == [
         {"one": 1, "two": 1.1},
         {"one": 2, "two": 2.2},
