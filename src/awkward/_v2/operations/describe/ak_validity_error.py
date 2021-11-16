d5ab86a29accae128f7ac204577f3f202eab1593
Ianna Osborne <ianna.osborne@cern.ch>
2021-11-16 17:40:18 +0100

    enable validity error check

--- src/awkward/_v2/operations/describe/ak_validity_error.py
+++ src/awkward/_v2/operations/describe/ak_validity_error.py
@@ -4,53 +4,30 @@ from __future__ import absolute_import
 
 import awkward as ak
 
-np = ak.nplike.NumpyMetadata.instance()
-
 
 def validity_error(array, exception=False):
-    raise NotImplementedError
+    """
+    Args:
+        array (#ak.Array, #ak.Record, #ak.layout.Content, #ak.layout.Record, #ak.ArrayBuilder, #ak.layout.ArrayBuilder):
+            Array or record to check.
+        exception (bool): If True, validity errors raise exceptions.
 
+    Returns None (or an empty string) if there are no errors and a str containing the error message
+    if there are.
 
-#     """
-#     Args:
-#         array (#ak.Array, #ak.Record, #ak.layout.Content, #ak.layout.Record, #ak.ArrayBuilder, #ak.layout.ArrayBuilder):
-#             Array or record to check.
-#         exception (bool): If True, validity errors raise exceptions.
+    Checks for errors in the structure of the array, such as indexes that run
+    beyond the length of a node's `content`, etc. Either an error is raised or
+    a string describing the error is returned.
 
-#     Returns None if there are no errors and a str containing the error message
-#     if there are.
+    See also #ak.is_valid.
+    """
+    layout = ak._v2.operations.convert.to_layout(
+        array, allow_record=False, allow_other=False
+    )
+    out = layout.validityerror(path="layout")
 
-#     Checks for errors in the structure of the array, such as indexes that run
-#     beyond the length of a node's `content`, etc. Either an error is raised or
-#     a string describing the error is returned.
-
-#     See also #ak.is_valid.
-#     """
-#     if isinstance(array, (ak._v2.highlevel.Array, ak._v2.highlevel.Record)):
-#         return validity_error(array.layout, exception=exception)
-
-#     elif isinstance(array, ak._v2.highlevel.ArrayBuilder):
-#         return validity_error(array.snapshot().layout, exception=exception)
-
-#     elif isinstance(
-#         array,
-#         (
-#             ak._v2.contents.Content,
-#             ak._v2.record.Record,
-#             ak.partition.PartitionedArray,   # NO PARTITIONED ARRAY
-#         ),
-#     ):
-#         out = array.validityerror()
-#         if out is not None and exception:
-#             raise ValueError(out)
-#         else:
-#             return out
-
-#     elif isinstance(array, ak.layout.ArrayBuilder):
-#         return validity_error(array.snapshot(), exception=exception)
-
-#     else:
-#         raise TypeError(
-#             "not an awkward array: {0}".format(repr(array))
-#
-#         )
+    # FIXME?: should it return None instead of an empty string?
+    if out not in (None, "") and exception:
+        raise ValueError(out)
+    else:
+        return out
--- tests/v2/test_0138-emptyarray-type.py
+++ tests/v2/test_0138-emptyarray-type.py
@@ -12,7 +12,7 @@ pytestmark = pytest.mark.skipif(
 
 
 def test():
-    empty1 = ak._v2.highlevel.Array(ak._v2.contents.EmptyArray(), check_valid=False)
+    empty1 = ak._v2.highlevel.Array(ak._v2.contents.EmptyArray(), check_valid=True)
     empty2 = ak._v2.highlevel.Array(
         ak._v2.contents.ListOffsetArray(
             ak._v2.index.Index64(np.array([0, 0, 0, 0], dtype=np.int64)),
@@ -20,7 +20,7 @@ def test():
         ),
         check_valid=False,
     )
-    array = ak._v2.highlevel.Array([[1.1, 2.2, 3.3], [], [4.4, 5.5]], check_valid=False)
+    array = ak._v2.highlevel.Array([[1.1, 2.2, 3.3], [], [4.4, 5.5]], check_valid=True)
 
     assert ak._v2.operations.convert.to_numpy(empty1).dtype.type is np.float64
 
