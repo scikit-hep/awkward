fcd2df589526cb5d940395147bbc179177e8b885
Ianna Osborne <ianna.osborne@cern.ch>
2021-11-30 09:18:25 +0100

    move common classes to a common header

--- src/python/io.cpp
+++ src/python/io.cpp
@@ -13,60 +13,36 @@
 #include "awkward/python/io.h"
 
 namespace ak = awkward;
-namespace {
-  class NumpyBuffersContainer: public ak::BuffersContainer {
-  public:
-    py::dict container() {
-      return container_;
-    }
-
-    void
-      copy_buffer(const std::string& name, const void* source, int64_t num_bytes) override {
-        py::object pyarray = py::module::import("numpy").attr("empty")(num_bytes, "u1");
-        py::array_t<uint8_t> rawarray = pyarray.cast<py::array_t<uint8_t>>();
-        py::buffer_info rawinfo = rawarray.request();
-        std::memcpy(rawinfo.ptr, source, num_bytes);
-        container_[py::str(name)] = pyarray;
-      }
-
-    void
-      full_buffer(const std::string& name, int64_t length, int64_t value, const std::string& dtype) override {
-        py::object pyarray = py::module::import("numpy").attr("full")(py::int_(length), py::int_(value), py::str(dtype));
-        container_[py::str(name)] = pyarray;
-      }
-
-  private:
-    py::dict container_;
-  };
-
-  /// @brief Turns the accumulated data into a Content array.
-  ///
-  /// This operation only converts Builder nodes into Content nodes; the
-  /// buffers holding array data are shared between the Builder and the
-  /// Content. Hence, taking a snapshot is a constant-time operation.
-  ///
-  /// It is safe to take multiple snapshots while accumulating data. The
-  /// shared buffers are only appended to, which affects elements beyond
-  /// the limited view of old snapshots.
-  py::object
-  builder_snapshot(const ak::BuilderPtr builder) {
-    ::NumpyBuffersContainer container;
-    int64_t form_key_id = 0;
-    std::string form = builder.get()->to_buffers(container, form_key_id);
-    py::dict kwargs;
-    kwargs[py::str("form")] = py::str(form);
-    kwargs[py::str("length")] = py::int_(builder.get()->length());
-    kwargs[py::str("container")] = container.container();
-    kwargs[py::str("key_format")] = py::str("{form_key}-{attribute}");
-    kwargs[py::str("highlevel")] = py::bool_(false);
-    return py::module::import("awkward").attr("from_buffers")(**kwargs);
-  }
-}
+// namespace {
+//   /// @brief Turns the accumulated data into a Content array.
+//   ///
+//   /// This operation only converts Builder nodes into Content nodes; the
+//   /// buffers holding array data are shared between the Builder and the
+//   /// Content. Hence, taking a snapshot is a constant-time operation.
+//   ///
+//   /// It is safe to take multiple snapshots while accumulating data. The
+//   /// shared buffers are only appended to, which affects elements beyond
+//   /// the limited view of old snapshots.
+//   py::object
+//   builder_snapshot(const ak::BuilderPtr builder) {
+//     ::NumpyBuffersContainer container;
+//     int64_t form_key_id = 0;
+//     std::string form = builder.get()->to_buffers(container, form_key_id);
+//     py::dict kwargs;
+//     kwargs[py::str("form")] = py::str(form);
+//     kwargs[py::str("length")] = py::int_(builder.get()->length());
+//     kwargs[py::str("container")] = container.container();
+//     kwargs[py::str("key_format")] = py::str("{form_key}-{attribute}");
+//     kwargs[py::str("highlevel")] = py::bool_(false);
+//     return py::module::import("awkward").attr("from_buffers")(**kwargs);
+//   }
+// }
 
 ////////// fromjson
 
 void
 make_fromjson(py::module& m, const std::string& name) {
+  std::cout << "make_fromjson\n";
   m.def(name.c_str(),
         [](const std::string& source,
            const char* nan_string,
