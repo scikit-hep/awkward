9629b641b447060ec508155e0e62a124ad9960ee
Jim Pivarski <jpivarski@users.noreply.github.com>
2021-12-07 16:19:23 -0600

    Fix leading zeros in ak.unflatten. (#1178)

---
+++ tests/test_0905-leading-zeros-in-unflatten.py
@@ -0,0 +1,14 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+from __future__ import absolute_import
+
+import pytest  # noqa: F401
+import numpy as np  # noqa: F401
+import awkward as ak  # noqa: F401
+
+
+def test():
+    array = ak.Array([[1, 2, 3]])
+    assert ak.unflatten(array, [2, 1], axis=1).tolist() == [[[1, 2], [3]]]
+    assert ak.unflatten(array, [0, 2, 1], axis=1).tolist() == [[[], [1, 2], [3]]]
+    assert ak.unflatten(array, [0, 0, 2, 1], axis=1).tolist() == [[[], [], [1, 2], [3]]]
