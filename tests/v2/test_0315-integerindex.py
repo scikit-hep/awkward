6a0c3bd3be4430e32819e748c3da606a187ebc13
Jim Pivarski <jpivarski@gmail.com>
2022-01-10 17:25:05 -0600

    Done through test_0449-merge-many-arrays-in-one-pass.py.

--- tests/v2/test_0315-integerindex.py
+++ tests/v2/test_0315-integerindex.py
@@ -6,23 +6,15 @@ import pytest  # noqa: F401
 import numpy as np  # noqa: F401
 import awkward as ak  # noqa: F401
 
-from awkward._v2.tmp_for_testing import v1_to_v2
-
 to_list = ak._v2.operations.convert.to_list
 
 
 def test_integerindex_null():
-    a = ak.Array([[0, 1, 2], None, [5, 6], [7]])
-    b = ak.Array([[0, 1, 2], [3, 4], [5, 6], [7]])
-    c = ak.Array([[1], [1], [0], [0]])
-    d = ak.Array([[1], None, [0], [0]])
-    e = ak.Array([[1], None, None, [0]])
-
-    a = v1_to_v2(a.layout)
-    b = v1_to_v2(b.layout)
-    c = v1_to_v2(c.layout)
-    d = v1_to_v2(d.layout)
-    e = v1_to_v2(e.layout)
+    a = ak._v2.highlevel.Array([[0, 1, 2], None, [5, 6], [7]]).layout
+    b = ak._v2.highlevel.Array([[0, 1, 2], [3, 4], [5, 6], [7]]).layout
+    c = ak._v2.highlevel.Array([[1], [1], [0], [0]]).layout
+    d = ak._v2.highlevel.Array([[1], None, [0], [0]]).layout
+    e = ak._v2.highlevel.Array([[1], None, None, [0]]).layout
 
     assert to_list(a[c]) == [[1], None, [5], [7]]
     assert to_list(a[d]) == [[1], None, [5], [7]]
@@ -40,18 +32,13 @@ def test_integerindex_null():
 
 
 def test_boolindex_null():
-    a = ak.Array([[0, 1, 2], None, [5, 6]])
-    b = ak.Array([[0, 1, 2], [3, 4], [5, 6]])
-    c = ak.Array([[False, True, False], [False, True], [True, False]])
-    d = ak.Array([[False, True, False], None, [True, False]])
-    e = ak.Array([[False, True, False], None, None])
-    b2 = b.mask[[True, False, True]]
-
-    a = v1_to_v2(a.layout)
-    b = v1_to_v2(b.layout)
-    c = v1_to_v2(c.layout)
-    d = v1_to_v2(d.layout)
-    e = v1_to_v2(e.layout)
+    a = ak._v2.highlevel.Array([[0, 1, 2], None, [5, 6]]).layout
+    b = ak._v2.highlevel.Array([[0, 1, 2], [3, 4], [5, 6]]).layout
+    c = ak._v2.highlevel.Array(
+        [[False, True, False], [False, True], [True, False]]
+    ).layout
+    d = ak._v2.highlevel.Array([[False, True, False], None, [True, False]]).layout
+    e = ak._v2.highlevel.Array([[False, True, False], None, None]).layout
 
     assert to_list(a[c]) == [[1], None, [5]]
     assert to_list(a[d]) == [[1], None, [5]]
@@ -67,7 +54,11 @@ def test_boolindex_null():
     assert b.typetracer[d].form == b[d].form
     assert b.typetracer[e].form == b[e].form
 
-    b2 = v1_to_v2(b2.layout)
+    b2 = ak._v2.contents.ByteMaskedArray(
+        ak._v2.index.Index8([True, False, True]),
+        b,
+        valid_when=True,
+    )
 
     assert to_list(b2[c]) == [[1], None, [5]]
     assert to_list(b2[d]) == [[1], None, [5]]
@@ -79,15 +70,10 @@ def test_boolindex_null():
 
 
 def test_integerindex_null_more():
-    f = ak.Array([[0, None, 2], None, [3, 4], []])
-    g1 = ak.Array([[1, 2, None], None, [], [None]])
-    g2 = ak.Array([[], None, None, []])
-    g3 = ak.Array([[], [], [], []])
-
-    f = v1_to_v2(f.layout)
-    g1 = v1_to_v2(g1.layout)
-    g2 = v1_to_v2(g2.layout)
-    g3 = v1_to_v2(g3.layout)
+    f = ak._v2.highlevel.Array([[0, None, 2], None, [3, 4], []]).layout
+    g1 = ak._v2.highlevel.Array([[1, 2, None], None, [], [None]]).layout
+    g2 = ak._v2.highlevel.Array([[], None, None, []]).layout
+    g3 = ak._v2.highlevel.Array([[], [], [], []]).layout
 
     assert to_list(f[g1]) == [[None, 2, None], None, [], [None]]
     assert to_list(f[g2]) == [[], None, None, []]
@@ -96,43 +82,38 @@ def test_integerindex_null_more():
     assert f.typetracer[g2].form == f[g2].form
     assert f.typetracer[g3].form == f[g3].form
 
-    a = ak.Array([[0, 1, 2, None], None])
-    b = ak.Array([[2, 1, None, 3], None])
-
-    a = v1_to_v2(a.layout)
-    b = v1_to_v2(b.layout)
+    a = ak._v2.highlevel.Array([[0, 1, 2, None], None]).layout
+    b = ak._v2.highlevel.Array([[2, 1, None, 3], None]).layout
 
     assert to_list(a[b]) == [[2, 1, None, None], None]
     assert a.typetracer[b].form == a[b].form
 
-    b = ak.Array([[2, 1, None, 3], []])
-    b = v1_to_v2(b.layout)
+    b = ak._v2.highlevel.Array([[2, 1, None, 3], []]).layout
 
     assert to_list(a[b]) == [[2, 1, None, None], None]
     assert a.typetracer[b].form == a[b].form
 
-    b = ak.Array([[2, 1, None, 3], [0, 1]])
-    b = v1_to_v2(b.layout)
+    b = ak._v2.highlevel.Array([[2, 1, None, 3], [0, 1]]).layout
     assert to_list(a[b]) == [[2, 1, None, None], None]
     assert a.typetracer[b].form == a[b].form
 
 
 @pytest.mark.skip(reason="FIXME: erroneous slicing case")
 def test_integerindex_null_more_2():
-    a = ak.Array([[[0, 1, 2, None], None], [[3, 4], [5]], None, [[6]]])
-    b = ak.Array([[[2, 1, None, 3], [0, 1]], [[0], None], None, [None]])
-    c = ak.Array(
+    a = ak._v2.highlevel.Array(
+        [[[0, 1, 2, None], None], [[3, 4], [5]], None, [[6]]]
+    ).layout
+    b = ak._v2.highlevel.Array(
+        [[[2, 1, None, 3], [0, 1]], [[0], None], None, [None]]
+    ).layout
+    c = ak._v2.highlevel.Array(
         [
             [[False, True, None, False], [False, True]],
             [[True, False], None],
             None,
             [None],
         ]
-    )
-
-    a = v1_to_v2(a.layout)
-    b = v1_to_v2(b.layout)
-    c = v1_to_v2(c.layout)
+    ).layout
 
     assert to_list(a[b]) == [
         [[2, 1, None, None], None],
@@ -145,24 +126,19 @@ def test_integerindex_null_more_2():
 
 
 def test_silly_stuff():
-    a = ak.Array([[0, 1, 2], 3])
+    a = ak._v2.highlevel.Array([[0, 1, 2], 3]).layout
     b = [[2], [0]]
-
-    a = v1_to_v2(a.layout)
 
     with pytest.raises(IndexError):
         a[b]
 
-    a = ak.Array([[0, 1, 2], [3, 4], [5, 6], [7]])
-    a = v1_to_v2(a.layout)
+    a = ak._v2.highlevel.Array([[0, 1, 2], [3, 4], [5, 6], [7]]).layout
 
-    b = ak.Array([[0, 2], None, [1], None])
-    b = v1_to_v2(b.layout)
+    b = ak._v2.highlevel.Array([[0, 2], None, [1], None]).layout
 
     assert to_list(a[b]) == [[0, 2], None, [6], None]
 
-    b = ak.Array([[0, 2], None, None, None, None, None])
-    b = v1_to_v2(b.layout)
+    b = ak._v2.highlevel.Array([[0, 2], None, None, None, None, None]).layout
 
     with pytest.raises(IndexError):
         a[b]
