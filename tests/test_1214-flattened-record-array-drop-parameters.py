a42b0481fe6a5a15ec56a3228b6d2394e9ecaeb7
Angus Hollands <goosey15@gmail.com>
2022-01-11 23:01:15 +0000

    Test: expand test cases to handle v1

--- tests/test_1214-flattened-record-array-drop-parameters.py
+++ tests/test_1214-flattened-record-array-drop-parameters.py
@@ -4,24 +4,20 @@ from __future__ import absolute_import
 
 import pytest  # noqa: F401
 import numpy as np  # noqa: F401
-import awkward._v2 as ak  # noqa: F401
+import awkward as ak  # noqa: F401
+from awkward._v2.tmp_for_testing import v1_to_v2
 
 
 def test():
-    a = ak.contents.RecordArray(
-        [
-            ak.contents.RegularArray(
-                ak.contents.RegularArray(
-                    ak.contents.NumpyArray(np.arange(4 * 3 * 2)), 4
-                ),
-                3,
-            )
-        ],
-        None,
-        2,
-        parameters={"__record__": "recordname"},
+    array = ak.Array(
+        [[{"x": [[1, 2], []]}, {"x": []}, {"x": [[3]]}]], with_name="Record"
     )
-    assert a.parameters.get("__record__") == "recordname"
-    b = a.flatten(axis=2)
+    array_v2 = ak._v2.highlevel.Array(v1_to_v2(array.layout))
 
-    assert "__record__" not in b.parameters
+    # Flattening inside record does not drop parameters
+    assert "__record__" not in ak.parameters(ak.flatten(array, axis=3))
+    assert "__record__" not in array_v2.layout.flatten(axis=3).parameters
+
+    # Flattening before record does not drop parameters
+    assert "__record__" in ak.parameters(ak.flatten(array, axis=1))
+    assert "__record__" in array_v2.layout.flatten(axis=1).parameters
