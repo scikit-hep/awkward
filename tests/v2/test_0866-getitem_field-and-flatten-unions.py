ebdfda157b90fba44e4ea42ab7acb35d0c4883ab
Ioana Ifrim <ioana.ifrim@gmail.com>
2022-01-13 02:29:53 -0500

    Pre-commit fixes

--- tests/v2/test_0866-getitem_field-and-flatten-unions.py
+++ tests/v2/test_0866-getitem_field-and-flatten-unions.py
@@ -8,12 +8,14 @@ import awkward as ak  # noqa: F401
 
 to_list = ak._v2.operations.convert.to_list
 
+
 @pytest.mark.skip(reason="FIXME: ak.zip needs to be implemented")
 def test_getitem_field():
     a1 = ak.zip({"a": [[1], [], [2, 3]], "b": [[4], [], [5, 6]]}, with_name="a1")
     a2 = ak.zip({"a": [[7, 8], [9], []], "b": [[10, 11], [12], []]}, with_name="a2")
     union = ak.where([True, False, True], a1, a2)
     assert str(union.a.type) == "3 * var * int64"
+
 
 @pytest.mark.skip(reason="FIXME: ak.zip needs to be implemented")
 def test_flatten_axis_none():
