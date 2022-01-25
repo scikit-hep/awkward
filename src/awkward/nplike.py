b26770afe90b6562acc7029d584acf903628e65d
Angus Hollands <goosey15@gmail.com>
2022-01-25 00:45:28 +0000

    Refactor: special-case np.ndarray

--- src/awkward/nplike.py
+++ src/awkward/nplike.py
@@ -400,10 +400,12 @@ class NumpyKernel:
 
 class Numpy(NumpyLike):
     def to_rectilinear(self, array, *args, **kwargs):
-        if isinstance(
+        if isinstance(array, numpy.ndarray):
+            return array
+
+        elif isinstance(
             array,
             (
-                numpy.ndarray,
                 ak.Array,
                 ak.Record,
                 ak.ArrayBuilder,
