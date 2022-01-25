2f80d3e924b33bc254ffd9585663acd25e6386dc
Anish Biswas <anishbiswas271@gmail.com>
2022-01-25 09:11:49 +0100

    Do we need Docker if we can setup cuda-toolkit in GHA?

--- cuda-build.sh
+++ cuda-build.sh
@@ -40,7 +40,7 @@ echo "docker_image ='docker.io/nvidia/cuda:"$DOCKER_IMAGE_TAG"'" >> build/awkwar
 export DOCKER_ARGS="-v`pwd`:/home -w/home docker.io/nvidia/cuda:"$DOCKER_IMAGE_TAG
 export BUILD_SHARED_LIBRARY="nvcc -std=c++11 -Xcompiler -fPIC -Xcompiler -DVERSION_INFO="$AWKWARD_VERSION" -Iinclude src/cuda-kernels/*.cu --shared -o build/awkward_cuda_kernels/libawkward-cuda-kernels.so"
 
-docker run $DOCKER_ARGS $BUILD_SHARED_LIBRARY
+# docker run $DOCKER_ARGS $BUILD_SHARED_LIBRARY
 
 cat > build/cuda-setup.py << EOF
 
