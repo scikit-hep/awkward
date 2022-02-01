d1ba834049e4f60c793718860d910c0b91587293
Anish Biswas <anishbiswas271@gmail.com>
2022-02-01 12:17:52 +0100

    Add minimal test

---
+++ tests-cuda/test_1270-install-test.py
@@ -0,0 +1,17 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import pytest  # noqa: F401
+import ctypes
+
+def test_import_and_loading_shared_lib():
+    try:
+        import awkward_cuda_kernels
+    except ModuleNotFoundError:
+        pytest.fail("Could not import awkward_cuda_kernels")
+
+    try:
+        import ctypes
+        ctypes.cdll.LoadLibrary(awkward_cuda_kernels.shared_library_path)
+    except Exception:
+        pytest.fail("Could not load the shared library")
+
