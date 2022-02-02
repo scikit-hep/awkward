19ff48303b8e94fc6e32e17839636a2fe222367e
Ianna Osborne <ianna.osborne@cern.ch>
2022-02-02 13:55:20 +0100

    read file as text and add encoding

--- src/awkward/_v2/operations/io/ak_from_json_file.py
+++ src/awkward/_v2/operations/io/ak_from_json_file.py
@@ -83,7 +83,7 @@ def from_json_file(
     #             out.send(block)
     #     out.close()
 
-    with open(source, "rb") as f:  # pylint: disable=unspecified-encoding
+    with open(source, encoding="locale") as f:
         builder = ak.layout.ArrayBuilder(initial=initial, resize=resize)
         # FIXME: for line in f:
         num = ak._ext.fromjson(
