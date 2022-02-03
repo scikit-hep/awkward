061f799e9765b0ad88ca1a84dc12e1343c0afe18
Anish Biswas <anishbiswas271@gmail.com>
2022-02-03 11:23:47 +0100

    Added the _cuda_kernels file

---
+++ src/awkward/_cuda_kernels.py
@@ -0,0 +1,30 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+# v2: no change; keep this file.
+
+
+import ctypes
+import platform
+
+import awkward._kernel_signatures
+
+if platform.system() == "Windows":
+    raise ValueError("There is no support for Awkward CUDA on Windows. Please use a Linux system to use CUDA accelerated Awkward Arrays.")
+elif platform.system() == "Darwin":
+    raise ValueError("There is no support for Awkward CUDA on MacOS. Please use a Linux system to use CUDA accelerated Awkward Arrays.")
+else:
+    try:
+        import awkward_cuda_kernels 
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError(
+            """to use CUDA awkward arrays in Python, install the 'awkward[cuda]' package with:
+
+    pip install awkward[cuda] --upgrade
+
+or
+
+    conda install awkward[cuda]"""
+        ) from None
+
+lib = ctypes.cdll.LoadLibrary(awkward_cuda_kernels.shared_library_path)
+kernel = awkward._kernel_signatures.by_signature(lib)
