b8263b451ef61b04df9fa94419d8841831e03d6e
ioanaif <ioana.ifrim@gmail.com>
2022-02-18 20:01:25 +0200

    C++ refactoring: ak.full_like, ak.zeros_like, ak.ones_like (#1293)
    * ak.full_like, ak.zeros_like, ak.ones_like and testing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Expanded data-type
    
    * Fixed conflict
    
    * Fixed the docstrings
    
    * Fixed 'bool' comparison
    
    * Use Numpy numeric type for 'bool
    
    * Fixed fill_value for datatypes
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/awkward/_v2/operations/structure/ak_ones_like.py
+++ src/awkward/_v2/operations/structure/ak_ones_like.py
@@ -8,23 +8,23 @@ np = ak.nplike.NumpyMetadata.instance()
 
 # @ak._v2._connect.numpy.implements("ones_like")
 def ones_like(array, highlevel=True, behavior=None, dtype=None):
-    raise NotImplementedError
 
+    """
+    Args:
+        array: Array to use as a model for a replacement that contains only `1`.
+        highlevel (bool, default is True): If True, return an #ak.Array;
+            otherwise, return a low-level #ak.layout.Content subclass.
+        behavior (None or dict): Custom #ak.behavior for the output array, if
+            high-level.
+        dtype (None or NumPy dtype): Overrides the data type of the result.
 
-#     """
-#     Args:
-#         array: Array to use as a model for a replacement that contains only `1`.
-#         highlevel (bool, default is True): If True, return an #ak.Array;
-#             otherwise, return a low-level #ak.layout.Content subclass.
-#         behavior (None or dict): Custom #ak.behavior for the output array, if
-#             high-level.
-#         dtype (None or type): Overrides the data type of the result.
+    This is the equivalent of NumPy's `np.ones_like` for Awkward Arrays.
 
-#     This is the equivalent of NumPy's `np.ones_like` for Awkward Arrays.
+    See #ak.full_like for details, and see also #ak.zeros_like.
 
-#     See #ak.full_like for details, and see also #ak.zeros_like.
-
-#     (There is no equivalent of NumPy's `np.empty_like` because Awkward Arrays
-#     are immutable.)
-#     """
-#     return full_like(array, 1, highlevel=highlevel, behavior=behavior, dtype=dtype)
+    (There is no equivalent of NumPy's `np.empty_like` because Awkward Arrays
+    are immutable.)
+    """
+    return ak._v2.operations.structure.full_like(
+        array, 1, highlevel=highlevel, behavior=behavior, dtype=dtype
+    )
--- src/awkward/_v2/operations/structure/ak_zeros_like.py
+++ src/awkward/_v2/operations/structure/ak_zeros_like.py
@@ -6,30 +6,32 @@ import awkward as ak
 np = ak.nplike.NumpyMetadata.instance()
 
 
+_ZEROS = object()
+
+
 # @ak._v2._connect.numpy.implements("zeros_like")
 def zeros_like(array, highlevel=True, behavior=None, dtype=None):
-    raise NotImplementedError
 
+    """
+    Args:
+        array: Array to use as a model for a replacement that contains only `0`.
+        highlevel (bool, default is True): If True, return an #ak.Array;
+            otherwise, return a low-level #ak.layout.Content subclass.
+        behavior (None or dict): Custom #ak.behavior for the output array, if
+            high-level.
+        dtype (None or NumPy dtype)): Overrides the data type of the result.
 
-#     """
-#     Args:
-#         array: Array to use as a model for a replacement that contains only `0`.
-#         highlevel (bool, default is True): If True, return an #ak.Array;
-#             otherwise, return a low-level #ak.layout.Content subclass.
-#         behavior (None or dict): Custom #ak.behavior for the output array, if
-#             high-level.
-#         dtype (None or type): Overrides the data type of the result.
+    This is the equivalent of NumPy's `np.zeros_like` for Awkward Arrays.
 
-#     This is the equivalent of NumPy's `np.zeros_like` for Awkward Arrays.
+    See #ak.full_like for details, and see also #ak.ones_like.
 
-#     See #ak.full_like for details, and see also #ak.ones_like.
-
-#     (There is no equivalent of NumPy's `np.empty_like` because Awkward Arrays
-#     are immutable.)
-#     """
-#     if dtype is not None:
-#         return full_like(array, 0, highlevel=highlevel, behavior=behavior, dtype=dtype)
-#     return full_like(array, _ZEROS, highlevel=highlevel, behavior=behavior, dtype=dtype)
-
-
-_ZEROS = object()
+    (There is no equivalent of NumPy's `np.empty_like` because Awkward Arrays
+    are immutable.)
+    """
+    if dtype is not None:
+        return ak._v2.operations.structure.full_like(
+            array, 0, highlevel=highlevel, behavior=behavior, dtype=dtype
+        )
+    return ak._v2.operations.structure.full_like(
+        array, _ZEROS, highlevel=highlevel, behavior=behavior, dtype=dtype
+    )
