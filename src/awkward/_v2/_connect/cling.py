8040bbc0983ea18ba91ff7d8a51d664ae66369bb
Jim Pivarski <jpivarski@gmail.com>
2022-02-18 17:50:05 -0600

    Placeholder for implementing Awkward --> C++ with Cling.

---
+++ src/awkward/_v2/_connect/cling.py
@@ -0,0 +1,6 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+# import ROOT
+
+# This is where I'll make Awkward Arrays iterable in C++.
+# It will be needed for @ianna's Awkward --> RDataFrame conversions.
