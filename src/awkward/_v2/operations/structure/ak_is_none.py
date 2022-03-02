89765bf903143ccd5fa1b603537c5f74062bbbca
Angus Hollands <goosey15@gmail.com>
2022-03-02 11:50:24 +0000

    Feat: add `is_none` for v2

--- src/awkward/_v2/operations/structure/ak_is_none.py
+++ src/awkward/_v2/operations/structure/ak_is_none.py
@@ -6,54 +6,46 @@ np = ak.nplike.NumpyMetadata.instance()
 
 
 def is_none(array, axis=0, highlevel=True, behavior=None):
-    raise NotImplementedError
+    """
+    Args:
+        array: Data to check for missing values (None).
+        axis (int): The dimension at which this operation is applied. The
+            outermost dimension is `0`, followed by `1`, etc., and negative
+            values count backward from the innermost: `-1` is the innermost
+            dimension, `-2` is the next level up, etc.
+        highlevel (bool): If True, return an #ak.Array; otherwise, return
+            a low-level #ak.layout.Content subclass.
+        behavior (None or dict): Custom #ak.behavior for the output array, if
+            high-level.
 
+    Returns an array whose value is True where an element of `array` is None;
+    False otherwise (at a given `axis` depth).
+    """
 
-#     """
-#     Args:
-#         array: Data to check for missing values (None).
-#         axis (int): The dimension at which this operation is applied. The
-#             outermost dimension is `0`, followed by `1`, etc., and negative
-#             values count backward from the innermost: `-1` is the innermost
-#             dimension, `-2` is the next level up, etc.
-#         highlevel (bool): If True, return an #ak.Array; otherwise, return
-#             a low-level #ak.layout.Content subclass.
-#         behavior (None or dict): Custom #ak.behavior for the output array, if
-#             high-level.
+    # Determine the (potentially nested) bytemask
+    def getfunction(layout, depth, depth_context, **kwargs):
+        depth_context["posaxis"] = layout.axis_wrap_if_negative(
+            depth_context["posaxis"]
+        )
+        if depth_context["posaxis"] != depth - 1:
+            return
 
-#     Returns an array whose value is True where an element of `array` is None;
-#     False otherwise (at a given `axis` depth).
-#     """
+        nplike = ak.nplike.of(layout)
 
-#     def getfunction(layout, depth, posaxis):
-#         posaxis = layout.axis_wrap_if_negative(posaxis)
-#         if posaxis == depth - 1:
-#             nplike = ak.nplike.of(layout)
-#             if isinstance(layout, ak._v2._util.optiontypes):
-#                 return lambda: ak._v2.contents.NumpyArray(
-#                     nplike.asarray(layout.bytemask()).view(np.bool_)
-#                 )
-#             elif isinstance(
-#                 layout,
-#                 (
-#                     ak._v2._util.unknowntypes,
-#                     ak._v2._util.listtypes,
-#                     ak._v2._util.recordtypes,
-#                     ak._v2.contents.NumpyArray,
-#                 ),
-#             ):
-#                 return lambda: ak._v2.contents.NumpyArray(
-#                     nplike.zeros(len(layout), dtype=np.bool_)
-#                 )
-#             else:
-#                 return posaxis
-#         else:
-#             return posaxis
+        if layout.is_OptionType:
+            return ak._v2.contents.NumpyArray(
+                nplike.asarray(layout.bytemask()).view(np.bool_)
+            )
 
-#     layout = ak._v2.operations.convert.to_layout(array)
+        elif (
+            layout.is_UnknownType
+            or layout.is_ListType
+            or layout.is_RecordType
+            or layout.is_NumpyType
+        ):
+            return ak._v2.contents.NumpyArray(nplike.zeros(len(layout), dtype=np.bool_))
 
-#     out = ak._v2._util.recursively_apply(
-#         layout, getfunction, pass_depth=True, pass_user=True, user=axis
-#     )
-
-#     return ak._v2._util.maybe_wrap_like(out, array, behavior, highlevel)
+    layout = ak._v2.operations.convert.to_layout(array)
+    behavior = ak._v2._util.behavior_of(array, behavior=behavior)
+    out = layout.recursively_apply(getfunction, depth_context={"posaxis": axis})
+    return ak._v2._util.wrap(out, behavior, highlevel)
