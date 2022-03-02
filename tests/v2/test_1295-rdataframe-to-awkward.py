966eca1521f9004833a3b1f67e3c2b50a1729078
Ianna Osborne <ianna.osborne@cern.ch>
2022-03-02 12:42:20 +0100

    test array builder functions

--- tests/v2/test_1295-rdataframe-to-awkward.py
+++ tests/v2/test_1295-rdataframe-to-awkward.py
@@ -3,31 +3,83 @@
 import pytest  # noqa: F401
 import numpy as np  # noqa: F401
 import awkward as ak  # noqa: F401
-from awkward._v2._connect.rdataframe._from_rdataframe import *
+
+import awkward._v2._connect.rdataframe._from_rdataframe  # noqa: E402
 
 
 ROOT = pytest.importorskip("ROOT")
 
-def test_from_rdf_to_awkward():
-    rdf = ROOT.RDataFrame(100)
-    array = rdf.AsAwkward()
-    assert array.tolist() == []
+compiler = ROOT.gInterpreter.Declare
 
-    rdf_x = rdf.Define("x", "gRandom->Rndm()")
-    array_x = rdf_x.AsAwkward()
-    assert (
-        str(array_x.form)
-        == """{
-    "class": "RecordArray",
-    "contents": {
-        "x": "float64"
-    }
-}"""
+
+def test_array_builder():
+    import ctypes
+
+    builder = ak.ArrayBuilder()
+
+    ROOT.gInterpreter.Declare(
+        f"""
+    #include <functional>
+
+    typedef uint8_t (*FuncPtr)(void*);
+    typedef uint8_t (*FuncIntPtr)(void*, int64_t);
+
+    uint8_t
+    test_beginlist() {{
+        return std::invoke(reinterpret_cast<FuncPtr>(reinterpret_cast<long>({ctypes.cast(ak._libawkward.ArrayBuilder_beginlist, ctypes.c_voidp).value})), reinterpret_cast<void *>({builder._layout._ptr}));
+    }}
+
+    uint8_t
+    test_endlist() {{
+        return std::invoke(reinterpret_cast<FuncPtr>(reinterpret_cast<long>({ctypes.cast(ak._libawkward.ArrayBuilder_endlist, ctypes.c_void_p).value})), reinterpret_cast<void *>({builder._layout._ptr}));
+    }}
+
+    uint8_t
+    test_integer(int64_t x) {{
+        return std::invoke(reinterpret_cast<FuncIntPtr>(reinterpret_cast<long>({ctypes.cast(ak._libawkward.ArrayBuilder_integer, ctypes.c_void_p).value})), reinterpret_cast<void *>({builder._layout._ptr}), x);
+    }}
+    """
+    )
+    ROOT.test_beginlist()
+    ROOT.test_integer(1)
+    ROOT.test_integer(2)
+    ROOT.test_integer(3)
+    ROOT.test_endlist()
+
+    assert ak.to_list(builder.snapshot()) == [[1, 2, 3]]
+
+    ROOT.test_beginlist()
+    ROOT.test_integer(1)
+    ROOT.test_integer(2)
+    ROOT.test_integer(3)
+    ROOT.test_endlist()
+
+    assert ak.to_list(builder.snapshot()) == [[1, 2, 3], [1, 2, 3]]
+
+
+def test_array_builder_root():
+    builder = ak.ArrayBuilder()
+    func = ak._v2._connect.rdataframe._from_rdataframe.connect_ArrayBuilder(
+        compiler, builder
     )
 
-    array_none = rdf_x.AsAwkward(exclude=["x"])
-    assert array_none.to_list() == []
+    getattr(ROOT, func["beginlist"])()
+    getattr(ROOT, func["integer"])(1)
+    getattr(ROOT, func["integer"])(2)
+    getattr(ROOT, func["integer"])(3)
+    getattr(ROOT, func["endlist"])()
+    getattr(ROOT, func["real"])(3.3)
 
-def test_vectors():
-    list = []
-    print(ROOT.is_iterable(list))
+    assert ak.to_list(builder.snapshot()) == [[1, 2, 3], 3.3]
+
+
+@pytest.mark.skip(reason="Failed to instantiate 'Foreach(function)'")
+def test_as_ak_array():
+    builder = ak.ArrayBuilder()
+    func = ak._v2._connect.rdataframe._from_rdataframe.connect_ArrayBuilder(
+        compiler, builder
+    )
+
+    rdf = ROOT.RDataFrame(100)
+    rdf_x = rdf.Define("x", "gRandom->Rndm()")
+    rdf_x.Foreach(lambda x: getattr(ROOT, func["real"])(x))
