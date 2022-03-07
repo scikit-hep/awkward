1471a62d56f10d1bef71b54fde8f36510f8d60ef
Aryan Roy <aryanroy5678@gmail.com>
2022-03-07 19:40:48 +0530

    Added tests skeleton

---
+++ tests/test_1345-avro-reader.py
@@ -0,0 +1,34 @@
+import pytest  # noqa: F401
+import awkward as ak  # noqa: F401
+
+
+def test_int():
+    raise NotImplementedError
+
+
+def test_boolean():
+    raise NotImplementedError
+
+
+def test_long():
+    raise NotImplementedError
+
+
+def test_float():
+    raise NotImplementedError
+
+
+def test_double():
+    raise NotImplementedError
+
+
+def test_bytes():
+    raise NotImplementedError
+
+
+def test_string():
+    raise NotImplementedError
+
+
+def test_null():
+    raise NotImplementedError
