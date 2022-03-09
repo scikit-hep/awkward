396766a494a425642f100875d3ccc08272c5c156
Angus Hollands <goosey15@gmail.com>
2022-03-09 12:13:00 +0000

    Refactor: use new `is_tuple` method

--- src/awkward/_v2/operations/describe/ak_is_tuple.py
+++ src/awkward/_v2/operations/describe/ak_is_tuple.py
@@ -22,21 +22,4 @@ def is_tuple(array):
 def _impl(array):
     layout = ak._v2.to_layout(array, allow_record=True)
 
-    if isinstance(layout, ak._v2.record.Record):
-        return layout.is_tuple
-
-    def visitor(form):
-        if form.is_RecordType:
-            return form.is_tuple
-        elif form.is_ListType or form.is_OptionType or form.is_IndexedType:
-            return visitor(form.content)
-        elif form.is_UnionType:
-            return all(visitor(x) for x in form.contents)
-        elif form.is_NumpyType or form.is_UnknownType:
-            return False
-        else:
-            raise ak._v2._util.error(
-                ValueError(f"Unexpected layout type {type(layout).__name__}")
-            )
-
-    return visitor(layout.form)
+    return layout.is_tuple
