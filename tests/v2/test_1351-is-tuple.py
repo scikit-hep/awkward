ebb49b132f7987fc88be5d4f03cf8099669592cd
Angus Hollands <goosey15@gmail.com>
2022-03-09 12:14:36 +0000

    Test: test layout.is_tuple too

--- tests/v2/test_1351-is-tuple.py
+++ tests/v2/test_1351-is-tuple.py
@@ -14,6 +14,7 @@ def test_record():
     )
 
     assert not ak._v2.is_tuple(array)
+    assert not array.layout.is_tuple
 
 
 def test_record_list():
@@ -28,6 +29,7 @@ def test_record_list():
     )
 
     assert not ak._v2.is_tuple(array)
+    assert not array.layout.is_tuple
 
 
 def test_tuple():
@@ -40,6 +42,7 @@ def test_tuple():
     )
 
     assert ak._v2.is_tuple(array)
+    assert array.layout.is_tuple
 
 
 def test_tuple_list():
@@ -54,21 +57,25 @@ def test_tuple_list():
     )
 
     assert ak._v2.is_tuple(array)
+    assert array.layout.is_tuple
 
 
 def test_list():
     array = ak._v2.Array([[10, 11, 12]])
 
     assert not ak._v2.is_tuple(array)
+    assert not array.layout.is_tuple
 
 
 def test_record_tuple():
     array = ak._v2.Array([{"x": (10,)}])
 
     assert not ak._v2.is_tuple(array)
+    assert not array.layout.is_tuple
 
 
 def test_tuple_record():
     array = ak._v2.Array([({"x": 10},)])
 
     assert ak._v2.is_tuple(array)
+    assert array.layout.is_tuple
