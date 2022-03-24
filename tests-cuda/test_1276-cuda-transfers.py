d5acfce43a31648c0c8e3b96286126cc846c06a1
Jim Pivarski <jpivarski@gmail.com>
2022-03-24 16:24:29 -0500

    Fix annoying gap at the top of all test files to trigger a new build.

--- tests-cuda/test_1276-cuda-transfers.py
+++ tests-cuda/test_1276-cuda-transfers.py
@@ -1,6 +1,5 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
 
-
 import pytest  # noqa: F401
 import numpy as np  # noqa: F401
 import cupy as cp  # noqa: F401
