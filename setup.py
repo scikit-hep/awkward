eb7404c0feff24731416da4f1e459e2f0a8521c4
Jim Pivarski <jpivarski@gmail.com>
2022-04-11 08:38:43 -0500

    Windows builds stopped working; be looser about directory name.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import re
 import shutil
 import subprocess
 import sys
+import glob
 
 import setuptools
 import setuptools.command.build_ext
@@ -206,18 +207,11 @@ class Install(setuptools.command.install.install):
 
         if platform.system() == "Windows":
             print("--- copying libraries -----------------------------------------")
-            dlldir = os.path.join(
-                os.path.join(
-                    "build",
-                    "temp.%s-%d.%d"
-                    % (
-                        get_platform(),
-                        sys.version_info[0],
-                        sys.version_info[1],
-                    ),
-                ),
-                "Release",
+            dlldir = glob.glob(
+                f"build/temp.*-{sys.version_info[0]}.{sys.version_info[1]}/Release"
             )
+            assert len(dlldir) == 1
+            dlldir = dlldir[0]
             found = False
             for x in os.listdir(dlldir):
                 if x.endswith(".lib") or x.endswith(".exp") or x.endswith(".dll"):
