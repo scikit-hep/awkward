e78a3f29c048decc5fc034c03ff3eb38621cef26
Angus Hollands <goosey15@gmail.com>
2022-04-11 19:12:43 +0100

    Test: test for `list(option(list` and control

--- tests/v2/test_1405-slicing-untested-cases.py
+++ tests/v2/test_1405-slicing-untested-cases.py
@@ -47,3 +47,51 @@ def test_index_unmapped():
     )
 
     assert content[index].to_list() == [[[], [2]]]
+
+
+def test_list_option_list():
+    """Check that non-offset list(option(list indexes correctly"""
+    content = ak._v2.contents.ListOffsetArray(
+        ak._v2.index.Index64(np.array([0, 2], dtype=np.int64)),
+        ak._v2.contents.ListOffsetArray(
+            ak._v2.index.Index64(np.array([2, 2, 3], dtype=np.int64)),
+            ak._v2.contents.NumpyArray(np.array([2, 2, 2], dtype=np.int64)),
+        ),
+    )
+
+    index = ak._v2.contents.ListOffsetArray(
+        ak._v2.index.Index64(np.array([0, 2], dtype=np.int64)),
+        ak._v2.contents.IndexedOptionArray(
+            ak._v2.index.Index64(np.array([0, 1], dtype=np.int64)),
+            ak._v2.contents.ListOffsetArray(
+                ak._v2.index.Index64(np.array([0, 0, 1], dtype=np.int64)),
+                ak._v2.contents.NumpyArray(np.array([0], dtype=np.int64)),
+            ),
+        ),
+    )
+
+    assert content[index].to_list() == [[[], [2]]]
+
+
+def test_list_option_list_offset():
+    """Check that offset list(option(list indexes correctly"""
+    content = ak._v2.contents.ListOffsetArray(
+        ak._v2.index.Index64(np.array([1, 3], dtype=np.int64)),
+        ak._v2.contents.ListOffsetArray(
+            ak._v2.index.Index64(np.array([0, 2, 2, 3], dtype=np.int64)),
+            ak._v2.contents.NumpyArray(np.array([2, 2, 2], dtype=np.int64)),
+        ),
+    )
+
+    index = ak._v2.contents.ListOffsetArray(
+        ak._v2.index.Index64(np.array([0, 2], dtype=np.int64)),
+        ak._v2.contents.IndexedOptionArray(
+            ak._v2.index.Index64(np.array([0, 1], dtype=np.int64)),
+            ak._v2.contents.ListOffsetArray(
+                ak._v2.index.Index64(np.array([0, 0, 1], dtype=np.int64)),
+                ak._v2.contents.NumpyArray(np.array([0], dtype=np.int64)),
+            ),
+        ),
+    )
+
+    assert content[index].to_list() == [[[], [2]]]
