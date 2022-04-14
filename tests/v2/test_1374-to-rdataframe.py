50ef9eb39b6bb4a2ed9d08e1c84331ef6ba5231a
Ianna Osborne <ianna.osborne@cern.ch>
2022-04-14 13:49:09 +0200

    add minimal example

--- tests/v2/test_1374-to-rdataframe.py
+++ tests/v2/test_1374-to-rdataframe.py
@@ -112,7 +112,7 @@ public:
     std::vector<std::pair<ULong64_t, ULong64_t>> GetEntryRanges() {{
         cout << "#4. GetEntryRanges" << endl;
         auto entryRanges(std::move(fEntryRanges)); // empty fEntryRanges
-        return entryRanges;
+        return {{ {{0ull,1ull}} }} ;
     }}
 
     bool SetEntry(unsigned int slot, ULong64_t entry) {{
