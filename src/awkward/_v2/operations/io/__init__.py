ec9eec9aa5fae80dac44c300c6a48e3766afd1cc
Doug Davis <ddavis@ddavis.io>
2022-04-20 14:25:17 -0400

    Remove duplicated import of to/from-parquet (#1435)
    * parquet implementation is in _v2.operations.convert
    
    * rm to/from parquet files

--- src/awkward/_v2/operations/io/__init__.py
+++ src/awkward/_v2/operations/io/__init__.py
@@ -2,5 +2,3 @@
 
 from awkward._v2.operations.io.ak_from_json_file import from_json_file  # noqa: F401
 from awkward._v2.operations.io.ak_to_json_file import to_json_file  # noqa: F401
-from awkward._v2.operations.io.ak_from_parquet import from_parquet  # noqa: F401
-from awkward._v2.operations.io.ak_to_parquet import to_parquet  # noqa: F401
