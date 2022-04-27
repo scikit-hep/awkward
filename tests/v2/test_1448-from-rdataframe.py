ed469bb149a5e3dbb5852d0fe9b16388534771aa
Ianna Osborne <ianna.osborne@cern.ch>
2022-04-27 16:11:04 +0200

    rdataframe to awkward

--- tests/v2/test_1448-from-rdataframe.py
+++ tests/v2/test_1448-from-rdataframe.py
@@ -4,6 +4,9 @@ import pytest  # noqa: F401
 import numpy as np  # noqa: F401
 import awkward as ak  # noqa: F401
 
+import awkward._v2._lookup  # noqa: E402
+import awkward._v2._connect.cling  # noqa: E402
+
 ROOT = pytest.importorskip("ROOT")
 
 
