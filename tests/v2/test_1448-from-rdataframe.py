84c281406827bf509de7538fd98dbfd9879a0edc
Ianna Osborne <ianna.osborne@cern.ch>
2022-04-28 16:28:17 +0200

    add nested types test

--- tests/v2/test_1448-from-rdataframe.py
+++ tests/v2/test_1448-from-rdataframe.py
@@ -10,7 +10,7 @@ ROOT = pytest.importorskip("ROOT")
 compiler = ROOT.gInterpreter.Declare
 
 
-def test_data_frame():
+def test_data_frame_vecs():
     data_frame = ROOT.RDataFrame(10).Define("x", "gRandom->Rndm()")
     data_frame_xy = data_frame.Define("y", "x*2")
 
@@ -37,3 +37,26 @@ def test_data_frame():
         ],
         fields=None,
     )
+
+
+@pytest.mark.skip(reason="FIXME: nested types have not been implemented yet")
+def test_data_frame_rvecs():
+    data_frame = ROOT.RDataFrame(1024)
+    coordDefineCode = """ROOT::VecOps::RVec<double> {0}(len);
+                     std::transform({0}.begin(), {0}.end(), {0}.begin(), [](double){{return gRandom->Uniform(-1.0, 1.0);}});
+                     return {0};"""
+
+    d = (
+        data_frame.Define("len", "gRandom->Uniform(0, 16)")
+        .Define("x", coordDefineCode.format("x"))
+        .Define("y", coordDefineCode.format("y"))
+    )
+
+    # Now we have in hands d, a RDataFrame with two columns, x and y, which
+    # hold collections of coordinates. The size of these collections vary.
+    # Let's now define radii out of x and y. We'll do it treating the collections
+    # stored in the columns without looping on the individual elements.
+    d1 = d.Define("r", "sqrt(x*x + y*y)")
+
+    array = ak._v2.from_rdataframe(d1, column="r", column_as_record=True)
+    print(array)
