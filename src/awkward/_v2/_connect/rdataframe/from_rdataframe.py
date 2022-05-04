20105c9b5444b249946bde8bc60b18cc1956b05c
Ianna Osborne <ianna.osborne@cern.ch>
2022-05-04 15:13:17 +0200

    cleanup

--- src/awkward/_v2/_connect/rdataframe/from_rdataframe.py
+++ src/awkward/_v2/_connect/rdataframe/from_rdataframe.py
@@ -12,59 +12,6 @@ compiler = ROOT.gInterpreter.Declare
 
 numpy = ak.nplike.Numpy.instance()
 
-cppyy.cppdef(
-    """
-#include <type_traits>
-#include <iterator>
-
-namespace is_iterable_impl
-{
-    using std::begin;
-    using std::end;
-
-    template<class T>
-    using check_specs = std::void_t<
-        std::enable_if_t<std::is_same_v<
-            decltype(begin(std::declval<T&>())), // has begin()
-            decltype(end(std::declval<T&>()))    // has end()
-        >>,                                      // ... begin() and end() are the same type ...
-        decltype(*begin(std::declval<T&>()))     // ... which can be dereferenced
-    >;
-
-    template<class T, class = void>
-    struct is_iterable
-    : std::false_type
-    {};
-
-    template<class T>
-    struct is_iterable<T, check_specs<T>>
-    : std::true_type
-    {};
-}
-
-template<class T>
-using is_iterable = is_iterable_impl::is_iterable<T>;
-
-template<class T>
-constexpr bool is_iterable_v = is_iterable<T>::value;
-
-template<typename T>
-bool _is_arithmetic() {
-    return std::is_arithmetic<T>::value;
-};
-
-template<typename T>
-bool _is_iterable() {
-    return is_iterable<T>::value;
-};
-
-template<typename T, template<typename...> class Ref>
-struct is_specialization : std::false_type {};
-
-template<template<typename...> class Ref, typename... Args>
-struct is_specialization<Ref<Args...>, Ref>: std::true_type {};
-"""
-)
 
 
 def from_rdataframe(data_frame, column, column_as_record=True):
@@ -74,13 +21,6 @@ def from_rdataframe(data_frame, column, column_as_record=True):
             if column_as_record
             else ak._v2.highlevel.Array(array)
         )
-
-    # FIXME: not used, but would be needed if we want to copy the data
-    # to a contiguous content
-    def _offsets(vec_of_vecs):
-        offsets = [0]
-        offsets = offsets + [vec_of_vecs[i].size() for i in range(vec_of_vecs.size())]
-        return offsets
 
     def _recurse(cpp_ref):
         # Note, the conversion of STL vectors and TVec to numpy arrays in ROOT
