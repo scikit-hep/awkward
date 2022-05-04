e30cdab700b5985e4b99b8eaaea36c01b760ea55
Ianna Osborne <ianna.osborne@cern.ch>
2022-05-04 15:59:34 +0200

    pre-commit fixes

--- tests/v2/test_1448-from-rdataframe.py
+++ tests/v2/test_1448-from-rdataframe.py
@@ -93,7 +93,9 @@ def test_to_from_data_frame_rvec_of_rvec():
 
 @pytest.mark.skip(reason="FIXME: support variant")
 def test_to_from_data_frame_rvec_of_rvec_of_rvec():
-    ak_array_in = ak._v2.Array([[[[1.1]]], [[[2.2], [3.3]], [], [4.4]], [], [[[5.5], 6.6], []]])
+    ak_array_in = ak._v2.Array(
+        [[[[1.1]]], [[[2.2], [3.3]], [], [4.4]], [], [[[5.5], 6.6], []]]
+    )
 
     data_frame = ak._v2.to_rdataframe({"x": ak_array_in})
 
