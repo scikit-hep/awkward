c38d4c6c0574a1437dcace0d8926c4c8a0f387a4
Jim Pivarski <jpivarski@users.noreply.github.com>
2022-06-11 14:11:40 -0500

    Fixed typo in unmaskedarray.py. (#1498)

--- src/awkward/_v2/contents/unmaskedarray.py
+++ src/awkward/_v2/contents/unmaskedarray.py
@@ -429,7 +429,7 @@ class UnmaskedArray(Content):
         keepdims,
     ):
         next = self._content
-        if isinstance(next, ak._v2_contents.RegularArray):
+        if isinstance(next, ak._v2.contents.RegularArray):
             next = next.toListOffsetArray64(True)
 
         return next._reduce_next(
