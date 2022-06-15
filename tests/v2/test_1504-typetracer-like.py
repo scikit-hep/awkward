de35b996fb410ea7ecd1a4c5b79bdd6f8ae8d35f
Angus Hollands <goosey15@gmail.com>
2022-06-15 13:03:39 +0100

    Refactor: cleanup test names

--- tests/v2/test_1504-typetracer-like.py
+++ tests/v2/test_1504-typetracer-like.py
@@ -8,34 +8,34 @@ typetracer = ak._v2._typetracer.TypeTracer.instance()
 
 @pytest.mark.parametrize("dtype", [np.float64, np.int64, np.uint8, None])
 @pytest.mark.parametrize("like_dtype", [np.float64, np.int64, np.uint8, None])
-def test(dtype, like_dtype):
-    a = ak._v2.contents.numpyarray.NumpyArray(
+def test_ones_like(dtype, like_dtype):
+    array = ak._v2.contents.numpyarray.NumpyArray(
         np.array([99, 88, 77, 66, 66], dtype=dtype)
     )
-    ones = ak._v2.ones_like(a.typetracer, dtype=like_dtype, highlevel=False)
-    assert ones.typetracer.shape == a.shape
-    assert ones.typetracer.dtype == like_dtype or a.dtype
+    ones = ak._v2.ones_like(array.typetracer, dtype=like_dtype, highlevel=False)
+    assert ones.typetracer.shape == array.shape
+    assert ones.typetracer.dtype == like_dtype or array.dtype
 
 
 @pytest.mark.parametrize("dtype", [np.float64, np.int64, np.uint8, None])
 @pytest.mark.parametrize("like_dtype", [np.float64, np.int64, np.uint8, None])
-def test(dtype, like_dtype):
-    a = ak._v2.contents.numpyarray.NumpyArray(
+def test_zeros_like(dtype, like_dtype):
+    array = ak._v2.contents.numpyarray.NumpyArray(
         np.array([99, 88, 77, 66, 66], dtype=dtype)
     )
 
-    full = ak._v2.zeros_like(a.typetracer, dtype=like_dtype, highlevel=False)
-    assert full.typetracer.shape == a.shape
-    assert full.typetracer.dtype == like_dtype or a.dtype
+    full = ak._v2.zeros_like(array.typetracer, dtype=like_dtype, highlevel=False)
+    assert full.typetracer.shape == array.shape
+    assert full.typetracer.dtype == like_dtype or array.dtype
 
 
 @pytest.mark.parametrize("dtype", [np.float64, np.int64, np.uint8, None])
 @pytest.mark.parametrize("like_dtype", [np.float64, np.int64, np.uint8, None])
 @pytest.mark.parametrize("value", [1.0, -20, np.iinfo(np.uint64).max])
-def test(dtype, like_dtype, value):
-    a = ak._v2.contents.numpyarray.NumpyArray(
+def test_full_like(dtype, like_dtype, value):
+    array = ak._v2.contents.numpyarray.NumpyArray(
         np.array([99, 88, 77, 66, 66], dtype=dtype)
     )
-    full = ak._v2.full_like(a.typetracer, value, dtype=like_dtype, highlevel=False)
-    assert full.typetracer.shape == a.shape
-    assert full.typetracer.dtype == like_dtype or a.dtype
+    full = ak._v2.full_like(array.typetracer, value, dtype=like_dtype, highlevel=False)
+    assert full.typetracer.shape == array.shape
+    assert full.typetracer.dtype == like_dtype or array.dtype
