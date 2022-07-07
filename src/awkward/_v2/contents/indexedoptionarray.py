dd04361f098c86ffb759c5e095d8ae15375d59aa
Jim Pivarski <jpivarski@users.noreply.github.com>
2022-07-07 08:23:59 -0500

    Catch all possible dtypes.

--- src/awkward/_v2/contents/indexedoptionarray.py
+++ src/awkward/_v2/contents/indexedoptionarray.py
@@ -1545,12 +1545,18 @@ class IndexedOptionArray(Content):
 
                 data[~mask0] = content
 
-                if content.dtype == bool:
+                if issubclass(content.dtype.type, (bool, np.bool_)):
                     data[mask0] = False
-                elif content.dtype == float:
+                elif issubclass(content.dtype.type, np.floating):
                     data[mask0] = np.nan
-                else:
+                elif issubclass(content.dtype.type, np.complexfloating):
+                    data[mask0] = np.nan+np.nan*1j
+                elif issubclass(content.dtype.type, np.integer):
                     data[mask0] = np.iinfo(content.dtype).max
+                elif issubclass(content.dtype.type, (np.datetime64, np.timedelta64)):
+                    data[mask0] = numpy.array([np.iinfo(np.int64).max], content.dtype)[0]
+                else:
+                    raise ak._v2._util.error(AssertionError(f"unrecognized dtype: {content.dtype}"))
 
                 return numpy.ma.MaskedArray(data, mask)
             else:
