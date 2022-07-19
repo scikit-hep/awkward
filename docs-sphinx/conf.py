4bbc2b46c72b1cff746efc271843d66e4e4dde1e
Angus Hollands <goosey15@gmail.com>
2022-07-19 11:45:53 +0100

    WIP: initial structure in place

--- docs-sphinx/conf.py
+++ docs-sphinx/conf.py
@@ -37,7 +37,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "_templates", "jupyter_execute"]
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -55,13 +55,13 @@ html_theme_options = {
   "collapse_navigation": True,
   # Add light/dark mode and documentation version switcher:
   "navbar_end": ["theme-switcher", "navbar-icon-links"],
-  "footer_items": ["copyright", "sphinx-version"],
+  "footer_items": ["copyright", "sphinx-version", "funding"],
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ["_static", "_assets"]
 
 
 # MyST settings
