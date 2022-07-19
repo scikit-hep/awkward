5709b298e4596dd48ac40971754219834f410823
Angus Hollands <goosey15@gmail.com>
2022-07-19 18:53:59 +0100

    Fix: use same build strategy on RTD and CI

--- docs-sphinx/conf.py
+++ docs-sphinx/conf.py
@@ -37,7 +37,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "_templates", "jupyter_execute"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "_templates", "jupyter_execute", ".jupyter_cache"]
 
 # -- Options for HTML output -------------------------------------------------
 
