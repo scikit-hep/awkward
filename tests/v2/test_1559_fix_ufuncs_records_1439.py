55eac8a47a83828536fda7749e19c0095362f958
ioanaif <ioana.ifrim@gmail.com>
2022-07-28 16:58:14 +0200

    Added test for overloaded absolutefunction

--- tests/v2/test_1559_fix_ufuncs_records_1439.py
+++ tests/v2/test_1559_fix_ufuncs_records_1439.py
@@ -7,10 +7,29 @@ import numpy as np  # noqa: F401
 to_list = ak._v2.operations.to_list
 
 
-def test_ufuncs_on_records_1439():
+def test_ufuncs_on_records_1439_raise_a_warning():
     array = ak._v2.Array([{"x": 1.0, "y": 1.1}, {"x": 2.0, "y": 2.2}])
     with pytest.raises(ValueError):
         np.absolute(array)
+
+
+def test_ufuncs_on_records_1439_without_warning():
+    def overload_abs(self):
+        return np.sqrt(self.x**2 + self.y**2)
+
+    behavior = {}
+    behavior[np.absolute, "Overload"] = overload_abs
+
+    one = ak._v2.Array(
+        [
+            [{"x": 4, "y": 3}, {"x": 6, "y": 8}, {"x": 5, "y": 12}],
+            [],
+            [{"x": 9, "y": 12}, {"x": 15, "y": 20}],
+        ],
+        with_name="Overload",
+        behavior=behavior,
+    )
+    assert np.absolute(one).tolist() == [[5.0, 10.0, 13.0], [], [15.0, 25.0]]
 
 
 def test_this_should_raise_a_warning():
