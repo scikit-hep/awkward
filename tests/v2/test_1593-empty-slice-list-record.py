f80e327aabdd4444481f872d8c04db49bc14c58b
Angus Hollands <goosey15@gmail.com>
2022-08-15 21:25:22 +0100

    Update tests/v2/test_1593-empty-slice-list-record.py
    Co-authored-by: ioanaif <ioana.ifrim@gmail.com>

--- tests/v2/test_1593-empty-slice-list-record.py
+++ tests/v2/test_1593-empty-slice-list-record.py
@@ -4,3 +4,4 @@ import awkward as ak
 def test():
     array = ak._v2.Array([{"x": [[1, 2, 3], [4, 5, 6]]}])
     assert array[:, []].fields == ["x"]
+    assert ak._v2.to_list(ak._v2.Array([{"x": [[1, 2, 3], [4, 5, 6]]}])[:, []]) == [{}]
