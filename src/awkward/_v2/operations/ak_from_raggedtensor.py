ad7cf690104dfaa7b5301717b71223aa37e6b3f4
Angus Hollands <goosey15@gmail.com>
2022-08-24 15:54:28 +0100

    fix: handle string/bytes encoding in `from_raggedtensor`

--- src/awkward/_v2/operations/ak_from_raggedtensor.py
+++ src/awkward/_v2/operations/ak_from_raggedtensor.py
@@ -7,11 +7,12 @@ np = ak.nplike.NumpyMetadata.instance()
 numpy = ak.nplike.Numpy.instance()
 
 
-def from_raggedtensor(tensor, highlevel=True, behavior=None):
+def from_raggedtensor(tensor, string_encoding="UTF-8", highlevel=True, behavior=None):
     """
     Args:
         tensor: (`tensorflow.RaggedTensor` or `tensorflow.Tensor`) RaggedTensor to convert to an {class}`ak.Array`.
             Must be either a `RaggedTensor
+        string_encoding (str): If not None, the encoding of string values. Otherwise, bytestrings are produced.
         highlevel (bool): If True, return an #ak.Array; otherwise, return
             a low-level #ak.layout.Content subclass.
         behavior (None or dict): Custom #ak.behavior for the output array, if
@@ -24,10 +25,16 @@ def from_raggedtensor(tensor, highlevel=True, behavior=None):
     """
     with ak._v2._util.OperationErrorContext(
         "ak._v2.from_ragggedtensor",
-        {"tensor": tensor},
+        {
+            "tensor": tensor,
+            "string_encoding": string_encoding,
+            "highlevel": highlevel,
+            "behavior": behavior,
+        },
     ):
         return _impl(
             tensor,
+            string_encoding,
             highlevel,
             behavior,
         )
@@ -35,16 +42,13 @@ def from_raggedtensor(tensor, highlevel=True, behavior=None):
 
 def _impl(
     tensor,
+    string_encoding,
     highlevel,
     behavior,
 ):
     tensorflow = awkward._v2._connect.tensorflow.import_tensorflow()  # noqa: F401
-    if not isinstance(tensor, tensorflow.RaggedTensor):
-        raise ak._v2._util.error(
-            TypeError(f"expected `tensorflow.RaggedTensor` object, not {type(tensor)}")
-        )
 
-    layout = tensor_to_layout(tensor, tensorflow)
+    layout = tensor_to_layout(tensor, string_encoding, tensorflow)
     if highlevel:
         return ak._v2._util.wrap(layout, behavior, highlevel)
     else:
@@ -57,28 +61,51 @@ def stops_to_offsets(stops):
     return offsets
 
 
-def tensor_to_layout(tensor, tensorflow, parameters=None):
+def tensor_to_layout(tensor, string_encoding, tensorflow, parameters=None):
     if isinstance(tensor, tensorflow.RaggedTensor):
         offsets = stops_to_offsets(tensor.row_limits().numpy())
         return ak._v2.contents.ListOffsetArray(
             ak._v2.index.Index64(offsets),
-            tensor_to_layout(tensor.values, tensorflow),
+            tensor_to_layout(tensor.values, string_encoding, tensorflow),
             parameters=parameters,
         )
     # Strings are not treated by TensorFlow as a ragged dimension, but instead a regular
     # dimension with a tensorflow.string dtype
-    elif tensor.dtype == tensorflow.string:
-        tensor = tensorflow.cast(
-            tensorflow.strings.unicode_decode(tensor, "UTF-8"), tensorflow.uint8
-        )
-        offsets = stops_to_offsets(tensor.row_limits().numpy())
-        return ak._v2.contents.ListOffsetArray(
-            ak._v2.index.Index64(offsets),
-            ak._v2.contents.NumpyArray(
-                tensor.values.numpy(),
-                parameters={"__array__": "char"},
-            ),
-            parameters={"__array__": "string"},
-        )
+    elif isinstance(tensor, tensorflow.Tensor):
+        if tensor.dtype == tensorflow.string:
+            # For encoded strings, transcode them to UTF8
+            if string_encoding is not None:
+                # Convert into UTF8
+                raw_tensor = tensorflow.strings.unicode_transcode(
+                    tensor, string_encoding, "UTF-8"
+                )
+                string_type = "string"
+                char_type = "char"
+            # Otherwise, read the raw bytes
+            else:
+                raw_tensor = tensor
+                string_type = "bytestring"
+                char_type = "byte"
+
+            content_tensor = tensorflow.strings.bytes_split(tensor).values
+            byte_count = tensorflow.strings.length(raw_tensor, unit="BYTE").numpy()
+            offsets = stops_to_offsets(numpy.cumsum(byte_count))
+            content = tensorflow.io.decode_raw(content_tensor, tensorflow.uint8)
+
+            return ak._v2.contents.ListOffsetArray(
+                ak._v2.index.Index64(offsets),
+                ak._v2.contents.NumpyArray(
+                    content,
+                    parameters={"__array__": char_type},
+                ),
+                parameters={"__array__": string_type},
+            )
+        else:
+            return ak._v2.contents.NumpyArray(tensor.numpy(), parameters=parameters)
     else:
-        return ak._v2.contents.NumpyArray(tensor.numpy(), parameters=parameters)
+        raise ak._v2._util.error(
+            TypeError(
+                f"expected `tensorflow.RaggedTensor`, or `tensorflow.Tensor` object, "
+                f"not {type(tensor)}"
+            )
+        )
