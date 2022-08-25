db4e6959d2033ffea0b8b6be1a46d2a1fade34df
Angus Hollands <goosey15@gmail.com>
2022-08-25 21:34:27 +0100

    test: update test to show crash happens with extension arrays

--- tests/v2/test_1619-from-parquet-empty-field.py
+++ tests/v2/test_1619-from-parquet-empty-field.py
@@ -6,27 +6,26 @@ import pytest  # noqa: F401
 import numpy as np  # noqa: F401
 import awkward as ak  # noqa: F401
 
-
 pytest.importorskip("pyarrow.parquet")
 
 
-def test(tmp_path):
+def test_no_extension(tmp_path):
     array = ak._v2.Array(
         [
-            {"": {"x": 1, "y": 1.1}},
-            {"": {"x": 2, "y": 2.2}},
-            {"": {"x": 3, "y": 3.3}},
-            {"": {"x": 4, "y": 4.4}},
-            {"": {"x": 5, "y": 5.5}},
-            {"": {"x": 6, "y": 6.6}},
-            {"": {"x": 7, "y": 7.7}},
-            {"": {"x": 8, "y": 8.8}},
-            {"": {"x": 9, "y": 9.9}},
+            [
+                {"x": 1, "y": 1.1},
+                {"x": 2, "y": 2.2},
+                {"x": 3, "y": 3.3},
+            ],
+            [
+                {"x": 1, "y": 1.1},
+                {"x": 2, "y": 2.2},
+            ],
         ]
     )
-    path = os.path.join(tmp_path, "array3.parquet")
+    path = os.path.join(tmp_path, "array-no-ext.parquet")
 
-    ak._v2.to_parquet(array, path)
+    ak._v2.to_parquet(array, path, extensionarray=False)
 
     result = ak._v2.from_parquet(path, columns=["x"])
     assert result.fields == ["x"]
