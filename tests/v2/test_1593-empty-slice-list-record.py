f4be1a03df501c94349fe3ca15ff51814478071c
Angus Hollands <goosey15@gmail.com>
2022-08-25 14:20:52 +0100

    test: fix expected output of slice

--- tests/v2/test_1593-empty-slice-list-record.py
+++ tests/v2/test_1593-empty-slice-list-record.py
@@ -4,6 +4,6 @@ import awkward as ak
 def test():
     array = ak._v2.Array([{"x": [[1, 2, 3], [4, 5, 6]]}])
     assert array[:, []].fields == ["x"]
-    assert ak._v2.to_list(ak._v2.Array([{"x": [[1, 2, 3], [4, 5, 6]]}])[:, []]) == {
-        "x": []
-    }
+    assert ak._v2.to_list(ak._v2.Array([{"x": [[1, 2, 3], [4, 5, 6]]}])[:, []]) == [
+        {"x": []}
+    ]
