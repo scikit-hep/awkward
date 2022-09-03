def484b57d6cb36208a91c877aa22509738b1d44
Angus Hollands <goosey15@gmail.com>
2022-09-03 19:22:23 +0100

    chore: drop 3.6 support (#1661)
    * chore: drop 3.6 from nox
    
    * chore: drop 3.6 from CI
    
    * chore: switch to manylinux2014
    
    manylinux2014 support
    was added in pip 19.3. Python 3.7 ships with pip 20
    
    * chore: drop 3.6 from pyprojec.toml
    
    Bump manylinux version
    
    * chore: bump Python version in requirements & package info
    
    * chore: bump Python version in docs
    
    * chore: bump Python version in README and CONTRIBUTING
    
    * chore: test numpy==1.13.1 on 3.7
    
    * chore: remove reference to un-specified CIBW variable
    
    * chore: be explicit about manylinux version
    
    * chore: drop Python 3.6 guard in jax connect
    
    * fix: use oldest-supported-numpy for numpy test
    
    * fix: workflow syntax
    
    * docs: increase Python UB in README
    
    * docs: use CPython EoL
    
    * ci: remove redundant CIBW configuration from CI
    
    * chore: update pre-commit config
    
    * Scanned through the text and brought a lot of things up to date.
    
    Co-authored-by: Jim Pivarski <jpivarski@users.noreply.github.com>

--- docs-sphinx/index.rst
+++ docs-sphinx/index.rst
@@ -20,8 +20,8 @@
    :alt: Conda-Forge
    :target: https://github.com/conda-forge/awkward-feedstock
 
-.. image:: https://img.shields.io/badge/python-3.6%E2%80%923.10-blue
-   :alt: Python 3.6‒3.10
+.. image:: https://img.shields.io/badge/python-3.7%E2%80%923.10-blue
+   :alt: Python 3.7‒3.10
    :target: https://www.python.org
 
 .. image:: https://img.shields.io/badge/license-BSD%203--Clause-blue.svg
--- src/awkward/_v2/_connect/jax/__init__.py
+++ src/awkward/_v2/_connect/jax/__init__.py
@@ -1,7 +1,6 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
 
 import awkward as ak
-import sys
 
 try:
     import jax
@@ -54,13 +53,6 @@ def register_pytrees():
 def import_jax(name="Awkward Arrays with JAX"):
     if jax is None:
         raise ak._v2._util.error(ModuleNotFoundError(error_message.format(name)))
-
-    if not (sys.version_info.major == 3 and sys.version_info.minor >= 7):
-        raise ak._v2._util.error(
-            AssertionError(
-                "Using Awkward Arrays with JAX requires Python version >= 3.7"
-            )
-        )
 
     global pytrees_registered
 
