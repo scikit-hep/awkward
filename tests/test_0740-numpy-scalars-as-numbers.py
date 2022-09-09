6eb7a565890dea01ba359f155fcf52e7d59963f5
Angus Hollands <goosey15@gmail.com>
2022-09-09 14:36:36 +0100

    refactor: (remove v1 highlevel) remove v1 tests

--- tests/test_0740-numpy-scalars-as-numbers.py
+++
@@ -1,9 +0,0 @@
-# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
-
-import pytest  # noqa: F401
-import numpy as np  # noqa: F401
-import awkward as ak  # noqa: F401
-
-
-def test():
-    assert ak.from_iter([np.array(1.0)]).tolist() == [1.0]
