9d754a0bddf0009830817dbc69a1a8b2e9683a03
Angus Hollands <goosey15@gmail.com>
2022-09-09 15:04:13 +0100

    refactor: (remove v1 highlevel) remove v1 imports in v1.__init__

--- src/awkward/__init__.py
+++ src/awkward/__init__.py
@@ -7,42 +7,11 @@ import awkward.nplike
 
 # shims for C++ (now everything is compiled into one 'awkward._ext' module)
 import awkward.layout
-import awkward.types
-import awkward.forms
-import awkward.partition
 
 # internal
 import awkward._v2
 import awkward._cpu_kernels
 import awkward._libawkward
-import awkward._util
-
-# third-party connectors
-import awkward._connect._numpy
-import awkward._connect._numba
-import awkward._connect._numexpr
-import awkward._connect._autograd
-import awkward.numba
-
-# high-level interface
-behavior = {}
-from awkward.highlevel import Array
-from awkward.highlevel import Record
-from awkward.highlevel import ArrayBuilder
-
-# third-party jax connectors
-import awkward._connect._jax
-
-# behaviors
-from awkward.behaviors.mixins import *
-from awkward.behaviors.string import *
-from awkward.behaviors.categorical import *
-
-# operations
-from awkward.operations.convert import *
-from awkward.operations.describe import *
-from awkward.operations.structure import *
-from awkward.operations.reducers import *
 
 # version
 __version__ = awkward._ext.__version__
