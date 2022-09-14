aeee1f31482d2b548eb4965dc99d30d988d01ef0
Angus Hollands <goosey15@gmail.com>
2022-09-14 15:41:24 +0100

    test: check records too!

--- tests/v2/test_1511-set-attribute.py
+++ tests/v2/test_1511-set-attribute.py
@@ -5,7 +5,7 @@ import awkward as ak  # noqa: F401
 import numpy as np
 
 
-def test():
+def test_array():
     record = ak._v2.contents.RecordArray(
         [ak._v2.contents.NumpyArray(np.arange(10))], ["x"]
     )
@@ -21,7 +21,24 @@ def test():
     assert array._not_an_existing_attribute == 10
 
 
-class BadBehavior(ak._v2.Array):
+def test_record():
+    record = ak._v2.contents.RecordArray(
+        [ak._v2.contents.NumpyArray(np.arange(10))], ["x"]
+    )
+    array = ak._v2.Array(record)
+    record = array[0]
+
+    with pytest.raises(AttributeError):
+        record.x = 10
+
+    with pytest.raises(AttributeError):
+        record.not_an_existing_attribute = 10
+
+    record._not_an_existing_attribute = 10
+    assert record._not_an_existing_attribute == 10
+
+
+class BadBehaviorBase:
     FIELD_STRING = "I am not a list of fields!"
 
     @property
@@ -33,16 +50,37 @@ class BadBehavior(ak._v2.Array):
         self.__class__.FIELD_STRING = value
 
 
-behavior = {("*", "bad"): BadBehavior}
+class BadBehaviorArray(BadBehaviorBase, ak._v2.Array):
+    pass
 
 
-def test_bad_behavior():
+class BadBehaviorRecord(BadBehaviorBase, ak._v2.Record):
+    pass
+
+
+behavior = {("*", "bad"): BadBehaviorArray, "bad": BadBehaviorRecord}
+
+
+def test_bad_behavior_array():
     record = ak._v2.contents.RecordArray(
         [ak._v2.contents.NumpyArray(np.arange(10))], ["x"]
     )
     array = ak._v2.Array(record, with_name="bad", behavior=behavior)
-    assert isinstance(array, BadBehavior)
+    assert isinstance(array, BadBehaviorArray)
 
-    assert array.fields == BadBehavior.FIELD_STRING
+    assert array.fields == BadBehaviorArray.FIELD_STRING
     array.fields = "yo ho ho and a bottle of rum"
     assert array.fields == "yo ho ho and a bottle of rum"
+
+
+def test_bad_behavior_record():
+    record = ak._v2.contents.RecordArray(
+        [ak._v2.contents.NumpyArray(np.arange(10))], ["x"]
+    )
+    array = ak._v2.Array(record, with_name="bad", behavior=behavior)
+    record = array[0]
+    assert isinstance(record, BadBehaviorRecord)
+
+    assert record.fields == BadBehaviorRecord.FIELD_STRING
+    record.fields = "yo ho ho and a bottle of rum"
+    assert record.fields == "yo ho ho and a bottle of rum"
