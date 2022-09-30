9a3620d687a8f974f3466c4ff59d9a41a541b3cf
Jim Pivarski <jpivarski@users.noreply.github.com>
2022-09-30 15:16:28 -0500

    refactor: make _v2 facade a static module. (#1758)
    * refactor: make _v2 facade a static module.
    
    * style: pre-commit fixes
    
    * Reformat for sorted imports and ignore pylint warning.
    
    * Make the new _v2 more like the old _v2, including 'numba' and 'operations'.
    
    Co-authored-by: Angus Hollands <goosey15@gmail.com>
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Angus Hollands <goosey15@gmail.com>

---
+++ src/awkward/_v2.py
@@ -0,0 +1,24 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+# Following https://github.com/scikit-hep/awkward/blob/main-v1/src/awkward/_v2/__init__.py
+
+from awkward import Array  # noqa: F401
+from awkward import ArrayBuilder  # noqa: F401
+from awkward import Record  # noqa: F401
+from awkward import _broadcasting  # noqa: F401
+from awkward import _connect  # noqa: F401
+from awkward import _lookup  # noqa: F401
+from awkward import _slicing  # noqa: F401
+from awkward import _typetracer  # noqa: F401
+from awkward import _util  # noqa: F401
+from awkward import behavior  # noqa: F401
+from awkward import behaviors  # noqa: F401
+from awkward import contents  # noqa: F401
+from awkward import forms  # noqa: F401
+from awkward import identifier  # noqa: F401
+from awkward import index  # noqa: F401
+from awkward import numba  # noqa: F401
+from awkward import operations  # noqa: F401
+from awkward import record  # noqa: F401
+from awkward import types  # noqa: F401
+from awkward.operations import *  # noqa: F401,F403 pylint: disable=W0401,W0614
