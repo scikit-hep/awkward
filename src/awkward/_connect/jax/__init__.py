42dec7ac731bf3fee17141f4d1507b2538969987
Angus Hollands <goosey15@gmail.com>
2022-10-01 11:06:56 +0100

    refactor: add `get_jax_ufunc`

--- src/awkward/_connect/jax/__init__.py
+++ src/awkward/_connect/jax/__init__.py
@@ -1,4 +1,6 @@
-import awkward._connect.jax.reducers  # noqa: F401
+import jax.numpy
+
+from awkward._connect.jax.reducers import get_jax_reducer  # noqa: F401
 from awkward._connect.jax.trees import (  # noqa: F401
     AuxData,
     find_numpyarray_nodes,
@@ -6,3 +8,7 @@ from awkward._connect.jax.trees import (  # noqa: F401
     jax_unflatten_highlevel,
     replace_numpyarray_nodes,
 )
+
+
+def get_jax_ufunc(ufunc):
+    return getattr(jax.numpy, ufunc.__name__)
