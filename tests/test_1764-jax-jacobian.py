df377bd719926343e88a254cede3deacd3bc07d0
Angus Hollands <goosey15@gmail.com>
2022-10-05 21:16:53 +0100

    fix: handle more buffer types & fix nplike mixing (#1769)
    * fix: handle more buffer types, fix nplike mixing
    
    * test: add test for Jacobian computation
    
    * fix: don't regularise `NumpyArray`s
    
    Remove assert - NumpyArray-only layouts might have Numpy nplike
    
    * fix: use node parameters and identifiers
    
    * test: skip Jacobian test for now.
    
    * fix: restore `TypeError`
    
    * refactor: use `_errors` module directly
    .

---
+++ tests/test_1764-jax-jacobian.py
@@ -0,0 +1,25 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import pytest
+
+import awkward as ak
+
+jax = pytest.importorskip("jax")
+jax.config.update("jax_platform_name", "cpu")
+jax.config.update("jax_enable_x64", True)
+
+ak.jax.register_and_check()
+
+
+@pytest.mark.skip("Jacobian support not implemented")
+def test():
+    array = ak.Array([[1, 2, 3], [4, 5, 6.0]])
+
+    def func(x):
+        return x * 2 - 1
+
+    array_np = ak.to_numpy(array)
+    jac_np = jax.jacfwd(func)(array_np)
+
+    jac = jax.jacfwd(func)(array)
+    assert jac.to_list() == jac_np.tolist()
