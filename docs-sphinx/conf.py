1edbe71aac8b58c7ed6ca16e302559cb3be0f88a
Angus Hollands <goosey15@gmail.com>
2022-10-12 14:06:56 +0100

    chore: remove dotfiles from Sphinx source path

--- docs-sphinx/conf.py
+++ docs-sphinx/conf.py
@@ -47,11 +47,10 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
     "_build",
-    "Thumbs.db",
-    ".DS_Store",
     "_templates",
+    "Thumbs.db",
     "jupyter_execute",
-    ".jupyter_cache",
+    ".*"
 ]
 
 # -- Options for HTML output -------------------------------------------------
