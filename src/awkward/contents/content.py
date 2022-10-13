6b13906dbcefee11bfc1279fc5e3b46a21013b4a
Angus Hollands <goosey15@gmail.com>
2022-10-13 19:46:58 +0100

    feat: add `_transform` to dispatch

--- src/awkward/contents/content.py
+++ src/awkward/contents/content.py
@@ -907,88 +907,16 @@ class Content:
         )
 
     def argsort(self, axis=-1, ascending=True, stable=False, kind=None, order=None):
-        negaxis = -axis
-        branch, depth = self.branch_depth
-        if branch:
-            if negaxis <= 0:
-                raise ak._errors.wrap_error(
-                    ValueError(
-                        "cannot use non-negative axis on a nested list structure "
-                        "of variable depth (negative axis counts from the leaves "
-                        "of the tree; non-negative from the root)"
-                    )
-                )
-            if negaxis > depth:
-                raise ak._errors.wrap_error(
-                    ValueError(
-                        "cannot use axis={} on a nested list structure that splits into "
-                        "different depths, the minimum of which is depth={} from the leaves".format(
-                            axis, depth
-                        )
-                    )
-                )
-        else:
-            if negaxis <= 0:
-                negaxis = negaxis + depth
-            if not (0 < negaxis and negaxis <= depth):
-                raise ak._errors.wrap_error(
-                    ValueError(
-                        "axis={} exceeds the depth of the nested list structure "
-                        "(which is {})".format(axis, depth)
-                    )
-                )
-
-        starts = ak.index.Index64.zeros(1, self._nplike)
-        parents = ak.index.Index64.zeros(self.length, self._nplike)
         transformer = ak._transformers.ArgSort(ascending, stable)
-        return self._transform_next(transformer, negaxis, starts, None, parents, 1)
+        return self._transform(transformer, axis)
 
     def cumsum(self, axis=None, dtype=None):
         transformer = ak._transformers.CumSum(dtype)
-        starts = ak.index.Index64.zeros(1, self._nplike)
-        parents = ak.index.Index64.zeros(self.length, self._nplike)
-        if axis is not None:
-            negaxis = -axis
-        else:
-            negaxis = None
-        return self._transform_next(transformer, negaxis, starts, None, parents, 1)
+        return self._transform(transformer, axis)
 
     def sort(self, axis=-1, ascending=True, stable=False, kind=None, order=None):
-        negaxis = -axis
-        branch, depth = self.branch_depth
-        if branch:
-            if negaxis <= 0:
-                raise ak._errors.wrap_error(
-                    ValueError(
-                        "cannot use non-negative axis on a nested list structure "
-                        "of variable depth (negative axis counts from the leaves "
-                        "of the tree; non-negative from the root)"
-                    )
-                )
-            if negaxis > depth:
-                raise ak._errors.wrap_error(
-                    ValueError(
-                        "cannot use axis={} on a nested list structure that splits into "
-                        "different depths, the minimum of which is depth={} from the leaves".format(
-                            axis, depth
-                        )
-                    )
-                )
-        else:
-            if negaxis <= 0:
-                negaxis = negaxis + depth
-            if not (0 < negaxis and negaxis <= depth):
-                raise ak._errors.wrap_error(
-                    ValueError(
-                        "axis={} exceeds the depth of the nested list structure "
-                        "(which is {})".format(axis, depth)
-                    )
-                )
-
-        starts = ak.index.Index64.zeros(1, self._nplike)
-        parents = ak.index.Index64.zeros(self.length, self._nplike)
         transformer = ak._transformers.Sort(ascending, stable)
-        return self._transform_next(transformer, negaxis, starts, None, parents, 1)
+        return self._transform(transformer, axis)
 
     def _combinations_axis0(self, n, replacement, recordlookup, parameters):
         size = self.length
@@ -1547,3 +1475,39 @@ class Content:
 
     def _transform_next(self, transformer, negaxis, starts, shifts, parents, outlength):
         raise ak._errors.wrap_error(NotImplementedError)
+
+    def _transform(self, transformer, axis=-1):
+        negaxis = -axis
+        branch, depth = self.branch_depth
+        if branch:
+            if negaxis <= 0:
+                raise ak._errors.wrap_error(
+                    ValueError(
+                        "cannot use non-negative axis on a nested list structure "
+                        "of variable depth (negative axis counts from the leaves "
+                        "of the tree; non-negative from the root)"
+                    )
+                )
+            if negaxis > depth:
+                raise ak._errors.wrap_error(
+                    ValueError(
+                        "cannot use axis={} on a nested list structure that splits into "
+                        "different depths, the minimum of which is depth={} from the leaves".format(
+                            axis, depth
+                        )
+                    )
+                )
+        else:
+            if negaxis <= 0:
+                negaxis = negaxis + depth
+            if not (0 < negaxis and negaxis <= depth):
+                raise ak._errors.wrap_error(
+                    ValueError(
+                        "axis={} exceeds the depth of the nested list structure "
+                        "(which is {})".format(axis, depth)
+                    )
+                )
+
+        starts = ak.index.Index64.zeros(1, self._nplike)
+        parents = ak.index.Index64.zeros(self.length, self._nplike)
+        return self._transform_next(transformer, negaxis, starts, None, parents, 1)
