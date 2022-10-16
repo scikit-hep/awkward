6e7f39c762a7f7ac5e1fea053d0bd0df164f4127
Angus Hollands <goosey15@gmail.com>
2022-10-16 15:47:34 +0100

    feat: add highlevel, behavior arguments to max

--- src/awkward/operations/ak_max.py
+++ src/awkward/operations/ak_max.py
@@ -13,6 +13,8 @@ def max(
     initial=None,
     mask_identity=True,
     flatten_records=False,
+    highlevel=True,
+    behavior=None,
 ):
     """
     Args:
@@ -35,6 +37,10 @@ def max(
             results in the operation's identity.
         flatten_records (bool): If True, axis=None combines fields from different
             records; otherwise, records raise an error.
+        highlevel (bool): If True, return an #ak.Array; otherwise, return
+            a low-level #ak.contents.Content subclass.
+        behavior (None or dict): Custom #ak.behavior for the output array, if
+            high-level.
 
     Returns the maximum value in each group of elements from `array` (many
     types supported, including all Awkward Arrays and Records). The identity
@@ -60,9 +66,20 @@ def max(
             initial=initial,
             mask_identity=mask_identity,
             flatten_records=flatten_records,
+            highlevel=highlevel,
+            behavior=behavior,
         ),
     ):
-        return _impl(array, axis, keepdims, initial, mask_identity, flatten_records)
+        return _impl(
+            array,
+            axis,
+            keepdims,
+            initial,
+            mask_identity,
+            flatten_records,
+            highlevel,
+            behavior,
+        )
 
 
 @ak._connect.numpy.implements("nanmax")
@@ -73,6 +90,8 @@ def nanmax(
     initial=None,
     mask_identity=True,
     flatten_records=False,
+    highlevel=True,
+    behavior=None,
 ):
     """
     Args:
@@ -95,6 +114,10 @@ def nanmax(
             results in the operation's identity.
         flatten_records (bool): If True, axis=None combines fields from different
             records; otherwise, records raise an error.
+        highlevel (bool): If True, return an #ak.Array; otherwise, return
+            a low-level #ak.contents.Content subclass.
+        behavior (None or dict): Custom #ak.behavior for the output array, if
+            high-level.
 
     Like #ak.max, but treating NaN ("not a number") values as missing.
 
@@ -115,17 +138,30 @@ def nanmax(
             initial=initial,
             mask_identity=mask_identity,
             flatten_records=flatten_records,
+            highlevel=highlevel,
+            behavior=behavior,
         ),
     ):
-        array = ak.operations.ak_nan_to_none._impl(array, False, None)
+        array = ak.operations.ak_nan_to_none._impl(array, highlevel, behavior)
 
-        return _impl(array, axis, keepdims, initial, mask_identity, flatten_records)
+        return _impl(
+            array,
+            axis,
+            keepdims,
+            initial,
+            mask_identity,
+            flatten_records,
+            highlevel,
+            behavior,
+        )
 
 
-def _impl(array, axis, keepdims, initial, mask_identity, flatten_records):
+def _impl(
+    array, axis, keepdims, initial, mask_identity, flatten_records, highlevel, behavior
+):
     layout = ak.operations.to_layout(array, allow_record=False, allow_other=False)
     reducer = ak.reducers.Max(initial)
-    behavior = ak._util.behavior_of(array)
+    behavior = ak._util.behavior_of(array, behavior=behavior)
 
     if axis is None:
         return layout.reduce_flattened(
@@ -141,6 +177,6 @@ def _impl(array, axis, keepdims, initial, mask_identity, flatten_records):
             reducer, axis=axis, mask=mask_identity, keepdims=keepdims, behavior=behavior
         )
         if isinstance(out, (ak.contents.Content, ak.record.Record)):
-            return ak._util.wrap(out, behavior)
+            return ak._util.wrap(out, behavior, highlevel)
         else:
             return out
