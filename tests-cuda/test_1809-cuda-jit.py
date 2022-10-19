bc5597a9f4634c3f3bc9bd402fd6cb3045b546ea
Ianna Osborne <ianna.osborne@cern.ch>
2022-10-19 14:48:25 +0200

    test: split tests

--- tests-cuda/test_1809-cuda-jit.py
+++ tests-cuda/test_1809-cuda-jit.py
@@ -15,7 +15,7 @@ ak_numba_arrayview = pytest.importorskip("awkward._connect.numba.arrayview")
 ak.numba.register_and_check()
 
 
-def test_arrays():
+def test_array_njit():
     @numba.njit
     def something(array):
         for index in range(len(array)):
@@ -24,15 +24,30 @@ def test_arrays():
     akarray = ak.Array([[1.1, 2.2, 3.3], [], [4.4, 5.5], [6.6, 7.7, 8.8, 9.9]])
     something(akarray)
 
+
+def test_array_cuda_jit_cuda_backend():
     @numba.cuda.jit
-    def something_else(array):
+    def something(array):
         index = numba.cuda.threadIdx.x
         if index > len(array):
             return
 
-    another_akarray = ak.Array(
+    akarray = ak.Array(
         [[1.1, 2.2, 3.3], [], [4.4, 5.5], [6.6, 7.7, 8.8, 9.9]], backend="cuda"
     )
     threadsperblock = 32
     blockspergrid = 128
-    something_else[blockspergrid, threadsperblock](another_akarray)
+    something[blockspergrid, threadsperblock](akarray)
+
+
+def test_array_cuda_jit():
+    @numba.cuda.jit
+    def something(array):
+        index = numba.cuda.threadIdx.x
+        if index > len(array):
+            return
+
+    akarray = ak.Array([[1.1, 2.2, 3.3], [], [4.4, 5.5], [6.6, 7.7, 8.8, 9.9]])
+    threadsperblock = 32
+    blockspergrid = 128
+    something[blockspergrid, threadsperblock](akarray)
