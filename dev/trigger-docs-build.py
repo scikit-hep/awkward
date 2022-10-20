2511d4b28034862653b3154835db163403d17545
Angus Hollands <goosey15@gmail.com>
2022-10-20 07:38:17 +0100

    ci: enable versions for built docs (#1812)

---
+++ dev/trigger-docs-build.py
@@ -0,0 +1,24 @@
+"""
+Trigger a build on ReadTheDocs of the given branch
+"""
+import argparse
+import os
+import re
+
+import requests
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("version")
+    args = parser.parse_args()
+
+    version_slug = re.sub(r"[\-/]", "-", args.version)
+
+    url = f"https://readthedocs.org/api/v3/projects/awkward-array/versions/{version_slug}/builds/"
+    token = os.environ["RTD_TOKEN"]
+    response = requests.post(
+        url,
+        headers={"Authorization": f"token {token}"},
+    )
+    response.raise_for_status()
+    print(response.json())
