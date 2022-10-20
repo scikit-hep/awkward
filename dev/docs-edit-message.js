4ccc7321cda86e387dc1d2edb71bf0bd2dd58cf5
Angus Hollands <goosey15@gmail.com>
2022-10-20 09:41:36 +0100

    ci: remove need for Python, use custom message

---
+++ dev/docs-edit-message.js
@@ -0,0 +1,30 @@
+export default async function update_pr_description({github, context}) {
+
+    const {data: pull} = await github.rest.pulls.get({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        pull_number: context.issue.number,
+    });
+    const {VERSION_SLUG} = context.env;
+    const MESSAGE_START = `<!-- docs-preview-start -->`;
+    const MESSAGE_END = `<!-- docs-preview-end -->`;
+    const MESSAGE_BODY = `----\n:books: The documentation for this PR will be available at <https://awkward-array.readthedocs.io/en/${VERSION_SLUG}/> once Read the Docs has finished building :hammer:`;
+    const MESSAGE = `\r\n\r\n${MESSAGE_START}\r\n${MESSAGE_BODY}\r\n${MESSAGE_END}`
+
+    // Only include message if this is the first time.
+    let body = "";
+    if (!pull.body) {
+        body = MESSAGE;
+    } else if (pull.body.indexOf(MESSAGE_START) === -1) {
+        body = pull.body + MESSAGE;
+    } else {
+        return;
+    }
+    // Update description
+    github.rest.pulls.update({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        pull_number: context.issue.number,
+        body: body,
+    });
+}
