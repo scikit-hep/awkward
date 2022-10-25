f675a80b5840c6f038e7afbade8f82df7dde8885
Jim Pivarski <jpivarski@gmail.com>
2022-10-25 13:37:23 -0500

    Just verifying that RDataFrame sees these values as boolean, too.

--- tests/test_1829-to-from-rdataframe-bool.py
+++ tests/test_1829-to-from-rdataframe-bool.py
@@ -25,3 +25,19 @@ def test_data_frame_boolean():
         columns=("x",),
     )
     assert ak_array_in.to_list() == ak_array_out["x"].to_list()
+
+
+def test_does_rdataframe_see_these_as_boolean():
+    ak_array_in = ak.Array([True, False, True])
+
+    data_frame = ak.to_rdataframe({"x": ak_array_in})
+
+    assert data_frame.GetColumnType("x") == "bool"
+
+    data_frame_2 = data_frame.Define("y", "!x")
+
+    ak_array_out = ak.from_rdataframe(
+        data_frame_2,
+        columns=("y",),
+    )
+    assert [not x for x in ak_array_in] == ak_array_out["y"].to_list()
