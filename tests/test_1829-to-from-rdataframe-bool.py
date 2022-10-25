815fe8d0813ae1c6cf6db9b234d573b0ec89a3d0
Jim Pivarski <jpivarski@gmail.com>
2022-10-25 13:40:03 -0500

    One more:  is a good test of boolean-ness.

--- tests/test_1829-to-from-rdataframe-bool.py
+++ tests/test_1829-to-from-rdataframe-bool.py
@@ -41,3 +41,19 @@ def test_does_rdataframe_see_these_as_boolean():
         columns=("y",),
     )
     assert [not x for x in ak_array_in] == ak_array_out["y"].to_list()
+
+
+def test_filters_as_well():
+    ak_array_in = ak.Array([True, False, True])
+
+    data_frame = ak.to_rdataframe({"x": ak_array_in})
+
+    assert data_frame.GetColumnType("x") == "bool"
+
+    data_frame_2 = data_frame.Filter("x")
+
+    ak_array_out = ak.from_rdataframe(
+        data_frame_2,
+        columns=("x",),
+    )
+    assert [True, True] == ak_array_out["x"].to_list()
