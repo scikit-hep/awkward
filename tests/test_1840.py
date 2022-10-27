d761579e0a3e0317295acc44a39df011430c86ec
Lindsey Gray <lindsey.gray@gmail.com>
2022-10-27 09:13:20 -0500

    add in test for #1840 fixed by #1841

---
+++ tests/test_1840.py
@@ -0,0 +1,13 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import numpy as np  # noqa: F401
+import pytest  # noqa: F401
+
+import awkward as ak  # noqa: F401
+
+
+def test():
+    array = np.random.random(size=512).astype(dtype=np.float64)
+    assert ak.type(array) == ak.types.ArrayType(
+        ak.types.NumpyType("float64"), 512
+    )
