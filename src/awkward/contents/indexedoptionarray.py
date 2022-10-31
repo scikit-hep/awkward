aa9d7930b073a16a4aed985a0367da1564b27f7d
Angus Hollands <goosey15@gmail.com>
2022-10-31 11:47:13 +0000

    fix: resolve use of unknown data in `IndexedOptionArray`

--- src/awkward/contents/indexedoptionarray.py
+++ src/awkward/contents/indexedoptionarray.py
@@ -1180,9 +1180,12 @@ class IndexedOptionArray(Content):
             nextoutindex, out, parameters=self._parameters
         )
 
-        inject_nones = (
-            True if (numnull[0] > 0 and not branch and negaxis != depth) else False
-        )
+        if self.nplike.known_data:
+            inject_nones = (
+                True if (numnull[0] > 0 and not branch and negaxis != depth) else False
+            )
+        else:
+            inject_nones = False
 
         # If we want the None's at this depth to be injected
         # into the dense ([x y z None None]) rearranger result.
