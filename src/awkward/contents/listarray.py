06a124f14de4f39942385a943eb36c90f459b2fd
Angus Hollands <goosey15@gmail.com>
2022-10-31 13:07:09 +0000

    fix: correct mistake with index
    I was thinking of a `ByteMaskedArray`

--- src/awkward/contents/listarray.py
+++ src/awkward/contents/listarray.py
@@ -1240,14 +1240,13 @@ class ListArray(Content):
             if posaxis is not None and posaxis + 1 == depth:
                 return self._pad_none_axis0(target, clip)
             elif posaxis is not None and posaxis + 1 == depth + 1:
+                # Without known data, all we can say is that there's now
+                # an option here
                 if not self._backend.nplike.known_data:
-                    nextcontent = ak.contents.IndexedOptionArray.simplified(
-                        ak.index.Index64(
-                            self._backend.index_nplike.empty(len(self._content))
-                        ),
+                    nextcontent = ak.contents.UnmaskedArray.simplified(
                         self._content,
                         parameters=None,
-                    ).simplify_optiontype()
+                    )
                     return ak.contents.ListArray(
                         self._starts,
                         self._stops,
@@ -1257,13 +1256,10 @@ class ListArray(Content):
 
                 min_ = self._backend.index_nplike.min(self._stops.data - self._starts.data)
                 if target <= min_:
-                    nextcontent = ak.contents.IndexedOptionArray.simplified(
-                        ak.index.Index64(
-                            self._backend.index_nplike.zeros(len(self._content))
-                        ),
+                    nextcontent = ak.contents.UnmaskedArray.simplified(
                         self._content,
                         parameters=None
-                    ).simplify_optiontype()
+                    )
                     return ak.contents.ListArray(
                         self._starts,
                         self._stops,
