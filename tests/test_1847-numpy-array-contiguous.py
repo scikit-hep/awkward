88b1ff6862646ae23e9c139d630a3d59222ebbed
Angus Hollands <goosey15@gmail.com>
2022-11-01 10:40:49 +0000

    test: focus only on reduction

--- tests/test_1847-numpy-array-contiguous.py
+++ tests/test_1847-numpy-array-contiguous.py
@@ -37,33 +37,3 @@ def test_reduce_2d():
         ak.sum(layout, axis=-1).to_list()
         == np.sum(non_contiguous_array, axis=-1).tolist()
     )
-
-
-def test_unique_1d_strided():
-    non_contiguous_array = np.array([0, 0, 1, 1, 2, 2, 3, 3], dtype=np.int64)[::2]
-    layout = ak.contents.NumpyArray(
-        non_contiguous_array,
-    )
-    assert not layout.is_contiguous
-    assert layout.unique(-1).to_list() == [0, 1, 2, 3]
-    assert layout.is_unique(-1)
-
-    non_contiguous_array = np.array([0, 0, 0, 1, 1, 2, 2, 3, 3], dtype=np.int64)[::2]
-    layout = ak.contents.NumpyArray(
-        non_contiguous_array,
-    )
-    assert not layout.is_contiguous
-    assert layout.unique(-1).to_list() == [0, 1, 2, 3]
-    assert not layout.is_unique(-1)
-
-
-def test_unique_2d_strided():
-    transposed_array = (
-        np.array([0, 0, 1, 0, 2, 2, 3, 2, 3, 3, 4, 4, 4, 5, 6], dtype=np.int64)
-        .reshape(-1, 3)
-        .T
-    )
-    layout = ak.contents.NumpyArray(transposed_array)
-    assert not layout.is_contiguous
-    assert layout.unique(-1).to_list() == [[0, 3, 4], [0, 2, 4, 5], [1, 2, 3, 4, 6]]
-    assert not layout.is_unique(-1)
