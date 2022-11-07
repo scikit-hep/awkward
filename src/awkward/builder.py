f9c48bab16ed79fcad546253961ca42356e546f8
Angus Hollands <goosey15@gmail.com>
2022-11-07 13:40:54 +0000

    feat: add `ak.builder` module to re-export `ArrayBuilder`

---
+++ src/awkward/builder.py
@@ -0,0 +1,11 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+# v2: no change; keep this file.
+
+from awkward_cpp.lib._ext import ArrayBuilder
+
+__all__ = ["ArrayBuilder"]
+
+
+def __dir__():
+    return __all__
