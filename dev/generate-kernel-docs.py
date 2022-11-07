ae2c91ea5d0cf45c4ff669d702d4ff65a9113d01
Angus Hollands <goosey15@gmail.com>
2022-11-07 16:26:35 +0000

    chore: make generated docs directory if it doesn't exist

--- dev/generate-kernel-docs.py
+++ dev/generate-kernel-docs.py
@@ -29,16 +29,15 @@ A second implementation, ``libawkward-cuda-kernels.so``, is provided as a separa
 
 The functions are implemented in C with templates for integer specializations (cpu-kernels) and as CUDA (cuda-kernels), but the function signatures and normative definitions are expressed below using a subset of the Python language. These normative definitions are used as a stable and easy-to-read standard that both implementations must reproduce in tests, regardless of how they are optimized.\n
 """
+    generated_dir = os.path.join(CURRENT_DIR, "..", "docs", "reference", "generated")
+    os.makedirs(generated_dir, exist_ok=True)
+
     with open(
         os.path.join(CURRENT_DIR, "..", "awkward-cpp", "kernel-specification.yml")
     ) as specfile:
         with open(
             os.path.join(
-                CURRENT_DIR,
-                "..",
-                "docs",
-                "reference",
-                "generated",
+                generated_dir,
                 "kernels.rst",
             ),
             "w",
