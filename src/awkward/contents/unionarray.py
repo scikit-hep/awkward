93c813acff901e7dfcf2b6fcba5d36f0c9fe422f
Angus Hollands <goosey15@gmail.com>
2022-11-30 14:00:10 +0000

    refactor: import future annotations

--- src/awkward/contents/unionarray.py
+++ src/awkward/contents/unionarray.py
@@ -1,6 +1,7 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
 
 # pylint: disable=consider-using-enumerate
+from __future__ import annotations
 
 import copy
 import ctypes
