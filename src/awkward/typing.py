1795fbb1425a6737ff09ec3e32d71a7b3c145d32
Angus Hollands <goosey15@gmail.com>
2022-11-30 16:53:30 +0000

    fix: correct imports

--- src/awkward/typing.py
+++ src/awkward/typing.py
@@ -2,18 +2,16 @@
 from __future__ import annotations
 
 import sys
+import typing
+from typing import *  # noqa: F401, F403
+
+__all__ = typing.__all__
 
 if sys.version_info < (3, 11):
     import typing_extensions
     from typing_extensions import *  # noqa: F401, F403
 
-    __all__ = typing_extensions.__all__
-
-else:
-    import typing
-    from typing import *  # noqa: F401, F403
-
-    __all__ = typing.__all__
+    __all__ += typing_extensions.__all__
 
 
 def __dir__() -> tuple[str, ...]:
