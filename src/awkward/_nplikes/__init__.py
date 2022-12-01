7db5ed0270abdb363041169d4c758f31dc7c3879
Angus Hollands <goosey15@gmail.com>
2022-12-01 10:06:40 +0000

    refactor: split `numpy` into `array_module`

---
+++ src/awkward/_nplikes/__init__.py
@@ -0,0 +1,5 @@
+import awkward._nplikes.dtypes  # noqa: F401
+from awkward._nplikes.cupy import Cupy  # noqa: F401
+from awkward._nplikes.numpy import Numpy  # noqa: F401
+from awkward._nplikes.numpylike import NumpyLike  # noqa: F401
+from awkward._nplikes.typetracer import TypeTracer  # noqa: F401
