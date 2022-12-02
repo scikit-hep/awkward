45e20244390487787745cf176454ea1e9589213d
Angus Hollands <goosey15@gmail.com>
2022-12-02 21:42:11 +0000

    refactor: simplify backend functions

--- src/awkward/operations/ak_backend.py
+++ src/awkward/operations/ak_backend.py
@@ -3,32 +3,15 @@
 import awkward as ak
 
 
-def backend(*arrays):
+def backend(*arrays) -> str:
     """
     Returns the names of the backend used by `arrays`. May be
 
-       * `"cpu"` for `libawkward-cpu-kernels.so`;
-       * `"cuda"` for `libawkward-cuda-kernels.so`;
-       * `"mixed"` if any of the arrays have different labels within their
-         structure or any arrays have different labels from each other;
-       * None if the objects are not Awkward, NumPy, or CuPy arrays (e.g.
+       * `"cpu"` for arrays backed by NumPy;
+       * `"cuda"` for arrays backed by CuPy;
+       * `"jax"` for arrays backed by JAX;
+       * None if the objects are not Awkward, NumPy, JAX, or CuPy arrays (e.g.
          Python numbers, booleans, strings).
-
-    Mixed arrays can't be used in any operations, and two arrays on different
-    devices can't be used in the same operation.
-
-    To use `"cuda"`, the package
-    [awkward-cuda-kernels](https://pypi.org/project/awkward-cuda-kernels)
-    be installed, either by
-
-        pip install awkward-cuda-kernels
-
-    or as an optional dependency with
-
-        pip install awkward[cuda] --upgrade
-
-    It is only available for Linux as a binary wheel, and only supports Nvidia
-    GPUs (it is written in CUDA).
 
     See #ak.to_backend.
     """
@@ -39,32 +22,6 @@ def backend(*arrays):
         return _impl(arrays)
 
 
-def _impl(arrays):
-    backends = set()
-    for array in arrays:
-        layout = ak.operations.to_layout(
-            array,
-            allow_record=True,
-            allow_other=True,
-        )
-        # Find the nplike, if it is explicitly associated with this object
-        nplike = ak.nplikes.nplike_of(layout, default=None)
-        if nplike is None:
-            continue
-        if isinstance(nplike, ak.nplikes.Jax):
-            backends.add("jax")
-        elif isinstance(nplike, ak.nplikes.Cupy):
-            backends.add("cuda")
-        elif isinstance(nplike, ak.nplikes.Numpy):
-            backends.add("cpu")
-
-    if backends == set():
-        return None
-    elif backends == {"cpu"}:
-        return "cpu"
-    elif backends == {"cuda"}:
-        return "cuda"
-    elif backends == {"jax"}:
-        return "jax"
-    else:
-        return "mixed"
+def _impl(arrays) -> str:
+    backend_impl = ak._backends.backend_of(*arrays, default=None)
+    return backend_impl.name
