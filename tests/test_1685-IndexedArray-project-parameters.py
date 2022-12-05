097011281d83de08c6b2046ddc7f769ddfca663b
Jim Pivarski <jpivarski@users.noreply.github.com>
2022-12-05 12:17:25 -0600

    More testing.

--- tests/test_1685-IndexedArray-project-parameters.py
+++ tests/test_1685-IndexedArray-project-parameters.py
@@ -9,7 +9,8 @@ import awkward as ak
 def test():
     layout = ak.contents.IndexedArray(
         ak.index.Index64(np.array([0, 1, 2, 3], np.int64)),
-        ak.contents.NumpyArray(np.arange(10), parameters={"this": "that"}),
+        ak.contents.NumpyArray(np.arange(10), parameters={"this": "that", "some": "hidden"}),
         parameters={"some": "other"},
     )
     assert layout.project().parameter("some") == "other"
+    assert layout.project().parameter("this") == "that"
