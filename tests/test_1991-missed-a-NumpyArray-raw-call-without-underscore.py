34167f0e559befeb59cb5afc6698b74d0d34ed1b
Jim Pivarski <jpivarski@gmail.com>
2022-12-09 19:34:55 -0600

    fix: missed a NumpyArray.raw call without an underscore.

---
+++ tests/test_1991-missed-a-NumpyArray-raw-call-without-underscore.py
@@ -0,0 +1,16 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import json
+
+import numpy as np
+import pytest
+
+import awkward as ak
+
+
+def test():
+    assert ak.Record({"x": "hello"})["x"] == "hello"
+    assert ak.Record({"x": b"hello"})["x"] == b"hello"
+
+    assert ak.Array([{"x": "hello"}])[0, "x"] == "hello"
+    assert ak.Array([{"x": b"hello"}])[0, "x"] == b"hello"
