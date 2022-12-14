4af96c482d2defa1b75712d34224fa77dd9a59a4
Angus Hollands <goosey15@gmail.com>
2022-12-14 21:32:17 +0000

    docs: redirect paths for user-guide

--- dev/generate-redirects.py
+++ dev/generate-redirects.py
@@ -6,7 +6,7 @@ REDIRECT_TEMPLATE = """
 <!doctype html>
 <html>
   <head>
-  <meta http-equiv="refresh" content="0; url=https://awkward-array.org/doc/main/{dest}">
+  <meta http-equiv="refresh" content="{delay}; url=https://awkward-array.org/doc/{version}/{dest}">
   </head>
 </html>"""
 
@@ -15,6 +15,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input", type=pathlib.Path)
     parser.add_argument("output", type=pathlib.Path)
+    parser.add_argument("--delay", default=0)
+    parser.add_argument("--version", default="main")
     args = parser.parse_args()
 
     with open(args.input) as f:
@@ -24,10 +26,10 @@ if __name__ == "__main__":
         src_file = src.replace("any-ext", "html")
         dst_file = dst.removeprefix("../")
 
-        src_content = REDIRECT_TEMPLATE.format(dest=dst_file)
+        src_content = REDIRECT_TEMPLATE.format(
+            version=args.version, delay=args.delay, dest=dst_file
+        )
 
         output_path = args.output / src_file
         output_path.parent.mkdir(parents=True, exist_ok=True)
         output_path.write_text(src_content)
-
-    (args.output / "index.html").write_text(REDIRECT_TEMPLATE.format(dest=""))
