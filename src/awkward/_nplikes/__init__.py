616ae136adc7ec8aa70bafc4c7e2f8de46269faf
Angus Hollands <goosey15@gmail.com>
2022-12-20 17:50:14 +0000

    feat: add `nplike_of` to new nplikes

--- src/awkward/_nplikes/__init__.py
+++ src/awkward/_nplikes/__init__.py
@@ -1,5 +1,10 @@
 import awkward._nplikes.dtypes  # noqa: F401
+from awkward._nplikes.array_module import (  # noqa: F401
+    ArrayModuleArray,
+    ArrayModuleNumpyLike,
+)
 from awkward._nplikes.cupy import Cupy  # noqa: F401
+from awkward._nplikes.factory import nplike_of  # noqa: F401
 from awkward._nplikes.jax import Jax  # noqa: F401
 from awkward._nplikes.numpy import Numpy  # noqa: F401
 from awkward._nplikes.numpylike import Array, NumpyLike  # noqa: F401
