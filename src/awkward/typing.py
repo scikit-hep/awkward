4b361bd33e183e32f757f5043249a84596713217
Angus Hollands <goosey15@gmail.com>
2023-01-03 18:52:44 +0000

    chore: explicitly name typing_extension imports

--- src/awkward/typing.py
+++ src/awkward/typing.py
@@ -3,20 +3,40 @@ from __future__ import annotations
 
 import sys
 import typing
-from typing import *  # noqa: F401, F403
+from typing import *  # noqa: F403
 
-__all__ = [*typing.__all__, "AxisMaybeNone"]
+__all__ = [
+    "Final",
+    "Self",
+    "final",
+    "Protocol",
+    "Unpack",
+    "TypeAlias",
+    "runtime_checkable",
+    "AxisMaybeNone",
+] + typing.__all__
 
 
 AxisMaybeNone = TypeVar("AxisMaybeNone", int, None)
 
 
 if sys.version_info < (3, 11):
-    import typing_extensions
-    from typing_extensions import *  # noqa: F401, F403
-
-    __all__ += typing_extensions.__all__
-
-
-def __dir__() -> tuple[str, ...]:
-    return __all__
+    from typing_extensions import (  # noqa: F401, F403
+        Final,
+        Protocol,
+        Self,
+        TypeAlias,
+        Unpack,
+        final,
+        runtime_checkable,
+    )
+else:
+    from typing import (  # noqa: F401, F403
+        Final,
+        Protocol,
+        Self,
+        TypeAlias,
+        Unpack,
+        final,
+        runtime_checkable,
+    )
