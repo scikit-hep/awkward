0a1d4c239e87137d2b84e4436f21ec6fa45e806f
Ianna Osborne <ianna.osborne@cern.ch>
2023-01-10 10:06:34 +0100

    fix: try int64 as a default

--- header-only/awkward/utils.h
+++ header-only/awkward/utils.h
@@ -18,8 +18,7 @@ namespace awkward {
   template <typename T>
   const std::string
   type_to_name() {
-    std::cout << "Type " << typeid(T).name() << " is not recognized." << std::endl;
-    return typeid(T).name();
+    return "int64";
   }
 
   /// @brief Returns `bool` string when the primitive type
