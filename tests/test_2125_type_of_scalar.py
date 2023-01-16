0ae85a20372bd5ea9c31c035bb922687df06ac1c
Angus Hollands <goosey15@gmail.com>
2023-01-16 09:57:23 +0000

    test: more tests of scalar types

--- tests/test_2125_type_of_scalar.py
+++ tests/test_2125_type_of_scalar.py
@@ -12,3 +12,19 @@ def test():
     assert ak.type(np.dtype("complex128")) == ak.types.ScalarType(
         ak.types.NumpyType("complex128")
     )
+    assert ak.type("hello") == ak.types.ArrayType(
+        ak.types.NumpyType("uint8", parameters={"__array__": "char"}), 5
+    )
+    assert ak.type("int16") == ak.types.ArrayType(
+        ak.types.NumpyType("uint8", parameters={"__array__": "char"}), 5
+    )
+    assert ak.type(["int16"]) == ak.types.ArrayType(
+        ak.types.ListType(
+            ak.types.NumpyType(
+                "uint8", parameters={"__array__": "char"}, typestr="char"
+            ),
+            parameters={"__array__": "string"},
+            typestr="string",
+        ),
+        1,
+    )
