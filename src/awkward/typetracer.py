ed6766961da3a47425d4844d1646e74c5068abc8
Angus Hollands <goosey15@gmail.com>
2023-01-28 15:11:05 +0000

    refactor: these imports don't actually need to be private

--- src/awkward/typetracer.py
+++ src/awkward/typetracer.py
@@ -1,18 +1,18 @@
 from __future__ import annotations
 
-__all__ = ["is_unknown_array", "is_unknown_scalar"]
+__all__ = ["is_unknown_array", "is_unknown_scalar", "empty_if_typetracer"]
 
+from awkward._backends import TypeTracerBackend
 from awkward._nplikes.typetracer import is_unknown_array, is_unknown_scalar
+from awkward._util import behavior_of, wrap
 from awkward.highlevel import Array, Record
+from awkward.operations.ak_to_layout import to_layout
 from awkward.typing import TypeVar
 
 T = TypeVar("T", Array, Record)
 
 
 def empty_if_typetracer(array: T) -> T:
-    from awkward._backends import TypeTracerBackend
-    from awkward._util import behavior_of, wrap
-    from awkward.operations.ak_to_layout import to_layout
 
     typetracer_backend = TypeTracerBackend.instance()
 
