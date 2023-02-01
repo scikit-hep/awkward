0dc232c0047b612baec2fc9a67217eeae24a4642
Angus Hollands <goosey15@gmail.com>
2023-02-01 16:43:40 +0000

    wip: add `ak.merge_option_of_records`

--- src/awkward/operations/ak_merge_union_of_records.py
+++ src/awkward/operations/ak_merge_union_of_records.py
@@ -46,8 +46,15 @@ def _impl(array, axis, highlevel, behavior):
     def apply_displace_index(layout, backend, **kwargs):
         if layout.is_record:
             return layout
-        elif (layout.is_option or layout.is_indexed) and layout.content.is_record:
-            # Transpose option-of-record to record-of-option
+        elif layout.is_option and layout.content.is_record:
+            raise ak._errors.wrap_error(
+                TypeError(
+                    "optional records cannot be merged by this function. First call `ak.merge_option_of_records` "
+                    "to convert these into records of options."
+                )
+            )
+        elif layout.is_indexed and layout.content.is_record:
+            # Transpose index-of-record to record-of-index
             return ak.contents.RecordArray(
                 [layout.copy(content=c) for c in layout.content.contents],
                 layout.content.fields,
@@ -57,95 +64,103 @@ def _impl(array, axis, highlevel, behavior):
         else:
             raise ak._errors.wrap_error(TypeError(layout))
 
-    def apply(layout, depth, backend, **kwargs):
+    def apply(layout, depth, backend, continuation, **kwargs):
         posaxis = ak._util.maybe_posaxis(layout, axis, depth)
-        if posaxis + 1 == depth and layout.is_union:
-            # First, find all ordered fields, regularising any index-of-record
-            # such that we have record-of-index
-            seen_fields = set()
-            all_fields = []
-            regularised_contents = []
-            for content in layout.contents:
-                # Ensure that we have record-of-index
-                regularised_content = ak._do.recursively_apply(
-                    content, apply_displace_index
-                )
-                regularised_contents.append(regularised_content)
-
-                # Find new fields
-                for field in regularised_content.fields:
-                    if field not in seen_fields:
-                        seen_fields.add(field)
-                        all_fields.append(field)
-
-            # Build unions for each field
-            outer_field_contents = []
-            for field in all_fields:
-                field_tags = backend.index_nplike.asarray(layout.tags, copy=True)
-                field_index = backend.index_nplike.asarray(layout.index, copy=True)
-
-                # Build contents for union representing current field
-                field_contents = [
-                    c.content(field) for c in regularised_contents if c.has_field(field)
-                ]
-
-                # Find the best location for option type.
-                # We will potentially have fewer contents in this per-field union
-                # than the original outer union-of-records, because some recordarrays
-                # may not have the given field.
-                tag_for_missing = 0
-                for i, content in enumerate(field_contents):
-                    if content.is_option:
-                        tag_for_missing = i
-                        break
-
-                # If at least one recordarray doesn't have this field, we add
-                # a special option
-                if len(field_contents) < len(regularised_contents):
-                    # Make the tagged content an option, growing by one to ensure we
-                    # have a known `None` value to index into
-                    tagged_content = field_contents[tag_for_missing]
-                    indexedoption_index = backend.index_nplike.arange(
-                        tagged_content.length + 1, dtype=np.int64
+        if posaxis + 1 == depth:
+            if layout.is_union:
+                # First, find all ordered fields, regularising any index-of-record
+                # such that we have record-of-index
+                seen_fields = set()
+                all_fields = []
+                regularised_contents = []
+                for content in layout.contents:
+                    # Ensure that we have record-of-index
+                    regularised_content = ak._do.recursively_apply(
+                        content, apply_displace_index
                     )
-                    indexedoption_index[tagged_content.length] = -1
-                    field_contents[
-                        tag_for_missing
-                    ] = ak.contents.IndexedOptionArray.simplified(
-                        ak.index.Index64(indexedoption_index), tagged_content
-                    )
+                    regularised_contents.append(regularised_content)
 
-                # Now build contents for union, by looping over outermost index
-                # Overwrite tags to adjust for new contents length
-                # and use the tagged content for any missing values
-                k = 0
-                for j, content in enumerate(regularised_contents):
-                    tag_is_j = field_tags == j
+                    # Find new fields
+                    for field in regularised_content.fields:
+                        if field not in seen_fields:
+                            seen_fields.add(field)
+                            all_fields.append(field)
 
-                    if content.has_field(field):
-                        # Rewrite tags to account for missing fields
-                        field_tags[tag_is_j] = k
-                        k += 1
+                # Build unions for each field
+                outer_field_contents = []
+                for field in all_fields:
+                    field_tags = backend.index_nplike.asarray(layout.tags, copy=True)
+                    field_index = backend.index_nplike.asarray(layout.index, copy=True)
 
-                    else:
-                        # Rewrite tags to point to option content
-                        field_tags[tag_is_j] = tag_for_missing
-                        # Point each value to missing value
-                        field_index[tag_is_j] = (
-                            field_contents[tag_for_missing].length - 1
+                    # Build contents for union representing current field
+                    field_contents = [
+                        c.content(field)
+                        for c in regularised_contents
+                        if c.has_field(field)
+                    ]
+
+                    # Find the best location for option type.
+                    # We will potentially have fewer contents in this per-field union
+                    # than the original outer union-of-records, because some recordarrays
+                    # may not have the given field.
+                    tag_for_missing = 0
+                    for i, content in enumerate(field_contents):
+                        if content.is_option:
+                            tag_for_missing = i
+                            break
+
+                    # If at least one recordarray doesn't have this field, we add
+                    # a special option
+                    if len(field_contents) < len(regularised_contents):
+                        # Make the tagged content an option, growing by one to ensure we
+                        # have a known `None` value to index into
+                        tagged_content = field_contents[tag_for_missing]
+                        indexedoption_index = backend.index_nplike.arange(
+                            tagged_content.length + 1, dtype=np.int64
+                        )
+                        indexedoption_index[tagged_content.length] = -1
+                        field_contents[
+                            tag_for_missing
+                        ] = ak.contents.IndexedOptionArray.simplified(
+                            ak.index.Index64(indexedoption_index), tagged_content
                         )
 
-                outer_field_contents.append(
-                    ak.contents.UnionArray.simplified(
-                        ak.index.Index8(field_tags),
-                        ak.index.Index64(field_index),
-                        field_contents,
-                    )
-                )
-            return ak.contents.RecordArray(
-                outer_field_contents, all_fields, backend=backend
-            )
+                    # Now build contents for union, by looping over outermost index
+                    # Overwrite tags to adjust for new contents length
+                    # and use the tagged content for any missing values
+                    k = 0
+                    for j, content in enumerate(regularised_contents):
+                        tag_is_j = field_tags == j
 
+                        if content.has_field(field):
+                            # Rewrite tags to account for missing fields
+                            field_tags[tag_is_j] = k
+                            k += 1
+
+                        else:
+                            # Rewrite tags to point to option content
+                            field_tags[tag_is_j] = tag_for_missing
+                            # Point each value to missing value
+                            field_index[tag_is_j] = (
+                                field_contents[tag_for_missing].length - 1
+                            )
+
+                    outer_field_contents.append(
+                        ak.contents.UnionArray.simplified(
+                            ak.index.Index8(field_tags),
+                            ak.index.Index64(field_index),
+                            field_contents,
+                        )
+                    )
+                return ak.contents.RecordArray(
+                    outer_field_contents, all_fields, backend=backend
+                )
+            elif layout.is_option or layout.is_indexed or layout.is_record:
+                return continuation()
+            else:
+                return layout
+
+        # Can only hit this branch if we're above the action axis
         elif layout.is_leaf:
             raise ak._errors.wrap_error(
                 np.AxisError(f"axis={axis} exceeds the depth of this array ({depth})")
