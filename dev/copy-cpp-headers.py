25480504d192a3405023f61e06a83f9779894871
Angus Hollands <goosey15@gmail.com>
2023-03-03 16:44:09 +0000

    wip: use project for header-only

--- dev/copy-cpp-headers.py
+++ dev/copy-cpp-headers.py
@@ -3,14 +3,25 @@ import pathlib
 import shutil
 
 root_path = pathlib.Path(__file__).absolute().parents[1]
-source_path = root_path / "header-only" / "include"
+header_only_path = root_path / "header-only"
 dest_paths = (
     root_path / "awkward-cpp" / "header-only",
     root_path / "src" / "awkward" / "_connect" / "header-only",
 )
 
+
+def copy_and_replace(source, dest):
+    if dest.exists():
+        shutil.rmtree(dest)
+    shutil.copytree(source, dest)
+
+
 if __name__ == "__main__":
-    for path in dest_paths:
-        if path.exists():
-            shutil.rmtree(path)
-        shutil.copytree(source_path, path)
+    copy_and_replace(
+        header_only_path / "include",
+        root_path / "src" / "awkward" / "_connect" / "header-only",
+    )
+    copy_and_replace(
+        header_only_path,
+        root_path / "awkward-cpp" / "header-only",
+    )
