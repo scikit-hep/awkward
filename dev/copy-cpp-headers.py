5c9f4a6aafdd311025786b02212a3f80d250d348
Angus Hollands <goosey15@gmail.com>
2023-03-05 21:31:05 +0000

    fix: copy headers into right location

--- dev/copy-cpp-headers.py
+++ dev/copy-cpp-headers.py
@@ -8,8 +8,10 @@ root_path = pathlib.Path(__file__).absolute().parents[1]
 if __name__ == "__main__":
     header_only_path = root_path / "header-only"
 
-    # Copy to Python package
-    connect_path = root_path / "src" / "awkward" / "_connect" / "header-only"
+    # Copy to Python package under `awkward`
+    connect_path = (
+        root_path / "src" / "awkward" / "_connect" / "header-only" / "awkward"
+    )
     if connect_path.exists():
         shutil.rmtree(connect_path)
     connect_path.mkdir(parents=True)
