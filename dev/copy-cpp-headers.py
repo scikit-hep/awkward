71b334a82570b15eb07f87f6992552d93accabfa
Angus Hollands <goosey15@gmail.com>
2023-03-06 19:46:14 +0000

    feat: make `header-only` libraries a CMake project (#2280)
    * wip: use project for header-only
    
    * wip: more work on linking headers
    
    * ci: bundle header-only as zip
    
    * chore: rename header-only target
    
    * docs: add docs for linking against target
    
    * chore: finer-grained header-only targets
    
    * fix: copy headers into right location
    
    * refactor: move alias to `add_component`
    
    * refactor: move alias to `add_component`
    
    * chore: exclude header-only from ALL
    
    * docs: mention pybind11 example
    
    * Update docs/user-guide/how-to-use-header-only-layoutbuilder.md

--- dev/copy-cpp-headers.py
+++ dev/copy-cpp-headers.py
@@ -3,14 +3,24 @@ import pathlib
 import shutil
 
 root_path = pathlib.Path(__file__).absolute().parents[1]
-source_path = root_path / "header-only" / "include"
-dest_paths = (
-    root_path / "awkward-cpp" / "header-only",
-    root_path / "src" / "awkward" / "_connect" / "header-only",
-)
+
 
 if __name__ == "__main__":
-    for path in dest_paths:
-        if path.exists():
-            shutil.rmtree(path)
-        shutil.copytree(source_path, path)
+    header_only_path = root_path / "header-only"
+
+    # Copy to Python package under `awkward`
+    connect_path = (
+        root_path / "src" / "awkward" / "_connect" / "header-only" / "awkward"
+    )
+    if connect_path.exists():
+        shutil.rmtree(connect_path)
+    connect_path.mkdir(parents=True)
+    for path in header_only_path.rglob("*.h"):
+        dest_path = connect_path / path.name
+        shutil.copy(path, dest_path)
+
+    # Copy to C++ package
+    cpp_path = root_path / "awkward-cpp" / "header-only"
+    if cpp_path.exists():
+        shutil.rmtree(cpp_path)
+    shutil.copytree(header_only_path, cpp_path)
---
+++ header-only/README.md
@@ -0,0 +1,5 @@
+# Awkward Header-only Libraries
+
+This CMake project defined a set of header-only libraries for working with Awkward Array. Use of these libraries is
+described in the [documentation](https://awkward-array.org/doc/main/user-guide/). If you are not using CMake, then
+ensure that the contents of `builder-options`, `growable-buffer`, `layout-builder` are on the include path.
