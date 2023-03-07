d12b2c25f7756e1fda6ff7d5aa4d9183a35f66f8
Angus Hollands <goosey15@gmail.com>
2023-03-07 09:27:45 +0000

    chore: add examples

---
+++ header-only/examples/cython/demo.pyx
@@ -0,0 +1,14 @@
+from libcpp.vector cimport vector
+
+def create_demo_array():
+    cdef vector[int] vect
+    cdef int i, x
+
+    for i in range(10):
+        vect.push_back(i)
+
+    for i in range(10):
+        print(vect[i])
+
+    for x in vect:
+        print(x)
---
+++ header-only/examples/cython/setup.py
@@ -0,0 +1,9 @@
+from skbuild import setup  # This line replaces 'from setuptools import setup'
+
+setup(
+    name="demo",
+    version="0.0.1",
+    license="MIT",
+    packages=["demo"],
+    python_requires=">=3.7",
+)
