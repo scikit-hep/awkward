60a43ec3c5d00a1a2a04aa932dacd9a5e22fd634
Ianna Osborne <ianna.osborne@cern.ch>
2023-03-22 14:45:23 +0100

    fix: generate array view

--- src/awkward/highlevel.py
+++ src/awkward/highlevel.py
@@ -1644,6 +1644,8 @@ class Array(NDArrayOperatorsMixin, Iterable, Sized):
 
         import awkward._connect.cling  # noqa: F401
 
+        import cppyy
+
         if self._cpp_type is None:
             self._generator = ak._connect.cling.togenerator(
                 self._layout.form, flatlist_as_rvec=False
