2b4b6d3fe93e514ccf1fcfaae90ca8b6c7e8fb93
Angus Hollands <goosey15@gmail.com>
2023-04-18 15:04:51 +0100

    fix: return empty list if broadcasting no arrays (#2407)
    * fix: return empty list if broadcasting no arrays
    
    * test: add simple test!

---
+++ tests/test_2407_broadcast_no_arrays.py
@@ -0,0 +1,10 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import pytest  # noqa: F401
+
+import awkward as ak
+
+
+def test():
+    result = ak.broadcast_arrays()
+    assert result == []
