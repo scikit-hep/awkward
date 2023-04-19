fix: handle emptyarray in broadcasting contexts

src/awkward/operations/ak_mask.py
@@ -103,9 +103,16 @@ def mask(array, mask, *, valid_when=True, highlevel=True, behavior=None):
 
 def _impl(array, mask, valid_when, highlevel, behavior):
     def action(inputs, backend, **kwargs):
+        # Empty arrays are mainly placeholders; they should fail most operations
+        if any(x.is_unknown for x in inputs):
+            raise TypeError(
+                "cannot evaluate ak.mask for EmptyArray(s), use `ak.values_astype` "
+                "to convert these to arrays with known dtypes"
+            )
+
         layoutarray, layoutmask = inputs
-        if isinstance(layoutmask, ak.contents.NumpyArray):
-            m = backend.nplike.asarray(layoutmask)
+        if layoutmask.is_numpy:
+            m = layoutmask.data
             if not issubclass(m.dtype.type, (bool, np.bool_)):
                 raise ValueError(f"mask must have boolean type, not {repr(m.dtype)}")
             bytemask = ak.index.Index8(m.view(np.int8))

src/awkward/operations/ak_where.py
@@ -111,7 +111,7 @@ def _impl3(condition, x, y, mergebool, highlevel, behavior):
 
     def action(inputs, **kwargs):
         akcondition, left, right = inputs
-        if isinstance(akcondition, ak.contents.NumpyArray):
+        if akcondition.is_numpy:
             npcondition = backend.index_nplike.asarray(akcondition)
             tags = ak.index.Index8((npcondition == 0).view(np.int8))
             index = ak.index.Index64(
@@ -131,6 +131,11 @@ def _impl3(condition, x, y, mergebool, highlevel, behavior):
                     [left, right],
                     mergebool=mergebool,
                 ),
+            )
+        elif akcondition.is_unknown:
+            raise TypeError(
+                "cannot evaluate ak.where for EmptyArray(s), use `ak.values_astype` "
+                "to convert these to arrays with known dtypes"
             )
         else:
             return None