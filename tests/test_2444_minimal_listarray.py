cef2beac9aaecc8f854ef255778e6470027c7369
Angus Hollands <goosey15@gmail.com>
2023-05-02 17:05:58 +0100

    test: split example into list and dict

--- tests/test_2444_minimal_listarray.py
+++ tests/test_2444_minimal_listarray.py
@@ -1,51 +1,21 @@
-# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
-
-import pytest  # noqa: F401
-
 import awkward as ak
 
 
-def test():
-    form = ak.forms.from_dict(
-        {
-            "class": "RecordArray",
-            "fields": ["muon"],
-            "contents": [
-                {
-                    "class": "ListOffsetArray",
-                    "offsets": "i64",
-                    "content": {
-                        "class": "RecordArray",
-                        "fields": ["pt"],
-                        "contents": [
-                            {
-                                "class": "NumpyArray",
-                                "primitive": "int64",
-                                "inner_shape": [],
-                                "parameters": {},
-                                "form_key": "muon_pt!",
-                            },
-                        ],
-                        "parameters": {},
-                        "form_key": "muon_record!",
-                    },
-                    "parameters": {},
-                    "form_key": "muon_list_outer!",
-                },
-            ],
-            "parameters": {},
-            "form_key": "outer!",
-        }
-    )
+def test_dict():
+    array = ak.Array([{"muon": [{"pt": 1}, {"pt": 4}]}])
+    ttarray = ak.to_backend(array, "typetracer")
 
-    ttlayout, report = ak._nplikes.typetracer.typetracer_with_report(
-        form, forget_length=True
-    )
-    ttarray = ak.Array(ttlayout)
-    array = ak.Array(ttlayout.form.length_zero_array(highlevel=False))
     array["emptydict"] = {}
-    array["emptylist"] = []
     ttarray["emptydict"] = {}
+
+    assert ttarray.layout.form == array.layout.form
+
+
+def test_list():
+    array = ak.Array([{"muon": [{"pt": 1}, {"pt": 4}]}])
+    ttarray = ak.to_backend(array, "typetracer")
+
+    array["emptylist"] = []
     ttarray["emptylist"] = []
 
     assert ttarray.layout.form == array.layout.form
