bc1b53674985d5b7817e5f93c31c9cb2475813de
Angus Hollands <goosey15@gmail.com>
2023-05-10 09:50:39 +0100

    fix: `ak.to_numpy` does not properly convert lists of empty string (#2449)
    * test: add test
    
    * fix: pass dtype in `to_backend_array` for strings

---
+++ tests/test_2364_empty_list_of_string.py
@@ -0,0 +1,33 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import numpy as np
+
+import awkward as ak
+
+
+def test_non_empty_string():
+    array = ak.Array(["this", "that", "foo", "bar"])
+    result = ak.to_numpy(array)
+    assert result.dtype == np.dtype("U4")
+    assert result.tolist() == array.tolist()
+
+
+def test_non_empty_bytestring():
+    array = ak.Array([b"this", b"that", b"foo", b"bar"])
+    result = ak.to_numpy(array)
+    assert result.dtype == np.dtype("S4")
+    assert result.tolist() == array.tolist()
+
+
+def test_empty_string():
+    array = ak.Array(["this", "that", "foo", "bar"])
+    result = ak.to_numpy(array[:0])
+    assert result.dtype == np.dtype("U1")
+    assert result.tolist() == []
+
+
+def test_empty_bytestring():
+    array = ak.Array([b"this", b"that", b"foo", b"bar"])
+    result = ak.to_numpy(array[:0])
+    assert result.dtype == np.dtype("S1")
+    assert result.tolist() == []
