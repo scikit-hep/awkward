4650c15cbd02b0a421aa99cab3f4a775d0e13b0e
Angus Hollands <goosey15@gmail.com>
2023-06-10 17:31:31 +0100

    test: add `axis=1` test

--- tests/test_2512_record_array_carry.py
+++ tests/test_2512_record_array_carry.py
@@ -5,26 +5,45 @@ import numpy as np
 import awkward as ak
 
 
-def test():
-    def _reduce_max_masked(array, mask):
-        assert mask
-        j = ak.from_regular(
-            ak.argmax(array["1"], axis=1, keepdims=True, mask_identity=True)
-        )
-        return ak.flatten(array[j], axis=1)
+def _reduce_max_masked(array, mask):
+    assert mask
+    j = ak.from_regular(
+        ak.argmax(array["1"], axis=1, keepdims=True, mask_identity=True)
+    )
+    return ak.flatten(array[j], axis=1)
 
-    behavior = {}
-    behavior[ak.max, "pair"] = _reduce_max_masked
 
+behavior = {}
+behavior[ak.max, "pair"] = _reduce_max_masked
+
+
+def test_axis_0():
     content = ak.contents.ListArray(
-        ak.index.Index64([0]),
-        ak.index.Index64([2]),
+        ak.index.Index64([0, 2]),
+        ak.index.Index64([2, 4]),
         ak.contents.ListOffsetArray(
-            ak.index.Index64([0, 3, 6]),
+            ak.index.Index64([0, 3, 6, 9, 11]),
             ak.contents.RecordArray(
                 [
-                    ak.contents.NumpyArray(np.arange(6, dtype=np.int64)),
-                    ak.contents.NumpyArray(np.linspace(0, 10, num=6, dtype=np.float64)),
+                    ak.contents.NumpyArray(np.arange(11, dtype=np.int64)),
+                    ak.contents.NumpyArray(
+                        np.array(
+                            [
+                                0.0,
+                                2.0,
+                                4.0,
+                                6.0,
+                                8.0,
+                                10.0,
+                                1.0,
+                                14.0,
+                                16.0,
+                                18.0,
+                                20.0,
+                            ],
+                            dtype=np.float64,
+                        )
+                    ),
                 ],
                 fields=None,
                 parameters={"__record__": "pair"},
@@ -41,21 +60,24 @@ def test():
         highlevel=False,
     )
 
-    content = ak.contents.ListArray(
+    expected_result = ak.contents.ListArray(
         ak.index.Index64([0]),
         ak.index.Index64([2]),
         ak.contents.ListArray(
             ak.index.Index64([0, 3]),
             ak.index.Index64([3, 6]),
             ak.contents.IndexedOptionArray(
-                ak.index.Index64([0, 2, 4, 1, 3, 5]),
+                ak.index.Index64([1, 5, 9, 3, 7, 10]),
                 ak.contents.RecordArray(
                     [
                         ak.contents.NumpyArray(
-                            np.array([0, 3, 1, 4, 2, 5], dtype=np.int64)
+                            np.array([0, 6, 3, 9, 1, 7, 4, 10, 2, 8, 5], dtype=np.int64)
                         ),
                         ak.contents.NumpyArray(
-                            np.array([0, 6, 2, 8, 4, 10], dtype=np.float64)
+                            np.array(
+                                [0, 1, 6, 18, 2, 14, 8, 20, 4, 16, 10],
+                                dtype=np.float64,
+                            )
                         ),
                     ],
                     fields=None,
@@ -64,4 +86,75 @@ def test():
             ),
         ),
     )
-    assert result.is_equal_to(content)
+    assert result.is_equal_to(expected_result)
+
+
+def test_axis_1():
+    content = ak.contents.ListArray(
+        ak.index.Index64([0, 2]),
+        ak.index.Index64([2, 4]),
+        ak.contents.ListOffsetArray(
+            ak.index.Index64([0, 3, 6, 9, 11]),
+            ak.contents.RecordArray(
+                [
+                    ak.contents.NumpyArray(np.arange(11, dtype=np.int64)),
+                    ak.contents.NumpyArray(
+                        np.array(
+                            [
+                                0.0,
+                                2.0,
+                                4.0,
+                                6.0,
+                                8.0,
+                                10.0,
+                                1.0,
+                                14.0,
+                                16.0,
+                                18.0,
+                                20.0,
+                            ],
+                            dtype=np.float64,
+                        )
+                    ),
+                ],
+                fields=None,
+                parameters={"__record__": "pair"},
+            ),
+        ),
+    )
+
+    result = ak.max(
+        content,
+        axis=1,
+        keepdims=True,
+        mask_identity=True,
+        behavior=behavior,
+        highlevel=False,
+    )
+
+    expected_result = ak.contents.RegularArray(
+        ak.contents.ListArray(
+            ak.index.Index64([0, 3]),
+            ak.index.Index64([3, 6]),
+            ak.contents.IndexedOptionArray(
+                ak.index.Index64([1, 5, 9, 3, 7, 10]),
+                ak.contents.RecordArray(
+                    [
+                        ak.contents.NumpyArray(
+                            np.array([0, 3, 6, 9, 1, 4, 7, 10, 2, 5, 8], dtype=np.int64)
+                        ),
+                        ak.contents.NumpyArray(
+                            np.array(
+                                [0, 6, 1, 18, 2, 8, 14, 20, 4, 10, 16],
+                                dtype=np.float64,
+                            )
+                        ),
+                    ],
+                    fields=None,
+                    parameters={"__record__": "pair"},
+                ),
+            ),
+        ),
+        size=1,
+    )
+    assert result.is_equal_to(expected_result)
