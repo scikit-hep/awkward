c57cd690e1f5d7a964c01bc6b0c9bf636342a900
Angus Hollands <goosey15@gmail.com>
2023-06-21 10:45:32 +0100

    test: add large form test

--- tests/test_2536_select_columns.py
+++ tests/test_2536_select_columns.py
@@ -84,3 +84,44 @@ def test():
     assert form.select_columns([]) == ak.forms.from_dict(
         {"class": "RecordArray", "fields": [], "contents": []}
     )
+
+
+def test_very_large_record():
+    form = ak.forms.from_dict(
+        {
+            "class": "RecordArray",
+            "fields": [f"x_{i}" for i in range(10_000)],
+            "contents": ["int64"] * 10_000,
+        }
+    )
+    assert form.select_columns(["*"]) == form
+    assert form.select_columns(["x*"]) == form
+    assert form.select_columns(["x_[0-9]"]) == ak.forms.from_dict(
+        {
+            "class": "RecordArray",
+            "fields": [
+                "x_0",
+                "x_1",
+                "x_2",
+                "x_3",
+                "x_4",
+                "x_5",
+                "x_6",
+                "x_7",
+                "x_8",
+                "x_9",
+            ],
+            "contents": [
+                "int64",
+                "int64",
+                "int64",
+                "int64",
+                "int64",
+                "int64",
+                "int64",
+                "int64",
+                "int64",
+                "int64",
+            ],
+        }
+    )
