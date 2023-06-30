04d410149181d55b190339e79e14564c5963953d
Angus Hollands <goosey15@gmail.com>
2023-06-30 14:13:29 +0100

    refactor: just use `parameters_intersect`

--- src/awkward/_connect/numpy.py
+++ src/awkward/_connect/numpy.py
@@ -22,6 +22,7 @@ from awkward._behavior import (
 from awkward._categorical import as_hashable
 from awkward._layout import wrap_layout
 from awkward._nplikes import to_nplike
+from awkward._parameters import parameters_intersect
 from awkward._regularize import is_non_string_like_iterable
 from awkward._typing import Any, Iterator, Mapping
 from awkward._util import Sentinel
@@ -392,7 +393,9 @@ def array_ufunc(ufunc, method: str, inputs, kwargs: dict[str, Any]):
             nplike = backend.nplike
 
             # Broadcast parameters against one another
-            (parameters,) = ak._broadcasting.intersection_parameters_factory(inputs, 1)
+            parameters = functools.reduce(
+                parameters_intersect, [x._parameters for x in contents]
+            )
 
             args = []
             for x in inputs:
