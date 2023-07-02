84b33eb1384aa28b5263967b30d04ddad4ec133b
Angus Hollands <goosey15@gmail.com>
2023-07-02 23:16:57 +0100

    test: add test

---
+++ tests/test_2560_minimal_listarray.py
@@ -0,0 +1,15 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import pytest  # noqa: F401
+
+import awkward as ak
+from awkward._nplikes.shape import unknown_length
+
+
+def test():
+    layout = ak.to_layout([[1, 2, 3, 4], [5, 6, 7, 8], [4, 5, 6, 9]]).to_typetracer(
+        forget_length=True
+    )
+    result = layout.to_RegularArray()
+    assert result.size is unknown_length
+    assert result.length is unknown_length
