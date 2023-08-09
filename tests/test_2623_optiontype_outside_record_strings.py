fc769e27da5022f2a4971ff5cef03f03573b0a0f
Angus Hollands <goosey15@gmail.com>
2023-08-09 11:17:26 +0100

    test: add test for fix

---
+++ tests/test_2623_optiontype_outside_record_strings.py
@@ -0,0 +1,33 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import awkward as ak
+
+
+def test():
+    record = ak.zip(
+        {
+            "x": ak.mask(["foo", "bar", "world"], [True, True, False]),
+            "y": ak.mask(["do", "re", "mi"], [False, True, True]),
+        },
+        optiontype_outside_record=True,
+    )
+    assert record.to_list() == [None, {"x": "bar", "y": "re"}, None]
+    assert record.type == ak.types.ArrayType(
+        ak.types.OptionType(
+            ak.types.RecordType(
+                [
+                    ak.types.ListType(
+                        ak.types.NumpyType("uint8", parameters={"__array__": "char"}),
+                        parameters={"__array__": "string"},
+                    ),
+                    ak.types.ListType(
+                        ak.types.NumpyType("uint8", parameters={"__array__": "char"}),
+                        parameters={"__array__": "string"},
+                    ),
+                ],
+                ["x", "y"],
+            )
+        ),
+        3,
+        None,
+    )
