40acf7584869c805d108d880d0f45199da55f887
Jim Pivarski <jpivarski@gmail.com>
2023-08-09 08:38:03 -0500

    Also test IndexedOptionArray.

--- tests/test_2623_optiontype_outside_record_strings.py
+++ tests/test_2623_optiontype_outside_record_strings.py
@@ -3,12 +3,39 @@
 import awkward as ak
 
 
-def test():
+def test_ByteMaskedArray():
     record = ak.zip(
         {
             "x": ak.mask(["foo", "bar", "world"], [True, True, False]),
             "y": ak.mask(["do", "re", "mi"], [False, True, True]),
         },
+        optiontype_outside_record=True,
+    )
+    assert record.to_list() == [None, {"x": "bar", "y": "re"}, None]
+    assert record.type == ak.types.ArrayType(
+        ak.types.OptionType(
+            ak.types.RecordType(
+                [
+                    ak.types.ListType(
+                        ak.types.NumpyType("uint8", parameters={"__array__": "char"}),
+                        parameters={"__array__": "string"},
+                    ),
+                    ak.types.ListType(
+                        ak.types.NumpyType("uint8", parameters={"__array__": "char"}),
+                        parameters={"__array__": "string"},
+                    ),
+                ],
+                ["x", "y"],
+            )
+        ),
+        3,
+        None,
+    )
+
+
+def test_IndexedOptionArray():
+    record = ak.zip(
+        {"x": ["foo", "bar", None], "y": [None, "re", "mi"]},
         optiontype_outside_record=True,
     )
     assert record.to_list() == [None, {"x": "bar", "y": "re"}, None]
