984f8f3e2b4936cad98e690b0bff69d55b39ee9a
Angus Hollands <goosey15@gmail.com>
2023-08-10 00:23:52 +0100

    test: add test

---
+++ tests/test_2632_unflatten_length.py
@@ -0,0 +1,15 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import numpy as np
+import pytest  # noqa: F401
+
+import awkward as ak
+
+
+def test():
+    x = ak.from_numpy(np.arange(3, dtype=np.int64))
+    y = ak.unflatten(x, len(x))
+    assert y.to_list() == [[0, 1, 2]]
+    assert y.type == ak.types.ArrayType(
+        ak.types.RegularType(ak.types.NumpyType("int64"), 3), 1
+    )
