2d21296e224dc85044ae9df1a052454af4a36520
Angus Hollands <goosey15@gmail.com>
2023-08-11 13:11:41 +0100

    feat: add CPU kernel for `to_numpy` support for strings/bytestrings (#2631)

---
+++ tests/test_2631_vectorised_to_numpy_strings.py
@@ -0,0 +1,21 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import numpy as np
+
+import awkward as ak
+
+
+def test_string():
+    source = ak.Array(["abc$¢€", "d¢#", "€e¢", "💰💰"])
+    result = source.to_numpy(False)
+    expected = np.array(["abc$¢€", "d¢#", "€e¢", "💰💰"])
+    assert result.dtype == expected.dtype
+    np.testing.assert_equal(result, expected)
+
+
+def test_bytestring():
+    source = ak.Array([b"foo", b"bar", b"catastrophic", b"\x03\x07"])
+    result = source.to_numpy(False)
+    expected = np.array([b"foo", b"bar", b"catastrophic", b"\x03\x07"])
+    assert result.dtype == expected.dtype
+    np.testing.assert_equal(result, expected)
