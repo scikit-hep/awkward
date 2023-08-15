a28499228968e51daed8ebd5f91bf6aa7527783e
Angus Hollands <goosey15@gmail.com>
2023-08-15 14:07:14 +0100

    test: `importorskip`

--- tests/test_2646_from_parquet_highlevel.py
+++ tests/test_2646_from_parquet_highlevel.py
@@ -1,8 +1,10 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
 
-import pytest  # noqa: F401
+import pytest
 
 import awkward as ak
+
+pytest.importorskip("pyarrow")
 
 
 def test(tmp_path):
