cc3379b0544e19ce4f12309255a6dce7df97c9b4
Angus Hollands <goosey15@gmail.com>
2023-08-15 13:48:24 +0100

    test: add simple test

---
+++ tests/test_2646_from_parquet_highlevel.py
@@ -0,0 +1,18 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import pytest  # noqa: F401
+
+import awkward as ak
+
+
+def test(tmp_path):
+    ak.to_parquet([1, 2, 3], tmp_path / "test.parquet", extensionarray=True)
+    ak.to_parquet([1, 2, 3], tmp_path / "test2.parquet", extensionarray=True)
+
+    # Single paths
+    layout = ak.from_parquet(tmp_path / "test.parquet", highlevel=False)
+    assert isinstance(layout, ak.contents.Content)
+
+    # Multiple paths
+    layout = ak.from_parquet(tmp_path / "test*.parquet", highlevel=False)
+    assert isinstance(layout, ak.contents.Content)
