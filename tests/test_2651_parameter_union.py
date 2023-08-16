735a3776427fc1da2db48fdd6c684b80f4245f4c
Angus Hollands <goosey15@gmail.com>
2023-08-17 00:21:48 +0100

    fix: support non-hashable values in `parameters_union` (#2651)

---
+++ tests/test_2651_parameter_union.py
@@ -0,0 +1,20 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import numpy as np
+import pytest  # noqa: F401
+
+import awkward as ak
+
+
+def test():
+    layout = ak.contents.IndexedArray(
+        ak.index.Index64([0, 1, 2]),
+        ak.contents.NumpyArray(np.array([1, 2, 3], dtype=np.uint32)),
+        parameters={"foo": {"bar": "baz"}},
+    )
+    result = layout.project()
+    assert result.is_equal_to(
+        ak.contents.NumpyArray(
+            np.array([1, 2, 3], dtype=np.uint32), parameters={"foo": {"bar": "baz"}}
+        )
+    )
