61c3660aa28795ef4c6d6aa11c9e59f110832e39
Angus Hollands <goosey15@gmail.com>
2023-08-17 00:25:06 +0100

    fix: remove unneeded cast (#2653)
    * fix: remove unneeded cast
    
    * test: ensure slice behaves properly
    
    * test: explicitly set dtypes

---
+++ tests/test_2653_slice_listoffsetarray.py
@@ -0,0 +1,44 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import numpy as np
+import pytest  # noqa: F401
+
+import awkward as ak
+
+
+def test():
+    layout = ak.contents.RecordArray(
+        [
+            ak.contents.ListOffsetArray(
+                ak.index.Index64([0, 3]),
+                ak.contents.NumpyArray(np.array([1, 2, 3], dtype=np.uint16)),
+            ),
+            ak.contents.ListOffsetArray(
+                ak.index.Index64([0, 2]),
+                ak.contents.NumpyArray(np.array([4, 5], dtype=np.uint16)),
+            ),
+        ],
+        ["x", "y"],
+    )
+    sliced = layout[..., np.newaxis]
+    assert sliced.is_equal_to(
+        ak.contents.RecordArray(
+            [
+                ak.contents.RegularArray(
+                    ak.contents.ListOffsetArray(
+                        ak.index.Index64([0, 3]),
+                        ak.contents.NumpyArray(np.array([1, 2, 3], dtype=np.uint16)),
+                    ),
+                    1,
+                ),
+                ak.contents.RegularArray(
+                    ak.contents.ListOffsetArray(
+                        ak.index.Index64([0, 2]),
+                        ak.contents.NumpyArray(np.array([4, 5], dtype=np.uint16)),
+                    ),
+                    1,
+                ),
+            ],
+            ["x", "y"],
+        )
+    )
