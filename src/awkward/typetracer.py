dea0139a9413cdfb51081caf643d47c9a10cc199
Angus Hollands <goosey15@gmail.com>
2023-08-27 13:47:19 +0100

    feat!: deprecate `forget_length`, add parameters to `typetracer_with_report` (#2671)
    * feat!: deprecate the `forget_length` argument of `typetracer_with_report`
    
    * docs: fix typo in `from_buffers`
    
    * feat: add `highlevel`, `behavior` arguments to `typetracer_with_report`.
    
    Support non-form forms
    
    * test: add a comprehensive test
    
    * feat: add ak.typetracer.typetracer_from_form
    
    * fix: make `highlevel` True by default

--- src/awkward/typetracer.py
+++ src/awkward/typetracer.py
@@ -11,6 +11,7 @@ __all__ = [
     "touch_data",
 ]
 
+import awkward.forms
 from awkward._backends.typetracer import TypeTracerBackend
 from awkward._behavior import behavior_of
 from awkward._do import touch_data as _touch_data
@@ -22,12 +23,17 @@ from awkward._nplikes.typetracer import (
     TypeTracerReport,
     is_unknown_array,
     is_unknown_scalar,
-    typetracer_with_report,
+)
+from awkward._nplikes.typetracer import (
+    typetracer_with_report as _typetracer_with_report,
 )
 from awkward._typing import TypeVar
+from awkward._util import UNSET
+from awkward.contents import Content
 from awkward.forms import Form
 from awkward.highlevel import Array, Record
 from awkward.operations.ak_to_layout import to_layout
+from awkward.types.numpytype import is_primitive
 
 T = TypeVar("T", Array, Record)
 
@@ -100,4 +106,73 @@ def touch_data(array, *, highlevel: bool = True, behavior=None) -> T:
     behavior = behavior_of(array, behavior=behavior)
     layout = to_layout(array, allow_other=False)
     _touch_data(layout)
+    return wrap_layout(layout, behavior=behavior, highlevel=highlevel)
+
+
+def typetracer_with_report(
+    form, forget_length: bool = UNSET, *, highlevel: bool = False, behavior=None
+) -> tuple[Content, TypeTracerReport]:
+    """
+    Args:
+        form (#ak.forms.Form or str/dict equivalent): The form of the Awkward
+            Array to build a typetracer-backed array from.
+        highlevel (bool): If True, return an #ak.Array; otherwise, return
+            a low-level #ak.contents.Content subclass.
+        behavior (None or dict): Custom #ak.behavior for the output array, if
+            high-level.
+
+    Returns a typetracer array and associated report object built from a form
+    with labelled form keys.
+    """
+    if isinstance(form, str):
+        if is_primitive(form):
+            form = awkward.forms.NumpyForm(form)
+        else:
+            form = awkward.forms.from_json(form)
+    elif isinstance(form, dict):
+        form = awkward.forms.from_dict(form)
+    elif not isinstance(form, awkward.forms.Form):
+        raise TypeError(
+            "'form' argument must be a Form or its Python dict/JSON string representation"
+        )
+
+    if forget_length is UNSET:
+        forget_length = True
+    else:
+        deprecate(
+            "passing `forget_length` to typetracer_with_report is deprecated. "
+            "In future, this argument will be removed, and the function will "
+            "always forget lengths",
+            "2.5.0",
+        )
+    layout, report = _typetracer_with_report(form, forget_length=forget_length)
+    return wrap_layout(layout, behavior=behavior, highlevel=highlevel), report
+
+
+def typetracer_from_form(form, *, highlevel: bool = True, behavior=None):
+    """
+    Args:
+        form (#ak.forms.Form or str/dict equivalent): The form of the Awkward
+            Array to build a typetracer-backed array from.
+        highlevel (bool): If True, return an #ak.Array; otherwise, return
+            a low-level #ak.contents.Content subclass.
+        behavior (None or dict): Custom #ak.behavior for the output array, if
+            high-level.
+
+    Returns a typetracer array built from a form.
+    """
+    if isinstance(form, str):
+        if is_primitive(form):
+            form = awkward.forms.NumpyForm(form)
+        else:
+            form = awkward.forms.from_json(form)
+    elif isinstance(form, dict):
+        form = awkward.forms.from_dict(form)
+    elif not isinstance(form, awkward.forms.Form):
+        raise TypeError(
+            "'form' argument must be a Form or its Python dict/JSON string representation"
+        )
+
+    layout = form.length_zero_array(highlevel=False).to_typetracer(forget_length=True)
+
     return wrap_layout(layout, behavior=behavior, highlevel=highlevel)
