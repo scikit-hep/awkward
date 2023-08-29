fix: guard `broadcast_and_apply` from mixed backends

src/awkward/operations/ak_linear_fit.py
@@ -76,22 +76,28 @@ def linear_fit(x, y, weight=None, axis=None, *, keepdims=False, mask_identity=Fa
 def _impl(x, y, weight, axis, keepdims, mask_identity):
     axis = regularize_axis(axis)
     behavior = behavior_of(x, y, weight)
+    backend = backend_of(x, y, weight, coerce_to_common=True)
     x = ak.highlevel.Array(
-        ak.operations.to_layout(x, allow_record=False, allow_other=False),
+        ak.operations.to_layout(x, allow_record=False, allow_other=False).to_backend(
+            backend
+        ),
         behavior=behavior,
     )
     y = ak.highlevel.Array(
-        ak.operations.to_layout(y, allow_record=False, allow_other=False),
+        ak.operations.to_layout(y, allow_record=False, allow_other=False).to_backend(
+            backend
+        ),
         behavior=behavior,
     )
     if weight is not None:
         weight = ak.highlevel.Array(
-            ak.operations.to_layout(weight, allow_record=False, allow_other=False),
+            ak.operations.to_layout(
+                weight, allow_record=False, allow_other=False
+            ).to_backend(backend),
             behavior=behavior,
         )
 
     with np.errstate(invalid="ignore", divide="ignore"):
-        backend = backend_of(x, y, weight)
         if weight is None:
             sumw = ak.operations.ak_count._impl(
                 x,