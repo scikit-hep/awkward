519bba6ed2eec4e227994d2fd1a62b2a51f15e20
Angus Hollands <goosey15@gmail.com>
2023-09-01 21:13:25 +0100

    feat: add support for custom picklers (#2682)
    * chore: move metadata to proper destination
    
    * feat: add support for custom pickling
    
    * feat: add support for custom pickling
    
    * test: ensure custom pickler works

---
+++ tests/test_2682_custom_pickler.py
@@ -0,0 +1,93 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+import multiprocessing
+import os
+import pickle
+import sys
+from concurrent.futures import ProcessPoolExecutor
+
+import pytest
+
+import awkward as ak
+
+
+def _init_process_with_pickler(pickler_source: str, tmp_path):
+    # Create custom plugin
+    (tmp_path / "impl_pickler.py").write_bytes(pickler_source.encode("UTF-8"))
+    dist_info = tmp_path / "impl_pickler-0.0.0.dist-info"
+    dist_info.mkdir()
+    (dist_info / "entry_points.txt").write_bytes(
+        b"[awkward.pickle.reduce]\nimpl = impl_pickler:plugin\n"
+    )
+    sys.path.insert(0, os.fsdecode(tmp_path))
+
+
+def _pickle_complex_array_and_return_form_impl():
+    array = ak.Array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])[[0, 2]]
+    return pickle.loads(pickle.dumps(array)).layout.form
+
+
+def pickle_complex_array_and_return_form(pickler_source, tmp_path):
+    """Create a new (spawned) process, and register the given pickler source
+    via entrypoints"""
+    with ProcessPoolExecutor(
+        1,
+        initializer=_init_process_with_pickler,
+        initargs=(pickler_source, tmp_path),
+        # Don't fork the current process with all of its state
+        mp_context=multiprocessing.get_context("spawn"),
+    ) as executor:
+        pickle_future = executor.submit(_pickle_complex_array_and_return_form_impl)
+        return pickle_future.result()
+
+
+def test_default_pickler():
+    assert _pickle_complex_array_and_return_form_impl() == ak.forms.from_dict(
+        {"class": "ListOffsetArray", "offsets": "i64", "content": "int64"}
+    )
+
+
+def test_noop_pickler(tmp_path):
+    assert (
+        pickle_complex_array_and_return_form(
+            """
+def plugin(obj, protocol: int):
+    return NotImplemented""",
+            tmp_path,
+        )
+        == ak.forms.from_dict(
+            {"class": "ListOffsetArray", "offsets": "i64", "content": "int64"}
+        )
+    )
+
+
+def test_non_packing_pickler(tmp_path):
+    assert (
+        pickle_complex_array_and_return_form(
+            """
+def plugin(obj, protocol):
+    import awkward as ak
+    if isinstance(obj, ak.Array):
+        form, length, container = ak.to_buffers(obj)
+        return (
+            object.__new__,
+            (ak.Array,),
+            (form.to_dict(), length, container, obj.behavior),
+        )
+    else:
+        return NotImplemented""",
+            tmp_path,
+        )
+        == ak.forms.from_dict(
+            {"class": "ListArray", "starts": "i64", "stops": "i64", "content": "int64"}
+        )
+    )
+
+
+def test_malformed_pickler(tmp_path):
+    with pytest.raises(RuntimeError, match=r"malformed pickler!"):
+        pickle_complex_array_and_return_form(
+            """
+def plugin(obj, protocol: int):
+    raise RuntimeError('malformed pickler!')""",
+            tmp_path,
+        )
