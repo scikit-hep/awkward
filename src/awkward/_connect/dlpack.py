98fce245877dc4c36f59ae24bc3a183c2011a7d2
Angus Hollands <goosey15@gmail.com>
2023-09-06 21:00:24 +0200

    chore: drop unused functions

--- src/awkward/_connect/dlpack.py
+++ src/awkward/_connect/dlpack.py
@@ -1,11 +1,8 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
 from __future__ import annotations
 
-__all__ = ("DLPackDevice", "get_layout_device", "to_dlpack")
+__all__ = ("DLPackDevice",)
 from enum import IntEnum
-
-from awkward._typing import Any
-from awkward.contents import Content
 
 
 class DLPackDevice(IntEnum):
@@ -19,26 +16,3 @@ class DLPackDevice(IntEnum):
     ROCM = 10  # GPU
     ROCM_PINNED = 11  # GPU & CPU
     CUDA_MANAGED = 13  # GPU & CPU
-
-
-def get_layout_device(layout: Content) -> tuple[int, int]:
-    while True:
-        if layout.is_numpy:
-            break
-        elif layout.is_regular:
-            layout = layout.content
-        else:
-            raise TypeError(
-                "Cannot determine the DLPack device for this array layout."
-                "DLPack is only supported for regular arrays."
-            )
-
-    return layout.data.__dlpack_device__()
-
-
-def to_dlpack(layout: Content, stream: Any = None) -> Any:
-    array = layout.to_backend_array(allow_missing=False)
-    if stream is None:
-        return array.__dlpack__()
-    else:
-        return array.__dlpack__(stream)
