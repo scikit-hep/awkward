7f1402ab4ff753f0a529c82aa623cf87a6c5cc8b
Angus Hollands <goosey15@gmail.com>
2023-09-14 09:05:36 +0100

    refactor: split singleton into public and private implementations1

--- src/awkward/_singleton.py
+++ src/awkward/_singleton.py
@@ -3,19 +3,37 @@ from __future__ import annotations
 from awkward._typing import Protocol, Self
 
 
-class Singleton(Protocol):
-    _instance: type[Self]
-
-    def __new__(cls, *args, **kwargs):
-        return super().__new__(cls, *args, **kwargs)
-
-    def __reduce__(self):
-        return type(self).instance, ()
+class PrivateSingleton(Protocol):
+    _instance: Self
 
     @classmethod
-    def instance(cls) -> Self:
+    def _new(cls, *args, **kwargs) -> Self:
+        if hasattr(cls, "_instance"):
+            raise RuntimeError
+
+        self = super().__new__(cls)
+        self.__init__(*args, **kwargs)
+        cls._instance = self
+
+        return self
+
+    def __new__(cls, *args, **kwargs):
+        raise TypeError(
+            f"internal_error: {cls.__name__} class should never be directly instantiated."
+        )
+
+    def __reduce__(self):
+        return type(self)._reduce_constructor, ()
+
+    @classmethod
+    def _reduce_constructor(cls) -> Self:
+        return cls._instance
+
+
+class PublicSingleton(PrivateSingleton, Protocol):
+    @classmethod
+    def instance(cls, *args, **kwargs) -> Self:
         try:
             return cls._instance
         except AttributeError:
-            cls._instance = cls()
-            return cls._instance
+            return cls._new(*args, **kwargs)
