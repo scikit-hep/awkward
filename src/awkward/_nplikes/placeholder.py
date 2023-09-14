5749fdbee356d25edb8d30e0687bb3e8543f6338
Angus Hollands <goosey15@gmail.com>
2023-09-14 17:14:21 +0100

    fix: ensure that slicing enforces typetracer invariant

--- src/awkward/_nplikes/placeholder.py
+++ src/awkward/_nplikes/placeholder.py
@@ -67,13 +67,29 @@ class PlaceholderArray(ArrayLike):
         return type(self)(self._nplike, shape, dtype)
 
     def __getitem__(self, index):
+        # Typetracers permit slices that don't touch data or shapes
         if isinstance(index, slice):
-            if self._shape[0] is unknown_length:
-                return type(self)(self._nplike, self._shape, self._dtype)
+            length = self._shape[0]
+
+            # Unknown-length placeholders never need a known shape, so return unknown!
+            if length is unknown_length:
+                raise AssertionError(
+                    "placeholder arrays that are sliced should have known shapes"
+                )
+            # Known-length placeholders *always* need a known shape
+            elif (
+                index.start is unknown_length
+                or index.stop is unknown_length
+                or index.step is unknown_length
+            ):
+                raise AssertionError(
+                    "known-length placeholders should never encounter unknown lengths in slices"
+                )
             else:
-                start, stop, step = index.indices(self._shape[0])
-                new_shape = ((stop - start) // step,)
-                return type(self)(self._nplike, new_shape, self._dtype)
+                start, stop, step = index.indices(length)
+                new_length = (stop - start) // step
+
+            return type(self)(self._nplike, (new_length,), self._dtype)
         else:
             raise TypeError(
                 f"{type(self).__name__} supports only trivial slices, not {type(index).__name__}"
