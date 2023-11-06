548c8901bf7e1aa34ede975b214e54d941a38f20
Angus Hollands <goosey15@gmail.com>
2023-11-06 10:25:56 +0000

    test: add simple test

---
+++ tests/test_2799_numba_ufunc_resolution.py
@@ -0,0 +1,45 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/main/LICENSE
+
+import numpy as np
+import packaging.version
+import pytest
+
+import awkward as ak
+
+numba = pytest.importorskip("numba")
+
+NUMBA_HAS_NEP_50 = packaging.version.parse(
+    numba.__version__
+) >= packaging.version.Version("0.59.0")
+
+
+@pytest.mark.skipif(not NUMBA_HAS_NEP_50, reason="Numba does not have NEP-50 support")
+def test_numba_ufunc_nep_50():
+    raise NotImplementedError
+
+    @numba.vectorize
+    def add(x, y):
+        return x + y
+
+    array = ak.values_astype([[1, 2, 3], [4]], np.int8)
+
+    # FIXME: what error will Numba throw here for an out-of-bounds integer?
+    with pytest.warns(FutureWarning, match=r"not create a writeable array"):
+        result = add(array, 2**63 - 1)
+
+    flattened = ak.to_numpy(ak.flatten(result))
+    assert flattened.dtype == np.dtype(np.int8)
+
+
+@pytest.mark.skipif(NUMBA_HAS_NEP_50, reason="Numba has NEP-50 support")
+def test_numba_ufunc_legacy():
+    @numba.vectorize
+    def add(x, y):
+        return x + y
+
+    array = ak.values_astype([[1, 2, 3], [4]], np.int8)
+    with pytest.warns(FutureWarning, match=r"not create a writeable array"):
+        result = add(array, 2**63 - 1)
+
+    flattened = ak.to_numpy(ak.flatten(result))
+    assert flattened.dtype == np.dtype(np.int64)
