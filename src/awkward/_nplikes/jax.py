ec69835afc11eb3d765ac895e7dd2ef473831ec3
Angus Hollands <goosey15@gmail.com>
2023-12-11 12:29:58 +0000

    chore: bump pylint, appease inheritance
    We use this heirarchy internally and it feels sensible

--- src/awkward/_nplikes/jax.py
+++ src/awkward/_nplikes/jax.py
@@ -11,7 +11,7 @@ from awkward._typing import Final, cast
 
 
 @register_nplike
-class Jax(ArrayModuleNumpyLike):
+class Jax(ArrayModuleNumpyLike):  # pylint: disable=too-many-ancestors
     is_eager: Final = True
     supports_structured_dtypes: Final = False
 
