34410044d563e075f943d7ff12eb49b783bb66ba
Zoë Bilodeau <zobilodeau@gmail.com>
2023-12-13 16:42:02 +0100

    added to_parquet_dataset, passes simple test

---
+++ src/awkward/operations/ak_to_parquet_dataset.py
@@ -0,0 +1,124 @@
+from __future__ import annotations
+
+__all__ = ("to_parquet_dataset",)
+
+
+def write_metadata(dir_path, fs, *metas, global_metadata=True):
+    """Generate metadata file(s) from list of arrow metadata instances"""
+    assert metas
+    md = metas[0]
+    with fs.open("/".join([dir_path, "_common_metadata"]), "wb") as fil:
+        md.write_metadata_file(fil)
+    if global_metadata:
+        for meta in metas[1:]:
+            md.append_row_groups(meta)
+        with fs.open("/".join([dir_path, "_metadata"]), "wb") as fil:
+            md.write_metadata_file(fil)
+
+
+def to_parquet_dataset(directory, filenames=None, filename_extension=".parquet"):
+    """
+    Args:
+        directory (str or Path): A local directory in which to write `_common_metadata`
+            and `_metadata`, making the directory of Parquet files into a dataset.
+        filenames (None or list of str or Path): If None, the `directory` will be
+            recursively searched for files ending in `filename_extension` and
+            sorted lexicographically. Otherwise, this explicit list of files is
+            taken and row-groups are concatenated in its given order. If any
+            filenames are relative, they are interpreted relative to `directory`.
+        filename_extension (str): Filename extension (including `.`) to use to
+            search for files recursively. Ignored if `filenames` is None.
+
+    Creates a `_common_metadata` and a `_metadata` in a directory of Parquet files.
+
+    The `_common_metadata` contains the schema that all files share. (If the files
+    have different schemas, this function raises an exception.)
+
+    The `_metadata` contains row-group metadata used to seek to specific row-groups
+    within the multi-file dataset.
+    """
+
+    # Implementation
+
+    from os import fsdecode, path
+
+    import awkward._connect.pyarrow
+
+    pyarrow_parquet = awkward._connect.pyarrow.import_pyarrow_parquet(
+        "ak.to_parquet_dataset"
+    )
+
+    try:  # Second is probably better...
+        directory = fsdecode(directory)
+    except TypeError:
+        raise TypeError(
+            f"'directory' argument of 'ak.to_parquet' must be a path-like, not {type(directory).__name__}"
+        ) from None
+
+    directory = _regularize_path(directory)
+    if not path.isdir(directory):
+        raise ValueError(
+            f"{directory!r} is not a local filesystem directory" + {__file__}
+        )
+
+    if filenames is None:
+        import glob
+
+        filenames = sorted(
+            glob.glob(directory + f"/**/*{filename_extension}", recursive=True)
+        )
+    else:
+        filenames = [_regularize_path(x) for x in filenames]
+        filenames = [x if path.isabs(x) else path.join(directory, x) for x in filenames]
+
+    relpaths = [path.relpath(x, directory) for x in filenames]
+    schema, metadata_collector = _common_parquet_schema(
+        pyarrow_parquet, filenames, relpaths
+    )
+
+    pyarrow_parquet.write_metadata(
+        schema,
+        path.join(directory, "_metadata"),
+        metadata_collector=metadata_collector,
+    )
+
+
+def _regularize_path(path):
+    import os
+
+    if isinstance(path, getattr(os, "PathLike", ())):
+        path = os.fspath(path)
+
+    elif hasattr(path, "__fspath__"):
+        path = path.__fspath__()
+
+    elif path.__class__.__module__ == "pathlib":
+        import pathlib
+
+        if isinstance(path, pathlib.Path):
+            path = str(path)
+
+    if isinstance(path, str):
+        path = os.path.expanduser(path)
+
+    return path
+
+
+def _common_parquet_schema(pq, filenames, relpaths):
+    assert len(filenames) != 0
+
+    schema = None
+    metadata_collector = []
+    for filename, relpath in zip(filenames, relpaths):
+        if schema is None:
+            schema = pq.ParquetFile(filename).schema_arrow
+            first_filename = filename
+        elif not schema.equals(pq.ParquetFile(filename).schema_arrow):
+            raise ValueError(
+                "schema in {} differs from the first schema (in {})".format(
+                    repr(filename), repr(first_filename)
+                )
+            )
+        metadata_collector.append(pq.read_metadata(filename))
+        metadata_collector[-1].set_file_path(relpath)
+    return schema, metadata_collector
---
+++ tests/test_to_parquet_dataset.py
@@ -0,0 +1,34 @@
+import awkward as ak
+import os
+
+import os
+def simple_test(path1, path2, path3):
+    array = ak.Array([[1.1, 2.2, 3.3], [], [4.4, 5.5]])
+    array1 = ak.Array([[1.1, 2.2, 3.3, 4.4], [4.0], [4.4, 5.5]])
+    array2 = ak.Array([[1.0, 3.0, 3.3, 4.4], [4.0], [4.4, 10.0], [11.11]])
+
+    ak.to_parquet(array, path1, parquet_compliant_nested=True)
+    ak.to_parquet(array1, path2, parquet_compliant_nested=True)
+    ak.to_parquet(array2, path3, parquet_compliant_nested=True)
+    try:
+        os.remove("/Users/zobil/Documents/awkward/tests/samples/to_parquet/_common_metadata")
+        os.remove("/Users/zobil/Documents/awkward/tests/samples/to_parquet/_metadata")
+    except:
+        print("not there")
+
+    no_metadata = ak.from_parquet("/Users/zobil/Documents/awkward/tests/samples/to_parquet")
+    assert no_metadata.tolist() == [[1, 2, 3], [], [4, 5], [6], [7, 8, 9, 10]]
+
+    ak.to_parquet_dataset("/Users/zobil/Documents/awkward/tests/samples/to_parquet")
+
+    assert os.path.exists("/Users/zobil/Documents/awkward/tests/samples/to_parquet/_common_metadata")
+    assert os.path.exists("/Users/zobil/Documents/awkward/tests/samples/to_parquet/_metadata")
+
+    with_metadata = ak.from_parquet("/Users/zobil/Documents/awkward/tests/samples/to_parquet")
+    assert with_metadata.tolist() == [[1, 2, 3], [], [4, 5], [6], [7, 8, 9, 10]]
+
+    os.remove(path1)
+    os.remove(path2)
+    os.remove(path3)
+    os.remove("/Users/zobil/Documents/awkward/tests/samples/to_parquet/_common_metadata")
+    os.remove("/Users/zobil/Documents/awkward/tests/samples/to_parquet/_metadata")
