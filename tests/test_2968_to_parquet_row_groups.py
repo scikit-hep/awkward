84caa7005ed73450b5a99ff21190118960cea8af
Zoë Bilodeau <zobilodeau@gmail.com>
2024-01-24 14:56:12 +0100

    added importerskip for uproot

--- tests/test_2968_to_parquet_row_groups.py
+++ tests/test_2968_to_parquet_row_groups.py
@@ -4,20 +4,19 @@ import math
 import os
 
 import pytest
-import uproot
-from skhep_testdata import data_path
 
 import awkward as ak
 
 pyarrow = pytest.importorskip("pyarrow")
-
+uproot = pytest.importorskip("uproot")
+skhep_testdata = pytest.importorskip("skhep_testdata")
 
 def HZZ_test(tmp_file, batch_size):
     iterator = uproot.iterate(
-        uproot.open(data_path("uproot-HZZ.root"))["events"], step_size=batch_size
+        uproot.open(skhep_testdata.data_path("uproot-HZZ.root"))["events"], step_size=batch_size
     )
 
-    data = uproot.open(data_path("uproot-HZZ.root"))["events"].arrays()
+    data = uproot.open(skhep_testdata.data_path("uproot-HZZ.root"))["events"].arrays()
 
     ak.to_parquet_row_groups(iterator, tmp_file, row_group_size=batch_size)
     test = ak.from_parquet(tmp_file)
@@ -35,6 +34,3 @@ def HZZ_test(tmp_file, batch_size):
     assert int(math.ceil(num_rows / batch_size)) == row_groups
 
     os.remove(tmp_file)
-
-
-HZZ_test("array.parquet", 100)
