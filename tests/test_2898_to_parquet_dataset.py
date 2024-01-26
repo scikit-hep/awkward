838f15644238ef10b2b73097b68900fd7111f8d0
Zoë Bilodeau <zobilodeau@gmail.com>
2024-01-26 10:28:42 +0100

    removed unnecessary check from test

--- tests/test_2898_to_parquet_dataset.py
+++ tests/test_2898_to_parquet_dataset.py
@@ -74,25 +74,6 @@ def complex_test(tmp_path):
         array2, os.path.join(tmp_path, "arr3.parquet"), parquet_compliant_nested=True
     )
 
-    no_metadata = ak.from_parquet(tmp_path)
-    assert no_metadata.tolist() != [
-        {"x": 1.1, "y": [1]},
-        {"x": 2.2, "y": [1, 2]},
-        {"x": 3.3, "y": [1, 2, 3]},
-        {"x": 1.8, "y": [3, 5, 6]},
-        {"x": 4.4, "y": [1, 2, 3, 4]},
-        {"x": 5.5, "y": [1, 2, 3, 4, 5]},
-    ]
-    no_metadata = ak.from_parquet(tmp_path)
-    assert no_metadata.tolist() != [
-        {"x": 1.1, "y": [1]},
-        {"x": 2.2, "y": [1, 2]},
-        {"x": 3.3, "y": [1, 2, 3]},
-        {"x": 1.8, "y": [3, 5, 6]},
-        {"x": 4.4, "y": [1, 2, 3, 4]},
-        {"x": 5.5, "y": [1, 2, 3, 4, 5]},
-    ]
-
     ak.to_parquet_dataset(tmp_path, ["arr1.parquet", "arr2.parquet", "arr3.parquet"])
 
     assert os.path.exists(os.path.join(tmp_path, "_common_metadata"))
