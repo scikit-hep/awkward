061952502c583726b93d9cd569da954a61a84f7e
ManasviGoyal <mg.manasvi@gmail.com>
2024-02-05 17:25:09 +0100

    fix: awkward_ListArray_compact_offsets kernel

--- src/awkward/_connect/cuda/cuda_kernels/awkward_ListArray_compact_offsets.cu
+++ src/awkward/_connect/cuda/cuda_kernels/awkward_ListArray_compact_offsets.cu
@@ -7,9 +7,12 @@ enum class LISTARRAY_COMPACT_OFFSETS_ERRORS {
 // BEGIN PYTHON
 // def f(grid, block, args):
 //     (tooffsets, fromstarts, fromstops, length, invocation_index, err_code) = args
-//     cuda_kernel_templates.get_function(fetch_specialization(["awkward_ListArray_compact_offsets_a", tooffsets.dtype, fromstarts.dtype, fromstops.dtype]))(grid, block, (tooffsets, fromstarts, fromstops, length, invocation_index, err_code))
-//     tooffsets = inclusive_scan(grid, block, (tooffsets, invocation_index, err_code))
+//     scan_in_array = cupy.empty(length, dtype=cupy.int64)
+//     cuda_kernel_templates.get_function(fetch_specialization(["awkward_ListArray_compact_offsets_a", tooffsets.dtype, fromstarts.dtype, fromstops.dtype]))(grid, block, (tooffsets, fromstarts, fromstops, length, scan_in_array, invocation_index, err_code))
+//     scan_in_array = cupy.cumsum(scan_in_array)
+//     cuda_kernel_templates.get_function(fetch_specialization(["awkward_ListArray_compact_offsets_b", tooffsets.dtype, fromstarts.dtype, fromstops.dtype]))(grid, block, (tooffsets, fromstarts, fromstops, length, scan_in_array, invocation_index, err_code))
 // out["awkward_ListArray_compact_offsets_a", {dtype_specializations}] = None
+// out["awkward_ListArray_compact_offsets_b", {dtype_specializations}] = None
 // END PYTHON
 
 template <typename T, typename C, typename U>
@@ -19,6 +22,30 @@ awkward_ListArray_compact_offsets_a(
     const C* fromstarts,
     const U* fromstops,
     int64_t length,
+    int64_t* scan_in_array,
+    uint64_t invocation_index,
+    uint64_t* err_code) {
+  if (err_code[0] == NO_ERROR) {
+    int64_t thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+    if (thread_id < length) {
+      C start = fromstarts[thread_id];
+      U stop = fromstops[thread_id];
+      if (stop < start) {
+        RAISE_ERROR(LISTARRAY_COMPACT_OFFSETS_ERRORS::ERROR_START_STOP)
+      }
+      scan_in_array[thread_id] = (stop - start);
+    }
+  }
+}
+
+template <typename T, typename C, typename U>
+__global__ void
+awkward_ListArray_compact_offsets_b(
+    T* tooffsets,
+    const C* fromstarts,
+    const U* fromstops,
+    int64_t length,
+    int64_t* scan_in_array,
     uint64_t invocation_index,
     uint64_t* err_code) {
   if (err_code[0] == NO_ERROR) {
@@ -30,7 +57,7 @@ awkward_ListArray_compact_offsets_a(
       if (stop < start) {
         RAISE_ERROR(LISTARRAY_COMPACT_OFFSETS_ERRORS::ERROR_START_STOP)
       }
-      tooffsets[thread_id + 1] = (stop - start);
+      tooffsets[thread_id + 1] = scan_in_array[thread_id];
     }
   }
 }
