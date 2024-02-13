cbd466a6e504cd369ddc73a7d767e278ec9791b3
ManasviGoyal <mg.manasvi@gmail.com>
2024-02-13 09:35:54 +0100

    fix: awkward_IndexedArray_flatten_none2empty

--- src/awkward/_connect/cuda/cuda_kernels/awkward_IndexedArray_flatten_none2empty.cu
+++ src/awkward/_connect/cuda/cuda_kernels/awkward_IndexedArray_flatten_none2empty.cu
@@ -8,7 +8,7 @@ enum class INDEXEDARRAY_FLATTEN_NONE2EMPTY_ERRORS {
 // def f(grid, block, args):
 //     (outoffsets, outindex, outindexlength, offsets, offsetslength, invocation_index, err_code) = args
 //     scan_in_array_k = cupy.empty(outindexlength, dtype=cupy.int64)
-//     scan_in_array_outoffsets = cupy.empty(outindexlength, dtype=cupy.int64)
+//     scan_in_array_outoffsets = cupy.empty(outindexlength + 1, dtype=cupy.int64)
 //     cuda_kernel_templates.get_function(fetch_specialization(["awkward_IndexedArray_flatten_none2empty_a", outoffsets.dtype, outindex.dtype, offsets.dtype]))(grid, block, (outoffsets, outindex, outindexlength, offsets, offsetslength, scan_in_array_k, scan_in_array_outoffsets, invocation_index, err_code))
 //     scan_in_array_k = cupy.cumsum(scan_in_array_k)
 //     scan_in_array_outoffsets = cupy.cumsum(scan_in_array_outoffsets)
@@ -32,18 +32,20 @@ awkward_IndexedArray_flatten_none2empty_a(
     uint64_t* err_code) {
   if (err_code[0] == NO_ERROR) {
     int64_t thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+    scan_in_array_outoffsets[0] = offsets[0];
+
     if (thread_id < outindexlength) {
       C idx = outindex[thread_id];
       if (idx < 0) {
         scan_in_array_k[thread_id] = 1;
-        scan_in_array_outoffsets[thread_id] = 0;
+        scan_in_array_outoffsets[thread_id + 1] = 0;
       } else if (idx + 1 >= offsetslength) {
         RAISE_ERROR(INDEXEDARRAY_FLATTEN_NONE2EMPTY_ERRORS::OFF_OUT_OF_RANGE)
       } else {
           T count =
             offsets[idx + 1] - offsets[idx];
         scan_in_array_k[thread_id] = 1;
-        scan_in_array_outoffsets[thread_id] = count;
+        scan_in_array_outoffsets[thread_id + 1] = count;
       }
     }
   }
@@ -63,16 +65,16 @@ awkward_IndexedArray_flatten_none2empty_b(
     uint64_t* err_code) {
   if (err_code[0] == NO_ERROR) {
     int64_t thread_id = blockIdx.x * blockDim.x + threadIdx.x;
-    outoffsets[0] = offsets[0];
+    outoffsets[0] = scan_in_array_outoffsets[0];
 
     if (thread_id < outindexlength) {
       C idx = outindex[thread_id];
       if (idx < 0) {
-        outoffsets[scan_in_array_k[thread_id]] = scan_in_array_outoffsets[thread_id];
+        outoffsets[scan_in_array_k[thread_id]] = scan_in_array_outoffsets[thread_id + 1];
       } else if (idx + 1 >= offsetslength) {
         RAISE_ERROR(INDEXEDARRAY_FLATTEN_NONE2EMPTY_ERRORS::OFF_OUT_OF_RANGE)
       } else {
-        outoffsets[scan_in_array_k[thread_id]] = scan_in_array_outoffsets[thread_id];
+        outoffsets[scan_in_array_k[thread_id]] = scan_in_array_outoffsets[thread_id + 1];
       }
     }
   }
