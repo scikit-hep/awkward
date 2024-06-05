0e8d0bfe5f93d057ec8dc4b6f45bd9f4f616c131
ManasviGoyal <mg.manasvi@gmail.com>
2024-06-05 09:43:45 +0200

    test: more slicing integration tests

--- tests-cuda/test_3140_cuda_jagged_and_masked_getitem.py
+++ tests-cuda/test_3140_cuda_jagged_and_masked_getitem.py
@@ -1253,3 +1253,279 @@ def test_0111_jagged_and_masked_getitem_missing():
             cuda_maskedarray,
         ].form
     )
+
+
+def test_0111_jagged_and_masked_getitem_new_slices():
+    content = ak.contents.NumpyArray(np.array([1, 0, 9, 3, 2, 2, 5], dtype=np.int64))
+    index = ak.index.Index64(np.array([6, 5, -1, 3, 2, -1, 0], dtype=np.int64))
+    indexedarray = ak.contents.IndexedOptionArray(index, content)
+    cuda_indexedarray = ak.to_backend(indexedarray, backend="cuda")
+
+    assert to_list(cuda_indexedarray) == [5, 2, None, 3, 9, None, 1]
+
+    offsets = ak.index.Index64(np.array([0, 4, 4, 7], dtype=np.int64))
+    listoffsetarray = ak.contents.ListOffsetArray(offsets, content)
+    cuda_listoffsetarray = ak.to_backend(listoffsetarray, backend="cuda")
+
+    assert to_list(cuda_listoffsetarray) == [[1, 0, 9, 3], [], [2, 2, 5]]
+
+    offsets = ak.index.Index64(np.array([1, 4, 4, 6], dtype=np.int64))
+    listoffsetarray = ak.contents.ListOffsetArray(offsets, content)
+    cuda_listoffsetarray = ak.to_backend(listoffsetarray, backend="cuda")
+
+    assert to_list(cuda_listoffsetarray) == [[0, 9, 3], [], [2, 2]]
+
+    starts = ak.index.Index64(np.array([1, 99, 5], dtype=np.int64))
+    stops = ak.index.Index64(np.array([4, 99, 7], dtype=np.int64))
+    listarray = ak.contents.ListArray(starts, stops, content)
+    cuda_listarray = ak.to_backend(listarray, backend="cuda")
+
+    assert to_list(cuda_listarray) == [[0, 9, 3], [], [2, 5]]
+
+
+def test_0111_jagged_and_masked_getitem_record():
+    array = ak.highlevel.Array(
+        [
+            {"x": [0, 1, 2], "y": [0.0, 1.1, 2.2, 3.3]},
+            {"x": [3, 4, 5, 6], "y": [4.4, 5.5]},
+            {"x": [7, 8], "y": [6.6, 7.7, 8.8, 9.9]},
+        ],
+        check_valid=True,
+    ).layout
+    cuda_array = ak.to_backend(array, "cuda", highlevel=False)
+
+    array2 = ak.highlevel.Array([[-1, 1], [0, 0, 1], [-1, -2]], check_valid=True).layout
+    array3 = ak.highlevel.Array(
+        [[-1, 1], [0, 0, None, 1], [-1, -2]], check_valid=True
+    ).layout
+    array4 = ak.highlevel.Array([[-1, 1], None, [-1, -2]], check_valid=True).layout
+    cuda_array2 = ak.to_backend(array2, "cuda")
+    cuda_array3 = ak.to_backend(array3, "cuda")
+    cuda_array4 = ak.to_backend(array4, "cuda")
+
+    assert to_list(cuda_array[cuda_array2]) == [
+        {"x": [2, 1], "y": [3.3, 1.1]},
+        {"x": [3, 3, 4], "y": [4.4, 4.4, 5.5]},
+        {"x": [8, 7], "y": [9.9, 8.8]},
+    ]
+    assert cuda_array.to_typetracer()[cuda_array2].form == cuda_array[cuda_array2].form
+    assert to_list(cuda_array[cuda_array3]) == [
+        {"x": [2, 1], "y": [3.3, 1.1]},
+        {"x": [3, 3, None, 4], "y": [4.4, 4.4, None, 5.5]},
+        {"x": [8, 7], "y": [9.9, 8.8]},
+    ]
+    assert cuda_array.to_typetracer()[cuda_array3].form == cuda_array[cuda_array3].form
+    assert to_list(cuda_array[cuda_array4]) == [
+        {"x": [2, 1], "y": [3.3, 1.1]},
+        None,
+        {"x": [8, 7], "y": [9.9, 8.8]},
+    ]
+    assert cuda_array.to_typetracer()[cuda_array4].form == cuda_array[cuda_array4].form
+
+
+def test_0111_jagged_and_masked_getitem_records_missing():
+    array = ak.highlevel.Array(
+        [
+            {"x": 0, "y": 0.0},
+            {"x": 1, "y": 1.1},
+            {"x": 2, "y": 2.2},
+            {"x": 3, "y": 3.3},
+            {"x": 4, "y": 4.4},
+            {"x": 5, "y": 5.5},
+            {"x": 6, "y": 6.6},
+            {"x": 7, "y": 7.7},
+            {"x": 8, "y": 8.8},
+            {"x": 9, "y": 9.9},
+        ],
+        check_valid=True,
+    ).layout
+    array2 = ak.highlevel.Array([3, 1, None, 1, 7], check_valid=True).layout
+
+    cuda_array = ak.to_backend(array, "cuda", highlevel=False)
+    cuda_array2 = ak.to_backend(array2, "cuda")
+
+    assert to_list(cuda_array[cuda_array2]) == [
+        {"x": 3, "y": 3.3},
+        {"x": 1, "y": 1.1},
+        None,
+        {"x": 1, "y": 1.1},
+        {"x": 7, "y": 7.7},
+    ]
+    assert cuda_array.to_typetracer()[cuda_array2].form == cuda_array[cuda_array2].form
+
+    array = ak.highlevel.Array(
+        [
+            [
+                {"x": 0, "y": 0.0},
+                {"x": 1, "y": 1.1},
+                {"x": 2, "y": 2.2},
+                {"x": 3, "y": 3.3},
+            ],
+            [
+                {"x": 4, "y": 4.4},
+                {"x": 5, "y": 5.5},
+                {"x": 6, "y": 6.6},
+                {"x": 7, "y": 7.7},
+                {"x": 8, "y": 8.8},
+                {"x": 9, "y": 9.9},
+            ],
+        ],
+        check_valid=True,
+    ).layout
+    array2 = ak.highlevel.Array([1, None, 2, -1], check_valid=True).layout
+
+    cuda_array = ak.to_backend(array, "cuda", highlevel=False)
+    cuda_array2 = ak.to_backend(array2, "cuda")
+
+    assert to_list(cuda_array[:, cuda_array2]) == [
+        [{"x": 1, "y": 1.1}, None, {"x": 2, "y": 2.2}, {"x": 3, "y": 3.3}],
+        [{"x": 5, "y": 5.5}, None, {"x": 6, "y": 6.6}, {"x": 9, "y": 9.9}],
+    ]
+    assert (
+        cuda_array.to_typetracer()[:, cuda_array2].form
+        == cuda_array[:, cuda_array2].form
+    )
+
+    array = ak.highlevel.Array(
+        [
+            {"x": [0, 1, 2, 3], "y": [0.0, 1.1, 2.2, 3.3]},
+            {"x": [4, 5, 6, 7], "y": [4.4, 5.5, 6.6, 7.7]},
+            {"x": [8, 9, 10, 11], "y": [8.8, 9.9, 10.0, 11.1]},
+        ],
+        check_valid=True,
+    ).layout
+    cuda_array = ak.to_backend(array, "cuda", highlevel=False)
+
+    assert to_list(cuda_array[:, cuda_array2]) == [
+        {"x": [1, None, 2, 3], "y": [1.1, None, 2.2, 3.3]},
+        {"x": [5, None, 6, 7], "y": [5.5, None, 6.6, 7.7]},
+        {"x": [9, None, 10, 11], "y": [9.9, None, 10.0, 11.1]},
+    ]
+    assert (
+        cuda_array.to_typetracer()[:, cuda_array2].form
+        == cuda_array[:, cuda_array2].form
+    )
+    assert to_list(cuda_array[1:, cuda_array2]) == [
+        {"x": [5, None, 6, 7], "y": [5.5, None, 6.6, 7.7]},
+        {"x": [9, None, 10, 11], "y": [9.9, None, 10.0, 11.1]},
+    ]
+    assert (
+        cuda_array.to_typetracer()[1:, cuda_array2].form
+        == cuda_array[1:, cuda_array2].form
+    )
+
+
+def test_0111_jagged_and_masked_getitem_regular_regular():
+    content = ak.contents.NumpyArray(np.arange(2 * 3 * 5))
+    regulararray1 = ak.contents.RegularArray(content, 5, zeros_length=0)
+    regulararray2 = ak.contents.RegularArray(regulararray1, 3, zeros_length=0)
+
+    cuda_regulararray2 = ak.to_backend(regulararray2, "cuda", highlevel=False)
+
+    array1 = ak.highlevel.Array(
+        [[[2], [1, -2], [-1, 2, 0]], [[-3], [-4, 3], [-5, -3, 4]]],
+        check_valid=True,
+    ).layout
+    array2 = ak.highlevel.Array(
+        [[[2], [1, -2], [-1, None, 0]], [[-3], [-4, 3], [-5, None, 4]]],
+        check_valid=True,
+    ).layout
+
+    cuda_array1 = ak.to_backend(array1, "cuda")
+    cuda_array2 = ak.to_backend(array2, "cuda")
+
+    assert to_list(cuda_regulararray2[cuda_array1]) == [
+        [[2], [6, 8], [14, 12, 10]],
+        [[17], [21, 23], [25, 27, 29]],
+    ]
+    assert (
+        cuda_regulararray2.to_typetracer()[cuda_array1].form
+        == cuda_regulararray2[cuda_array1].form
+    )
+
+    assert to_list(cuda_regulararray2[cuda_array2]) == [
+        [[2], [6, 8], [14, None, 10]],
+        [[17], [21, 23], [25, None, 29]],
+    ]
+    assert (
+        cuda_regulararray2.to_typetracer()[cuda_array2].form
+        == cuda_regulararray2[cuda_array2].form
+    )
+
+
+def test_0111_jagged_and_masked_getitem_sequential():
+    array = ak.highlevel.Array(
+        np.arange(2 * 3 * 5).reshape(2, 3, 5).tolist(), check_valid=True
+    ).layout
+    array2 = ak.highlevel.Array([[2, 1, 0], [2, 1, 0]], check_valid=True).layout
+
+    cuda_array = ak.to_backend(array, "cuda", highlevel=False)
+    cuda_array2 = ak.to_backend(array2, "cuda")
+
+    assert to_list(cuda_array[cuda_array2]) == [
+        [[10, 11, 12, 13, 14], [5, 6, 7, 8, 9], [0, 1, 2, 3, 4]],
+        [[25, 26, 27, 28, 29], [20, 21, 22, 23, 24], [15, 16, 17, 18, 19]],
+    ]
+    assert cuda_array.to_typetracer()[cuda_array2].form == cuda_array[cuda_array2].form
+    assert to_list(cuda_array[cuda_array2, :2]) == [
+        [[10, 11], [5, 6], [0, 1]],
+        [[25, 26], [20, 21], [15, 16]],
+    ]
+    assert (
+        cuda_array.to_typetracer()[cuda_array2, :2].form
+        == cuda_array[cuda_array2, :2].form
+    )
+
+
+def test_0111_jagged_and_masked_getitem_union():
+    one = ak.operations.from_iter(
+        [["1.1", "2.2", "3.3"], [], ["4.4", "5.5"]], highlevel=False
+    )
+    two = ak.operations.from_iter(
+        [[6.6], [7.7, 8.8], [], [9.9, 10.0, 11.1, 12.2]], highlevel=False
+    )
+    tags = ak.index.Index8(np.array([0, 0, 0, 1, 1, 1, 1], dtype=np.int8))
+    index = ak.index.Index64(np.array([0, 1, 2, 0, 1, 2, 3], dtype=np.int64))
+    unionarray = ak.contents.UnionArray(tags, index, [one, two])
+
+    cuda_unionarray = ak.to_backend(unionarray, "cuda")
+
+    assert to_list(cuda_unionarray) == [
+        ["1.1", "2.2", "3.3"],
+        [],
+        ["4.4", "5.5"],
+        [6.6],
+        [7.7, 8.8],
+        [],
+        [9.9, 10.0, 11.1, 12.2],
+    ]
+
+
+def test_0111_jagged_and_masked_getitem_union_2():
+    one = ak.operations.from_iter([[1.1, 2.2, 3.3], [], [4.4, 5.5]], highlevel=False)
+    two = ak.operations.from_iter(
+        [[6.6], [7.7, 8.8], [], [9.9, 10.0, 11.1, 12.2]], highlevel=False
+    )
+    tags = ak.index.Index8(np.array([0, 0, 0, 1, 1, 1, 1], dtype=np.int8))
+    index = ak.index.Index64(np.array([0, 1, 2, 0, 1, 2, 3], dtype=np.int64))
+    unionarray = ak.contents.UnionArray.simplified(tags, index, [one, two])
+    array = ak.highlevel.Array(
+        [[0, -1], [], [1, 1], [], [-1], [], [1, -2, -1]], check_valid=True
+    ).layout
+
+    cuda_unionarray = ak.to_backend(unionarray, "cuda", highlevel=False)
+    cuda_array = ak.to_backend(array, "cuda")
+
+    assert to_list(cuda_unionarray[cuda_array]) == [
+        [1.1, 3.3],
+        [],
+        [5.5, 5.5],
+        [],
+        [8.8],
+        [],
+        [10.0, 11.1, 12.2],
+    ]
+    assert (
+        cuda_unionarray.to_typetracer()[cuda_array].form
+        == cuda_unionarray[cuda_array].form
+    )
