0a554ea2a88baeda4368961687f6bbd32446565a
Topher Cawlfield <4094385+tcawlfield@users.noreply.github.com>
2024-06-13 15:28:38 -0600

    Fixing (deprecated) _prettyprint with explicit imports.

--- src/awkward/_prettyprint.py
+++ src/awkward/_prettyprint.py
@@ -4,4 +4,32 @@ from __future__ import annotations
 
 # We're renaming awkward._prettyprint to this module, and gently deprecating the
 # private submodule.
-from awkward.prettyprint import *
+from awkward.prettyprint import (
+    Formatter,
+    FormatterOptions,
+    FormatterType,
+    PlaceholderValue,
+    alternate,
+    custom_str,
+    get_at,
+    get_field,
+    half,
+    is_identifier,
+    valuestr,
+    valuestr_horiz,
+)
+
+__all__ = [
+    "Formatter",
+    "FormatterOptions",
+    "FormatterType",
+    "PlaceholderValue",
+    "alternate",
+    "custom_str",
+    "get_at",
+    "get_field",
+    "half",
+    "is_identifier",
+    "valuestr",
+    "valuestr_horiz",
+]
