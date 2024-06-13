9eae1143e0de56856fcf9f106ddd91424961f304
Topher Cawlfield <4094385+tcawlfield@users.noreply.github.com>
2024-06-13 15:05:38 -0600

    Removing semipublic submodule, providing _prettyprint for backward compatability

---
+++ src/awkward/_prettyprint.py
@@ -0,0 +1,7 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward/blob/main/LICENSE
+
+from __future__ import annotations
+
+# We're renaming awkward._prettyprint to this module, and gently deprecating the
+# private submodule.
+from awkward.prettyprint import *
