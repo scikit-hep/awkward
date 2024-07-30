55a25637613c4059f9495d71aba090a1839b359e
Massimiliano Galli <massimiliano.galli@cern.ch>
2024-07-30 18:37:55 -0500

    Add test for 3502225e9246ce0197d6143c9d94c9e1aeec88b8

---
+++ tests/test_2947_to_list_numpy_integer.py
@@ -0,0 +1,15 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward/blob/main/LICENSE
+
+import awkward as ak
+import numpy as np
+
+def test():
+    """ Check that to_list() does not break when the array is built from buffers
+    with a length of type np.int64.
+    """
+    awk = ak.Array(np.ones((7, 0)))
+    form, length, container = ak.to_buffers(ak.to_packed(awk))
+    awk_from_buf = ak.from_buffers(form, np.int64(length), container)
+    lst = awk_from_buf.to_list()
+
+    assert len(lst) == length
