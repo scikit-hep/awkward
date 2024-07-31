defbf8c968f4a57b11f307db82962dfb8a7841ad
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2024-07-31 10:36:20 -0500

    [test] add two tests, one with 2 and one with 3 arrays

--- tests/test_3118_prevent_exponential_memory_growth_in_unionarray.py
+++ tests/test_3118_prevent_exponential_memory_growth_in_unionarray.py
@@ -5,7 +5,37 @@ from __future__ import annotations
 import awkward as ak
 
 
-def test():
+def check(layout, assert_length):
+    if hasattr(layout, "contents"):
+        for x in layout.contents:
+            check(x, assert_length)
+    elif hasattr(layout, "content"):
+        check(layout.content, assert_length)
+    else:
+        assert layout.length <= assert_length
+
+
+def test_2arrays():
+    one_a = ak.Array([{"x": 1, "y": 2}], with_name="T")
+    one_b = ak.Array([{"x": 1, "y": 2}], with_name="T")
+    two_a = ak.Array([{"x": 1, "z": 3}], with_name="T")
+    two_b = ak.Array([{"x": 1, "z": 3}], with_name="T")
+    three = ak.Array([{"x": 4}, {"x": 4}], with_name="T")
+
+    first = ak.zip({"a": one_a, "b": one_b})
+    second = ak.zip({"a": two_a, "b": two_b})
+
+    cat = ak.concatenate([first, second], axis=0)
+
+    cat["another"] = three
+
+    for _ in range(5):
+        check(cat.layout, 2)
+
+        cat["another", "w"] = three.x
+
+
+def test_3arrays():
     zero_a = ak.Array([{"x": 1, "y": 1}], with_name="T")
     zero_b = ak.Array([{"x": 1, "v": 1}], with_name="T")
     one_a = ak.Array([{"x": 1, "y": 2}], with_name="T")
@@ -22,16 +52,7 @@ def test():
 
     cat["another"] = three
 
-    def check(layout):
-        if hasattr(layout, "contents"):
-            for x in layout.contents:
-                check(x)
-        elif hasattr(layout, "content"):
-            check(layout.content)
-        else:
-            assert layout.length <= 3
-
     for _ in range(5):
-        check(cat.layout)
+        check(cat.layout, 3)
 
         cat["another", "w"] = three.x
