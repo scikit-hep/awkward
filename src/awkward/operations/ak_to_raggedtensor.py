3c981941774b1c3fd60d348500771636d7abc627
maxymnaumchyk <maxymnaumchyk@gmail.com>
2024-08-12 21:31:20 +0300

    fix the tensorflow library import

--- src/awkward/operations/ak_to_raggedtensor.py
+++ src/awkward/operations/ak_to_raggedtensor.py
@@ -2,60 +2,68 @@
 
 from __future__ import annotations
 
-import tensorflow as tf
-
 import awkward as ak
 from awkward._dispatch import high_level_function
 
 __all__ = ("to_raggedtensor",)
 
-
 @high_level_function()
 def to_raggedtensor(ak_arr):
-    """A function that transforms
-    ListOffsetArray, ListArray, RegularArray and NumpyArray
-    data types into RaggedTensor
-    """
 
-    # Dispatch
-    yield (ak_arr,)
+  ''' A function that transforms
+  ListOffsetArray, ListArray, RegularArray and NumpyArray
+  data types into RaggedTensor
+  '''
 
-    # Implementation
+  # Dispatch
+  yield (ak_arr,)
 
-    # unwrap the awkward array if it was made with ak.Array function
-    # also transforms a python list to awkward array
-    ak_arr = ak.to_layout(ak_arr, allow_record=False)
+  # Implementation
+  return _impl(ak_arr)
 
-    if isinstance(ak_arr, ak.contents.numpyarray.NumpyArray):
-        return tf.RaggedTensor.from_row_splits(
-            values=ak_arr.data, row_splits=[0, ak_arr.__len__()]
-        )
-    else:
-        return _impl(ak_arr, ())
+def _impl(ak_arr):
+  try:
+    import tensorflow as tf
+  except ImportError as err:
+    raise ImportError(
+      """install the 'tensorflow' package with:
 
+        pip install tensorflow"""
+    ) from err
 
-def _impl(layout, offsets_arr):
-    try:
-        # change all the possible layout types to ListOffsetArray
-        if isinstance(layout, ak.contents.listarray.ListArray):
-            layout = layout.to_ListOffsetArray64()
-        elif isinstance(layout, ak.contents.regulararray.RegularArray):
-            layout = layout.to_ListOffsetArray64()
-        elif (not isinstance(layout, ak.contents.listoffsetarray.ListOffsetArray)) and (
-            not isinstance(layout, ak.contents.numpyarray.NumpyArray)
-        ):
-            raise TypeError(
-                "Only arrays containing variable-length lists (var *) or"
-                " regular-length lists (# *) of numbers can be converted into a TensorFlow RaggedTensor"
-            )
+  # unwrap the awkward array if it was made with ak.Array function
+  # also transforms a python list to awkward array
+  ak_arr = ak.to_layout(ak_arr, allow_record=False)
 
-        # recursively gather all of the offsets of an array
-        offsets_arr += (layout.offsets.data,)
+  if isinstance(ak_arr, ak.contents.numpyarray.NumpyArray):
+    return tf.RaggedTensor.from_row_splits(
+        values = ak_arr.data,
+        row_splits = [0,ak_arr.__len__()])
+  else:
+    flat_values, nested_row_splits = _recursive_call(ak_arr, ())
 
-    except:
-        # at the last iteration form a ragged tensor from the
-        # accumulated offsets and flattened values of the array
-        return tf.RaggedTensor.from_nested_row_splits(
-            flat_values=layout.data, nested_row_splits=offsets_arr
-        )
-    return _impl(layout.content, offsets_arr)
+    return tf.RaggedTensor.from_nested_row_splits(
+      flat_values,
+      nested_row_splits)
+
+def _recursive_call(layout, offsets_arr):
+  try:
+    # change all the possible layout types to ListOffsetArray
+    if isinstance(layout, ak.contents.listarray.ListArray):
+      layout = layout.to_ListOffsetArray64()
+    elif isinstance(layout, ak.contents.regulararray.RegularArray):
+      layout = layout.to_ListOffsetArray64()
+    elif (not isinstance(layout, ak.contents.listoffsetarray.ListOffsetArray)) and (
+    not isinstance(layout, ak.contents.numpyarray.NumpyArray)):
+      raise TypeError("Only arrays containing variable-length lists (var *) or"
+                      " regular-length lists (# *) of numbers can be converted into a TensorFlow RaggedTensor")
+
+    # recursively gather all of the offsets of an array
+    offsets_arr += (layout.offsets.data,)
+
+  except:
+    # at the last iteration form a ragged tensor from the
+    # accumulated offsets and flattened values of the array
+    return layout.data, offsets_arr
+  return _recursive_call(layout.content, offsets_arr)
+
