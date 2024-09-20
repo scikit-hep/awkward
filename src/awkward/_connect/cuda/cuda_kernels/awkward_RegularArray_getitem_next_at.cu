11f3f4e35b9f3b6a4e1c24c010460a71e7460f94
Ianna Osborne <ianna.osborne@cern.ch>
2024-09-20 17:13:40 +0200

    same for awkward_RegularArray_getitem_next_at.cu

--- src/awkward/_connect/cuda/cuda_kernels/awkward_RegularArray_getitem_next_at.cu
+++ src/awkward/_connect/cuda/cuda_kernels/awkward_RegularArray_getitem_next_at.cu
@@ -8,14 +8,14 @@ template <typename T>
 __global__ void
 awkward_RegularArray_getitem_next_at(
     T* tocarry,
-    int64_t at,
+    int64_t* at,
     int64_t length,
     int64_t size,
     uint64_t invocation_index,
     uint64_t* err_code) {
   if (err_code[0] == NO_ERROR) {
     int64_t thread_id = blockIdx.x * blockDim.x + threadIdx.x;
-    int64_t regular_at = at;
+    int64_t regular_at = at[0];
     if (regular_at < 0) {
       regular_at += size;
     }
