6b0c7cd6720da34b2d1064845b04a94dea89a6a7
pfackeldey <fackeldey.peter@gmail.com>
2024-12-12 16:56:49 -0500

    add flag to restore old (jax<0.4.36) tracing behavior

--- tests/test_1447_jax_autodiff_slices_ufuncs.py
+++ tests/test_1447_jax_autodiff_slices_ufuncs.py
@@ -10,6 +10,7 @@ import awkward as ak
 jax = pytest.importorskip("jax")
 jax.config.update("jax_platform_name", "cpu")
 jax.config.update("jax_enable_x64", True)
+jax.config.update("jax_data_dependent_tracing_fallback", True)
 
 ak.jax.register_and_check()
 
