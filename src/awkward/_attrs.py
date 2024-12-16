dc4a7d6718a0d7e84d60a6d6b91ec10e810ab4f0
pfackeldey <fackeldey.peter@gmail.com>
2024-12-16 12:47:51 -0500

    control attrs better as described in issue #3277

--- src/awkward/_attrs.py
+++ src/awkward/_attrs.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from collections.abc import Mapping
+from types import MappingProxyType
 
 from awkward._typing import Any, JSONMapping
 
@@ -42,3 +43,32 @@ def attrs_of(*arrays, attrs: Mapping | None = None) -> Mapping:
 
 def without_transient_attrs(attrs: dict[str, Any]) -> JSONMapping:
     return {k: v for k, v in attrs.items() if not k.startswith("@")}
+
+
+class Attrs(Mapping):
+    def __init__(self, ref, data: Mapping[str, Any]):
+        self._ref = ref
+        self._data = _freeze_attrs(data)
+
+    def __getitem__(self, key: str):
+        return self._data[key]
+
+    def __setitem__(self, key: str, value: Any):
+        self._ref._attrs = _unfreeze_attrs(self._data) | {key: value}
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __len__(self):
+        return len(self._data)
+
+    def __repr__(self):
+        return repr(_unfreeze_attrs(self._data))
+
+
+def _freeze_attrs(attrs: Mapping[str, Any]) -> Mapping[str, Any]:
+    return MappingProxyType(attrs)
+
+
+def _unfreeze_attrs(attrs: Mapping[str, Any]) -> dict[str, Any]:
+    return dict(attrs)
