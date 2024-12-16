113edbeb9629b0a3a3d2da3844e41225e2e5867a
pfackeldey <fackeldey.peter@gmail.com>
2024-12-16 16:38:55 -0500

    add test

---
+++ tests/test_3347_weakref_mask_highlevel_array.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import pytest
+
+import awkward as ak
+
+
+def test_Array_mask_weakref():
+    arr = ak.Array([1])
+    m = arr.mask
+
+    assert ak.all(m[[True]] == arr)
+
+    del arr
+    with pytest.raises(
+        ValueError,
+        match="The array to mask was deleted before it could be masked. If you want to construct this mask, you must either keep the array alive or use 'ak.mask' explicitly.",
+    ):
+        _ = m[[True]]
