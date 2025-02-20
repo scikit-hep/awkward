408159714742c88b18042ade52b34b1b79eadd7a
pfackeldey <fackeldey.peter@gmail.com>
2025-02-20 09:58:03 -0500

    add tolist() to typetracer array

--- src/awkward/_nplikes/typetracer.py
+++ src/awkward/_nplikes/typetracer.py
@@ -580,6 +580,9 @@ class TypeTracerArray(NDArrayOperatorsMixin, ArrayLike):
             raise ValueError("TypeTracerArray does not support kwargs for ufuncs")
         return self.nplike.apply_ufunc(ufunc, method, inputs, kwargs)
 
+    def tolist(self) -> list:
+        raise RuntimeError("cannot realise an unknown value")
+
     def __bool__(self) -> bool:
         raise RuntimeError("cannot realise an unknown value")
 
