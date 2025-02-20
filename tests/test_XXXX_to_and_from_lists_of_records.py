dea83e694d3696254c3595214baa1ba3378ae4cf
Nikolai Krug <nikoladze@posteo.de>
2025-02-20 14:49:48 +0100

    add test for depth_limit and typestr

--- tests/test_XXXX_to_and_from_lists_of_records.py
+++ tests/test_XXXX_to_and_from_lists_of_records.py
@@ -11,6 +11,7 @@ def test_to_lists_of_records():
             {"a": [], "b": []},
         ]
     )
+    assert ak.to_lists_of_records(a).typestr == "3 * var * {a: int64, b: int64}"
     assert ak.to_lists_of_records(a).tolist() == [
         [{"a": 1, "b": 4}, {"a": 2, "b": 5}, {"a": 3, "b": 6}],
         [{"a": 7, "b": 9}, {"a": 8, "b": 10}],
@@ -25,9 +26,27 @@ def test_to_lists_of_records_axis_1():
             [{"a": [7, 8], "b": [9, 10]}, {"a": [], "b": []}],
         ]
     )
+    assert (
+        ak.to_lists_of_records(b, axis=1).typestr
+        == "2 * var * var * {a: int64, b: int64}"
+    )
     assert ak.to_lists_of_records(b, axis=1).tolist() == [
         [[{"a": 1, "b": 4}, {"a": 2, "b": 5}, {"a": 3, "b": 6}]],
         [[{"a": 7, "b": 9}, {"a": 8, "b": 10}], []],
+    ]
+
+
+def test_to_lists_of_records_depth_limit_2():
+    c = ak.Array(
+        [{"a": [[1, 2, 3]], "b": [[4, 5, 6]]}, {"a": [[7, 8], []], "b": [[9, 10], []]}]
+    )
+    assert (
+        ak.to_lists_of_records(c, depth_limit=2).typestr
+        == "2 * var * {a: var * int64, b: var * int64}"
+    )
+    assert ak.to_lists_of_records(c, depth_limit=2).tolist() == [
+        [{"a": [1, 2, 3], "b": [4, 5, 6]}],
+        [{"a": [7, 8], "b": [9, 10]}, {"a": [], "b": []}],
     ]
 
 
@@ -39,6 +58,7 @@ def test_from_lists_of_records():
             [],
         ]
     )
+    assert ak.from_lists_of_records(a).typestr == "3 * {a: var * int64, b: var * int64}"
     assert ak.from_lists_of_records(a).tolist() == [
         {"a": [1, 2, 3], "b": [4, 5, 6]},
         {"a": [7, 8], "b": [9, 10]},
@@ -51,7 +71,11 @@ def test_from_lists_of_records_axis_1():
         [[{"a": 1, "b": 4}, {"a": 2, "b": 5}, {"a": 3, "b": 6}]],
         [[{"a": 7, "b": 9}, {"a": 8, "b": 10}], []],
     ]
+    assert (
+        ak.from_lists_of_records(b, axis=1).typestr
+        == "2 * var * {a: var * int64, b: var * int64}"
+    )
     assert ak.from_lists_of_records(b, axis=1).tolist() == [
-            [{"a": [1, 2, 3], "b": [4, 5, 6]}],
-            [{"a": [7, 8], "b": [9, 10]}, {"a": [], "b": []}],
-        ]
+        [{"a": [1, 2, 3], "b": [4, 5, 6]}],
+        [{"a": [7, 8], "b": [9, 10]}, {"a": [], "b": []}],
+    ]
