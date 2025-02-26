7e5ce5ae7823729877aeec9002567117e9ed9f9a
Iason Krommydas <iason.krom@gmail.com>
2025-02-26 17:04:03 -0600

    we should be able to convert virtual to typetracer

--- src/awkward/_nplikes/__init__.py
+++ src/awkward/_nplikes/__init__.py
@@ -29,18 +29,22 @@ def to_nplike(
         return array
 
     if isinstance(array, awkward._nplikes.virtual.VirtualArray):
-        if not array.is_materialized:
+        if not array.is_materialized and not isinstance(
+            nplike, awkward._nplikes.typetracer.TypeTracer
+        ):
             raise TypeError(
-                "Cannot convert a VirtualArray to a different nplike without materializing it first. Use ak.materialize on the array to do so."
+                "Cannot convert a VirtualArray to a different nplike with known data without materializing it first. Use ak.materialize on the array to do so."
             )
         else:
             if isinstance(
                 nplike, (awkward._nplikes.numpy.Numpy, awkward._nplikes.cupy.Cupy)
             ):
                 array = array.materialize()
+            elif isinstance(nplike, awkward._nplikes.typetracer.TypeTracer):
+                pass
             else:
                 raise TypeError(
-                    f"Only numpy and cupy nplikes are supported for VirtualArray. Received {type(nplike)}"
+                    f"Can only convert a VirtualArray to numpy, cupy or typetracer nplikes. Received {type(nplike)}"
                 )
 
     if nplike.known_data and not from_nplike.known_data:
