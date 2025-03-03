41cc923675cd94d3fd58411ddff4959011571f98
Ianna Osborne <ianna.osborne@cern.ch>
2025-03-03 15:57:54 +0100

    add one more test

--- tests/test_3180_unionarrays_order_structure_removal.py
+++ tests/test_3180_unionarrays_order_structure_removal.py
@@ -8,19 +8,25 @@ import pytest
 import awkward as ak
 
 
-def test():
+def test_ravel_boolean():
     x = ak.Array([None, [False], [True]])
     y = ak.ravel(ak.fill_none(x, False, axis=0))
     assert y.tolist() == [False, False, True]
 
 
-def test_ListOffsetArray():
+def test_ravel_ListOffsetArray():
     a = ak.Array([[1, 2, 3], 4, [5, 6], 7, 8])
     b = ak.ravel(a)
     assert b.tolist() == [1, 2, 3, 4, 5, 6, 7, 8]
 
 
-def test_ravel_incompatible_types():
+def test_ravel_nested_ListOffsetArrays():
+    a = ak.Array([[[1, 2], 3], 4, [5, [[[6]]]], 7, 8])
+    b = ak.ravel(a)
+    assert b.tolist() == [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def test_ravel_incompatible_contents():
     a = ak.Array([1, "high", 2, "low"])
     with pytest.raises(
         AssertionError,
