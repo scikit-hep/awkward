6d80b93b0f4f05228ea2da81a49f91cfabc9224d
pfackeldey <fackeldey.peter@gmail.com>
2025-04-11 14:48:32 -0400

    fix jax import in test

--- tests/test_3454_jax_ak_firsts.py
+++ tests/test_3454_jax_ak_firsts.py
@@ -1,13 +1,16 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/awkward/blob/main/LICENSE
-
 from __future__ import annotations
+
+import pytest
 
 import awkward as ak
 
-ak.jax.register_and_check()
+jax = pytest.importorskip("jax")
 
 
 def test_jax_ak_firsts():
+    ak.jax.register_and_check()
+
     jax_array = ak.Array([[1.1], [2.2], [], [3.3], [], [], [4.4], [5.5]], backend="jax")
     jax_firsts = ak.firsts(jax_array)
     cpu_array = ak.to_backend(jax_array, "cpu")
