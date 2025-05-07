495ed10c6a8324f1121b0b6f860613d0add3dae7
Iason Krommydas <iason.krom@gmail.com>
2025-05-06 23:13:04 -0500

    better api for materialize

--- src/awkward/operations/ak_materialize.py
+++ src/awkward/operations/ak_materialize.py
@@ -1,7 +1,6 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/awkward/blob/main/LICENSE
 from __future__ import annotations
 
-import awkward as ak
 from awkward._dispatch import high_level_function
 from awkward._layout import HighLevelContext
 
@@ -17,8 +16,8 @@ def materialize(
 ):
     """
     Args:
-        array : array_like
-            An array with possible virtual buffers materialize.
+        array : Array-like data (anything #ak.to_layout recognizes).
+            An array that may contain virtual buffers to be materialized.
         highlevel (bool, default is True): If True, return an #ak.Array;
             otherwise, return a low-level #ak.contents.Content subclass.
         behavior (None or dict): Custom #ak.behavior for the output array, if
@@ -27,10 +26,11 @@ def materialize(
             high-level.
 
     Traverses the input array and materializes any virtual buffers.
-    The buffers of the returned array are no longer `VirtualArray` objects.
-    They will become either `numpy.ndarray` or `cupy.ndarray` objects depending on the array's backend.
-    Possible inputs that will be traversed are instances of #ak.Array, #ak.Record, #ak.contents.Content, and #ak.record.Record.
-    All other types of inputs will be returned as is.
+    If the input array is not one of #ak.Array, #ak.Record, #ak.contents.Content, or #ak.record.Record,
+    it will just be cast to an #ak.Array.
+    The buffers of the returned array are no longer `VirtualArray` objects if there were any.
+    They will become one of `numpy.ndarray`, `cupy.ndarray`, or `jax.numpy.ndarray` objects,
+    depending on the array's backend.
     """
     # Dispatch
     yield (array,)
@@ -40,18 +40,7 @@ def materialize(
 
 
 def _impl(array, highlevel, behavior, attrs):
-    if not isinstance(
-        array,
-        (
-            ak.highlevel.Array,
-            ak.highlevel.Record,
-            ak.contents.Content,
-            ak.record.Record,
-        ),
-    ):
-        return array
-
     with HighLevelContext(behavior=behavior, attrs=attrs) as ctx:
-        layout = ctx.unwrap(array, allow_record=True, allow_unknown=False)
+        layout = ctx.unwrap(array, primitive_policy="error", string_policy="error")
     out = layout.materialize()
     return ctx.wrap(out, highlevel=highlevel)
