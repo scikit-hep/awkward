b75e54dada0c39b60e88512fe0ef59b1f4d25119
pfackeldey <fackeldey.peter@gmail.com>
2025-06-17 10:36:34 -0400

    prettify branch name and SHA in table header

--- benchmarks/compare.py
+++ benchmarks/compare.py
@@ -85,9 +85,10 @@ def compare_benchmarks(
 
                 def _parse_branch_sha(file_path):
                     assert file_path.endswith(".json")
-                    file_path = file_path.replace(".json", "")
-                    branch, sha = file_path.rsplit("__", 1)
-                    return f"branch: {branch} (sha: {sha})"
+                    file_path = file_path.replace(".json", "") # remove file ending
+                    file_path = file_path.replace(os.getenv("BASE_OUTPUT_DIR", "results") + "/", "", 1) # remove base dir
+                    branch, sha = file_path.rsplit("__", 1) # get branch & SHA
+                    return f"branch: `{branch}` (sha: {sha})"
 
                 headers = [
                     "Metric",
