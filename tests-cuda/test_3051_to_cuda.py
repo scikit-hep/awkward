a43e35e2c900efc6661f5455409f6daa6653ef40
Ianna Osborne <ianna.osborne@cern.ch>
2025-06-24 18:57:49 +0200

    fix cudf test

--- tests-cuda/test_3051_to_cuda.py
+++ tests-cuda/test_3051_to_cuda.py
@@ -47,7 +47,6 @@ def test_null():
     assert out.to_arrow().tolist() == [12, None, 21, 12]
 
 
-@pytest.mark.xfail(reason="The StringColumn constructor in cudf was changed. See #3480")
 def test_strings():
     arr = ak.Array(["hey", "hi", "hum"])
     out = ak.to_cudf(arr)
