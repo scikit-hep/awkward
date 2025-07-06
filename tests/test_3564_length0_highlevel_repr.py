19c97b006147098a0712dea3b707fe84661036bf
pfackeldey <fackeldey.peter@gmail.com>
2025-07-06 14:56:55 -0400

    properly test against _mime_bundle_ as well

--- tests/test_3564_length0_highlevel_repr.py
+++ tests/test_3564_length0_highlevel_repr.py
@@ -4,4 +4,9 @@ import awkward as ak
 
 
 def test():
-    assert repr(ak.Array([])) == "<Array [] type='0 * unknown'>"
+    length0 = ak.Array([])
+    assert repr(length0) == "<Array [] type='0 * unknown'>"
+    assert length0._repr_mimebundle_() == {
+        "text/html": "<pre>\nbackend: cpu\nnbytes: 0 B\ntype: 0 * unknown</pre>",
+        "text/plain": "<Array [] type='0 * unknown'>",
+    }
