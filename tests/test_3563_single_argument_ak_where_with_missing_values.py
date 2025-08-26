d654016dc80dd8c90ade110905c26ea275feb502
Iason Krommydas <iason.krom@gmail.com>
2025-08-26 15:09:45 +0300

    test versus numpy as well

--- tests/test_3563_single_argument_ak_where_with_missing_values.py
+++ tests/test_3563_single_argument_ak_where_with_missing_values.py
@@ -10,29 +10,41 @@ def test():
     onedim = ak.Array([1, 2, 3, None, 4, 5])
     result = ak.where(onedim)
     assert to_list(result[0]) == [0, 1, 2, 4, 5]
+    assert to_list(result[0]) == ak.to_numpy(onedim).nonzero()[0].tolist()
 
     onedim = ak.Array([True, False, True, None, False])
     result = ak.where(onedim)
     assert to_list(result[0]) == [0, 2]
+    assert to_list(result[0]) == ak.to_numpy(onedim).nonzero()[0].tolist()
 
     twodim = ak.Array([[1, 2, 3], [None, 4, 5]])
     result = ak.where(twodim)
     assert to_list(result[0]) == [0, 0, 0, 1, 1]
+    assert to_list(result[0]) == ak.to_numpy(twodim).nonzero()[0].tolist()
     assert to_list(result[1]) == [0, 1, 2, 1, 2]
+    assert to_list(result[1]) == ak.to_numpy(twodim).nonzero()[1].tolist()
 
     twodim = ak.Array([[True, False, True], [None, False, True]])
     result = ak.where(twodim)
     assert to_list(result[0]) == [0, 0, 1]
+    assert to_list(result[0]) == ak.to_numpy(twodim).nonzero()[0].tolist()
     assert to_list(result[1]) == [0, 2, 2]
+    assert to_list(result[1]) == ak.to_numpy(twodim).nonzero()[1].tolist()
 
     threedim = ak.Array([[[1, 2], [3, None]], [[4, 5], [None, None]]])
     result = ak.where(threedim)
     assert to_list(result[0]) == [0, 0, 0, 1, 1]
+    assert to_list(result[0]) == ak.to_numpy(threedim).nonzero()[0].tolist()
     assert to_list(result[1]) == [0, 0, 1, 0, 0]
+    assert to_list(result[1]) == ak.to_numpy(threedim).nonzero()[1].tolist()
     assert to_list(result[2]) == [0, 1, 0, 0, 1]
+    assert to_list(result[2]) == ak.to_numpy(threedim).nonzero()[2].tolist()
 
     threedim = ak.Array([[[True, False], [True, None]], [[False, True], [None, None]]])
     result = ak.where(threedim)
     assert to_list(result[0]) == [0, 0, 1]
+    assert to_list(result[0]) == ak.to_numpy(threedim).nonzero()[0].tolist()
     assert to_list(result[1]) == [0, 1, 0]
+    assert to_list(result[1]) == ak.to_numpy(threedim).nonzero()[1].tolist()
     assert to_list(result[2]) == [0, 0, 1]
+    assert to_list(result[2]) == ak.to_numpy(threedim).nonzero()[2].tolist()
