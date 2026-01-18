ebc3631cebed5be11076f87a5c8d6997dd5b284e
Ianna Osborne <ianna.osborne@cern.ch>
2026-01-18 13:29:12 -0500

    implement reducers using cupy.ufunc.at and atomic fallbacks

--- src/awkward/_connect/cuda/cuda_kernels/awkward_reduce_sum.cu
+++ src/awkward/_connect/cuda/cuda_kernels/awkward_reduce_sum.cu
@@ -3,72 +3,19 @@
 // BEGIN PYTHON
 // def f(grid, block, args):
 //     (toptr, fromptr, parents, lenparents, outlength, invocation_index, err_code) = args
-//     if block[0] > 0:
-//         grid_size = math.floor((lenparents + block[0] - 1) / block[0])
+//     supported_types = (cupy.int32, cupy.float16, cupy.float32, cupy.float64, cupy.uint32, cupy.uint64)
+//     if fromptr.dtype not in supported_types:
+//        # Promote to int32 (or float64 if original was float)
+//        working_dtype = cupy.int32 if cupy.issubdtype(fromptr.dtype, cupy.integer) else cupy.float32
+//        data_to_reduce = fromptr[:lenparents].astype(working_dtype)
 //     else:
-//         grid_size = 1
-//     temp = cupy.zeros(lenparents, dtype=toptr.dtype)
-//     cuda_kernel_templates.get_function(fetch_specialization(["awkward_reduce_sum_a", cupy.dtype(toptr.dtype).type, cupy.dtype(fromptr.dtype).type, parents.dtype]))((grid_size,), block, (toptr, fromptr, parents, lenparents, outlength, temp, invocation_index, err_code))
-//     cuda_kernel_templates.get_function(fetch_specialization(["awkward_reduce_sum_b", cupy.dtype(toptr.dtype).type, cupy.dtype(fromptr.dtype).type, parents.dtype]))((grid_size,), block, (toptr, fromptr, parents, lenparents, outlength, temp, invocation_index, err_code))
-// out["awkward_reduce_sum_a", {dtype_specializations}] = None
-// out["awkward_reduce_sum_b", {dtype_specializations}] = None
+//        data_to_reduce = fromptr[:lenparents]
+//     if toptr.dtype not in supported_types:
+//        temp_sum = cupy.zeros(outlength, dtype=data_to_reduce.dtype)
+//        cupy.add.at(temp_sum, parents[:lenparents], data_to_reduce)
+//        # Cast back to the original requested output type
+//        toptr[:outlength] = temp_sum.astype(toptr.dtype)
+//     else:
+//        toptr[:outlength] = 0
+//        cupy.add.at(toptr[:outlength], parents[:lenparents], data_to_reduce)
 // END PYTHON
-
-template <typename T, typename C, typename U>
-__global__ void
-awkward_reduce_sum_a(
-    T* toptr,
-    const C* fromptr,
-    const U* parents,
-    int64_t lenparents,
-    int64_t outlength,
-    T* temp,
-    uint64_t invocation_index,
-    uint64_t* err_code) {
-  if (err_code[0] == NO_ERROR) {
-    int64_t thread_id = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (thread_id < outlength) {
-      toptr[thread_id] = 0;
-    }
-  }
-}
-
-template <typename T, typename C, typename U>
-__global__ void
-awkward_reduce_sum_b(
-    T* toptr,
-    const C* fromptr,
-    const U* parents,
-    int64_t lenparents,
-    int64_t outlength,
-    T* temp,
-    uint64_t invocation_index,
-    uint64_t* err_code) {
-  if (err_code[0] == NO_ERROR) {
-    int64_t idx = threadIdx.x;
-    int64_t thread_id = blockIdx.x * blockDim.x + idx;
-
-    if (thread_id < lenparents) {
-      temp[thread_id] = fromptr[thread_id];
-    }
-    __syncthreads();
-
-    if (thread_id < lenparents) {
-      for (int64_t stride = 1; stride < blockDim.x; stride *= 2) {
-        T val = 0;
-        if (idx >= stride && thread_id < lenparents && parents[thread_id] == parents[thread_id - stride]) {
-          val = temp[thread_id - stride];
-        }
-        __syncthreads();
-        temp[thread_id] += val;
-        __syncthreads();
-      }
-
-      int64_t parent = parents[thread_id];
-      if (idx == blockDim.x - 1 || thread_id == lenparents - 1 || parents[thread_id] != parents[thread_id + 1]) {
-        atomicAdd(&toptr[parent], temp[thread_id]);
-      }
-    }
-  }
-}
