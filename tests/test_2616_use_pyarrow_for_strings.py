9d8d37aa124ee06f2784db1bfe6289a22c69b7e6
rampofin <148707922+rampofin@users.noreply.github.com>
2026-02-15 01:52:57 +0530

    feat(ak.str): add uniques and distinct_counts for string arrays

--- tests/test_2616_use_pyarrow_for_strings.py
+++ tests/test_2616_use_pyarrow_for_strings.py
@@ -2187,3 +2187,65 @@ def test_to_categorical():
             ak.to_backend(["foo", "bar", "bar", "fee"], "typetracer")
         ).layout.form
     )
+
+
+def test_uniques():
+    assert ak.str.uniques(["foo", "bar", "bar", "fee", None, "foo"]).tolist() == [
+        "foo",
+        "bar",
+        "fee",
+        None,
+    ]
+    assert ak.str.uniques([b"foo", b"bar", b"bar", b"fee", None, b"foo"]).tolist() == [
+        b"foo",
+        b"bar",
+        b"fee",
+        None,
+    ]
+
+    assert (
+        ak.str.uniques(["foo", "bar", "bar", "fee", None, "foo"]).layout.form
+        == ak.str.uniques(
+            ak.to_backend(["foo", "bar", "bar", "fee", None, "foo"], "typetracer")
+        ).layout.form
+    )
+    assert (
+        ak.str.uniques([b"foo", b"bar", b"bar", b"fee", None, b"foo"]).layout.form
+        == ak.str.uniques(
+            ak.to_backend([b"foo", b"bar", b"bar", b"fee", None, b"foo"], "typetracer")
+        ).layout.form
+    )
+
+
+def test_distinct_counts():
+    assert ak.str.distinct_counts(
+        ["foo", "bar", "bar", "fee", None, "foo"]
+    ).tolist() == [
+        {"values": "foo", "counts": 2},
+        {"values": "bar", "counts": 2},
+        {"values": "fee", "counts": 1},
+        {"values": None, "counts": 1},
+    ]
+    assert ak.str.distinct_counts(
+        [b"foo", b"bar", b"bar", b"fee", None, b"foo"]
+    ).tolist() == [
+        {"values": b"foo", "counts": 2},
+        {"values": b"bar", "counts": 2},
+        {"values": b"fee", "counts": 1},
+        {"values": None, "counts": 1},
+    ]
+
+    assert (
+        ak.str.distinct_counts(["foo", "bar", "bar", "fee", None, "foo"]).layout.form
+        == ak.str.distinct_counts(
+            ak.to_backend(["foo", "bar", "bar", "fee", None, "foo"], "typetracer")
+        ).layout.form
+    )
+    assert (
+        ak.str.distinct_counts(
+            [b"foo", b"bar", b"bar", b"fee", None, b"foo"]
+        ).layout.form
+        == ak.str.distinct_counts(
+            ak.to_backend([b"foo", b"bar", b"bar", b"fee", None, b"foo"], "typetracer")
+        ).layout.form
+    )
