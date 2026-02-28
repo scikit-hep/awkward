6648ebe1882877b384c7392b2bae4b3a878828ba
rampofin <148707922+rampofin@users.noreply.github.com>
2026-03-01 01:48:22 +0530

    feat(ak.str): apply uniques/value_counts per nested string axis

--- src/awkward/operations/str/akstr_distinct_counts.py
+++ src/awkward/operations/str/akstr_distinct_counts.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from awkward._dispatch import high_level_function
+from awkward._do import recursively_apply
 from awkward._layout import HighLevelContext
 
 __all__ = ("distinct_counts",)
@@ -47,17 +48,52 @@ def distinct_counts(array, *, highlevel=True, behavior=None, attrs=None):
 
 
 def _impl(array, highlevel, behavior, attrs):
-    from awkward._connect.pyarrow import import_pyarrow_compute
+    from awkward._connect.pyarrow import import_pyarrow, import_pyarrow_compute
     from awkward.operations.str import _apply_through_arrow
 
+    pa = import_pyarrow("ak.str.distinct_counts")
     pc = import_pyarrow_compute("ak.str.distinct_counts")
+
+    def value_counts_per_sublist(list_array):
+        value_type = list_array.type.value_type
+        value_counts_type = pa.struct(
+            [
+                pa.field("values", value_type),
+                pa.field("counts", pa.int64()),
+            ]
+        )
+        output = []
+        for sublist in list_array.to_pylist():
+            if sublist is None:
+                output.append(None)
+            else:
+                subarray = pa.array(sublist, type=value_type)
+                output.append(pc.value_counts(subarray).to_pylist())
+        return pa.array(output, type=pa.large_list(value_counts_type))
+
+    def value_counts_whole_array(array_1d):
+        return pc.value_counts(array_1d)
+
+    any_list_of_strings = False
+
+    def action(layout, **kwargs):
+        nonlocal any_list_of_strings
+        if layout.is_list and _is_maybe_optional_list_of_string(layout.content):
+            any_list_of_strings = True
+            return _apply_through_arrow(
+                value_counts_per_sublist, layout, expect_option_type=False
+            )
+        elif _is_maybe_optional_list_of_string(layout):
+            any_list_of_strings = True
+            return _apply_through_arrow(
+                value_counts_whole_array, layout, expect_option_type=False
+            )
 
     with HighLevelContext(behavior=behavior, attrs=attrs) as ctx:
         layout = ctx.unwrap(array)
 
-    if _is_maybe_optional_list_of_string(layout):
-        out = _apply_through_arrow(pc.value_counts, layout, expect_option_type=True)
-    else:
+    out = recursively_apply(layout, action)
+    if not any_list_of_strings:
         out = layout
 
     return ctx.wrap(out, highlevel=highlevel)
--- src/awkward/operations/str/akstr_uniques.py
+++ src/awkward/operations/str/akstr_uniques.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from awkward._dispatch import high_level_function
+from awkward._do import recursively_apply
 from awkward._layout import HighLevelContext
 
 __all__ = ("uniques",)
@@ -46,17 +47,46 @@ def uniques(array, *, highlevel=True, behavior=None, attrs=None):
 
 
 def _impl(array, highlevel, behavior, attrs):
-    from awkward._connect.pyarrow import import_pyarrow_compute
+    from awkward._connect.pyarrow import import_pyarrow, import_pyarrow_compute
     from awkward.operations.str import _apply_through_arrow
 
+    pa = import_pyarrow("ak.str.uniques")
     pc = import_pyarrow_compute("ak.str.uniques")
+
+    def unique_per_sublist(list_array):
+        value_type = list_array.type.value_type
+        output = []
+        for sublist in list_array.to_pylist():
+            if sublist is None:
+                output.append(None)
+            else:
+                subarray = pa.array(sublist, type=value_type)
+                output.append(pc.unique(subarray).to_pylist())
+        return pa.array(output, type=pa.large_list(value_type))
+
+    def unique_whole_array(array_1d):
+        return pc.unique(array_1d)
+
+    any_list_of_strings = False
+
+    def action(layout, **kwargs):
+        nonlocal any_list_of_strings
+        if layout.is_list and _is_maybe_optional_list_of_string(layout.content):
+            any_list_of_strings = True
+            return _apply_through_arrow(
+                unique_per_sublist, layout, expect_option_type=False
+            )
+        elif _is_maybe_optional_list_of_string(layout):
+            any_list_of_strings = True
+            return _apply_through_arrow(
+                unique_whole_array, layout, expect_option_type=False
+            )
 
     with HighLevelContext(behavior=behavior, attrs=attrs) as ctx:
         layout = ctx.unwrap(array)
 
-    if _is_maybe_optional_list_of_string(layout):
-        out = _apply_through_arrow(pc.unique, layout, expect_option_type=True)
-    else:
+    out = recursively_apply(layout, action)
+    if not any_list_of_strings:
         out = layout
 
     return ctx.wrap(out, highlevel=highlevel)
