87b76d241465a5157b98715ecee735fdd1ecc837
Iason Krommydas <iason.krom@gmail.com>
2026-03-04 15:48:57 -0600

    add from_buffers testing

--- tests/test_3889_layout_length_generators.py
+++ tests/test_3889_layout_length_generators.py
@@ -66,6 +66,45 @@ def test_BitMaskedArray():
         None,
     ]
 
+    array = ak.contents.ListOffsetArray(
+        ak.index.Index64(np.array([0, 5, 10, 13], dtype=np.int64)),
+        ak.contents.BitMaskedArray(
+            mask,
+            content,
+            valid_when=False,
+            length=13,
+            lsb_order=False,
+        ),
+    )
+    form, length, buffers = ak.to_buffers(array)
+    container = {key: (lambda b=buffer: b) for key, buffer in buffers.items()}
+    array = ak.from_buffers(form, length, container, highlevel=False)
+    assert isinstance(array.content, ak.contents.BitMaskedArray)
+    assert array.content._length is unknown_length
+    assert array.content._length_generator is not None
+    assert array.content.length == 13
+    assert array.content._length == 13
+    assert array.content._length_generator is None
+    assert array.to_list() == [
+        [0, 1, None, None, None],
+        [5, None, 7, 8, 9],
+        [None, None, None],
+    ]
+    array = ak.from_buffers(
+        form, length, container, highlevel=False, allow_noncanonical_form=True
+    )
+    assert isinstance(array.content, ak.contents.BitMaskedArray)
+    assert array.content._length is unknown_length
+    assert array.content._length_generator is not None
+    assert array.content.length == 13
+    assert array.content._length == 13
+    assert array.content._length_generator is None
+    assert array.to_list() == [
+        [0, 1, None, None, None],
+        [5, None, 7, 8, 9],
+        [None, None, None],
+    ]
+
 
 def test_RecordArray():
     array = ak.contents.RecordArray(
@@ -192,6 +231,30 @@ def test_RecordArray():
     assert array._length == 10
     assert array.to_list() == [(i,) for i in range(10)]
 
+    array = ak.contents.ListOffsetArray(
+        ak.index.Index64(np.array([0, 5, 10, 11], dtype=np.int64)),
+        ak.contents.RecordArray(
+            [ak.contents.NumpyArray(np.arange(13))],
+            ["x"],
+            length=13,
+            length_generator=None,
+        ),
+    )
+    form, length, buffers = ak.to_buffers(array)
+    container = {key: (lambda b=buffer: b) for key, buffer in buffers.items()}
+    array = ak.from_buffers(form, length, container, highlevel=False)
+    assert isinstance(array.content, ak.contents.RecordArray)
+    assert array.content._length is unknown_length
+    assert array.content._length_generator is not None
+    assert array.content.length == 11
+    assert array.content._length == 11
+    assert array.content._length_generator is None
+    assert array.to_list() == [
+        [{"x": i} for i in range(5)],
+        [{"x": i} for i in range(5, 10)],
+        [{"x": i} for i in range(10, 11)],
+    ]
+
 
 def test_RegularArray():
     array = ak.contents.RegularArray(
@@ -281,3 +344,40 @@ def test_RegularArray():
         match="RegularArray length must be an integer for an array with concrete data",
     ):
         assert simplified.length == 2
+
+    # In [11]:     array = ak.contents.ListOffsetArray(
+    #     ...:         ak.index.Index64(np.array([0, 5, 10, 13], dtype=np.int64)),
+    #     ...:         ak.contents.RegularArray(
+    #     ...:             ak.contents.NumpyArray(np.arange(13)),
+    #     ...:             size=2,
+    #     ...:             zeros_length=0,
+    #     ...:             zeros_length_generator=None,
+    #     ...:         ),
+    #     ...:     )
+
+    # In [12]: array.to_list()
+    # Out[12]: [[[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]], [[10, 11]], []]
+
+    array = ak.contents.ListOffsetArray(
+        ak.index.Index64(np.array([0, 5, 10, 13], dtype=np.int64)),
+        ak.contents.RegularArray(
+            ak.contents.NumpyArray(np.arange(30)),
+            size=2,
+            zeros_length=0,
+            zeros_length_generator=None,
+        ),
+    )
+    form, length, buffers = ak.to_buffers(array)
+    container = {key: (lambda b=buffer: b) for key, buffer in buffers.items()}
+    array = ak.from_buffers(form, length, container, highlevel=False)
+    assert isinstance(array.content, ak.contents.RegularArray)
+    assert array.content._length is unknown_length
+    assert array.content._zeros_length_generator is not None
+    assert array.content.length == 13
+    assert array.content._length == 13
+    assert array.content._zeros_length_generator is None
+    assert array.to_list() == [
+        [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]],
+        [[10, 11], [12, 13], [14, 15], [16, 17], [18, 19]],
+        [[20, 21], [22, 23], [24, 25]],
+    ]
