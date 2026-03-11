fb6507884a9da77a6ed7c33467d0c6800c088e7d
Ianna Osborne <ianna.osborne@cern.ch>
2026-03-11 14:59:44 +0100

    move check out of the loop and allow vectorization

--- awkward-cpp/src/cpu-kernels/awkward_ListOffsetArray_drop_none_indexes.cpp
+++ awkward-cpp/src/cpu-kernels/awkward_ListOffsetArray_drop_none_indexes.cpp
@@ -11,19 +11,17 @@ ERROR awkward_ListOffsetArray_drop_none_indexes(
   const T* fromoffsets,
   int64_t length_offsets,
   int64_t length_indexes) {
+  if (length_offsets > 0 && fromoffsets[length_offsets - 1] > length_indexes) {
+    return failure("offsets[i] > len(content)", kSliceNone, kSliceNone, FILENAME(__LINE__));
+  }
   T nr_of_nones = 0;
   int64_t offset1 = 0;
   int64_t offset2 = 0;
 
   for (int64_t i = 0; i < length_offsets; i++) {
     offset2 = fromoffsets[i];
-    if (offset2 > length_indexes) {
-      return failure("offsets[i] > len(content)", i, kSliceNone, FILENAME(__LINE__));
-    }
     for (int64_t j = offset1; j < offset2; j++) {
-        if (noneindexes[j] < 0) {
-            nr_of_nones++;
-        }
+      nr_of_nones += (noneindexes[j] < 0);
     }
     tooffsets[i] = fromoffsets[i] - nr_of_nones;
     offset1 = offset2;
