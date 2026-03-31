30199d8649e7f33518dc9689a03897cc478e5024
Iason Krommydas <iason.krom@gmail.com>
2026-03-31 08:46:03 +0200

    more thorough testing

--- tests/test_3938_generator_expression_inputs.py
+++ tests/test_3938_generator_expression_inputs.py
@@ -4,29 +4,132 @@ from __future__ import annotations
 
 import awkward as ak
 
-to_list = ak.operations.to_list
-
 
 def test_concatenate():
-    result = ak.concatenate([i] for i in range(3))
-    assert to_list(result) == [0, 1, 2]
+    # Plain lists
+    arrays = [[i] for i in range(5)]
+    expected_list = ak.concatenate(arrays)
+    expected_tuple = ak.concatenate(tuple(arrays))
+    result = ak.concatenate([i] for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
+
+    # ak.Array instances
+    arrays = [ak.Array([i, i + 1]) for i in range(5)]
+    expected_list = ak.concatenate(arrays)
+    expected_tuple = ak.concatenate(tuple(arrays))
+    result = ak.concatenate(ak.Array([i, i + 1]) for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
+
+    # Nested arrays with axis=1
+    arrays = [ak.Array([[i, i + 1], [i + 2]]) for i in range(5)]
+    expected_list = ak.concatenate(arrays, axis=1)
+    expected_tuple = ak.concatenate(tuple(arrays), axis=1)
+    result = ak.concatenate((ak.Array([[i, i + 1], [i + 2]]) for i in range(5)), axis=1)
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
 
 
 def test_zip():
-    result = ak.zip([i, i + 1] for i in range(2))
-    assert to_list(result) == [(0, 1), (1, 2)]
+    # Plain lists
+    arrays = [[i, i + 1, i + 2] for i in range(5)]
+    expected_list = ak.zip(arrays)
+    expected_tuple = ak.zip(tuple(arrays))
+    result = ak.zip([i, i + 1, i + 2] for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
+
+    # ak.Array instances
+    arrays = [ak.Array([i, i + 1, i + 2]) for i in range(5)]
+    expected_list = ak.zip(arrays)
+    expected_tuple = ak.zip(tuple(arrays))
+    result = ak.zip(ak.Array([i, i + 1, i + 2]) for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
+
+    # Nested arrays
+    arrays = [ak.Array([[i, i + 1], [i + 2]]) for i in range(5)]
+    expected_list = ak.zip(arrays)
+    expected_tuple = ak.zip(tuple(arrays))
+    result = ak.zip(ak.Array([[i, i + 1], [i + 2]]) for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
 
 
 def test_zip_no_broadcast():
-    result = ak.zip_no_broadcast([i, i + 1] for i in range(2))
-    assert to_list(result) == [(0, 1), (1, 2)]
+    # Plain lists
+    arrays = [[i, i + 1, i + 2] for i in range(5)]
+    expected_list = ak.zip_no_broadcast(arrays)
+    expected_tuple = ak.zip_no_broadcast(tuple(arrays))
+    result = ak.zip_no_broadcast([i, i + 1, i + 2] for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
+
+    # ak.Array instances
+    arrays = [ak.Array([i, i + 1, i + 2]) for i in range(5)]
+    expected_list = ak.zip_no_broadcast(arrays)
+    expected_tuple = ak.zip_no_broadcast(tuple(arrays))
+    result = ak.zip_no_broadcast(ak.Array([i, i + 1, i + 2]) for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
+
+    # Nested arrays
+    arrays = [ak.Array([[i, i + 1], [i + 2]]) for i in range(5)]
+    expected_list = ak.zip_no_broadcast(arrays)
+    expected_tuple = ak.zip_no_broadcast(tuple(arrays))
+    result = ak.zip_no_broadcast(ak.Array([[i, i + 1], [i + 2]]) for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
 
 
 def test_cartesian():
-    result = ak.cartesian([[i, i + 1]] for i in range(2))
-    assert to_list(result) == [[(0, 1), (0, 2), (1, 1), (1, 2)]]
+    # Plain nested lists, default axis=1
+    arrays = [[[i, i + 1], [i + 2]] for i in range(5)]
+    expected_list = ak.cartesian(arrays)
+    expected_tuple = ak.cartesian(tuple(arrays))
+    result = ak.cartesian([[i, i + 1], [i + 2]] for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
+
+    # ak.Array instances, axis=0
+    arrays = [ak.Array([i, i + 1]) for i in range(5)]
+    expected_list = ak.cartesian(arrays, axis=0)
+    expected_tuple = ak.cartesian(tuple(arrays), axis=0)
+    result = ak.cartesian((ak.Array([i, i + 1]) for i in range(5)), axis=0)
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
+
+    # ak.Array instances, nested, default axis=1
+    arrays = [ak.Array([[i, i + 1], [i + 2]]) for i in range(5)]
+    expected_list = ak.cartesian(arrays)
+    expected_tuple = ak.cartesian(tuple(arrays))
+    result = ak.cartesian(ak.Array([[i, i + 1], [i + 2]]) for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
 
 
 def test_argcartesian():
-    result = ak.argcartesian([[i, i + 1]] for i in range(2))
-    assert to_list(result) == [[(0, 0), (0, 1), (1, 0), (1, 1)]]
+    # Plain nested lists, default axis=1
+    arrays = [[[i, i + 1], [i + 2]] for i in range(5)]
+    expected_list = ak.argcartesian(arrays)
+    expected_tuple = ak.argcartesian(tuple(arrays))
+    result = ak.argcartesian([[i, i + 1], [i + 2]] for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
+
+    # ak.Array instances, axis=0
+    arrays = [ak.Array([i, i + 1]) for i in range(5)]
+    expected_list = ak.argcartesian(arrays, axis=0)
+    expected_tuple = ak.argcartesian(tuple(arrays), axis=0)
+    result = ak.argcartesian((ak.Array([i, i + 1]) for i in range(5)), axis=0)
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
+
+    # ak.Array instances, nested, default axis=1
+    arrays = [ak.Array([[i, i + 1], [i + 2]]) for i in range(5)]
+    expected_list = ak.argcartesian(arrays)
+    expected_tuple = ak.argcartesian(tuple(arrays))
+    result = ak.argcartesian(ak.Array([[i, i + 1], [i + 2]]) for i in range(5))
+    assert ak.array_equal(result, expected_list)
+    assert ak.array_equal(result, expected_tuple)
