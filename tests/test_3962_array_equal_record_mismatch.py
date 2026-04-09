0e78f222f37fa40ac36aa18264159be348fd17e2
Iason Krommydas <iason.krom@gmail.com>
2026-04-09 11:20:41 +0200

    fix record array comparison in array equal

---
+++ tests/test_3962_array_equal_record_mismatch.py
@@ -0,0 +1,21 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/awkward/blob/main/LICENSE
+
+from __future__ import annotations
+
+import awkward as ak
+
+
+def test_tuple_different_arity():
+    a1 = ak.Array([(1,), (2,)])
+    a2 = ak.Array([(1, 10), (2, 20)])
+
+    assert ak.array_equal(a1, a2) is False
+    assert ak.array_equal(a2, a1) is False
+
+
+def test_record_different_field_names():
+    a1 = ak.Array([{"x": 1}, {"x": 2}])
+    a2 = ak.Array([{"y": 1}, {"y": 2}])
+
+    assert ak.array_equal(a1, a2) is False
+    assert ak.array_equal(a2, a1) is False
