b23b882e0d09cbfbde7921e82fbbc9f20f93530f
Iason Krommydas <iason.krom@gmail.com>
2026-04-09 11:23:49 +0200

    test different field order

--- tests/test_3962_array_equal_record_mismatch.py
+++ tests/test_3962_array_equal_record_mismatch.py
@@ -19,3 +19,11 @@ def test_record_different_field_names():
 
     assert ak.array_equal(a1, a2) is False
     assert ak.array_equal(a2, a1) is False
+
+
+def test_record_field_order_insensitive():
+    a1 = ak.Array([{"x": 1, "y": 10}, {"x": 2, "y": 20}])
+    a2 = ak.Array([{"y": 10, "x": 1}, {"y": 20, "x": 2}])
+
+    assert ak.array_equal(a1, a2) is True
+    assert ak.array_equal(a2, a1) is True
