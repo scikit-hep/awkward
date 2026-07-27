54e69f7183c790ad203720faa516257a12c027fe
Ianna Osborne <ianna.osborne@cern.ch>
2026-07-27 22:30:36 +0200

    add invalid-UTF-8 tests

--- tests/test_4250_numba_native_strings.py
+++ tests/test_4250_numba_native_strings.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import numpy as np
 import pytest
 
 import awkward as ak
@@ -79,3 +80,69 @@ def test_numba_array_getitem_string_operations():
         "日本語🙂",
         True,
     )
+
+
+def array_from_raw_bytes(data, *, is_string):
+    content_parameter = "char" if is_string else "byte"
+    list_parameter = "string" if is_string else "bytestring"
+
+    form = ak.forms.ListOffsetForm(
+        "i64",
+        ak.forms.NumpyForm(
+            "uint8",
+            parameters={"__array__": content_parameter},
+            form_key="node1",
+        ),
+        parameters={"__array__": list_parameter},
+        form_key="node0",
+    )
+
+    return ak.from_buffers(
+        form,
+        1,
+        {
+            "node0-offsets": np.array([0, len(data)], dtype=np.int64),
+            "node1-data": np.frombuffer(data, dtype=np.uint8),
+        },
+    )
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        b"\xff",  # invalid leading byte
+        b"\xc3",  # truncated two-byte sequence
+        b"\xe2\x28\xa1",  # invalid continuation byte
+        b"\xc0\xaf",  # overlong encoding
+        b"\xed\xa0\x80",  # UTF-8 encoding of a surrogate
+        b"\xf4\x90\x80\x80",  # code point above U+10FFFF
+    ],
+)
+def test_numba_string_getitem_rejects_invalid_utf8(data):
+    array = array_from_raw_bytes(data, is_string=True)
+
+    @numba.njit
+    def getitem(array):
+        return array[0]
+
+    with pytest.raises(UnicodeDecodeError):
+        getitem(array)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        b"\xff",
+        b"\xc3",
+        b"\xe2\x28\xa1",
+        b"\x00\xff",
+    ],
+)
+def test_numba_bytestring_getitem_preserves_arbitrary_bytes(data):
+    array = array_from_raw_bytes(data, is_string=False)
+
+    @numba.njit
+    def getitem(array):
+        return array[0]
+
+    assert getitem(array) == data
